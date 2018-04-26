f1914964eba731c36c9659cba266f30a0b35a4aa
Erlend Vige <erlend.vige@gmail.com>
2018-04-26 07:53:22 +0200

    Add prettier code-formatting
    Commit also makes sure we don't conflict with existing tslint tools.

--- src/app/bootstrap.tsx
+++ src/app/bootstrap.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import ForeldrepengeSøknad from './connected-components/ForeldrepengeSøknad';
 
 const root = document.getElementById('app');
-render(<p>Foreldrepengesøknad</p>, root);
+render(<ForeldrepengeSøknad />, root);
