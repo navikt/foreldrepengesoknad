313be391befa5bc22597c33eb35fb7b9f2d4e69a
Frode Hansen <frode.hansen@bekk.no>
2018-05-03 10:59:27 +0200

    Fornorske komponenter

--- src/app/redux/actions/søknad/søknadActionDefinitions.ts
+++ src/app/redux/actions/søknad/søknadActionDefinitions.ts
@@ -1,23 +1,23 @@
 import Søknad, { UtenlandsoppholdData } from '../../../types/søknad/Søknad';
 
 export enum SøknadActionKeys {
-  'UPDATE_SØKNAD_STATE' = 'updateSøknadState',
-  'UPDATE_UTENLANDSOPPHOLD' = 'updateUtenlandsopphold'
+    'UPDATE_SØKNAD_STATE' = 'updateSøknadState',
+    'UPDATE_UTENLANDSOPPHOLD' = 'updateUtenlandsopphold'
 }
 
 export interface UpdateSøknadState {
-  type: SøknadActionKeys.UPDATE_SØKNAD_STATE;
-  payload: Søknad;
+    type: SøknadActionKeys.UPDATE_SØKNAD_STATE;
+    payload: Søknad;
 }
 
 export type ListActionEvent = 'add' | 'update' | 'delete';
 
 export interface UpdateUtenlandsopppholdPayload {
-  action: ListActionEvent;
-  utenlandsopphold: UtenlandsoppholdData;
+    action: ListActionEvent;
+    utenlandsopphold: UtenlandsoppholdData;
 }
 
 export interface UpdateUtenlandsopphold {
-  type: SøknadActionKeys.UPDATE_UTENLANDSOPPHOLD;
-  payload: UpdateUtenlandsopppholdPayload;
+    type: SøknadActionKeys.UPDATE_UTENLANDSOPPHOLD;
+    payload: UpdateUtenlandsopppholdPayload;
 }
