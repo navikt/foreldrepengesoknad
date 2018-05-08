5485a1d11af72b4586e116b0683b334753666a04
Frode Hansen <frode.hansen@bekk.no>
2018-05-08 10:35:10 +0200

    nav-frontend-validform - enkel typescript port

---
+++ src/app/nav-frontend-validform/Feiloppsummering.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import classNames from 'classnames';
+import { SummaryError } from './types';
+
+import './validering-style.less';
+
+interface Props {
+    title: string;
+    show: boolean;
+    className?: string;
+    errors: SummaryError[];
+}
+
+const cls = (show: boolean, className?: string) =>
+    classNames('feil-oppsummering-boks', className, {
+        'feil-oppsummering-boks--visible': show
+    });
+
+class Feiloppsummering extends React.Component<Props, {}> {
+    element: HTMLElement | null;
+    componentDidMount() {
+        if (this.element) {
+            this.element.focus();
+        }
+    }
+    render() {
+        const { className, show, errors, title, ...other } = this.props;
+
+        const listItems = errors.map((error) => {
+            const link = '#' + error.name;
+            return (
+                <li key={error.name}>
+                    <a className="feil-oppsummering-boks__lenke" href={link}>
+                        {error.text}
+                    </a>
+                </li>
+            );
+        });
+
+        return (
+            <article
+                ref={(node) => {
+                    this.element = node;
+                }}
+                tabIndex={-1}
+                className={cls(show, className)}
+                {...other}>
+                <h1 className="typo-undertittel">{title}</h1>
+                <ul className="feil-oppsummering-boks__liste">{listItems}</ul>
+            </article>
+        );
+    }
+}
+
+export default Feiloppsummering;
---
+++ src/app/nav-frontend-validform/types.ts
@@ -0,0 +1,31 @@
+export interface ValidatorProps {
+    name?: string;
+    validators?: any[];
+}
+
+export interface InputProps {
+    label: string | React.ReactNode;
+}
+
+export interface Validator {
+    test: (value: any) => boolean;
+    failText: string;
+}
+
+export interface Feil {
+    tittel?: string;
+    feilmelding: string;
+}
+
+export interface SummaryError {
+    name: string;
+    text: string;
+}
+
+export interface ValidationResult {
+    name: string;
+    tests: any[];
+    valid: boolean;
+}
+
+export type ValidComponentProps = ValidatorProps & InputProps;
