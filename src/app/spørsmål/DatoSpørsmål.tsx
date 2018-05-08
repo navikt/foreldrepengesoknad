59ecf96627ab914d05ae761eb2f91c18a2fcee92
Frode Hansen <frode.hansen@bekk.no>
2018-05-08 12:35:44 +0200

    Flytte intl inn i DateInput

--- src/app/spørsmål/DatoSpørsmål.tsx
+++ src/app/spørsmål/DatoSpørsmål.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { Avgrensninger } from 'nav-datovelger';
 import DatoInput from '../components/dato-input/DatoInput';
-import { Språkkode } from '../intl/types';
 
 interface DatospørsmålProps {
     spørsmål: string;
     id: string;
-    språkkode: Språkkode;
     onChange: (dato: Date) => void;
     avgrensninger?: Avgrensninger;
     dato?: Date;
@@ -14,12 +12,11 @@ interface DatospørsmålProps {
 }
 
 const DatoSpørsmål = (props: DatospørsmålProps) => {
-    const { onChange, spørsmål, språkkode, ...rest } = props;
+    const { onChange, spørsmål, ...rest } = props;
     return (
         <DatoInput
             onChange={(dato: Date) => onChange(dato)}
             label={spørsmål}
-            språkkode={språkkode}
             {...rest}
         />
     );
