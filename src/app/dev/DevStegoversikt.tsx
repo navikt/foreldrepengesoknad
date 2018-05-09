42a4eda31e9f2cef7cfda84dd16be749a79b4e8b
Frode Hansen <frode.hansen@bekk.no>
2018-05-09 12:01:11 +0200

    Legge eksempelskjema inn i oversikt

--- src/app/dev/DevStegoversikt.tsx
+++ src/app/dev/DevStegoversikt.tsx
@@ -8,17 +8,33 @@ import { soknadStegPath } from '../connected-components/steg/StegRoutes';
 import Veilederinfo from '../components/veileder-info/Veilederinfo';
 
 import './dev.less';
+import routeConfig from '../util/routeConfig';
 
 class DevStegoversikt extends React.Component<{}, {}> {
     render() {
         return (
             <Applikasjonsside visSpråkvelger={true}>
-                <h1>Steg i Foreldrepengesøknad</h1>
+                <h1>Hjelpeside for utviklere</h1>
                 <Veilederinfo>
                     Denne siden er kun for utvikling, og viser en oversikt over,
                     og lenke til de stegene vi har registrert. Status på de
                     ulike stegene må en sjekke med utviklere.
                 </Veilederinfo>
+
+                <h2>Eksempelsøknad</h2>
+                <div className="blokk-l">
+                    <ul className="stegoversikt">
+                        <li>
+                            <Link
+                                to={`${routeConfig.APP_ROUTE_PREFIX}/eksempel`}
+                                className="lenke">
+                                Gå til eksempelskjemaet
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <h2>Enkeltsteg i søknadsskjemaet</h2>
                 <ul className="stegoversikt">
                     {Object.keys(stegConfig).map((key, index) => (
                         <li key={index}>
