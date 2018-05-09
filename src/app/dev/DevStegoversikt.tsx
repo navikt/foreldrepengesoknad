7c7d9b4c518fe15580f3ecb4ddb913a8c96928da
Frode Hansen <frode.hansen@bekk.no>
2018-05-09 10:33:02 +0200

    Presisering av stegoversikt

--- src/app/dev/DevStegoversikt.tsx
+++ src/app/dev/DevStegoversikt.tsx
@@ -5,13 +5,21 @@ import { Link } from 'react-router-dom';
 
 import stegConfig from '../util/stegConfig';
 import { soknadStegPath } from '../connected-components/steg/StegRoutes';
+import Veilederinfo from '../components/veileder-info/Veilederinfo';
+
+import './dev.less';
 
 class DevStegoversikt extends React.Component<{}, {}> {
     render() {
         return (
-            <Applikasjonsside>
+            <Applikasjonsside visSpråkvelger={true}>
                 <h1>Steg i Foreldrepengesøknad</h1>
-                <ul>
+                <Veilederinfo>
+                    Denne siden er kun for utvikling, og viser en oversikt over,
+                    og lenke til de stegene vi har registrert. Status på de
+                    ulike stegene må en sjekke med utviklere.
+                </Veilederinfo>
+                <ul className="stegoversikt">
                     {Object.keys(stegConfig).map((key, index) => (
                         <li>
                             <Link to={soknadStegPath(key)} className="lenke">
