4cd8bc62e19826e0b24ad1ee49faa35b51c41839
Frode Hansen <frode.hansen@bekk.no>
2018-05-09 09:41:41 +0200

    Overtakelse av foreldreansvar

--- src/app/util/stegConfig.ts
+++ src/app/util/stegConfig.ts
@@ -2,6 +2,7 @@ export enum StegID {
     'RELASJON_TIL_BARN' = 'relasjon-til-barn',
     'RELASJON_TIL_BARN_ADOPSJON' = 'relasjon-til-barn-adopsjon',
     'RELASJON_TIL_BARN_STEBARNSADOPSJON' = 'relasjon-til-barn-stebarnsadopsjon',
+    'RELASJON_TIL_BARN_FORELDREANSVAR' = 'relasjon-til-barn-foreldreansvar',
     'ANNEN_FORELDER' = 'annen-forelder'
 }
 
@@ -20,6 +21,10 @@ export default {
     },
     [StegID.RELASJON_TIL_BARN_STEBARNSADOPSJON]: {
         tittel: 'Relasjon til barn tidlig stebarnsadopsjon',
+        nesteKnapp: 'Fortsett'
+    },
+    [StegID.RELASJON_TIL_BARN_FORELDREANSVAR]: {
+        tittel: 'Relasjon til barn overtakelse av foreldreansvar',
         nesteKnapp: 'Fortsett'
     }
 };
