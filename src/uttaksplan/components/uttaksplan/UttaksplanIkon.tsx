998694f5ebe378c1e653679dca9d98a22ce444f4
Frode Hansen <frode.hansen@bekk.no>
2018-05-16 16:56:26 +0200

    UttaksplanIkoner
    Former-commit-id: 98bd4356f87662275712dc9468b942e1dba9948c

---
+++ src/uttaksplan/components/uttaksplan/UttaksplanIkon.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import SykehusIkon from 'uttaksplan/components/uttaksplan/ikoner/SykehusIkon';
+import FerieIkon from 'uttaksplan/components/uttaksplan/ikoner/FerieIkon';
+import UttakIkon from 'uttaksplan/components/uttaksplan/ikoner/UttakIkon';
+import GradertIkon from 'uttaksplan/components/uttaksplan/ikoner/GradertIkon';
+import PlasterIkon from 'uttaksplan/components/uttaksplan/ikoner/PlasterIkon';
+import TerminIkon from 'uttaksplan/elements/ikoner/TerminIkon';
+
+export type UttaksplanIkonKeys =
+    | 'arbeid'
+    | 'ferie'
+    | 'gradert'
+    | 'sykehus'
+    | 'sykdom'
+    | 'termin'
+    | 'uttak';
+
+export interface Props {
+    ikon: UttaksplanIkonKeys;
+}
+
+const UttaksplanIkon: React.StatelessComponent<Props> = ({ ikon }) => {
+    switch (ikon) {
+        case 'arbeid':
+            return <SykehusIkon />;
+        case 'ferie':
+            return <FerieIkon />;
+        case 'gradert':
+            return <GradertIkon />;
+        case 'sykdom':
+            return <PlasterIkon />;
+        case 'sykehus':
+            return <SykehusIkon />;
+        case 'termin':
+            return <TerminIkon />;
+        default:
+            return <UttakIkon />;
+    }
+};
+
+export default UttaksplanIkon;
