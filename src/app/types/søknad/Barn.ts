2de34d79c5ea3363199a72c9504e9626ffbc7989
Frode Hansen <frode.hansen@bekk.no>
2018-05-23 15:52:49 +0200

    Modify søknadState to reduce checks in eksempelsøknad
    Former-commit-id: 202dfa472ab5c394ae77211fc70ead2fec19852f

--- src/app/types/søknad/Barn.ts
+++ src/app/types/søknad/Barn.ts
@@ -1,14 +1,14 @@
-abstract class Barn {
+abstract class BarnBase {
     antallBarn?: number;
     erBarnetFødt: boolean;
 }
 
-export interface UfødtBarn extends Barn {
+export interface UfødtBarn extends BarnBase {
     termindato: Date;
     terminbekreftelseDato: Date;
 }
 
-export interface FødtBarn extends Barn {
+export interface FødtBarn extends BarnBase {
     fødselsdatoer: Date[];
 }
 
@@ -21,10 +21,10 @@ export interface ForeldreansvarBarn extends FødtBarn {
     foreldreansvarsdato: Date;
 }
 
+export type Barn = UfødtBarn | FødtBarn | Adopsjonsbarn | ForeldreansvarBarn;
+
 export type BarnPartial = Partial<Barn>;
 export type FødtBarnPartial = Partial<FødtBarn>;
 export type UfødtBarnPartial = Partial<UfødtBarn>;
 export type AdopsjonsbarnPartial = Partial<Adopsjonsbarn>;
 export type ForeldreansvarBarnPartial = Partial<ForeldreansvarBarn>;
-
-export default Barn;
