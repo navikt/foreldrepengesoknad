6ff2f52f31e6d4bfed071f4a08fadcd870414604
Erlend Vige <erlend.vige@gmail.com>
2018-05-23 08:27:58 +0200

    Remove hats from package.json

--- src/app/components/spørsmål/__tests__/spørsmål.test.tsx
+++ src/app/components/spørsmål/__tests__/spørsmål.test.tsx
@@ -4,11 +4,4 @@ describe('Spørsmål component', () => {
     it('should be defined', () => {
         expect(Spørsmål).toBeDefined();
     });
-
-    it('renders something', () => {
-        //const wrapper = shallow(
-        //    <Spørsmål synlig={true} render={() => <p>Testspørsmål</p>} />
-        //);
-        //expect(wrapper.find('div').contains('Testspørsmål')).toEqual(true);
-    });
 });
