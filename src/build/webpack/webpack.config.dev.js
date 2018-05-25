28dfe87048e5ad7d5a2b350aea697d901e422486
Frode Hansen <frode.hansen@bekk.no>
2018-05-25 09:40:48 +0200

    Skip URL injection in HTML

--- src/build/webpack/webpack.config.dev.js
+++ src/build/webpack/webpack.config.dev.js
@@ -1,24 +1,27 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpackConfig = require('./webpack.config.global.js');
 
+require('dotenv').config();
+
 webpackConfig.mode = 'development';
 
 webpackConfig.plugins.push(
-	new HtmlWebpackPlugin({
-		template: './src/app/index.html',
-		inject: 'body',
-		alwaysWriteToDisk: true
-	})
+    new HtmlWebpackPlugin({
+        template: './src/app/index.html',
+        inject: 'body',
+        alwaysWriteToDisk: true
+    })
 );
 
 webpackConfig.plugins.push(
-	new HtmlWebpackHarddiskPlugin({
-		outputPath: path.resolve(__dirname, '../../../dist/dev')
-	})
+    new HtmlWebpackHarddiskPlugin({
+        outputPath: path.resolve(__dirname, '../../../dist/dev')
+    })
 );
 
 module.exports = Object.assign(webpackConfig, {
-	devtool: 'inline-source-map'
+    devtool: 'inline-source-map'
 });
