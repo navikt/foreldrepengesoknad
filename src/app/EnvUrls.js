17661db8e25cdfded7cd0a87755bf7bc046bb57a
Frode Hansen <frode.hansen@bekk.no>
2018-05-25 12:52:01 +0200

    Rename AppSettings and skip separate file

---
+++ src/app/EnvUrls.js
@@ -0,0 +1,5 @@
+const EnvUrls = {
+    REST_API_URL: appSettings.REST_API_URL,
+    LOGIN_URL: appSettings.LOGIN_URL
+};
+export default EnvUrls;
--- src/app/util/login.ts
+++ src/app/util/login.ts
@@ -1,4 +1,6 @@
+import EnvUrls from '../../app/EnvUrls';
+
 export const redirectToLogin = () => {
     window.location.href =
-        (window as any).LOGIN_URL + '?redirect=' + window.location.href;
+        EnvUrls.LOGIN_URL + '?redirect=' + window.location.href;
 };
