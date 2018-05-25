0a03191323210fadd4960c20603bd2b5bcf9f63f
Frode Hansen <frode.hansen@bekk.no>
2018-05-25 16:47:16 +0200

    Bugfix - mockdata

--- src/uttaksplan/redux/reducers/mockdata.ts
+++ src/uttaksplan/redux/reducers/mockdata.ts
@@ -4,25 +4,25 @@ import {
     Utsettelsesperiode
 } from 'uttaksplan/types';
 
-const mockArbeid: Utsettelsesperiode = {
+export const mockArbeid: Utsettelsesperiode = {
     id: '380114646-12555-8102-5233-7705536919779',
     type: Periodetype.Utsettelse,
     årsak: UtsettelseÅrsakType.Arbeid,
     tidsperiode: {
-        startdato: new Date('2018-06-28T22:00:00.000Z'),
-        sluttdato: new Date('2018-06-28T22:00:00.000Z')
+        startdato: new Date('2018-07-30T22:00:00.000Z'),
+        sluttdato: new Date('2018-07-30T22:00:00.000Z')
     },
     forelder: 'forelder1'
 };
-const mockFerie: Utsettelsesperiode = {
+export const mockFerie: Utsettelsesperiode = {
     id: '380114646-12555-8102-5233-7705536919777',
     type: Periodetype.Utsettelse,
     årsak: UtsettelseÅrsakType.Ferie,
     tidsperiode: {
-        startdato: new Date('2018-07-05T22:00:00.000Z'),
-        sluttdato: new Date('2018-07-10T22:00:00.000Z')
+        startdato: new Date('2018-08-05T22:00:00.000Z'),
+        sluttdato: new Date('2018-08-10T22:00:00.000Z')
     },
     forelder: 'forelder1'
 };
 
-export const mockUtsettelser = [mockArbeid, mockFerie];
+export const mockUtsettelser = [mockArbeid]; // mockArbeid, mockFerie];
