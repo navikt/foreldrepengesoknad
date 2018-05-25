684fc62a2e443f9ef4f2505e72d076bcfd988cc6
Frode Hansen <frode.hansen@bekk.no>
2018-05-25 12:52:01 +0200

    Rename AppSettings and skip separate file
    Former-commit-id: b31fa31ca63a96b26b065d2d2efefb58befb319e

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import Søknad from '../types/søknad/Søknad';
+import EnvUrls from '../../app/EnvUrls';
 
 function getPerson() {
-    const endpoint = (window as any).REST_API_URL;
+    const endpoint = EnvUrls.REST_API_URL;
     return axios.get(`${endpoint}/personinfo`, {
         timeout: 15 * 1000,
         withCredentials: true
@@ -21,7 +22,7 @@ function sendSøknad(søknad: Søknad) {
     const { vedlegg } = søknad;
     formData.append('vedlegg', vedlegg[0]);
 
-    const url = `${(window as any).REST_API_URL}/soknad`;
+    const url = `${EnvUrls.REST_API_URL}/soknad`;
     return axios.post(url, formData, {
         withCredentials: true,
         headers: {
