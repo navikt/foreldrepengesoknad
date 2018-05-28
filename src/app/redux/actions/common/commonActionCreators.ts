307bfe71bba769fb7a4f335d3489d1798c9986e1
Erlend Vige <erlend.vige@gmail.com>
2018-05-28 11:11:49 +0200

    First flow of application
    Implements a simple flow for the first two steps of Søknad:
    - Inngangsside takes neccessary input and redirects user to next steps based on rolle.
    - Based on value of rolle, user is redirected to correct RelasjonTilBarn-steg.
    
    Also renames AnnenForelderSteg to RelasjonTilBarnFødselSteg.
    
    
    Former-commit-id: 079e9b74d43befc1c5900b71daaa80681ad015ed

--- src/app/redux/actions/common/commonActionCreators.ts
+++ src/app/redux/actions/common/commonActionCreators.ts
@@ -1,22 +1,6 @@
 import { CommonActionKeys, CommonActionTypes } from './commonActionDefinitions';
 import { Språkkode } from '../../../intl/types';
 
-export function setBekreftetInformasjon(
-    bekreftetInformasjon: boolean
-): CommonActionTypes {
-    return {
-        type: CommonActionKeys.SET_BEKREFTET_INFORMASJON,
-        bekreftetInformasjon
-    };
-}
-
-export function setGodkjentVilkar(godkjentVilkar: boolean): CommonActionTypes {
-    return {
-        type: CommonActionKeys.SET_GODKJENT_VILKAR,
-        godkjentVilkar
-    };
-}
-
 export function setSpråk(språkkode: Språkkode): CommonActionTypes {
     return {
         type: CommonActionKeys.SET_SPRÅK,
@@ -25,7 +9,5 @@ export function setSpråk(språkkode: Språkkode): CommonActionTypes {
 }
 
 export default {
-    setBekreftetInformasjon,
-    setGodkjentVilkar,
     setSpråk
 };
