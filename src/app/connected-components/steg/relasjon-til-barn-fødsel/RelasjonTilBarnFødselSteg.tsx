eafe6af737f3ca446d0f517b541d645b703a554d
Erlend Vige <erlend.vige@gmail.com>
2018-05-29 10:02:07 +0200

    Fixup RelasjonTilBarnFødsel-logic
    - Implement FødtBarnPartial and UfødtBarnPartial logic for Mor, and make sure the same
    partials are used regardless of søkerRolle or kjønn
    - Add <FortsettKnapp>-usage to partials
    - Move partials to /relasjon-til-barn-fødsel folder
    - Remove outdated annen-forelder configs

--- src/app/connected-components/steg/relasjon-til-barn-fødsel/RelasjonTilBarnFødselSteg.tsx
+++ src/app/connected-components/steg/relasjon-til-barn-fødsel/RelasjonTilBarnFødselSteg.tsx
@@ -10,68 +10,87 @@ import Spørsmål from '../../../components/spørsmål/Spørsmål';
 import ErBarnetFødtSpørsmål from '../../../spørsmål/ErBarnetFødtSpørsmål';
 import { BarnPartial, FødtBarn, UfødtBarn } from '../../../types/søknad/Barn';
 import { DispatchProps } from '../../../redux/types';
-import { partials } from '../../../partials';
-import Søknad from '../../../types/søknad/Søknad';
+import { partials } from './partials';
+import Søknad, { SøkerRolle } from '../../../types/søknad/Søknad';
 import { AppState } from '../../../redux/reducers';
 import Vedlegg from '../../../types/søknad/Vedlegg';
+import Person from '../../../types/Person';
+import { HistoryProps, Kjønn } from '../../../types/common';
 
 interface StateProps {
     barn: BarnPartial;
     søknad: Søknad;
     vedlegg: Vedlegg;
+    person?: Person;
 }
 
-type Props = StateProps & InjectedIntlProps & DispatchProps;
+type Props = StateProps & InjectedIntlProps & DispatchProps & HistoryProps;
 class RelasjonTilBarnFødsel extends React.Component<Props, StateProps> {
-    renderPartial() {
-        const { barn, vedlegg, dispatch, søknad } = this.props;
+    renderRelasjonTilBarnPartial(erFarEllerMedmor: boolean) {
+        const { barn, vedlegg, dispatch, søknad, history } = this.props;
         if (barn.erBarnetFødt === true) {
             return (
                 <partials.FødtBarnPartial
                     dispatch={dispatch}
                     barn={barn as FødtBarn}
                     vedlegg={vedlegg}
+                    history={history}
                 />
             );
         }
         return (
-            <partials.UFødtBarnAnnenForelderPartial
+            <partials.UfødtBarnPartial
                 dispatch={dispatch}
                 barn={barn as UfødtBarn}
                 vedlegg={vedlegg}
                 søknad={søknad}
+                erFarEllerMedmor={erFarEllerMedmor}
+                history={history}
             />
         );
     }
 
     render() {
-        const { barn, dispatch } = this.props;
-        return (
-            <Steg id={StegID.ANNEN_FORELDER}>
-                <Spørsmål
-                    render={() => (
-                        <ErBarnetFødtSpørsmål
-                            erBarnetFødt={barn.erBarnetFødt}
-                            onChange={(erBarnetFødt: boolean) =>
-                                dispatch(
-                                    søknadActions.updateBarn({
-                                        erBarnetFødt
-                                    })
-                                )
-                            }
-                        />
-                    )}
-                />
-                {barn.erBarnetFødt !== undefined && this.renderPartial()}
-            </Steg>
-        );
+        const { barn, dispatch, person, søknad } = this.props;
+
+        if (person) {
+            const { søkerRolle } = søknad;
+            const { kjønn } = person;
+            const erFarEllerMedmor =
+                kjønn === Kjønn.MANN || søkerRolle === SøkerRolle.MEDMOR;
+
+            return (
+                <Steg id={StegID.RELASJON_TIL_BARN_FØDSEL}>
+                    <Spørsmål
+                        render={() => (
+                            <ErBarnetFødtSpørsmål
+                                erBarnetFødt={barn.erBarnetFødt}
+                                onChange={(erBarnetFødt: boolean) =>
+                                    dispatch(
+                                        søknadActions.updateBarn({
+                                            erBarnetFødt
+                                        })
+                                    )
+                                }
+                            />
+                        )}
+                    />
+
+                    {barn.erBarnetFødt !== undefined &&
+                        this.renderRelasjonTilBarnPartial(erFarEllerMedmor)}
+                </Steg>
+            );
+        }
+
+        return null;
     }
 }
 
 const mapStateToProps = (state: AppState): StateProps => ({
     søknad: state.søknad,
     barn: state.søknad.barn,
-    vedlegg: state.søknad.vedlegg
+    vedlegg: state.søknad.vedlegg,
+    person: state.api.person
 });
 
 export default connect<StateProps, {}, {}>(mapStateToProps)(
