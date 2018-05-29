07108a999f164d702de1bc87fb07149cc4a95185
Frode Hansen <frode.hansen@bekk.no>
2018-05-29 20:20:42 +0200

    Rewrite mock-api-server

--- mock-api-server.js
+++ mock-api-server.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const app = express();
+const guid = require('nav-frontend-js-utils').guid;
 
 const server = express();
+const router = express.Router();
+const contextPath = '/foreldrepengesoknad-api';
 
 const mockResponse = {
     fnr: '11111111111',
@@ -14,50 +18,38 @@ const mockResponse = {
     ikkeNordiskEøsLand: true
 };
 
-const startServer = (html) => {
-    server.get(
-        ['/', '/foreldrepengesoknad-api/rest/personinfo?'],
-        (req, res) => {
-            res.setHeader(
-                'Access-Control-Allow-Origin',
-                'http://localhost:8080'
-            );
-            res.setHeader(
-                'Access-Control-Allow-Methods',
-                'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-            );
-            res.setHeader(
-                'Access-Control-Allow-Headers',
-                'X-Requested-With,content-type'
-            );
-            res.setHeader('Access-Control-Allow-Credentials', true);
-            res.send(mockResponse);
-        }
-    );
-
-    server.get('/health/isAlive', (req, res) => res.sendStatus(200));
-    server.get('/health/isReady', (req, res) => res.sendStatus(200));
-
-    server.post('/foreldrepengesoknad-api/rest/engangsstonad', (req, res) =>
-        res.sendStatus(200)
-    );
-
-    server.post('/foreldrepengesoknad-api/rest/storage/vedlegg', (req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        res.setHeader('Access-Control-Allow-Methods', 'POST');
-        res.location(
-            'https://localhost:8080/foreldrepengesoknad/mock-vedlegg.pdf'
-        );
-        res.sendStatus(201);
-    });
-
-    const port = process.env.PORT || 8888;
-    server.listen(port, () => {
-        console.log(`Mock-api listening on port: ${port}`);
-    });
+const allowCrossDomain = function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-XSRF-TOKEN');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 };
 
-const logError = (errorMessage, details) => console.log(errorMessage, details);
+app.use(allowCrossDomain);
 
-startServer();
+router.get(['/rest/personinfo'], (req, res) => {
+    res.send(mockResponse);
+});
+
+router.post('/rest/engangsstonad', (req, res) => res.sendStatus(200));
+router.post('/rest/storage/vedlegg', (req, res) => {
+    const randomFileName = guid();
+    res.location(
+        `https://localhost:8080/foreldrepengesoknad/${randomFileName}`
+    );
+    res.sendStatus(201);
+});
+
+router.delete('/rest/storage/vedlegg/:id', (req, res) => {
+    res.sendStatus(204);
+});
+
+app.use(contextPath + '/', router);
+
+const port = process.env.PORT || 8888;
+app.listen(port, () => {
+    console.log(`Mock-api listening on port: ${port}`);
+});
+
+const logError = (errorMessage, details) => console.log(errorMessage, details);
