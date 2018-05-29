58226fe2b42697d5b8241057493a2cacdce3d880
Erlend Vige <erlend.vige@gmail.com>
2018-05-29 10:02:07 +0200

    Fixup RelasjonTilBarnFødsel-logic
    - Implement FødtBarnPartial and UfødtBarnPartial logic for Mor, and make sure the same
    partials are used regardless of søkerRolle or kjønn
    - Add <FortsettKnapp>-usage to partials
    - Move partials to /relasjon-til-barn-fødsel folder
    - Remove outdated annen-forelder configs
    
    
    Former-commit-id: 3867afe2354033b9095a98152a31274c7c4492cc

--- src/app/partials/index.ts
+++
@@ -1,7 +0,0 @@
-import FødtBarnPartial from './FødtBarnPartial';
-import UFødtBarnAnnenForelderPartial from './UFødtBarnAnnenForelderPartial';
-
-export const partials = {
-    FødtBarnPartial,
-    UFødtBarnAnnenForelderPartial
-};
