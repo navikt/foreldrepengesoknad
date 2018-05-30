122ad88a1afb80f5f6226297774690bc90908dcf
Erlend Vige <erlend.vige@gmail.com>
2018-05-30 09:52:37 +0200

    Utenlandsopphold modal
    Implements modal for adding UtenlandsoppholdPeriode-objects to Søknad, and logic
    for opening and closing modal at correct times. Also implements Landvelger for
    choosing residing country.
    
    
    Former-commit-id: de646ec57e7eb2bb9e2e2e5208fc3a29971b4534

--- src/app/bolker/UtenlandsoppholdBolk.tsx
+++ src/app/bolker/UtenlandsoppholdBolk.tsx
@@ -2,46 +2,78 @@ import * as React from 'react';
 import { Knapp } from 'nav-frontend-knapper';
 import UtenlandsoppholdPeriodeListe from '../components/utenlandsopphold-periode-liste/UtenlandsoppholdPeriodeListe';
 import { UtenlandsoppholdPeriode } from '../types/søknad/Utenlandsopphold';
+import UtenlandsoppholdPeriodeModal, {
+    PeriodeType
+} from '../components/utenlandsopphold-periode-modal/UtenlandsoppholdPeriodeModal';
+import { Språkkode } from '../intl/types';
 
 interface UtenlandsoppholdBolkProps {
     renderSpørsmål: () => JSX.Element;
     showLandContent: boolean;
     oppfølgingsspørsmål: string;
     perioder: UtenlandsoppholdPeriode[];
-    onPeriodeLinkClick: (periode: UtenlandsoppholdPeriode) => void;
-    onPeriodeTrashClick: (periode: UtenlandsoppholdPeriode) => void;
+    periodeType: PeriodeType;
+    språk: Språkkode;
 }
 
-const UtenlandsoppholdBolk = (props: UtenlandsoppholdBolkProps) => {
-    const {
-        renderSpørsmål,
-        showLandContent,
-        oppfølgingsspørsmål,
-        perioder,
-        onPeriodeLinkClick,
-        onPeriodeTrashClick
-    } = props;
+interface UtenlandsoppholdBolkState {
+    modalIsOpen: boolean;
+}
 
-    return (
-        <div className="countryPicker">
-            {renderSpørsmål()}
-            {showLandContent && (
-                <React.Fragment>
-                    <label htmlFor="">{oppfølgingsspørsmål}</label>
-                    <UtenlandsoppholdPeriodeListe
-                        perioder={perioder}
-                        onPeriodeLinkClick={(
-                            periode: UtenlandsoppholdPeriode
-                        ) => onPeriodeLinkClick(periode)}
-                        onPeriodeTrashClick={(
-                            periode: UtenlandsoppholdPeriode
-                        ) => onPeriodeTrashClick(periode)}
-                    />
-                    <Knapp>Legg til land</Knapp>
-                </React.Fragment>
-            )}
-        </div>
-    );
-};
+class UtenlandsoppholdBolk extends React.Component<
+    UtenlandsoppholdBolkProps,
+    UtenlandsoppholdBolkState
+> {
+    constructor(props: UtenlandsoppholdBolkProps) {
+        super(props);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.state = {
+            modalIsOpen: false
+        };
+    }
+
+    toggleModal() {
+        this.setState({
+            modalIsOpen: !this.state.modalIsOpen
+        });
+    }
+
+    render() {
+        const {
+            renderSpørsmål,
+            showLandContent,
+            oppfølgingsspørsmål,
+            perioder,
+            periodeType,
+            språk
+        } = this.props;
+
+        return (
+            <React.Fragment>
+                {renderSpørsmål()}
+                {showLandContent && (
+                    <React.Fragment>
+                        <label htmlFor="">{oppfølgingsspørsmål}</label>
+                        <UtenlandsoppholdPeriodeListe
+                            perioder={perioder}
+                            onPeriodeLinkClick={this.toggleModal}
+                            onPeriodeTrashClick={this.toggleModal}
+                        />
+                        <Knapp onClick={this.toggleModal}>Legg til land</Knapp>
+                    </React.Fragment>
+                )}
+
+                <UtenlandsoppholdPeriodeModal
+                    type={periodeType}
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.toggleModal}
+                    contentLabel={`Landvelger for ${periodeType}`}
+                    children={null}
+                    språk={språk}
+                />
+            </React.Fragment>
+        );
+    }
+}
 
 export default UtenlandsoppholdBolk;
