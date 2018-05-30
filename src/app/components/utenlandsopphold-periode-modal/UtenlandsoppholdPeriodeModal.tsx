91ceab32592b985b743f2256832f54efdbf2f67a
Erlend Vige <erlend.vige@gmail.com>
2018-05-30 10:45:36 +0200

    UtenlandsoppholdPeriodeModal fields
    Adds title + fields for fra-dato and til-dato to UtenlandsoppholdPeriodeModal,
    and puts these aswell as Landvelger into <Spørsmål>-component render functions.
    
    
    Former-commit-id: 69cb5f7b48d191f4a2349eef4d4dcc5c3165b715

--- src/app/components/utenlandsopphold-periode-modal/UtenlandsoppholdPeriodeModal.tsx
+++ src/app/components/utenlandsopphold-periode-modal/UtenlandsoppholdPeriodeModal.tsx
@@ -4,7 +4,12 @@ import Landvelger from '../landvelger/Landvelger';
 import Labeltekst from '../labeltekst/Labeltekst';
 import { Språkkode } from '../../intl/types';
 import './utenlandsoppholdPeriodeModal.less';
-
+import Spørsmål from '../spørsmål/Spørsmål';
+import { injectIntl, InjectedIntlProps, FormattedMessage } from 'react-intl';
+import getMessage from '../../util/i18nUtils';
+import DatoInput from '../dato-input/DatoInput';
+import Knapp, { Hovedknapp } from 'nav-frontend-knapper';
+import { Undertittel } from 'nav-frontend-typografi';
 export type PeriodeType = 'neste12mnd' | 'siste12mnd';
 
 export interface UtenlandsoppholdPeriodeModalProps extends ModalProps {
@@ -13,25 +18,76 @@ export interface UtenlandsoppholdPeriodeModalProps extends ModalProps {
 }
 
 const UtenlandsoppholdPeriodeModal = (
-    props: UtenlandsoppholdPeriodeModalProps
+    props: UtenlandsoppholdPeriodeModalProps & InjectedIntlProps
 ) => {
-    const { type, språk, ...modalProps } = props;
+    const { type, språk, intl, onRequestClose, ...modalProps } = props;
 
     return (
-        <Modal {...modalProps} className="utenlandsoppholdPeriodeModal">
+        <Modal
+            className="utenlandsoppholdPeriodeModal"
+            onRequestClose={onRequestClose}
+            {...modalProps}>
             <form>
-                <Landvelger
-                    label={
-                        <Labeltekst
-                            intlId={`utenlandsopphold.select.spørsmål.${type}`}
+                <Undertittel className="utenlandsoppholdPeriodeModal__title">
+                    <FormattedMessage id="utenlandsopphold.tittel" />
+                </Undertittel>
+
+                <Spørsmål
+                    render={() => (
+                        <Landvelger
+                            label={
+                                <Labeltekst
+                                    intlId={`utenlandsopphold.select.spørsmål.${type}`}
+                                />
+                            }
+                            onChange={() => {}}
+                            språk={språk}
                         />
-                    }
-                    onChange={() => {}}
-                    språk={språk}
+                    )}
                 />
+
+                <Spørsmål
+                    render={() => (
+                        <DatoInput
+                            id="fraDatoInput"
+                            label={getMessage(
+                                intl,
+                                `utenlandsopphold.datoinput.fra`
+                            )}
+                            onChange={(dato: Date) => {}}
+                            dato={new Date()}
+                        />
+                    )}
+                />
+
+                <Spørsmål
+                    render={() => (
+                        <DatoInput
+                            id="tilDatoInput"
+                            label={getMessage(
+                                intl,
+                                `utenlandsopphold.datoinput.til`
+                            )}
+                            onChange={(dato: Date) => {}}
+                            dato={new Date()}
+                        />
+                    )}
+                />
+
+                <div className="utenlandsoppholdPeriodeModal__buttonBar">
+                    <Knapp
+                        type="standard"
+                        onClick={onRequestClose}
+                        htmlType="button">
+                        <FormattedMessage id="avbryt" />
+                    </Knapp>
+                    <Hovedknapp>
+                        <FormattedMessage id="leggtil" />
+                    </Hovedknapp>
+                </div>
             </form>
         </Modal>
     );
 };
 
-export default UtenlandsoppholdPeriodeModal;
+export default injectIntl(UtenlandsoppholdPeriodeModal);
