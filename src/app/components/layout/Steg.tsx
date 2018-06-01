e667267a62632bfcac1519c7fdb383717c29f853
Erlend Vige <erlend.vige@gmail.com>
2018-06-01 16:13:22 +0200

    Render <FortsettKnapp>-component in <Steg>
    Implements FortsettKnapp-rendering in Steg based on values provided by StegConfig.
    All existing steg-components have been rewritten to take StegProps in as a required prop, which
    is being provided by mapStateToProps, to make sure our actual component logic is being kept
    pure and simple.
    
    
    Former-commit-id: ecd7eb8915c6f12eed89b63b50fc7636ca4484c8

--- src/app/components/layout/Steg.tsx
+++ src/app/components/layout/Steg.tsx
@@ -1,17 +1,30 @@
 import * as React from 'react';
 import stegConfig, { StegID } from '../../util/stegConfig';
+import { søknadStegPath } from '../../connected-components/steg/StegRoutes';
+import { History } from 'history';
+import FortsettKnapp from 'common/components/fortsett-knapp/FortsettKnapp';
 
-export interface Props {
+export interface StegProps {
     id: StegID;
+    renderFortsettKnapp?: boolean;
+    history: History;
 }
 
-class Steg extends React.Component<Props, {}> {
+class Steg extends React.Component<StegProps, {}> {
     render() {
-        const { id } = this.props;
+        const { id, renderFortsettKnapp, history } = this.props;
         return (
             <div className="steg">
                 <h1 className="steg__tittel">{stegConfig[id].tittel}</h1>
                 {this.props.children}
+
+                {renderFortsettKnapp === true && (
+                    <FortsettKnapp
+                        history={history}
+                        location={søknadStegPath(stegConfig[id].nesteSteg)}>
+                        {stegConfig[id].fortsettKnappLabel}
+                    </FortsettKnapp>
+                )}
             </div>
         );
     }
