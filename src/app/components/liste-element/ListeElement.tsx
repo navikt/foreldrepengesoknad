78ef198bef217f6ecc959709fc03ce0a2098878e
Erlend Vige <erlend.vige@gmail.com>
2018-06-06 14:32:45 +0200

    Generalization of Utenlandsopphold
    Generalizes both UtenlandsoppholdPeriodeListe and -Element to generic-based components
    called Liste and ListeElement.
    
    
    Former-commit-id: 511389144f7058285113a3b1710618eb30c13a0d

---
+++ src/app/components/liste-element/ListeElement.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
+import LinkButton from '../link-button/LinkButton';
+import SlettKnapp from 'common/components/slett-knapp/SlettKnapp';
+
+import './listeElement.less';
+
+interface ListeElementProps<T> {
+    data: T;
+    onTrashClick?: (element: T) => void;
+    onLinkClick?: (element: T) => void;
+    renderElement: (element: T) => JSX.Element;
+}
+
+type Props<T> = ListeElementProps<T> & InjectedIntlProps;
+
+class ListeElement<T> extends React.Component<Props<T>> {
+    render() {
+        const { data, renderElement, onTrashClick, onLinkClick } = this.props;
+        const onEditClickHandler = () => {
+            if (onLinkClick !== undefined) {
+                onLinkClick(data);
+            }
+        };
+
+        return (
+            <li className="listeElement">
+                <div className="listeElement__stay">
+                    <LinkButton onClick={onEditClickHandler}>
+                        <div className="listeElement__navnOgDato">
+                            {renderElement(data)}
+                        </div>
+                    </LinkButton>
+                </div>
+                {onTrashClick && (
+                    <span className="listeElement__slettIkon">
+                        <SlettKnapp
+                            ariaLabel="Slett utenlandsopphold"
+                            onClick={() => onTrashClick(data)}
+                        />
+                    </span>
+                )}
+            </li>
+        );
+    }
+}
+
+export default injectIntl(ListeElement);
