62f70d759a5c2283606fd732ed2b9edd1f5822f7
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-06-06 15:15:32 +0200

    Change enum value in Periodetype.
    Our mottak app expects "uttak" as the type value.
    The new value is lowercase because mottak is not case-insensitive.
    
    
    Former-commit-id: 9861089ca29929df60f5ef489b5694e16c0f5c7f

--- src/uttaksplan/types/periodetyper.ts
+++ src/uttaksplan/types/periodetyper.ts
@@ -1,7 +1,7 @@
 import { Tidsperiode, Forelder } from '../types';
 
 export enum Periodetype {
-    'Stonadsperiode' = 'STØNADSPERIODE',
+    'Stonadsperiode' = 'uttak',
     'Utsettelse' = 'UTSETTELSE',
     'Opphold' = 'OPPHOLD',
     'TaptPeriode' = 'TAPT_PERIODE'
