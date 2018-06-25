a611a4b1aea321d926362696a283daf2e038d79d
Erlend Vige <erlend.vige@gmail.com>
2018-06-25 14:46:28 +0200

    Revert GenerellAnnenInntektBolk
    Removes <GenerellAnnenInntektBolk>-component as the component tree got unnecessarily
    complex after moving Selvstendig Næringsdrivende to its own Bolk outside of the AnnenInntektModal.
    
    
    Former-commit-id: b75227e97e1a5bd0c79e13d81fd55ec40aed48f8

--- src/app/components/annen-inntekt-modal/AnnenInntektModal.tsx
+++ src/app/components/annen-inntekt-modal/AnnenInntektModal.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import Modal, { ModalProps } from 'nav-frontend-modal';
-import { FormattedMessage } from 'react-intl';
+import { injectIntl, InjectedIntlProps, FormattedMessage } from 'react-intl';
 import Knapp, { Hovedknapp } from 'nav-frontend-knapper';
 import { Undertittel } from 'nav-frontend-typografi';
 import Labeltekst from 'common/components/labeltekst/Labeltekst';
 import Spørsmål from 'common/components/spørsmål/Spørsmål';
+import getMessage from 'common/util/i18nUtils';
 import {
     AnnenInntekt,
     AnnenInntektPartial,
@@ -12,10 +13,14 @@ import {
 } from '../../types/søknad/AnnenInntekt';
 import InntektstypeVelger from '../inntektstype-velger/InntektstypeVelger';
 import Knapperad from 'common/components/knapperad/Knapperad';
+import { Checkbox } from 'nav-frontend-skjema';
+import AttachmentsUploader from 'common/storage/attachment/components/AttachmentUploader';
+import { Attachment } from 'common/storage/attachment/types/Attachment';
 import BEMHelper from 'common/util/bem';
 import './annenInntektModal.less';
+import TidsperiodeBolk from '../../bolker/TidsperiodeBolk';
+import { TidsperiodeMedValgfriSluttdato } from 'common/types';
 import Bolk from '../layout/Bolk';
-import GenerellAnnenInntektBolk from '../../bolker/GenerellAnnenInntektBolk';
 
 export interface AnnenInntektModalProps extends ModalProps {
     annenInntekt?: AnnenInntekt;
@@ -24,19 +29,18 @@ export interface AnnenInntektModalProps extends ModalProps {
     onEdit: (annenInntekt: AnnenInntekt) => void;
 }
 
+type Props = AnnenInntektModalProps & InjectedIntlProps;
+
 interface State {
     annenInntekt: AnnenInntektPartial;
 }
 
-class AnnenInntektModal extends React.Component<AnnenInntektModalProps, State> {
-    static getDerivedStateFromProps(
-        props: AnnenInntektModalProps,
-        state: State
-    ) {
+class AnnenInntektModal extends React.Component<Props, State> {
+    static getDerivedStateFromProps(props: Props, state: State) {
         return AnnenInntektModal.buildStateFromProps(props, state);
     }
 
-    static buildStateFromProps(props: AnnenInntektModalProps, state?: State) {
+    static buildStateFromProps(props: Props, state?: State) {
         const { isOpen } = props;
 
         if (!isOpen) {
@@ -53,12 +57,11 @@ class AnnenInntektModal extends React.Component<AnnenInntektModalProps, State> {
         }
     }
 
-    constructor(props: AnnenInntektModalProps) {
+    constructor(props: Props) {
         super(props);
 
         this.state = AnnenInntektModal.buildStateFromProps(props);
         this.onSubmit = this.onSubmit.bind(this);
-        this.updateAnnenInntekt = this.updateAnnenInntekt.bind(this);
     }
 
     updateAnnenInntekt(annenInntektProperties: AnnenInntektPartial) {
@@ -68,6 +71,30 @@ class AnnenInntektModal extends React.Component<AnnenInntektModalProps, State> {
                 ...annenInntektProperties
             }
         });
+    }
+
+    updateVedleggList(vedlegg: Attachment[]) {
+        const { annenInntekt } = this.state;
+        this.setState({
+            annenInntekt: {
+                ...annenInntekt,
+                vedlegg
+            }
+        });
+    }
+
+    updateVedleggItem(vedlegg: Attachment) {
+        const { annenInntekt } = this.state;
+        if (annenInntekt && annenInntekt.vedlegg) {
+            const index = annenInntekt.vedlegg.indexOf(vedlegg);
+            annenInntekt.vedlegg[index] = vedlegg;
+            this.setState({
+                annenInntekt: {
+                    ...annenInntekt,
+                    vedlegg: annenInntekt.vedlegg
+                }
+            });
+        }
     }
 
     onSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -85,18 +112,16 @@ class AnnenInntektModal extends React.Component<AnnenInntektModalProps, State> {
     }
 
     render() {
-        const { onRequestClose, ...modalProps } = this.props;
+        const { intl, onRequestClose, ...modalProps } = this.props;
         const { annenInntekt } = this.state;
         const inntektstype = annenInntekt.type;
-
-        const cls = BEMHelper('annenInntektModal');
-        const renderGenerellBolk =
+        const trengerDokumentasjon =
             inntektstype === AnnenInntektType.SLUTTPAKKE ||
             inntektstype === AnnenInntektType.VENTELØNN ||
             inntektstype === AnnenInntektType.MILITÆRTJENESTE ||
-            inntektstype === AnnenInntektType.LØNN_VED_VIDEREUTDANNING ||
-            inntektstype === AnnenInntektType.JOBB_I_UTLANDET;
+            inntektstype === AnnenInntektType.LØNN_VED_VIDEREUTDANNING;
 
+        const cls = BEMHelper('annenInntektModal');
         return (
             <Modal
                 className={cls.className}
@@ -122,11 +147,65 @@ class AnnenInntektModal extends React.Component<AnnenInntektModalProps, State> {
                     />
 
                     <Bolk
-                        synlig={renderGenerellBolk}
                         render={() => (
-                            <GenerellAnnenInntektBolk
-                                annenInntekt={annenInntekt || {}}
-                                onChange={this.updateAnnenInntekt}
+                            <TidsperiodeBolk
+                                tidsperiode={annenInntekt.tidsperiode || {}}
+                                onChange={(
+                                    tidsperiode: TidsperiodeMedValgfriSluttdato
+                                ) => this.updateAnnenInntekt({ tidsperiode })}
+                                sluttdatoDisabled={annenInntekt.pågående}
+                            />
+                        )}
+                    />
+
+                    <Spørsmål
+                        render={() => (
+                            <Checkbox
+                                checked={annenInntekt.pågående || false}
+                                label={getMessage(
+                                    intl,
+                                    'annenInntekt.modal.pågående'
+                                )}
+                                onChange={() => {
+                                    this.updateAnnenInntekt({
+                                        pågående: !annenInntekt.pågående,
+                                        tidsperiode: {
+                                            ...annenInntekt.tidsperiode,
+                                            sluttdato: undefined
+                                        }
+                                    });
+                                }}
+                            />
+                        )}
+                    />
+
+                    <Spørsmål
+                        synlig={trengerDokumentasjon}
+                        render={() => (
+                            <AttachmentsUploader
+                                attachments={annenInntekt.vedlegg || []}
+                                onFilesUploadStart={(
+                                    attachments: Attachment[]
+                                ) => {
+                                    this.updateVedleggList([
+                                        ...(annenInntekt.vedlegg || []),
+                                        ...attachments
+                                    ]);
+                                }}
+                                onFileUploadFinish={(vedlegg: Attachment) =>
+                                    this.updateVedleggItem(vedlegg)
+                                }
+                                onFileDeleteStart={(vedlegg: Attachment) => {
+                                    this.updateVedleggItem(vedlegg);
+                                }}
+                                onFileDeleteFinish={(vedlegg: Attachment) => {
+                                    const vedleggList =
+                                        annenInntekt.vedlegg || [];
+                                    const index = vedleggList.indexOf(vedlegg);
+                                    vedleggList.splice(index, 1);
+                                    this.updateVedleggList(vedleggList);
+                                }}
+                                attachmentType="anneninntektdokumentasjon"
                             />
                         )}
                     />
@@ -148,4 +227,4 @@ class AnnenInntektModal extends React.Component<AnnenInntektModalProps, State> {
     }
 }
 
-export default AnnenInntektModal;
+export default injectIntl(AnnenInntektModal);
