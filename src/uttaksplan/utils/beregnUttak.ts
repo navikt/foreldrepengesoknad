5262e67cda390aa554e4227bc82b66f8e33740df
Frode Hansen <frode.hansen@bekk.no>
2018-06-29 10:33:52 +0200

    Remove stønadskontoUtils

--- src/uttaksplan/utils/beregnUttak.ts
+++ src/uttaksplan/utils/beregnUttak.ts
@@ -5,7 +5,6 @@ import {
     StønadskontoType
 } from 'uttaksplan/types';
 import { Periodene, Perioden } from 'uttaksplan/utils/dataUtils';
-import { Uttaksgrunnlag } from 'uttaksplan/uttak/uttaksgrunnlag';
 
 const summerDager = (a: number, b: StønadskontoUttak) => a + b.dager;
 const summerUttaksdager = (a: number, b: Uttaksperiode) =>
@@ -19,70 +18,70 @@ export interface BeregnetUttak {
 
 export function beregnUttak(
     perioder: Periode[],
-    kontoer: StønadskontoType[],
+    stønadskontoer: StønadskontoType[],
     tilgjengeligUttak: StønadskontoUttak[]
 ): number {
     const tilgjengelig = tilgjengeligUttak
-        .filter((tu) => kontoer.includes(tu.konto))
+        .filter((tu) => stønadskontoer.includes(tu.konto))
         .reduce(summerDager, 0);
     const brukt = Periodene(perioder)
         .getUttak()
-        .filter((up) => kontoer.includes(up.konto))
+        .filter((up) => stønadskontoer.includes(up.konto))
         .reduce(summerUttaksdager, 0);
 
     return tilgjengelig - brukt;
 }
 
-function getBeregnetUttak(
-    navn: string,
-    perioder: Periode[],
-    kontoer: StønadskontoType[],
-    tilgjengeligUttak: StønadskontoUttak[]
-): BeregnetUttak {
-    const dager = beregnUttak(perioder, kontoer, tilgjengeligUttak);
-    return {
-        navn,
-        dager,
-        overforbruk: dager < 0
-    };
-}
+// function getBeregnetUttak(
+//     navn: string,
+//     perioder: Periode[],
+//     kontoer: StønadskontoType[],
+//     tilgjengeligUttak: StønadskontoUttak[]
+// ): BeregnetUttak {
+//     const dager = beregnUttak(perioder, kontoer, tilgjengeligUttak);
+//     return {
+//         navn,
+//         dager,
+//         overforbruk: dager < 0
+//     };
+// }
 
-export function beregnAlleUttakEnkel(
-    perioder: Periode[],
-    uttaksgrunnlag: Uttaksgrunnlag
-): BeregnetUttak[] {
-    const uttak: BeregnetUttak[] = [];
-    const { søker, annenForelder, tilgjengeligeUttak } = uttaksgrunnlag;
-    if (annenForelder) {
-        uttak.push(
-            getBeregnetUttak(
-                søker.fornavn,
-                perioder,
-                [StønadskontoType.MorsDelFørTermin, StønadskontoType.MorsDel],
-                tilgjengeligeUttak
-            )
-        );
-        uttak.push(
-            getBeregnetUttak(
-                annenForelder.fornavn,
-                perioder,
-                [StønadskontoType.FarsDel],
-                tilgjengeligeUttak
-            )
-        );
-        uttak.push(
-            getBeregnetUttak(
-                'Fellesdel',
-                perioder,
-                [StønadskontoType.Fellesperiode],
-                uttaksgrunnlag.tilgjengeligeUttak
-            )
-        );
-    } else {
-        uttak.push({
-            navn: søker.fornavn,
-            dager: 0
-        });
-    }
-    return uttak;
-}
+// export function beregnAlleUttakEnkel(
+//     perioder: Periode[],
+//     uttaksgrunnlag: Uttaksgrunnlag
+// ): BeregnetUttak[] {
+//     const uttak: BeregnetUttak[] = [];
+//     const { søker, annenForelder, tilgjengeligeUttak } = uttaksgrunnlag;
+//     if (annenForelder) {
+//         uttak.push(
+//             getBeregnetUttak(
+//                 søker.fornavn,
+//                 perioder,
+//                 [StønadskontoType.MorsDelFørTermin, StønadskontoType.MorsDel],
+//                 tilgjengeligeUttak
+//             )
+//         );
+//         uttak.push(
+//             getBeregnetUttak(
+//                 annenForelder.fornavn,
+//                 perioder,
+//                 [StønadskontoType.FarsDel],
+//                 tilgjengeligeUttak
+//             )
+//         );
+//         uttak.push(
+//             getBeregnetUttak(
+//                 'Fellesdel',
+//                 perioder,
+//                 [StønadskontoType.Fellesperiode],
+//                 uttaksgrunnlag.tilgjengeligeUttak
+//             )
+//         );
+//     } else {
+//         uttak.push({
+//             navn: søker.fornavn,
+//             dager: 0
+//         });
+//     }
+//     return uttak;
+// }
