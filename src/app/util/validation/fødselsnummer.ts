928192a16b403b7e43c37bf4dee246d30010e553
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-07-13 13:36:44 +0200

    Add fødselsnummer validation
    Former-commit-id: 9f2dce0fe56b42b92b19c82de28c4e9f9aff1b98

---
+++ src/app/util/validation/fødselsnummer.ts
@@ -0,0 +1,40 @@
+import { InjectedIntl } from 'react-intl';
+import { Validator } from 'common/lib/validation/types';
+import getMessage from 'common/util/i18nUtils';
+import { AnnenForelderPartial } from '../../types/søknad/AnnenForelder';
+const isValidFødselsnummer = require('is-valid-fodselsnummer');
+
+const MAKS_FNR_LENGTH = 30;
+
+export const getFødselsnummerRegler = (
+    annenForelder: AnnenForelderPartial,
+    søkersFødselsnummer: string,
+    intl: InjectedIntl
+): Validator[] => {
+    const intlKey = 'valideringsfeil.fødselsnummer';
+    return [
+        {
+            test: () => {
+                let result = true;
+                if (!annenForelder.utenlandskFnr) {
+                    try {
+                        result = isValidFødselsnummer(annenForelder.fnr);
+                    } catch (e) {
+                        result = false;
+                    }
+                } else {
+                    result =
+                        annenForelder.fnr !== undefined &&
+                        annenForelder.fnr !== '' &&
+                        annenForelder.fnr.length <= MAKS_FNR_LENGTH;
+                }
+                return result;
+            },
+            failText: getMessage(intl, 'annenForelder.ugyldigFødselsnummer')
+        },
+        {
+            test: () => søkersFødselsnummer !== annenForelder.fnr,
+            failText: getMessage(intl, 'annenForelder.ugyldigEgetFødselsnummer')
+        }
+    ];
+};
