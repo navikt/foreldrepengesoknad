964da25eec934fb3e63e06e2645799c933daa15d
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-07-20 11:41:27 +0200

    Add parser for converting date strings to date objects
    Date strings must be parsed into date objects when fetching application-json from storage.
    DatoInput component will not convert the date string before the user triggers an onblur event.

--- src/common/components/dato-input/DatoInput.tsx
+++ src/common/components/dato-input/DatoInput.tsx
@@ -3,7 +3,6 @@ import SkjemaInputElement from '../skjema-input-element/SkjemaInputElement';
 import { Feil } from '../skjema-input-element/types';
 import Datovelger, { Props as DatovelgerProps } from 'nav-datovelger';
 import { injectIntl, InjectedIntlProps } from 'react-intl';
-import * as moment from 'moment';
 
 export interface DatoInputProps extends DatovelgerProps {
     label: string | React.ReactNode;
@@ -14,12 +13,11 @@ export type Props = DatoInputProps & InjectedIntlProps;
 
 class DatoInput extends React.Component<Props, {}> {
     render() {
-        const { dato, label, feil, intl, ...rest } = this.props;
+        const { label, feil, intl, ...rest } = this.props;
         return (
             <SkjemaInputElement id={this.props.id} feil={feil} label={label}>
                 <Datovelger
                     {...rest}
-                    dato={dato ? moment(dato).toDate() : dato}
                     locale={intl.locale}
                     inputProps={{ placeholder: 'dd.mm.åååå' }}
                 />
