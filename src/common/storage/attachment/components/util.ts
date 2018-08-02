88d3700ee3383a0b76de18c0c21c53699e9578d3
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-08-02 12:32:55 +0200

    Generate correlation IDs for attachments
    Former-commit-id: 8c1659de4ac4a67a9606e430f647a20c2c5d4e5a

--- src/common/storage/attachment/components/util.ts
+++ src/common/storage/attachment/components/util.ts
@@ -2,11 +2,20 @@ import { guid } from 'nav-frontend-js-utils';
 import { Attachment } from 'common/storage/attachment/types/Attachment';
 import { AttachmentType } from '../../../../app/types/søknad/Søknad';
 
+export const SKJEMANUMMER_FOR_TERMINBEKREFTELSE = 'I000062';
+
+export const createAttachmentCorrelationID = (type: AttachmentType) => {
+    if (type === AttachmentType.TERMINBEKREFTELSE) {
+        return SKJEMANUMMER_FOR_TERMINBEKREFTELSE;
+    }
+    return guid().replace(/-/g, '');
+};
+
 export const mapFileToAttachment = (
     file: File,
     type: AttachmentType
 ): Attachment => ({
-    id: guid(),
+    id: createAttachmentCorrelationID(type),
     file,
     filename: file.name,
     filesize: file.size,
