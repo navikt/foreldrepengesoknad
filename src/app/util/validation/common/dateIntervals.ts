9c3377e7f3dc4622e2d2ccd3dde3076a811f937c
Erlend Vige <erlend.vige@gmail.com>
2018-08-03 15:26:48 +0200

    Fix overlap validation
    Implements a fix for overlap validation of tidsperioder in UtenlandsoppholdModal.
    Also fixes ordering of questions about tidligere vs senere Utenlandsopphold.
    
    
    Former-commit-id: a64fea3ab66cf59cadc11f2442400f3f2eaac374

--- src/app/util/validation/common/dateIntervals.ts
+++ src/app/util/validation/common/dateIntervals.ts
@@ -5,18 +5,16 @@ export const harTidsperiodeOverlapp = (
     tidsperiode: Tidsperiode,
     andreTidsperioder: Tidsperiode[]
 ) =>
-    andreTidsperioder.some(
-        (currentTidsperiode: Tidsperiode) =>
-            moment(tidsperiode.startdato)
-                .startOf('day')
-                .isBetween(
-                    currentTidsperiode.startdato,
-                    currentTidsperiode.sluttdato
-                ) ||
-            moment(tidsperiode.sluttdato)
-                .endOf('day')
-                .isBetween(
-                    currentTidsperiode.startdato,
-                    currentTidsperiode.sluttdato
-                )
-    );
+    andreTidsperioder.some((t: Tidsperiode) => {
+        const startdato = moment(tidsperiode.startdato).startOf('day');
+        const sluttdato = moment(tidsperiode.sluttdato).endOf('day');
+
+        return (
+            startdato.isBetween(t.startdato, t.sluttdato) ||
+            sluttdato.isBetween(t.startdato, t.sluttdato) ||
+            (startdato.isBefore(t.startdato) &&
+                sluttdato.isSameOrAfter(t.startdato)) ||
+            (sluttdato.isAfter(t.sluttdato) &&
+                startdato.isSameOrBefore(t.sluttdato))
+        );
+    });
