30837b7459aa0350541e1372644e78bdea8bdbe2
Frode Hansen <frode.hansen@bekk.no>
2018-08-09 17:06:12 +0200

    Check harAktivtArbeidsfohorld
    Former-commit-id: b5ef0db702503d1dedb9742ed7106ef6096994f7

--- src/app/util/domain/arbeidsforhold.ts
+++ src/app/util/domain/arbeidsforhold.ts
@@ -1,7 +1,24 @@
 import Arbeidsforhold from '../../types/Arbeidsforhold';
+import * as moment from 'moment';
 
 export const harArbeidsforhold = (
     arbeidsforhold: Arbeidsforhold[] | undefined
 ): boolean => {
     return arbeidsforhold !== undefined && arbeidsforhold.length > 0;
+};
+
+export const harAktivtArbeidsforhold = (
+    arbeidsforhold: Arbeidsforhold[] | undefined,
+    dato: Date
+): boolean => {
+    if (arbeidsforhold === undefined || arbeidsforhold.length === 0) {
+        return false;
+    }
+    return (
+        arbeidsforhold.find(
+            (a) =>
+                a.tom === undefined ||
+                moment(dato).isBetween(a.fom, a.tom, 'days', '[]')
+        ) !== undefined
+    );
 };
