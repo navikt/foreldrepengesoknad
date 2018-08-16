c6ccf5cd97cd9c400bc2840849d93e68ffba489c
Frode Hansen <frode.hansen@bekk.no>
2018-08-16 10:11:49 +0200

    ModalForm knapperad
    Former-commit-id: 4c35b5f322757510b57c495460f82df156638dfe

--- src/common/components/modalForm/ModalForm.tsx
+++ src/common/components/modalForm/ModalForm.tsx
@@ -1,27 +1,26 @@
 import * as React from 'react';
-import * as classnames from 'classnames';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
 import Modal from 'nav-frontend-modal';
-import { Hovedknapp, Knapp } from 'nav-frontend-knapper';
-import { injectIntl, InjectedIntlProps, FormattedMessage } from 'react-intl';
+import { Undertittel } from 'nav-frontend-typografi';
+
+import Knapp, { Hovedknapp } from 'nav-frontend-knapper';
 import BEMHelper from 'common/util/bem';
 import ValiderbarForm from 'common/lib/validation/elements/ValiderbarForm';
+import Knapperad from 'common/components/knapperad/Knapperad';
 
 import './modalForm.less';
-import { Undertittel } from 'nav-frontend-typografi';
 
 export interface ModalFormProps {
     isOpen: boolean;
     title: string;
+    renderFormButtons: boolean;
     summary?: {
         title: string;
     };
-    renderFormButtons?: boolean;
-    className?: string;
     submitLabel?: string;
     cancelLabel?: string;
     onRequestClose?: () => void;
     onSubmit: () => void;
-    onCancel?: () => void;
 }
 
 const cls = BEMHelper('modalForm');
@@ -48,11 +47,9 @@ class ModalForm extends React.Component<Props, {}> {
         const {
             isOpen,
             title,
-            onCancel,
             cancelLabel,
             submitLabel,
             intl,
-            className,
             children,
             summary,
             renderFormButtons
@@ -65,40 +62,39 @@ class ModalForm extends React.Component<Props, {}> {
                 onRequestClose={this.handleOnRequestClose}
                 closeButton={true}
                 shouldCloseOnOverlayClick={false}
-                className={classnames(cls.className, className)}>
+                className={cls.className}>
                 <div className={cls.element('content')}>
                     <ValiderbarForm
                         onSubmit={this.handleOnSubmit}
                         noSummary={false}
                         summaryTitle={summary ? summary.title : undefined}>
                         <Undertittel className={cls.element('title')}>
-                            <FormattedMessage id="selvstendigNæringsdrivende.modal.tittel" />
+                            {title}
                         </Undertittel>
                         <div className="blokk-m">{children}</div>
                         {renderFormButtons && (
                             <div className={cls.element('buttonrow')}>
-                                <Hovedknapp
-                                    className={cls.element('submitButton')}>
-                                    {submitLabel ||
-                                        intl.formatMessage({
-                                            id:
-                                                'komponent.formDialog.submitLabel'
-                                        })}
-                                </Hovedknapp>
-                                <Knapp
-                                    htmlType="button"
-                                    className={cls.element('cancelButton')}
-                                    onClick={() =>
-                                        onCancel
-                                            ? onCancel()
-                                            : this.handleOnRequestClose()
-                                    }>
-                                    {cancelLabel ||
-                                        intl.formatMessage({
-                                            id:
-                                                'komponent.formDialog.cancelLabel'
-                                        })}
-                                </Knapp>
+                                <Knapperad>
+                                    <Knapp
+                                        type="standard"
+                                        htmlType="button"
+                                        className={cls.element('cancelButton')}
+                                        onClick={this.handleOnRequestClose}>
+                                        {cancelLabel ||
+                                            intl.formatMessage({
+                                                id:
+                                                    'komponent.modalForm.cancelLabel'
+                                            })}
+                                    </Knapp>
+                                    <Hovedknapp
+                                        className={cls.element('submitButton')}>
+                                        {submitLabel ||
+                                            intl.formatMessage({
+                                                id:
+                                                    'komponent.modalForm.submitLabel'
+                                            })}
+                                    </Hovedknapp>
+                                </Knapperad>
                             </div>
                         )}
                     </ValiderbarForm>
