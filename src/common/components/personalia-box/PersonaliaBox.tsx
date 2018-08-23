c4436d9cebc1ff79e022350d73fed8f3d5cad51d
Frode Hansen <frode.hansen@bekk.no>
2018-08-23 14:18:46 +0200

    Styling - personalia

--- src/common/components/personalia-box/PersonaliaBox.tsx
+++ src/common/components/personalia-box/PersonaliaBox.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { Element, Normaltekst } from 'nav-frontend-typografi';
+import { EtikettLiten, Element } from 'nav-frontend-typografi';
 
 import './personaliaBox.less';
 import { PersonBase } from 'app/types/Person';
 import { getAlderFraDato } from 'app/util/dates/dates';
 import { formaterNavn } from 'app/util/domain/personUtil';
+import { injectIntl, FormattedMessage } from 'react-intl';
 
 interface PersonaliaBoxProps {
     person: PersonBase;
@@ -13,12 +14,15 @@ interface PersonaliaBoxProps {
 const PersonaliaBox = ({ person }: PersonaliaBoxProps) => {
     return (
         <div className="personaliaBox">
-            <Normaltekst className="personaliaBox__fnr">
+            <EtikettLiten className="personaliaBox__fnr">
                 {person.fnr}
-            </Normaltekst>
-            <Normaltekst className="personaliaBox__alder">
-                {getAlderFraDato(person.fødselsdato).år}
-            </Normaltekst>
+            </EtikettLiten>
+            <EtikettLiten className="personaliaBox__alder">
+                <FormattedMessage
+                    id="personalia.år"
+                    values={{ år: getAlderFraDato(person.fødselsdato).år }}
+                />
+            </EtikettLiten>
             <Element className="personaliaBox__navn">
                 {formaterNavn(person.fornavn, person.etternavn, person.fornavn)}
             </Element>
@@ -26,4 +30,4 @@ const PersonaliaBox = ({ person }: PersonaliaBoxProps) => {
     );
 };
 
-export default PersonaliaBox;
+export default injectIntl(PersonaliaBox);
