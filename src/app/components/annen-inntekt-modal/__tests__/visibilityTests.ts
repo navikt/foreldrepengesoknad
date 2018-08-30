1fc9c82a2bea25b524a780b93c21b8ad652f1c7b
Erlend Vige <erlend.vige@gmail.com>
2018-08-30 12:07:13 +0200

    visibilityTests for AnnenInntektModal

---
+++ src/app/components/annen-inntekt-modal/__tests__/visibilityTests.ts
@@ -0,0 +1,38 @@
+import fns from './../visibility';
+import { AnnenInntektType } from '../../../types/søknad/AnnenInntekt';
+
+describe('AnnenInntektModal visibility', () => {
+    describe('landVisible', () => {
+        it('should be visible only when type is JOBB_I_UTLANDET', () => {
+            Object.values(AnnenInntektType).forEach((type: AnnenInntektType) => {
+                if (type === AnnenInntektType.JOBB_I_UTLANDET) {
+                    expect(fns.land({ type })).toBe(true);
+                } else {
+                    expect(fns.land({ type })).toBe(false);
+                }
+            });
+        });
+    });
+    describe('arbeidsgiverNavnVisible', () => {
+        it('should be visible only when type is JOBB_I_UTLANDET', () => {
+            Object.values(AnnenInntektType).forEach((type: AnnenInntektType) => {
+                if (type === AnnenInntektType.JOBB_I_UTLANDET) {
+                    expect(fns.arbeidsgiverNavn({ type })).toBe(true);
+                } else {
+                    expect(fns.arbeidsgiverNavn({ type })).toBe(false);
+                }
+            });
+        });
+    });
+    describe('vedleggVisible', () => {
+        it('should be visible only when type is not LØNN_VED_VIDEREUTDANNING', () => {
+            Object.values(AnnenInntektType).forEach((type: AnnenInntektType) => {
+                if (type !== AnnenInntektType.LØNN_VED_VIDEREUTDANNING) {
+                    expect(fns.vedlegg({ type })).toBe(true);
+                } else {
+                    expect(fns.vedlegg({ type })).toBe(false);
+                }
+            });
+        });
+    });
+});
