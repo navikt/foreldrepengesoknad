1144b0ec23e67d381b6e2d8c68a1ae43a0882449
Frode Hansen <frode.hansen@bekk.no>
2018-08-31 10:53:42 +0200

    Cleanup annenForelderSteg
    Former-commit-id: 9c71d45d3312c5e1a1771703ed62312d0c6abba6

---
+++ src/app/util/cleanup/cleanupAnnenForelderSteg.ts
@@ -0,0 +1,65 @@
+import AnnenForelder from '../../types/søknad/AnnenForelder';
+import { AppState } from '../../redux/reducers';
+import { getAnnenForelderVisibility } from '../../connected-components/steg/annen-forelder/visibility/annenForelderVisibility';
+import { Barn, ForeldreansvarBarn } from '../../types/s\u00F8knad/Barn';
+
+interface CleanedAnnenForelderSteg {
+    annenForelder: Partial<AnnenForelder>;
+    barn: Partial<Barn>;
+}
+
+const cleanupAnnenForelder = (state: AppState): Partial<AnnenForelder> => {
+    const { søknad } = state;
+    const {
+        navn,
+        fnr,
+        bostedsland,
+        harRettPåForeldrepenger,
+        erInformertOmSøknaden,
+        skalHaForeldrepenger,
+        erUfør,
+        ...rest
+    } = søknad.annenForelder;
+
+    const vis = getAnnenForelderVisibility(state);
+    return {
+        ...rest,
+        navn: vis.personalia.fødselsnummerInput ? navn : undefined,
+        fnr: vis.personalia.fødselsnummerInput ? fnr : undefined,
+        bostedsland: søknad.annenForelder.utenlandskFnr ? bostedsland : undefined,
+        skalHaForeldrepenger: vis.annenForelderOppfølging.skalAnnenForelderHaForeldrepengerSpørsmål
+            ? skalHaForeldrepenger
+            : undefined,
+        harRettPåForeldrepenger: vis.annenForelderOppfølging.harRettPåForeldrepengerSpørsmål
+            ? harRettPåForeldrepenger
+            : undefined,
+        erInformertOmSøknaden: vis.annenForelderOppfølging.erDenAndreForelderenInformertSpørsmål
+            ? erInformertOmSøknaden
+            : undefined,
+        erUfør: vis.annenForelderOppfølging.erMorUførSpørsmål ? erUfør : undefined
+    };
+};
+
+const cleanupAnnenForelderBarn = (state: AppState): Partial<Barn> => {
+    const vis = getAnnenForelderVisibility(state);
+    const { barn } = state.søknad;
+    if (!vis.annenForelderOppfølging.omsorgsovertakelseDatoSpørsmål) {
+        return {
+            ...barn,
+            foreldreansvarsdato: undefined
+        };
+    }
+    return {
+        ...barn,
+        foreldreansvarsdato: (barn as ForeldreansvarBarn).foreldreansvarsdato
+    };
+};
+
+const cleanupAnnenForelderSteg = (state: AppState): CleanedAnnenForelderSteg => {
+    return {
+        annenForelder: cleanupAnnenForelder(state),
+        barn: cleanupAnnenForelderBarn(state)
+    };
+};
+
+export default cleanupAnnenForelderSteg;
