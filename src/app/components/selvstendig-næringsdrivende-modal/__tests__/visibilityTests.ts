0fbb4139e74b7c5ee398e987cd32f1d9e8d25137
Erlend Vige <erlend.vige@gmail.com>
2018-08-31 15:48:32 +0200

    SelvstendigNæringsdrivendeModal visibility tests

--- src/app/components/selvstendig-næringsdrivende-modal/__tests__/visibilityTests.ts
+++ src/app/components/selvstendig-næringsdrivende-modal/__tests__/visibilityTests.ts
@@ -1,5 +1,9 @@
 import fns from './../visibility';
-import { Næringstype } from '../../../types/søknad/SelvstendigNæringsdrivendeInformasjon';
+import {
+    NæringPartial,
+    Næringsrelasjon,
+    Næringstype
+} from '../../../types/søknad/SelvstendigNæringsdrivendeInformasjon';
 
 describe('SelvstendigNæringsdrivendeModal visibility', () => {
     describe('Navn på næringen', () => {
@@ -77,6 +81,193 @@ describe('SelvstendigNæringsdrivendeModal visibility', () => {
         it('should be visible if tidsperiode is complete and visible when næringsinntekt is not visible', () => {
             fns.næringsinntekt = jest.fn(() => false);
             expect(fns.næringRegistrertINorge({})).toBe(true);
+        });
+    });
+
+    describe('Næring registrert i utlandet', () => {
+        it('should be visible if registrertINorge=false and næringRegistrertINorge is visible', () => {
+            fns.næringRegistrertINorge = jest.fn(() => true);
+            expect(fns.næringRegistrertILand({ registrertINorge: false })).toBe(true);
+        });
+
+        it('should be hidden if registrertINorge is not false', () => {
+            expect(fns.næringRegistrertILand({ registrertINorge: true })).toBe(false);
+            expect(fns.næringRegistrertILand({})).toBe(false);
+        });
+
+        it('should be hidden if registrertINorge field is not visible', () => {
+            fns.næringRegistrertINorge = jest.fn(() => false);
+            expect(fns.næringRegistrertILand({ registrertINorge: false })).toBe(false);
+        });
+    });
+
+    describe('Stillingsprosent', () => {
+        it('should be visible if registrertINorge is true and visible', () => {
+            fns.næringRegistrertINorge = jest.fn(() => true);
+            expect(fns.stillingsprosent({ registrertINorge: true })).toBe(true);
+        });
+
+        it('should be visible if registrertILand is defined and visible, and registrertINorge is false', () => {
+            fns.næringRegistrertILand = jest.fn(() => true);
+            expect(fns.stillingsprosent({ registrertINorge: false, registrertILand: 'country' }));
+        });
+
+        it('should be hidden if registrertILand is not visible', () => {
+            fns.næringRegistrertILand = jest.fn(() => false);
+            expect(fns.stillingsprosent({ registrertINorge: false })).toBe(false);
+        });
+
+        it('should be hidden if registrertINorge is not visible', () => {
+            fns.næringRegistrertINorge = jest.fn(() => false);
+            expect(fns.stillingsprosent({ registrertINorge: true })).toBe(false);
+        });
+    });
+
+    describe('Regnskapsfører-bolk', () => {
+        beforeEach(() => {
+            fns.nyIArbeidslivet = jest.fn(() => true);
+            fns.varigEndringAvNæringsinntekt = jest.fn(() => true);
+        });
+
+        it('should be visible if nyIArbeidslivet is defined and visible', () => {
+            expect(fns.regnskapsførerBolk({ nyIArbeidslivet: true })).toBe(true);
+        });
+
+        it('should be hidden if nyIArbeidslivet is undefined or not visible', () => {
+            expect(fns.regnskapsførerBolk({})).toBe(false);
+            fns.nyIArbeidslivet = jest.fn(() => false);
+            expect(fns.regnskapsførerBolk({ nyIArbeidslivet: true })).toBe(false);
+        });
+
+        it('should be visible if varigEndringAvNæringsinntekt is defined and visible', () => {
+            fns.nyIArbeidslivet = jest.fn(() => false);
+            expect(fns.regnskapsførerBolk({ hattVarigEndringAvNæringsinntektSiste4Kalenderår: true })).toBe(true);
+        });
+
+        it('should be hidden if varigEndringAvNæringsinntekt is undefined or not visible', () => {
+            expect(fns.regnskapsførerBolk({})).toBe(false);
+            fns.varigEndringAvNæringsinntekt = jest.fn(() => false);
+            expect(fns.regnskapsførerBolk({ hattVarigEndringAvNæringsinntektSiste4Kalenderår: true })).toBe(false);
+        });
+    });
+
+    describe('Revisor-bolk', () => {
+        it('should be visible if harRegnskapsfører is false and regnskapsfører-bolk is visible', () => {
+            fns.regnskapsførerBolk = jest.fn(() => true);
+            expect(fns.revisorBolk({ harRegnskapsfører: false })).toBe(true);
+        });
+
+        it('should be hidden if harRegnskapsfører is true or regnskapsfører-bolk is hidden', () => {
+            fns.regnskapsførerBolk = jest.fn(() => false);
+            expect(fns.revisorBolk({ harRegnskapsfører: false })).toBe(false);
+            fns.regnskapsførerBolk = jest.fn(() => true);
+            expect(fns.revisorBolk({ harRegnskapsfører: true })).toBe(false);
+        });
+    });
+
+    describe('Kan innhente opplysninger fra revisor', () => {
+        beforeEach(() => {
+            fns.revisorBolk = jest.fn(() => true);
+        });
+
+        it('should be visible if both revisor and erNærVennEllerFamilie is defined and visible', () => {
+            expect(
+                fns.kanInnhenteOpplysningerFraRevisor({
+                    harRevisor: true,
+                    revisor: { navn: 'asdf', telefonnummer: '1234', erNærVennEllerFamilie: true }
+                })
+            ).toBe(true);
+        });
+
+        it('should be hidden if harRevisor is false', () => {
+            expect(fns.kanInnhenteOpplysningerFraRevisor({ harRevisor: false })).toBe(false);
+        });
+
+        it('should be hidden if revisor is undefined', () => {
+            expect(fns.kanInnhenteOpplysningerFraRevisor({ revisor: undefined })).toBe(false);
+        });
+
+        it('should be hidden if revisorBolk is not visible', () => {
+            fns.revisorBolk = jest.fn(() => false);
+            expect(
+                fns.kanInnhenteOpplysningerFraRevisor({
+                    harRevisor: true,
+                    revisor: { navn: 'asdf', telefonnummer: '1234', erNærVennEllerFamilie: true }
+                })
+            ).toBe(false);
+        });
+
+        it('should be hidden if erNærVennEllerFamilie is undefined', () => {
+            const relasjon = { navn: 'asdf', telefonnummer: '1234' };
+            expect(
+                fns.kanInnhenteOpplysningerFraRevisor({
+                    harRevisor: true,
+                    revisor: relasjon as Næringsrelasjon
+                })
+            ).toBe(false);
+        });
+    });
+
+    describe('Form buttons', () => {
+        beforeEach(() => {
+            fns.kanInnhenteOpplysningerFraRevisor = jest.fn(() => true);
+            fns.nyIArbeidslivet = jest.fn(() => true);
+            fns.kanInnhenteOpplysningerFraRevisor = jest.fn(() => true);
+        });
+
+        it('should be visible if regnskapsfører data is complete', () => {
+            expect(
+                fns.formButtons({
+                    harRegnskapsfører: true,
+                    regnskapsfører: { navn: 'asdf', telefonnummer: '1234', erNærVennEllerFamilie: true }
+                })
+            ).toBe(true);
+        });
+
+        it('should be hidden if regnskapsfører data is complete except for erNærVennEllerFamilie', () => {
+            const relasjon = { navn: 'asdf', telefonnummer: '1234' };
+            expect(
+                fns.formButtons({
+                    harRegnskapsfører: true,
+                    regnskapsfører: relasjon as Næringsrelasjon
+                })
+            ).toBe(false);
+        });
+
+        it('should be visible if revisor data is complete', () => {
+            expect(
+                fns.formButtons({
+                    harRevisor: true,
+                    kanInnhenteOpplsyningerFraRevisor: true,
+                    revisor: { navn: 'asdf', telefonnummer: '1234', erNærVennEllerFamilie: true }
+                })
+            ).toBe(true);
+        });
+
+        it('should be hidden if revisor data is complete but kanInnhenteOpplysningerFraRevisor is undefined or hidden', () => {
+            const relasjon: Næringsrelasjon = { navn: 'asdf', telefonnummer: '1234', erNærVennEllerFamilie: true };
+            const data: NæringPartial = { revisor: relasjon };
+            expect(fns.formButtons(data)).toBe(false);
+            data.harRevisor = true;
+            expect(fns.formButtons(data)).toBe(false);
+            fns.kanInnhenteOpplysningerFraRevisor = jest.fn(() => false);
+            expect(fns.formButtons(data)).toBe(false);
+        });
+
+        it('should be visible if no revisor and no regnskapsfører, but nyIArbeidslivet is defined and visible', () => {
+            expect(
+                fns.formButtons({
+                    harRevisor: false,
+                    harRegnskapsfører: false,
+                    nyIArbeidslivet: true
+                })
+            ).toBe(true);
+        });
+
+        it('should be hidden if data about both revisor, regnskapsfører and nyIArbeidslivet is missing', () => {
+            expect(fns.formButtons({})).toBe(false);
+            expect(fns.formButtons({ harRegnskapsfører: false })).toBe(false);
+            expect(fns.formButtons({ harRevisor: false })).toBe(false);
         });
     });
 });
