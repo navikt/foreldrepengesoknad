6a1c528eb6aad541410858768b05e2c9332ca6ef
Frode Hansen <frode.hansen@bekk.no>
2018-09-03 15:49:38 +0200

    Delete uttaksplan folder
    Former-commit-id: 5ffb8d3dc4b3ce4257092368c0bec5c4a1ac49c4

---
+++ src/app/util/uttaksplan/Uttaksdagen.ts
@@ -0,0 +1,147 @@
+import { addDays, getISODay, isSameDay, isBefore } from 'date-fns';
+import { Tidsperioden } from './Tidsperiden';
+
+/**
+ * Wrapper en dato med uttaksdager-funksjonalitet
+ * @param dato
+ */
+export const Uttaksdagen = (dato: Date) => ({
+    erUttaksdag: (): boolean => erUttaksdag(dato),
+    forrige: (): Date => getUttaksdagFørDato(dato),
+    neste: (): Date => getUttaksdagEtterDato(dato),
+    denneEllerNeste: (): Date => getUttaksdagFraOgMedDato(dato),
+    denneEllerForrige: (): Date => getUttaksdagTilOgMedDato(dato),
+    getUttaksdagerFremTilDato: (tildato: Date) => getUttaksdagerFremTilDato(dato, tildato),
+    leggTil: (uttaksdager: number): Date => {
+        if (uttaksdager < 0) {
+            return trekkUttaksdagerFraDato(dato, uttaksdager);
+        } else if (uttaksdager > 0) {
+            return leggUttaksdagerTilDato(dato, uttaksdager);
+        }
+        return dato;
+    },
+    trekkFra: (uttaksdager: number): Date => trekkUttaksdagerFraDato(dato, uttaksdager)
+});
+
+/**
+ * Returnerer
+ * @param dato
+ */
+function getUkedag(dato: Date) {
+    return getISODay(dato);
+}
+
+/**
+ * Returnerer om en dato er en uttaksdag
+ * @param dato
+ */
+function erUttaksdag(dato: Date): boolean {
+    return getUkedag(dato) !== 6 && getUkedag(dato) !== 7;
+}
+
+/**
+ * Finner første uttaksdag før dato
+ * @param dato
+ */
+function getUttaksdagFørDato(dato: Date): Date {
+    return getUttaksdagTilOgMedDato(addDays(dato, -1));
+}
+
+/**
+ * Sjekker om dato er en ukedag, dersom ikke finner den foregående fredag
+ * @param dato
+ */
+function getUttaksdagTilOgMedDato(dato: Date): Date {
+    switch (getUkedag(dato)) {
+        case 6:
+            return addDays(dato, -1);
+        case 7:
+            return addDays(dato, -2);
+        default:
+            return dato;
+    }
+}
+
+/**
+ * Første gyldige uttaksdag etter dato
+ * @param termin
+ */
+function getUttaksdagEtterDato(dato: Date): Date {
+    return getUttaksdagFraOgMedDato(addDays(dato, 1));
+}
+
+/**
+ * Sjekker om dato er en ukedag, dersom ikke finner den nærmeste påfølgende mandag
+ * @param dato
+ */
+function getUttaksdagFraOgMedDato(dato: Date): Date {
+    switch (getUkedag(dato)) {
+        case 6:
+            return addDays(dato, 2);
+        case 7:
+            return addDays(dato, 1);
+        default:
+            return dato;
+    }
+}
+
+/**
+ * Legger uttaksdager til en dato og returnerer ny dato
+ * @param dato
+ * @param uttaksdager
+ */
+function leggUttaksdagerTilDato(dato: Date, uttaksdager: number): Date {
+    let nyDato = dato;
+    let dagteller = 0;
+    let uttaksdageteller = 0;
+    while (uttaksdageteller <= uttaksdager) {
+        const tellerdato = addDays(dato, dagteller++);
+        if (erUttaksdag(tellerdato)) {
+            nyDato = tellerdato;
+            uttaksdageteller++;
+        }
+    }
+    return nyDato;
+}
+
+/**
+ * Trekker uttaksdager fra en dato og returnerer ny dato
+ * @param dato
+ * @param uttaksdager
+ */
+function trekkUttaksdagerFraDato(dato: Date, uttaksdager: number): Date {
+    let nyDato = dato;
+    let dagteller = 0;
+    let uttaksdageteller = 0;
+    while (uttaksdageteller < Math.abs(uttaksdager)) {
+        const tellerdato = addDays(dato, --dagteller);
+        if (erUttaksdag(tellerdato)) {
+            nyDato = tellerdato;
+            uttaksdageteller++;
+        }
+    }
+    return nyDato;
+}
+
+/**
+ * Finner antall uttaksdager som er mellom to datoer. Dvs. fra og med startdato, og
+ * frem til sluttdato (ikke til og med)
+ * @param fra
+ * @param til
+ */
+function getUttaksdagerFremTilDato(fom: Date, tom: Date): number {
+    if (isSameDay(fom, tom)) {
+        return 0;
+    }
+    if (isBefore(fom, tom)) {
+        return Tidsperioden({ fom, tom }).getAntallUttaksdager() - 1;
+    }
+    return (
+        -1 *
+        (Tidsperioden({
+            fom: tom,
+            tom: fom
+        }).getAntallUttaksdager() -
+            1)
+    );
+}
---
+++ src/app/util/uttaksplan/forslag/aleneomsorgMor.ts
@@ -0,0 +1,40 @@
+import { normaliserDato } from 'common/util/datoUtils';
+import { guid } from 'nav-frontend-js-utils';
+import { Dekningsgrad } from 'common/types';
+import { Permisjonsregler } from '../../../types/uttaksplan/permisjonsregler';
+import { Uttaksperiode, Periodetype, StønadskontoType } from '../../../types/uttaksplan/periodetyper';
+import { getAntallUkerTotalt, getPermisjonStartdato } from '../permisjonUtils';
+import { getTidsperiode } from '../Tidsperioden';
+import { Uttaksdagen } from '../Uttaksdagen';
+import { sorterPerioder } from '../Periodene';
+
+const UTTAKSDAGER_I_UKE = 5;
+
+/** Oppretter default stønadsperioder ut fra familiehendelsedato ++ */
+export function opprettUttaksperioderAleneomsorgMor(
+    familiehendelsedato: Date,
+    dekningsgrad: Dekningsgrad,
+    permisjonsregler: Permisjonsregler
+): Uttaksperiode[] {
+    familiehendelsedato = normaliserDato(familiehendelsedato);
+    const dagerTotalt = getAntallUkerTotalt(permisjonsregler, dekningsgrad) * UTTAKSDAGER_I_UKE;
+    const dagerFørTermin = permisjonsregler.antallUkerForeldrepengerFørFødsel * UTTAKSDAGER_I_UKE;
+    const dagerEtterTermin = dagerTotalt - dagerFørTermin;
+    const perioder: Uttaksperiode[] = [
+        {
+            id: guid(),
+            type: Periodetype.Uttak,
+            forelder: 'forelder1',
+            konto: StønadskontoType.ForeldrepengerFørFødsel,
+            tidsperiode: getTidsperiode(getPermisjonStartdato(familiehendelsedato, permisjonsregler), dagerFørTermin)
+        },
+        {
+            id: guid(),
+            type: Periodetype.Uttak,
+            forelder: 'forelder1',
+            konto: StønadskontoType.Foreldrepenger,
+            tidsperiode: getTidsperiode(Uttaksdagen(familiehendelsedato).denneEllerNeste(), dagerEtterTermin)
+        }
+    ];
+    return perioder.sort(sorterPerioder);
+}
