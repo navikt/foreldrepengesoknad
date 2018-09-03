700325d3c28869f777836056a382d3008a310c08
Frode Hansen <frode.hansen@bekk.no>
2018-09-03 15:49:38 +0200

    Delete uttaksplan folder

---
+++ src/app/util/uttaksplan/Perioden.ts
@@ -0,0 +1,84 @@
+import { isSameDay } from 'date-fns';
+import { Periode, Periodetype } from '../../types/uttaksplan/periodetyper';
+import { getTidsperiode, Tidsperioden } from './Tidsperiden';
+import { Uttaksdagen } from './Uttaksdagen';
+
+export const Perioden = (periode: Periode) => ({
+    erUttak: () => erUttak(periode),
+    erUtsettelse: () => erUtsettelse(periode),
+    erOpphold: () => erOpphold(periode),
+    erLik: (periode2: Periode) => erPerioderLike(periode, periode2),
+    erSammenhengende: (periode2: Periode) => erPerioderSammenhengende(periode, periode2),
+    setStartdato: (fom: Date) => flyttPeriode(periode, fom),
+    setUttaksdager: (uttaksdager: number) =>
+        (periode.tidsperiode = getTidsperiode(periode.tidsperiode.fom, uttaksdager)),
+    getAntallUttaksdager: () => Tidsperioden(periode.tidsperiode).getAntallUttaksdager()
+});
+
+function erOpphold(periode: Periode): boolean {
+    return periode.type === Periodetype.Opphold;
+}
+
+function erUtsettelse(periode: Periode): boolean {
+    return periode.type === Periodetype.Utsettelse;
+}
+
+function erUttak(periode: Periode): boolean {
+    return periode.type === Periodetype.Uttak;
+}
+
+/**
+ * Sjekker om to perioder er sammenhengende/dvs. det er ingen
+ * uttaksdager mellom p1.sluttdato og p2.startdato
+ * @param p1
+ * @param p2
+ */
+function erPerioderSammenhengende(p1: Periode, p2: Periode) {
+    const p1NesteUttaksdato = Uttaksdagen(p1.tidsperiode.tom).neste();
+    const p2Startdato = p2.tidsperiode.fom;
+    return isSameDay(p1NesteUttaksdato, p2Startdato);
+}
+
+/**
+ * Sjekker om to perioder er kan slåes sammen til en periode.
+ * Dvs. de er like signaturer (type, forelder, konto etc.) og har
+ * tidsperioder som er sammenhengende
+ * @param p1 periode 1
+ * @param p2 periode 2
+ */
+function erPerioderLike(p1: Periode, p2: Periode) {
+    if (p1.type !== p2.type || p1.type === Periodetype.Utsettelse || p2.type === Periodetype.Utsettelse) {
+        return false;
+    }
+    const k1 = getPeriodeFootprint(p1);
+    const k2 = getPeriodeFootprint(p2);
+    return k1 === k2;
+}
+
+/**
+ * Lager et grunnlag for å sammenligne perioder med tanke
+ * på å slå dem sammen til en periode
+ * @param periode
+ */
+function getPeriodeFootprint(periode: Periode) {
+    switch (periode.type) {
+        case Periodetype.Opphold:
+            return `${periode.type}`;
+        case Periodetype.Utsettelse:
+            return `${periode.type}${periode.forelder}${periode.årsak}`;
+        case Periodetype.Uttak:
+            return `${periode.type}${periode.forelder}${periode.konto}`;
+    }
+}
+
+/**
+ * Flytter periode til ny startdato, beholder samme antall uttaksdager
+ * @param periode
+ * @param fom
+ */
+function flyttPeriode(periode: Periode, fom: Date): Periode {
+    return {
+        ...periode,
+        tidsperiode: Tidsperioden(periode.tidsperiode).setStartdato(fom)
+    };
+}
---
+++ src/app/util/uttaksplan/permisjonUtils.ts
@@ -0,0 +1,85 @@
+import { Permisjonsregler } from '../../types/uttaksplan/permisjonsregler';
+import { Tidsperiode, Dekningsgrad, Forelder } from 'common/types';
+import { Utsettelsesperiode, UtsettelseÅrsakType } from '../../types/uttaksplan/periodetyper';
+import { getPakrevdMødrekvoteEtterTermin } from './forslag/toForeldreEttBarn';
+import { Tidsperioden } from './Tidsperiden';
+import { Uttaksdagen } from './Uttaksdagen';
+
+/**
+ *
+ * Denne må nok modifiseres noe når de ulike søkertypene/rollene tas i bruk
+ *
+ */
+
+/**
+ * Finner default startdato før termin (antallUkerForeldrepengerFørFødsel)
+ * @param familiehendelsedato
+ * @param permisjonsregler
+ */
+export function getPermisjonStartdato(familiehendelsedato: Date, permisjonsregler: Permisjonsregler): Date {
+    return Uttaksdagen(
+        familiehendelsedato // Siste uttaksdag i denne perioden er dagen før termin
+    ).trekkFra(permisjonsregler.antallUkerForeldrepengerFørFødsel * 5);
+}
+
+/**
+ * Henter ut gyldig tidsrom å legge inn en utsettelse
+ * @param familiehendelsedato
+ * @param dekningsgrad
+ * @param permisjonsregler
+ */
+export function getGyldigTidsromForUtsettelse(
+    familiehendelsedato: Date,
+    permisjonsregler: Permisjonsregler,
+    sisteRegistrertePermisjonsdag: Date
+): Tidsperiode {
+    const mødrekvoteEtterTermin = getPakrevdMødrekvoteEtterTermin(familiehendelsedato, permisjonsregler);
+    const fom = Uttaksdagen(mødrekvoteEtterTermin.tom).neste();
+    return {
+        fom,
+        tom: sisteRegistrertePermisjonsdag
+    };
+}
+
+/**
+ * Finner antall uker for fellesperiode ut fra dekningsgrad
+ * @param permisjonsregler
+ * @param dekningsgrad
+ */
+export function getAntallUkerFellesperiode(permisjonsregler: Permisjonsregler, dekningsgrad: Dekningsgrad) {
+    const totaltAntallUker =
+        dekningsgrad === '80%' ? permisjonsregler.antallUkerTotalt80 : permisjonsregler.antallUkerTotalt100;
+    return (
+        totaltAntallUker -
+        permisjonsregler.antallUkerMødrekvote -
+        permisjonsregler.antallUkerFedrekvote -
+        permisjonsregler.antallUkerForeldrepengerFørFødsel
+    );
+}
+
+/**
+ * Summerer antall uttaksdager som er registrert som ferie for en forelder
+ * @param utsettelser
+ * @param forelder
+ */
+export const getAntallFeriedagerForForelder = (utsettelser: Utsettelsesperiode[], forelder: Forelder): number => {
+    const ferier = utsettelser.filter(
+        (utsettelse) => utsettelse.årsak === UtsettelseÅrsakType.Ferie && utsettelse.forelder === forelder
+    );
+    return ferier.length === 0
+        ? 0
+        : ferier.reduce(
+              (dager: number, periode: Utsettelsesperiode) =>
+                  dager + Tidsperioden(periode.tidsperiode).getAntallUttaksdager(),
+              0
+          );
+};
+
+/**
+ * Finner totalt antall uker tilgjengelig
+ * @param permisjonsregler
+ * @param dekningsgrad
+ */
+export function getAntallUkerTotalt(permisjonsregler: Permisjonsregler, dekningsgrad: Dekningsgrad) {
+    return dekningsgrad === '80%' ? permisjonsregler.antallUkerTotalt80 : permisjonsregler.antallUkerTotalt100;
+}
