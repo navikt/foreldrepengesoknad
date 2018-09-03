14dd305d1c76c790c59976e91c25503fa02ad849
Erlend Vige <erlend.vige@gmail.com>
2018-09-03 16:13:18 +0200

    cleanupAndreInntekterSteg tests

--- src/app/util/cleanup/__tests__/cleanupAndreInntekterSteg.ts
+++ src/app/util/cleanup/__tests__/cleanupAndreInntekterSteg.ts
@@ -17,12 +17,27 @@ const søkerPartial: SøkerPartial = {
     andreInntekterSiste10Mnd: [annenInntekt as AnnenInntekt]
 };
 
+jest.mock('../cleanupNæring', () => () => 1);
+jest.mock('../cleanupFrilansInformasjon', () => () => 2);
+jest.mock('../cleanupAnnenInntekt', () => () => 3);
+
 describe('cleanupAndreInntekterSteg', () => {
     describe('selvstendigNæringsdrivendeInformasjon', () => {
         it('should be undefined if harJobbetSomSelvstendigNæringsdrivendeSiste10Mnd is not true', () => {
             const søker = cleanup(søkerPartial as Søker);
             const { selvstendigNæringsdrivendeInformasjon } = søker;
             expect(selvstendigNæringsdrivendeInformasjon).toBeUndefined();
+        });
+
+        it('should assign return-values from cleanupNæring to every row in selvstendigNæringsdrivendeInformasjon if harJobbetSomSelvstendigNæringsdrivendeSiste10Mnd', () => {
+            søkerPartial.selvstendigNæringsdrivendeInformasjon = [næring1 as Næring];
+            søkerPartial.harJobbetSomSelvstendigNæringsdrivendeSiste10Mnd = true;
+
+            const søker = cleanup(søkerPartial as Søker);
+            expect(søker.selvstendigNæringsdrivendeInformasjon).toBeDefined();
+            if (søker.selvstendigNæringsdrivendeInformasjon) {
+                expect(søker.selvstendigNæringsdrivendeInformasjon[0]).toBe(1);
+            }
         });
     });
 
@@ -32,6 +47,13 @@ describe('cleanupAndreInntekterSteg', () => {
             const { frilansInformasjon } = søker;
             expect(frilansInformasjon).toBeUndefined();
         });
+
+        it('should set frilansInformasjon to return value from cleanupFrilansInformasjon if harJobbetSomFrilansSiste10Mnd', () => {
+            søkerPartial.harJobbetSomFrilansSiste10Mnd = true;
+            søkerPartial.frilansInformasjon = frilans as FrilansInformasjon;
+            const søker = cleanup(søkerPartial as Søker);
+            expect(søker.frilansInformasjon).toBe(2);
+        });
     });
 
     describe('andreInntekterSiste10Mnd', () => {
@@ -39,6 +61,17 @@ describe('cleanupAndreInntekterSteg', () => {
             const søker = cleanup(søkerPartial as Søker);
             const { andreInntekterSiste10Mnd } = søker;
             expect(andreInntekterSiste10Mnd).toBeUndefined();
+        });
+
+        it('should assign return-values from cleanupAnnenInntekt to every row in andreInntekterSiste10Mnd if harHattAnnenInntektSiste10Mnd', () => {
+            søkerPartial.andreInntekterSiste10Mnd = [annenInntekt as AnnenInntekt];
+            søkerPartial.harHattAnnenInntektSiste10Mnd = true;
+
+            const søker = cleanup(søkerPartial as Søker);
+            expect(søker.andreInntekterSiste10Mnd).toBeDefined();
+            if (søker.andreInntekterSiste10Mnd) {
+                expect(søker.andreInntekterSiste10Mnd[0]).toBe(3);
+            }
         });
     });
 });
