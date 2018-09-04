dae83829e9bb11f47260b786b3eb32f336af899e
Frode Hansen <frode.hansen@bekk.no>
2018-09-04 20:00:03 +0200

    Base-version uttaksoppsummering
    Former-commit-id: d0d9b33b1298dd71aca588e1f203b60bbedc0b6a

--- src/app/components/uttaksoppsummering/Uttaksoppsummering.tsx
+++ src/app/components/uttaksoppsummering/Uttaksoppsummering.tsx
@@ -3,23 +3,31 @@ import { StønadskontoType } from '../../types/uttaksplan/periodetyper';
 import { Undertittel } from 'nav-frontend-typografi';
 import Kontostatus from './Kontostatus';
 import TilesList from '../tiles-list/TilesList';
+import { Forelder } from 'common/types';
 
 export interface Stønadskontouttak {
     konto: StønadskontoType;
-    dager: number;
+    dagerGjennstående: number;
+    forelder?: Forelder;
 }
 
 export interface Props {
     uttak: Stønadskontouttak[];
+    navnForelder1: string;
+    navnForelder2?: string;
 }
 
-const Uttaksoppsummering: React.StatelessComponent<Props> = ({ uttak }) => (
-    <article>
+const Uttaksoppsummering: React.StatelessComponent<Props> = ({ uttak, navnForelder1, navnForelder2 }) => (
+    <section>
         <Undertittel tag="h1" className="blokk-xs">
             Gjenstående uttak
         </Undertittel>
-        <TilesList>{uttak.map((u, idx) => <Kontostatus key={idx} uttak={u} />)}</TilesList>
-    </article>
+        <TilesList>
+            {uttak.map((u, idx) => (
+                <Kontostatus key={idx} uttak={u} navnForelder1={navnForelder1} navnForelder2={navnForelder2} />
+            ))}
+        </TilesList>
+    </section>
 );
 
 export default Uttaksoppsummering;
