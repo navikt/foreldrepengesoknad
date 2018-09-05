6daa9a850417c48ee2bc26c141bc4fda28346b21
DYBDAHL Andreas <adybdahl@one.steria.dom>
2018-09-05 15:29:49 +0200

    Added tests

--- src/app/connected-components/steg/relasjon-til-barn-adopsjon/visibility.ts
+++ src/app/connected-components/steg/relasjon-til-barn-adopsjon/visibility.ts
@@ -1,50 +1,51 @@
 import { Adopsjonsbarn } from '../../../types/søknad/Barn';
 import { fødselsdatoerErFyltUt } from '../../../util/validation/fields/fødselsdato';
 
-const spørsmålOmAdopsjonsdatoVisible = (barn: Adopsjonsbarn): boolean => {
-    return barn.adopsjonAvEktefellesBarn !== undefined;
-};
-
-const spørsmålOmAntallBarnVisible = (barn: Adopsjonsbarn): boolean => {
-    return barn.adopsjonsdato !== undefined;
-};
-
-const spørsmålOmFødselsdatoerVisible = (barn: Adopsjonsbarn): boolean => {
-    return spørsmålOmAntallBarnVisible(barn) && barn.antallBarn !== undefined;
-};
-
-const spørsmålOmAdoptertIUtlandetVisible = (barn: Adopsjonsbarn): boolean => {
+export const utfyltAdoptertIUtlandet = (barn: Partial<Adopsjonsbarn>): boolean => {
     return (
-        !barn.adopsjonAvEktefellesBarn &&
-        spørsmålOmFødselsdatoerVisible(barn) &&
-        fødselsdatoerErFyltUt(barn.fødselsdatoer)
-    );
-};
-
-const spørsmålOmAnkomstdatoVisible = (barn: Adopsjonsbarn): boolean => {
-    return barn.adopsjonAvEktefellesBarn === false && barn.adoptertIUtlandet === true;
-};
-
-const utfyltAdoptertIUtlandetVisible = (barn: Adopsjonsbarn): boolean => {
-    return (
-        spørsmålOmAdoptertIUtlandetVisible(barn) &&
+        module.spørsmålOmAdoptertIUtlandet(barn) &&
         ((barn.adoptertIUtlandet && barn.ankomstdato !== undefined) || barn.adoptertIUtlandet === false)
     );
 };
 
-const spørsmålOmVedleggVisible = (barn: Adopsjonsbarn): boolean => {
+const spørsmålOmAdopsjonsdatoVisible = (barn: Partial<Adopsjonsbarn>): boolean => {
+    return barn.adopsjonAvEktefellesBarn !== undefined;
+};
+
+const spørsmålOmAntallBarnVisible = (barn: Partial<Adopsjonsbarn>): boolean => {
+    return barn.adopsjonsdato !== undefined;
+};
+
+const spørsmålOmFødselsdatoerVisible = (barn: Partial<Adopsjonsbarn>): boolean => {
+    return module.spørsmålOmAntallBarn(barn) && barn.antallBarn !== undefined;
+};
+
+const spørsmålOmAdoptertIUtlandetVisible = (barn: Partial<Adopsjonsbarn>): boolean => {
     return (
-        utfyltAdoptertIUtlandetVisible(barn) ||
+        !barn.adopsjonAvEktefellesBarn &&
+        module.spørsmålOmFødselsdatoer(barn) &&
+        fødselsdatoerErFyltUt(barn.fødselsdatoer)
+    );
+};
+
+const spørsmålOmAnkomstdatoVisible = (barn: Partial<Adopsjonsbarn>): boolean => {
+    return barn.adopsjonAvEktefellesBarn === false && barn.adoptertIUtlandet === true;
+};
+
+const spørsmålOmVedleggVisible = (barn: Partial<Adopsjonsbarn>): boolean => {
+    return (
+        utfyltAdoptertIUtlandet(barn) ||
         (barn.adopsjonAvEktefellesBarn === true && fødselsdatoerErFyltUt(barn.fødselsdatoer))
     );
 };
 
-export default {
+const module = {
     spørsmålOmAdopsjonsdato: spørsmålOmAdopsjonsdatoVisible,
     spørsmålOmAntallBarn: spørsmålOmAntallBarnVisible,
     spørsmålOmFødselsdatoer: spørsmålOmFødselsdatoerVisible,
     spørsmålOmAdoptertIUtlandet: spørsmålOmAdoptertIUtlandetVisible,
     spørsmålOmAnkomstdato: spørsmålOmAnkomstdatoVisible,
-    utfyltAdoptertIUtlandet: utfyltAdoptertIUtlandetVisible,
     spørsmålOmVedlegg: spørsmålOmVedleggVisible
 };
+
+export default module;
