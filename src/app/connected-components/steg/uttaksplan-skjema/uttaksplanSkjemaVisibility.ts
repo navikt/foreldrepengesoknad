c010aad6e7d562c0ea769cc4e79715a4232f2269
Frode Hansen <frode.hansen@bekk.no>
2018-09-07 10:45:29 +0200

    visDekningsgradSpørsmål

--- src/app/connected-components/steg/uttaksplan-skjema/uttaksplanSkjemaVisibility.ts
+++ src/app/connected-components/steg/uttaksplan-skjema/uttaksplanSkjemaVisibility.ts
@@ -1,16 +1,19 @@
-import Søknad, { SøkerRolle } from '../../../types/s\u00F8knad/S\u00F8knad';
+import Søknad, { SøkerRolle, Søkersituasjon } from '../../../types/s\u00F8knad/S\u00F8knad';
 
 export interface UttaksplanSkjemaStegVisibility {
     visDekningsgradSpørsmål: boolean;
 }
 
-const visDekningsgradSpørsmål = (rolle: SøkerRolle): boolean => {
-    return rolle === SøkerRolle.MOR;
+const visDekningsgradSpørsmål = (rolle: SøkerRolle, situasjon: Søkersituasjon): boolean => {
+    if (situasjon === Søkersituasjon.FØDSEL) {
+        return rolle === SøkerRolle.MOR;
+    }
+    return true;
 };
 
 const getUttaksplanSkjemaStegVisibility = (søknad: Søknad): UttaksplanSkjemaStegVisibility => {
     return {
-        visDekningsgradSpørsmål: visDekningsgradSpørsmål(søknad.søker.rolle)
+        visDekningsgradSpørsmål: visDekningsgradSpørsmål(søknad.søker.rolle, søknad.situasjon)
     };
 };
 
