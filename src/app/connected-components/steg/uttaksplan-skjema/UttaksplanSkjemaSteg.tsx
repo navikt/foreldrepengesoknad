c17d1a22b6a17c18c99a75b8866886e649cf6f13
Frode Hansen <frode.hansen@bekk.no>
2018-09-08 19:41:48 +0200

    Set default value on fordeling uttaksplan, logic fortsettKnapp

--- src/app/connected-components/steg/uttaksplan-skjema/UttaksplanSkjemaSteg.tsx
+++ src/app/connected-components/steg/uttaksplan-skjema/UttaksplanSkjemaSteg.tsx
@@ -18,25 +18,32 @@ import getUttaksplanSkjemaStegVisibility, { UttaksplanSkjemaStegVisibility } fro
 import StartdatoPermisjonBolk from '../../../bolker/StartdatoPermisjonBolk';
 import PlanlagtOppholdIUttakSpørsmål from '../../../spørsm\u00E5l/PlanlagtOppholdIUttakSpørsm\u00E5l';
 import FordelingFellesperiodeSpørsmål from '../../../spørsm\u00E5l/FordelingFellesperiodeSpørsm\u00E5l';
-import { Permisjonsregler } from '../../../types/uttaksplan/permisjonsregler';
 import { getPermisjonsregler } from '../../../util/uttaksplan/permisjonsregler';
 import { getAntallUkerFellesperiode } from '../../../util/uttaksplan/permisjonUtils';
 
 interface StateProps {
     stegProps: StegProps;
     søknad: SøknadPartial;
+    antallUkerFellesperiode: number;
     vis: UttaksplanSkjemaStegVisibility;
-    permisjonsregler: Permisjonsregler;
 }
 
 type Props = SøkerinfoProps & StateProps & InjectedIntlProps & DispatchProps & HistoryProps;
 
 class UttaksplanSkjemaSteg extends React.Component<Props> {
+    componentWillMount() {
+        const defaultAntallUkerAvFellesperiode = Math.round(this.props.antallUkerFellesperiode / 2);
+        if (this.props.søknad.temp.uttaksplanSkjema.fellesperiodeukerForelder1 === undefined) {
+            this.props.dispatch(
+                søknadActions.uttaksplanUpdateSkjemdata({
+                    fellesperiodeukerForelder1: defaultAntallUkerAvFellesperiode
+                })
+            );
+        }
+    }
     render() {
-        const { søknad, vis, permisjonsregler, stegProps, søkerinfo, dispatch } = this.props;
+        const { søknad, vis, antallUkerFellesperiode, stegProps, søkerinfo, dispatch } = this.props;
         const { uttaksplanSkjema } = søknad.temp;
-        const antallUkerFellesperiode = getAntallUkerFellesperiode(permisjonsregler, søknad.dekningsgrad!);
-        const defaultAntallUkerAvFellesperiode = Math.round(antallUkerFellesperiode / 2);
         return (
             <Steg {...stegProps}>
                 <Block visible={vis.dekningsgradSpørsmål}>
@@ -71,11 +78,7 @@ class UttaksplanSkjemaSteg extends React.Component<Props> {
                 <Block visible={vis.fordelingFellesperiodeSpørsmål}>
                     <FordelingFellesperiodeSpørsmål
                         ukerFellesperiode={antallUkerFellesperiode}
-                        ukerForelder1={
-                            uttaksplanSkjema.fellesperiodeukerForelder1 !== undefined
-                                ? uttaksplanSkjema.fellesperiodeukerForelder1
-                                : defaultAntallUkerAvFellesperiode
-                        }
+                        ukerForelder1={uttaksplanSkjema.fellesperiodeukerForelder1!}
                         navnForelder1={søkerinfo.person.fornavn}
                         navnForelder2={søknad.annenForelder.navn}
                         onChange={(fellesperiodeukerForelder1) =>
@@ -105,16 +108,18 @@ const mapStateToProps = (state: AppState, props: SøkerinfoProps & HistoryProps)
 
     const stegProps: StegProps = {
         id: StegID.UTTAKSPLAN_SKJEMA,
-        renderFortsettKnapp: uttaksplanSkjemaErGyldig(),
+        renderFortsettKnapp: uttaksplanSkjemaErGyldig(state.søknad, props.søkerinfo),
         history,
         isAvailable: isAvailable(StegID.UTTAKSPLAN_SKJEMA, state.søknad, props.søkerinfo)
     };
 
+    const permisjonsregler = getPermisjonsregler();
+
     return {
         stegProps,
         søknad: state.søknad,
-        vis: getUttaksplanSkjemaStegVisibility(state.søknad),
-        permisjonsregler: getPermisjonsregler()
+        antallUkerFellesperiode: getAntallUkerFellesperiode(permisjonsregler, state.søknad.dekningsgrad!),
+        vis: getUttaksplanSkjemaStegVisibility(state.søknad)
     };
 };
 
--- src/app/util/validation/steg/uttaksplaSkjema.ts
+++ src/app/util/validation/steg/uttaksplaSkjema.ts
@@ -1,3 +1,11 @@
-export const uttaksplanSkjemaErGyldig = (): boolean => {
-    return true;
+import { Søkerinfo } from '../../../types/s\u00F8kerinfo';
+import Søknad from '../../../types/s\u00F8knad/S\u00F8knad';
+
+export const uttaksplanSkjemaErGyldig = (søknad: Søknad, søkerinfo: Søkerinfo): boolean => {
+    const { uttaksplanSkjema } = søknad.temp;
+    return (
+        søknad.dekningsgrad !== undefined &&
+        (uttaksplanSkjema.startdatoPermisjon !== undefined || uttaksplanSkjema.skalIkkeHaUttakFørTermin === true) &&
+        uttaksplanSkjema.fellesperiodeukerForelder1 !== undefined
+    );
 };
