3f90814a2379ffa2e3de81d629cf4217a2b675ab
Frode Hansen <frode.hansen@bekk.no>
2018-09-09 16:37:00 +0200

    Layout - uttaksplan steg
    Former-commit-id: 06cf8ecbbaccce7183876b1e0e87c8d69edd848c

--- src/app/components/uttaksplanlegger/Uttaksplanlegger.tsx
+++ src/app/components/uttaksplanlegger/Uttaksplanlegger.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Periode } from '../../types/uttaksplan/periodetyper';
 import { Systemtittel } from 'nav-frontend-typografi';
-import Block from 'common/components/block/Block';
 import Periodeliste from '../periodeliste/Periodeliste';
 import NyPeriodeBolk from '../../bolker/ny-periode-bolk/NyPeriodeBolk';
 
 import './uttaksplanlegger.less';
+import BEMHelper from 'common/util/bem';
+import Block from 'common/components/block/Block';
+import LinkButton from '../link-button/LinkButton';
 
 export interface Props {
     uttaksplan: Periode[];
@@ -14,21 +16,34 @@ export interface Props {
     onAdd: (periode: Periode) => void;
     onUpdate?: (periode: Periode) => void;
     onDelete?: (periode: Periode) => void;
+    onRequestReset?: () => void;
 }
+
+const BEM = BEMHelper('uttaksplanlegger');
 
 class Uttaksplanlegger extends React.Component<Props, {}> {
     render() {
-        const { uttaksplan, navnForelder1, navnForelder2, onAdd } = this.props;
+        const { uttaksplan, navnForelder1, navnForelder2, onAdd, onRequestReset } = this.props;
         return (
-            <div className="uttaksplanlegger">
-                <Block margin="s">
-                    <Systemtittel>Din uttaksplan</Systemtittel>
-                </Block>
-                <Block margin="m">
+            <article className={BEM.className}>
+                <header className={BEM.element('header')}>
+                    <Systemtittel tag="h1" className={BEM.element('header__title')}>
+                        Din uttaksplan
+                    </Systemtittel>
+                    {onRequestReset && (
+                        <LinkButton className={'header__resetLink'} onClick={() => onRequestReset}>
+                            Nullstill
+                        </LinkButton>
+                    )}
+                    <span className={BEM.element('header__details')}>Termin den 3. oktober 2018</span>
+                </header>
+                <Block>
                     <Periodeliste perioder={uttaksplan} navnForelder1={navnForelder1} navnForelder2={navnForelder2} />
                 </Block>
-                <NyPeriodeBolk onSubmit={onAdd} />
-            </div>
+                <div className={BEM.element('addFormContainer')}>
+                    <NyPeriodeBolk onSubmit={onAdd} />
+                </div>
+            </article>
         );
     }
 }
