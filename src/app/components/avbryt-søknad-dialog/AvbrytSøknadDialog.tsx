2a8baa81f93c72b17b33ca3b822636be8ea7f325
Frode Hansen <frode.hansen@bekk.no>
2018-09-12 17:44:02 +0200

    intl

--- src/app/components/avbryt-søknad-dialog/AvbrytSøknadDialog.tsx
+++ src/app/components/avbryt-søknad-dialog/AvbrytSøknadDialog.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import BekreftDialog from 'common/components/dialog/BekreftDialog';
 import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
 import Block from 'common/components/block/Block';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
+import getMessage from 'common/util/i18nUtils';
 
 export interface Props {
     synlig: boolean;
@@ -9,25 +11,30 @@ export interface Props {
     onFortsettSøknad: () => void;
 }
 
-const AvbrytSøknadDialog: React.StatelessComponent<Props> = ({ synlig, onAvbrytSøknad, onFortsettSøknad }) => (
+const AvbrytSøknadDialog: React.StatelessComponent<Props & InjectedIntlProps> = ({
+    intl,
+    synlig,
+    onAvbrytSøknad,
+    onFortsettSøknad
+}) => (
     <>
         <BekreftDialog
             isOpen={synlig}
-            bekreftLabel="Ja, avbryt søknad"
-            avbrytLabel="Nei"
+            bekreftLabel={getMessage(intl, 'avbrytSøknadDialog.avbrytSøknadLabel')}
+            avbrytLabel={getMessage(intl, 'avbrytSøknadDialog.fortsettSøknadLabel')}
             closeButton={false}
-            contentLabel="Ønsker du å avbryte søknaden?"
+            contentLabel={getMessage(intl, 'avbrytSøknadDialog.tittel')}
             onBekreft={onAvbrytSøknad}
             onRequestClose={onFortsettSøknad}>
             <Block margin="xs">
-                <Undertittel tag="h1">Avbryt søknad</Undertittel>
+                <Undertittel tag="h1">{getMessage(intl, 'avbrytSøknadDialog.tittel')}</Undertittel>
             </Block>
             <Block margin="xs">
-                <Normaltekst>Dersom du avbryter søknaden vil all informasjon i skjemaet bli slettet.</Normaltekst>
+                <Normaltekst>{getMessage(intl, 'avbrytSøknadDialog.intro')}</Normaltekst>
             </Block>
-            <Normaltekst>Ønsker du å avbryte søknaden?</Normaltekst>
+            <Normaltekst>{getMessage(intl, 'avbrytSøknadDialog.spørsmål')}</Normaltekst>
         </BekreftDialog>
     </>
 );
 
-export default AvbrytSøknadDialog;
+export default injectIntl(AvbrytSøknadDialog);
