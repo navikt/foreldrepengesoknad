71aeffd79c6f9ac89673a5c90f7b24ab9e0127cd
Erlend Vige <erlend.vige@gmail.com>
2018-09-14 13:15:34 +0200

    Rewrite SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål
    Rewrites SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål to use JaNeiSpørsmål.
    
    
    Former-commit-id: 969c25b0ac6f8345c51d1d6ba5d51892b183b263

--- src/app/spørsmål/SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål.tsx
+++ src/app/spørsmål/SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål.tsx
@@ -1,49 +1,26 @@
 import * as React from 'react';
-import RadioPanelGruppeResponsive from 'common/components/skjema/elements/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive';
 import { injectIntl, InjectedIntlProps } from 'react-intl';
-import { InputChangeEvent } from '../types/dom/Events';
 import getMessage from 'common/util/i18nUtils';
+import JaNeiSpørsmål from '../components/ja-nei-spørsmål/JaNeiSpørsmål';
 
-export enum HjemmeSamtidig {
-    'SKAL_VÆRE_HJEMME_SAMTIDIG' = 'skalVæreHjemmeSamtidig',
-    'SKAL_IKKE_VÆRE_HJEMME_SAMTIDIG' = 'skalIkkeVæreHjemmeSamtidig'
+interface SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmålProps {
+    onChange: (ønskerSamtidigUttak: boolean) => void;
+    ønskerSamtidigUttak?: boolean;
 }
 
-interface SkalDuVæreHjemmeSamtidigMedDenAndreForelderenProps {
-    skalVæreHjemmeSamtidig?: boolean;
-    onChange: (samtidigGradertUttak: boolean) => void;
-}
+type Props = SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmålProps & InjectedIntlProps;
 
-type Props = SkalDuVæreHjemmeSamtidigMedDenAndreForelderenProps & InjectedIntlProps;
-
-const SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål = (props: Props) => {
-    const { skalVæreHjemmeSamtidig, onChange, intl } = props;
-
-    let checked;
-    if (skalVæreHjemmeSamtidig === true) {
-        checked = HjemmeSamtidig.SKAL_VÆRE_HJEMME_SAMTIDIG;
-    } else if (skalVæreHjemmeSamtidig === false) {
-        checked = HjemmeSamtidig.SKAL_IKKE_VÆRE_HJEMME_SAMTIDIG;
-    }
-
-    return (
-        <RadioPanelGruppeResponsive
-            name="samtidigGradertUttak"
-            checked={checked}
-            radios={[
-                {
-                    label: getMessage(intl, 'ja'),
-                    value: HjemmeSamtidig.SKAL_VÆRE_HJEMME_SAMTIDIG
-                },
-                {
-                    label: getMessage(intl, 'nei'),
-                    value: HjemmeSamtidig.SKAL_IKKE_VÆRE_HJEMME_SAMTIDIG
-                }
-            ]}
-            legend={getMessage(intl, 'skalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål.spørsmål')}
-            onChange={(e: InputChangeEvent, v: string) => onChange(v === HjemmeSamtidig.SKAL_VÆRE_HJEMME_SAMTIDIG)}
-        />
-    );
-};
+const SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål: React.StatelessComponent<Props> = ({
+    intl,
+    ønskerSamtidigUttak,
+    onChange
+}) => (
+    <JaNeiSpørsmål
+        navn="samtidigGradertUttak"
+        spørsmål={getMessage(intl, 'skalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål.spørsmål')}
+        valgtVerdi={ønskerSamtidigUttak}
+        onChange={(v) => onChange(v)}
+    />
+);
 
 export default injectIntl(SkalDuVæreHjemmeSamtidigMedDenAndreForelderenSpørsmål);
