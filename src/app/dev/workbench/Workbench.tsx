a594bf3852b3c596834fb8353554a0cbb78b9bbf
Andreas Dybdahl <andreas.dybdahl@nav.no>
2018-09-18 13:10:02 +0200

    Forslag for barn uten aleneomsorg

--- src/app/dev/workbench/Workbench.tsx
+++ src/app/dev/workbench/Workbench.tsx
@@ -1,90 +1,90 @@
-import * as React from 'react';
-import DocumentTitle from 'react-document-title';
-import Søknad from '../../types/søknad/Søknad';
-import { opprettUttaksperioderToForeldreEttBarn } from '../../util/uttaksplan/forslag/toForeldreEttBarn';
-import Uttaksoppsummering, { Stønadskontouttak } from '../../components/uttaksoppsummering/Uttaksoppsummering';
-import { getPermisjonsregler } from '../../util/uttaksplan/permisjonsregler';
-import { Periode, Periodetype, StønadskontoType } from '../../types/uttaksplan/periodetyper';
-import { DispatchProps } from 'common/redux/types';
-import Applikasjonsside from '../../connected-components/sider/Applikasjonsside';
-import Periodeliste from '../../components/periodeliste/Periodeliste';
-import { AppState } from '../../redux/reducers';
-import { connect } from 'react-redux';
-import Block from 'common/components/block/Block';
-import søknadActionCreators from '../../redux/actions/søknad/søknadActionCreators';
-import NyPeriodeBolk from '../../bolker/ny-periode-bolk/NyPeriodeBolk';
-import { Forelder } from 'common/types';
+// import * as React from 'react';
+// import DocumentTitle from 'react-document-title';
+// import Søknad, { SøkerRolle } from '../../types/søknad/Søknad';
+// import { opprettUttaksperioderToForeldreEttBarn } from '../../util/uttaksplan/forslag/toForeldreEttBarn';
+// import Uttaksoppsummering, { Stønadskontouttak } from '../../components/uttaksoppsummering/Uttaksoppsummering';
+// import { getPermisjonsregler } from '../../util/uttaksplan/permisjonsregler';
+// import { Periode, Periodetype, StønadskontoType } from '../../types/uttaksplan/periodetyper';
+// import { DispatchProps } from 'common/redux/types';
+// import Applikasjonsside from '../../connected-components/sider/Applikasjonsside';
+// import Periodeliste from '../../components/periodeliste/Periodeliste';
+// import { AppState } from '../../redux/reducers';
+// import { connect } from 'react-redux';
+// import Block from 'common/components/block/Block';
+// import søknadActionCreators from '../../redux/actions/søknad/søknadActionCreators';
+// import NyPeriodeBolk from '../../bolker/ny-periode-bolk/NyPeriodeBolk';
+// import { Forelder } from 'common/types';
 
-interface StateProps {
-    søknad: Søknad;
-}
+// interface StateProps {
+//     søknad: Søknad;
+// }
 
-type Props = StateProps & DispatchProps;
+// type Props = StateProps & DispatchProps;
 
-const perioder = opprettUttaksperioderToForeldreEttBarn(new Date(), 13, 13, getPermisjonsregler());
+// const perioder = opprettUttaksperioderToForeldreEttBarn(new Date(), 13, 13, getPermisjonsregler());
 
-const mockUttak: Stønadskontouttak[] = [
-    {
-        konto: StønadskontoType.ForeldrepengerFørFødsel,
-        dagerGjenstående: 10
-    },
-    {
-        konto: StønadskontoType.Mødrekvote,
-        dagerGjenstående: 0,
-        forelder: Forelder.MOR
-    },
-    {
-        konto: StønadskontoType.Fellesperiode,
-        dagerGjenstående: 0
-    },
-    {
-        konto: StønadskontoType.Fedrekvote,
-        dagerGjenstående: 10,
-        forelder: Forelder.FARMEDMOR
-    }
-];
+// const mockUttak: Stønadskontouttak[] = [
+//     {
+//         konto: StønadskontoType.ForeldrepengerFørFødsel,
+//         dagerGjenstående: 10
+//     },
+//     {
+//         konto: StønadskontoType.Mødrekvote,
+//         dagerGjenstående: 0,
+//         forelder: Forelder.MOR
+//     },
+//     {
+//         konto: StønadskontoType.Fellesperiode,
+//         dagerGjenstående: 0
+//     },
+//     {
+//         konto: StønadskontoType.Fedrekvote,
+//         dagerGjenstående: 10,
+//         forelder: Forelder.FARMEDMOR
+//     }
+// ];
 
-interface State {
-    periodetype?: Periodetype;
-}
+// interface State {
+//     periodetype?: Periodetype;
+// }
 
-class Workbench extends React.Component<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.handleOnSubmit = this.handleOnSubmit.bind(this);
-    }
+// class Workbench extends React.Component<Props, State> {
+//     constructor(props: Props) {
+//         super(props);
+//         this.handleOnSubmit = this.handleOnSubmit.bind(this);
+//     }
 
-    handleOnSubmit(periode: Periode) {
-        const { dispatch } = this.props;
-        dispatch(søknadActionCreators.uttaksplanAddPeriode(periode));
-    }
+//     handleOnSubmit(periode: Periode) {
+//         const { dispatch } = this.props;
+//         dispatch(søknadActionCreators.uttaksplanAddPeriode(periode));
+//     }
 
-    render() {
-        const navn = {
-            navnMor: 'Amalie',
-            navnFarMedmor: 'Henrik'
-        };
-        return (
-            <Applikasjonsside visSpråkvelger={true} margin={false}>
-                <DocumentTitle title="Workbench" />
-                <div className="m-gray-block">
-                    <Block margin="m">
-                        <Periodeliste perioder={perioder} {...navn} />
-                    </Block>
+//     render() {
+//         const navn = {
+//             navnMor: 'Amalie',
+//             navnFarMedmor: 'Henrik'
+//         };
+//         return (
+//             <Applikasjonsside visSpråkvelger={true} margin={false}>
+//                 <DocumentTitle title="Workbench" />
+//                 <div className="m-gray-block">
+//                     <Block margin="m">
+//                         <Periodeliste perioder={perioder} {...navn} />
+//                     </Block>
 
-                    <NyPeriodeBolk onSubmit={this.handleOnSubmit} />
+//                     <NyPeriodeBolk onSubmit={this.handleOnSubmit} />
 
-                    <Block margin="l">
-                        <Uttaksoppsummering uttak={mockUttak} {...navn} />
-                    </Block>
-                </div>
-            </Applikasjonsside>
-        );
-    }
-}
+//                     <Block margin="l">
+//                         <Uttaksoppsummering uttak={mockUttak} {...navn} />
+//                     </Block>
+//                 </div>
+//             </Applikasjonsside>
+//         );
+//     }
+// }
 
-const mapStateToProps = (state: AppState): StateProps => ({
-    søknad: state.søknad
-});
+// const mapStateToProps = (state: AppState): StateProps => ({
+//     søknad: state.søknad
+// });
 
-export default connect(mapStateToProps)(Workbench);
+// export default connect(mapStateToProps)(Workbench);
