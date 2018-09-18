59b1b4c5f9f2a54b7da2129b41afeaceb08c20f7
Frode Hansen <frode.hansen@bekk.no>
2018-09-18 13:58:16 +0200

    Cleanup moment locales

--- src/app/connected-components/Foreldrepengesøknad.tsx
+++ src/app/connected-components/Foreldrepengesøknad.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, RouteComponentProps, Switch, withRouter } from 'react-router-dom';
-import moment from 'moment';
 
 import routeConfig from '../util/routing/routeConfig';
 import StegRoutes from './steg/StegRoutes';
@@ -28,8 +27,6 @@ interface StateProps {
 }
 
 type Props = StateProps & DispatchProps & RouteComponentProps<{}>;
-
-moment.locale('nb');
 
 class Foreldrepengesøknad extends React.Component<Props> {
     componentWillMount() {
