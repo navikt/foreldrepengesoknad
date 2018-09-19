662247cbf41a1b54599b8672fe02f5a3df8d83c3
Frode Hansen <frode.hansen@bekk.no>
2018-09-19 18:37:42 +0200

    Remove uttakstatus from uttaksplanInfo
    Former-commit-id: 83ba3bef05ebeef1cb0dbf264664544f48e85ce8

--- src/app/util/uttaksplan/uttaksplanInfo.ts
+++ src/app/util/uttaksplan/uttaksplanInfo.ts
@@ -6,15 +6,12 @@ import { erFarEllerMedmor } from '../domain/personUtil';
 import { TilgjengeligStønadskonto, StønadskontoType } from '../../types/uttaksplan/periodetyper';
 import { getVelgbareStønadskontotyper } from './st\u00F8nadskontoer';
 import { Kjønn } from '../../types/common';
-import { Stønadskontouttak } from '../../components/uttaksoppsummering/Uttaksoppsummering';
-import { beregnGjenståendeUttaksdager } from '../uttaksPlanStatus';
 
 export interface UttaksplanInfo {
     familiehendelsesdato: Date;
     søkerErFarEllerMedmor: boolean;
     navnPåForeldre: NavnPåForeldre;
     tilgjengeligeStønadskontoer: TilgjengeligStønadskonto[];
-    uttakStatus: Stønadskontouttak[];
     velgbareStønadskontoer: StønadskontoType[];
     søkerKjønn: Kjønn;
 }
@@ -28,9 +25,8 @@ export const getAggregertUttaksplanInfo = (
         familiehendelsesdato: getFamiliehendelsedato(søknad.barn, søknad.situasjon),
         navnPåForeldre: getNavnPåForeldre(søknad, søkerinfo.person),
         søkerErFarEllerMedmor: erFarEllerMedmor(søkerinfo.person.kjønn, søknad.søker.rolle),
-        tilgjengeligeStønadskontoer,
         søkerKjønn: søkerinfo.person.kjønn,
-        velgbareStønadskontoer: getVelgbareStønadskontotyper(tilgjengeligeStønadskontoer),
-        uttakStatus: beregnGjenståendeUttaksdager(tilgjengeligeStønadskontoer, søknad.uttaksplan)
+        tilgjengeligeStønadskontoer,
+        velgbareStønadskontoer: getVelgbareStønadskontotyper(tilgjengeligeStønadskontoer)
     };
 };
