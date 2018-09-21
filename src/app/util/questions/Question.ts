ae9e0a7a29563f12d43194b94640e0f2e45c21db
Frode Hansen <frode.hansen@bekk.no>
2018-09-21 22:46:21 +0200

    Config with visibility and formComplete
    Former-commit-id: 220bd314ee468b638802748b695f64e6eecd5c67

--- src/app/util/questions/Question.ts
+++ src/app/util/questions/Question.ts
@@ -1,37 +1,45 @@
 export type QuestionValue = string | number | boolean | Date | undefined;
 
-export const questionHasValidInput = (value: QuestionValue) => {
+export const questionIsAnswered = (value: QuestionValue) => {
     return value !== undefined && value !== '';
 };
 
 export interface QuestionConfig<P> {
     [key: string]: {
         getValue: (props: P) => QuestionValue;
-        dependsOnQuestion?: {
-            question: string;
-            check: (value: QuestionValue) => boolean;
-        };
-        dependency?: (props: P) => boolean;
+        parentQuestion?: string;
+        ownDependency?: (props: P) => boolean;
+        isOptional?: boolean;
     };
 }
 
 export const Questions = <P>(questions: QuestionConfig<P>) => ({
-    checkVisibility: (question: string, payload: P, isVisible: boolean): boolean => {
+    questionIsVisible: (question: string, payload: P): boolean => {
         const config = questions[question];
-        if (config.dependsOnQuestion === undefined) {
-            return config.dependency ? config.dependency(payload) : isVisible;
+        const ownDependencyIsMet = config.ownDependency ? config.ownDependency(payload) : true;
+        if (ownDependencyIsMet === false) {
+            return false;
+        }
+        if (config.parentQuestion === undefined) {
+            return ownDependencyIsMet;
         } else {
-            const parentConfig = questions[config.dependsOnQuestion.question];
-            const value = parentConfig.getValue(payload);
-            const visible = config.dependsOnQuestion.check(value);
-            const ownDependencyIsMet = config.dependency ? config.dependency(payload) : true;
-            if (ownDependencyIsMet === false || visible === false) {
-                return false;
-            }
-            return Questions(questions).checkVisibility(config.dependsOnQuestion.question, payload, visible);
+            const parentHasValidValue = questionIsAnswered(questions[config.parentQuestion].getValue(payload));
+            return parentHasValidValue && Questions(questions).questionIsVisible(config.parentQuestion, payload);
         }
     },
     check: (question: string, payload: P) => {
-        return Questions(questions).checkVisibility(question, payload, false);
+        return Questions(questions).questionIsVisible(question, payload);
+    },
+
+    allQuestionsAnswered: (payload: P) => {
+        let allQuestionsHasAnswers = true;
+        Object.keys(questions).forEach((key) => {
+            const question = questions[key];
+            if (Questions(questions).questionIsVisible(key, payload)) {
+                const answered = questionIsAnswered(question.getValue(payload)) || question.isOptional === true;
+                allQuestionsHasAnswers = allQuestionsHasAnswers === true && answered;
+            }
+        });
+        return allQuestionsHasAnswers;
     }
 });
