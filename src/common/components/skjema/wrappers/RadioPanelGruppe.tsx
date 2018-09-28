5d7b6375c7968230ba2d7e0b1a1e30d80326776d
Frode Hansen <frode.hansen@bekk.no>
2018-09-29 01:08:28 +0200

    ValiderbarRadioPanelGruppe

---
+++ src/common/components/skjema/wrappers/RadioPanelGruppe.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { SkjemaelementProps } from 'common/components/skjema/wrappers/types/common';
+import { RadioPanelGruppeResponsiveProps } from 'common/components/skjema/elements/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive';
+import ValiderbarRadioPanelGruppeResponsive from 'common/lib/validation/elements/ValiderbarRadioPanelGruppeResponsive';
+
+type OwnProps = SkjemaelementProps & RadioPanelGruppeResponsiveProps;
+
+const RadioPanelGruppe: React.StatelessComponent<OwnProps> = (props: OwnProps) => {
+    return <ValiderbarRadioPanelGruppeResponsive {...props} />;
+};
+
+export default RadioPanelGruppe;
