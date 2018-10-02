2cd3a885890dda4a928868ddd7c46b5dcc7bb7b1
Frode Hansen <frode.hansen@bekk.no>
2018-10-02 09:47:45 +0200

    CodeClean

--- src/app/spørsmål/SkalIkkeHaUttakFørTerminSpørsmål.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export interface Props {}
-
-const SkalIkkeHaUttakFørTerminSpørsmål: React.StatelessComponent<Props> = (props) => (
-    <div>SkalIkkeHaUttakFørTerminSpørsmål</div>
-);
-
-export default SkalIkkeHaUttakFørTerminSpørsmål;
