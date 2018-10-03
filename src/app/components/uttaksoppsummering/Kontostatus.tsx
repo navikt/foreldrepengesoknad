0227a446f8b1378a81b53a0ae0b9ca2eccf6746d
Andreas Dybdahl <andreas.dyb@gmail.com>
2018-10-03 09:25:00 +0200

    Days and weeks show up as negative if they go below 0

--- src/app/components/uttaksoppsummering/Kontostatus.tsx
+++ src/app/components/uttaksoppsummering/Kontostatus.tsx
@@ -7,9 +7,9 @@ import { getVarighetString } from 'common/util/intlUtils';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { getStønadskontoNavn } from '../../util/uttaksplan';
 
-import './kontostatus.less';
 import StønadskontoIkon from '../uttaksplan-ikon/StønadskontoIkon';
 import { NavnPåForeldre } from 'common/types';
+import './kontostatus.less';
 
 export interface Props {
     uttak: Stønadskontouttak;
@@ -18,16 +18,25 @@ export interface Props {
 
 const BEM = BEMHelper('kontostatus');
 
-const Kontostatus: React.StatelessComponent<Props & InjectedIntlProps> = ({ uttak, navnPåForeldre, intl }) => (
-    <Normaltekst className={BEM.className} tag="div">
-        <div className={BEM.element('ikon')}>
-            <StønadskontoIkon konto={uttak.konto} navnPåForeldre={navnPåForeldre} />
-        </div>
-        <div className={BEM.element('content')}>
-            <div className={BEM.element('konto')}>{getStønadskontoNavn(intl, uttak.konto, navnPåForeldre)}</div>
-            <strong className={BEM.element('dager')}>{getVarighetString(uttak.dagerGjenstående, intl)}</strong>
-        </div>
-    </Normaltekst>
-);
+const Kontostatus: React.StatelessComponent<Props & InjectedIntlProps> = ({ uttak, navnPåForeldre, intl }) => {
+    const varighetString = getVarighetString(uttak.dagerGjenstående, intl);
+    const kontoErOvertrukket = uttak.dagerGjenstående < 0;
+
+    return (
+        <Normaltekst className={BEM.className} tag="div">
+            <div className={BEM.element('ikon')}>
+                <StønadskontoIkon konto={uttak.konto} navnPåForeldre={navnPåForeldre} />
+            </div>
+            <div className={BEM.element('content')}>
+                <div className={kontoErOvertrukket ? BEM.element('kontoOvertrukket') : BEM.element('konto')}>
+                    {getStønadskontoNavn(intl, uttak.konto, navnPåForeldre)}
+                </div>
+                <strong className={kontoErOvertrukket ? BEM.element('dagerOvertrukket') : BEM.element('dager')}>
+                    {kontoErOvertrukket ? `- ${varighetString}` : varighetString}
+                </strong>
+            </div>
+        </Normaltekst>
+    );
+};
 
 export default injectIntl(Kontostatus);
