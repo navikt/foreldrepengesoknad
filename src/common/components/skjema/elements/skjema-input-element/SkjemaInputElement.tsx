6a17fe6ea8f131c8579872ff2796e6ad46fe43b4
Frode Hansen <frode.hansen@bekk.no>
2018-10-04 10:01:41 +0200

    Label renderfix
    Former-commit-id: 539eb68a2055cde13c78afb6a502e3e1f3af548c

--- src/common/components/skjema/elements/skjema-input-element/SkjemaInputElement.tsx
+++ src/common/components/skjema/elements/skjema-input-element/SkjemaInputElement.tsx
@@ -24,7 +24,7 @@ const SkjemaInputElement: React.StatelessComponent<Props> = (props: Props) => {
                     {label}
                 </label>
             ) : (
-                { label }
+                label
             )}
             <div
                 className={classnames({
