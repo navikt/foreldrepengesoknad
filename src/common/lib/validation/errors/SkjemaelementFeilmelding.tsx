072103ae9acea3a9d4e79076a0393c87124bc4a6
Frode Hansen <frode.hansen@bekk.no>
2018-10-04 12:51:40 +0200

    Extend validator with function for rendering with intl
    Former-commit-id: 00538ff22971ee21e7e8ab2a3117e232ca4da210

--- src/common/lib/validation/errors/SkjemaelementFeilmelding.tsx
+++ src/common/lib/validation/errors/SkjemaelementFeilmelding.tsx
@@ -1,23 +1,32 @@
 import * as React from 'react';
 import { SkjemaelementFeil } from 'nav-frontend-skjema/src/skjemaelement-feilmelding';
+import { injectIntl, InjectedIntlProps, InjectedIntl } from 'react-intl';
+import { ValidatorFailTextIntl } from 'common/lib/validation/types';
 
 export interface Props {
-    feil: SkjemaelementFeil;
+    feil?: SkjemaelementFeil;
 }
 
-class SkjemaelementFeilmelding extends React.Component<Props> {
-    renderFeil() {
-        return <div className="skjemaelement__feilmelding">{this.props.feil.feilmelding}</div>;
+const renderFeil = (feil: SkjemaelementFeil, intl: InjectedIntl): JSX.Element => {
+    let msg;
+    if (typeof feil.feilmelding === 'string') {
+        msg = feil.feilmelding;
+    } else {
+        const failTextIntl = feil.feilmelding as ValidatorFailTextIntl;
+        msg = intl.formatMessage({ id: failTextIntl.intlKey }, failTextIntl.values);
     }
+    return <div className="skjemaelement__feilmelding">{msg}</div>;
+};
 
+class SkjemaelementFeilmelding extends React.Component<Props & InjectedIntlProps> {
     render() {
-        const { feil } = this.props;
+        const { feil, intl } = this.props;
         return (
             <div role="alert" aria-live="assertive">
-                {feil && this.renderFeil()}
+                {feil && renderFeil(feil, intl)}
             </div>
         );
     }
 }
 
-export default SkjemaelementFeilmelding;
+export default injectIntl(SkjemaelementFeilmelding);
