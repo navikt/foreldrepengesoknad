104c3c7422becf0889cd41b55666931ebc015711
Frode Hansen <frode.hansen@bekk.no>
2018-10-08 12:10:23 +0200

    Veiledning for fordelingFellesperiode

--- src/app/connected-components/steg/uttaksplan-skjema/enkeltspørsmål/FordelingFellesperiodeSpørsmål.tsx
+++ src/app/connected-components/steg/uttaksplan-skjema/enkeltspørsmål/FordelingFellesperiodeSpørsmål.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import RangeInput from 'common/components/skjema/elements/range-input/RangeInput';
 import { InjectedIntlProps, injectIntl, FormattedMessage } from 'react-intl';
-import { Ingress, Element } from 'nav-frontend-typografi';
+import { Ingress, Normaltekst } from 'nav-frontend-typografi';
 import UttaksplanSkjemaSpørsmål, { UttaksplanSkjemaspørsmålProps } from '../UttaksplanSkjemaSpørsmål';
 import { NavnPåForeldre } from 'common/types';
+import getMessage from 'common/util/i18nUtils';
+import Tittel from '../../../../components/tittel/Tittel';
+import Lenke from 'nav-frontend-lenker';
+import lenker from '../../../../util/routing/lenker';
+import Block from 'common/components/block/Block';
+import { getPermisjonsregler } from '../../../../util/uttaksplan/permisjonsregler';
 
 export interface OwnProps {
     navnPåForeldre: NavnPåForeldre;
@@ -12,63 +18,87 @@ export interface OwnProps {
 
 const FordelingFellesperiodeSpørsmål: React.StatelessComponent<
     OwnProps & UttaksplanSkjemaspørsmålProps & InjectedIntlProps
-> = ({ visible, navnPåForeldre, ukerFellesperiode, intl }) => (
-    <UttaksplanSkjemaSpørsmål
-        visible={visible}
-        render={(data, onChange) => (
-            <RangeInput
-                label={<Element tag="div">Hvor mange uker ønsker du å ta av fellesperioden</Element>}
-                ariaDescription={intl.formatMessage(
-                    { id: 'uttaksplan.skjema.fordeling.aria.fordeling' },
-                    {
-                        navnMor: navnPåForeldre.mor,
-                        ukerFellesperiode
-                    }
-                )}
-                value={data.fellesperiodeukerMor!}
-                min={0}
-                max={ukerFellesperiode}
-                onChange={(fellesperiodeukerMor) => onChange({ fellesperiodeukerMor })}
-                steppers={{
-                    reduceLabel: intl.formatMessage(
-                        { id: 'uttaksplan.skjema.fordeling.reduser.tooltip' },
-                        {
-                            navn: navnPåForeldre.mor || intl.formatMessage({ id: 'uttaksplan.mor' })
-                        }
-                    ),
-                    increaseLabel: intl.formatMessage(
-                        { id: 'uttaksplan.skjema.fordeling.reduser.tooltip' },
-                        {
-                            navn: navnPåForeldre.farMedmor || intl.formatMessage({ id: 'uttaksplan.farMedmor' })
-                        }
-                    )
-                }}
-                ariaValueChangedMessage={(value) =>
-                    intl.formatMessage(
-                        { id: 'uttaksplan.skjema.fordeling.valgtVerdi' },
-                        {
-                            ukerForelder: value,
-                            ukerTotalt: ukerFellesperiode,
-                            navnForelder: navnPåForeldre.mor || intl.formatMessage({ id: 'uttaksplan.mor' })
-                        }
-                    )
-                }
-                valueLabelRenderer={(options) => (
-                    <Ingress tag="p" className="m-text-center fordelingFellesperiode-valgtVerdi">
-                        <FormattedMessage
-                            id="uttaksplan.skjema.fordeling.valgtVerdi"
-                            values={{
-                                ukerForelder: options.value,
-                                ukerTotalt: options.max,
-                                navnForelder: navnPåForeldre.mor || intl.formatMessage({ id: 'uttaksplan.mor' })
+> = ({ visible, navnPåForeldre, ukerFellesperiode, intl }) => {
+    const permisjonsregler = getPermisjonsregler();
+    const infotekst = getMessage(intl, 'uttaksplan.skjema.fordeling.veiledning', {
+        pakrevdForelder1: permisjonsregler.antallUkerForeldrepengerFørFødsel + permisjonsregler.antallUkerMødrekvote,
+        pakrevdForelder2: permisjonsregler.antallUkerFedrekvote,
+        navnForelder1: navnPåForeldre.mor,
+        navnForelder2: navnPåForeldre.farMedmor
+    });
+    return (
+        <UttaksplanSkjemaSpørsmål
+            visible={visible}
+            render={(data, onChange) => (
+                <RangeInput
+                    label={
+                        <Tittel
+                            tittel={getMessage(intl, 'uttaksplan.skjema.fordeling.spørsmål')}
+                            info={{
+                                tekst: infotekst,
+                                rikTekst: (
+                                    <Normaltekst tag="div">
+                                        <Block margin="xxs">{infotekst}</Block>
+                                        <Lenke href={lenker.nav_aktivitetskrav} target="_blank">
+                                            <FormattedMessage id="uttaksplan.skjema.fordeling.veiledning.lenketekst" />
+                                        </Lenke>
+                                    </Normaltekst>
+                                )
                             }}
                         />
-                    </Ingress>
-                )}
-                valueLabelPlacement="below"
-            />
-        )}
-    />
-);
+                    }
+                    ariaDescription={intl.formatMessage(
+                        { id: 'uttaksplan.skjema.fordeling.aria.fordeling' },
+                        {
+                            navnMor: navnPåForeldre.mor,
+                            ukerFellesperiode
+                        }
+                    )}
+                    value={data.fellesperiodeukerMor!}
+                    min={0}
+                    max={ukerFellesperiode}
+                    onChange={(fellesperiodeukerMor) => onChange({ fellesperiodeukerMor })}
+                    steppers={{
+                        reduceLabel: intl.formatMessage(
+                            { id: 'uttaksplan.skjema.fordeling.reduser.tooltip' },
+                            {
+                                navn: navnPåForeldre.mor || intl.formatMessage({ id: 'uttaksplan.mor' })
+                            }
+                        ),
+                        increaseLabel: intl.formatMessage(
+                            { id: 'uttaksplan.skjema.fordeling.reduser.tooltip' },
+                            {
+                                navn: navnPåForeldre.farMedmor || intl.formatMessage({ id: 'uttaksplan.farMedmor' })
+                            }
+                        )
+                    }}
+                    ariaValueChangedMessage={(value) =>
+                        intl.formatMessage(
+                            { id: 'uttaksplan.skjema.fordeling.valgtVerdi' },
+                            {
+                                ukerForelder: value,
+                                ukerTotalt: ukerFellesperiode,
+                                navnForelder: navnPåForeldre.mor || intl.formatMessage({ id: 'uttaksplan.mor' })
+                            }
+                        )
+                    }
+                    valueLabelRenderer={(options) => (
+                        <Ingress tag="p" className="m-text-center fordelingFellesperiode-valgtVerdi">
+                            <FormattedMessage
+                                id="uttaksplan.skjema.fordeling.valgtVerdi"
+                                values={{
+                                    ukerForelder: options.value,
+                                    ukerTotalt: options.max,
+                                    navnForelder: navnPåForeldre.mor || intl.formatMessage({ id: 'uttaksplan.mor' })
+                                }}
+                            />
+                        </Ingress>
+                    )}
+                    valueLabelPlacement="below"
+                />
+            )}
+        />
+    );
+};
 
 export default injectIntl(FordelingFellesperiodeSpørsmål);
--- src/app/util/routing/lenker.ts
+++ src/app/util/routing/lenker.ts
@@ -2,5 +2,6 @@ export default {
     rettOgPlikt: 'https://nav.no/rettOgPlikt',
     papirsøknadForeldreansvar:
         'https://www.nav.no/no/Person/Skjemaer-for-privatpersoner/skjemaveileder/vedlegg?key=267385&veiledertype=privatperson&method=mail',
-    viktigeFrister: 'https://www.nav.no'
+    viktigeFrister: 'https://www.nav.no',
+    nav_aktivitetskrav: 'https://www.nav.no/no/Person/Familie/Venter+du+barn/spesielt-om-foreldrepenger-til-far--347656'
 };
