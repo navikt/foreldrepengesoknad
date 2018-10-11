45db2bf450a58087b6daebf10503f43107cafe58
Frode Hansen <frode.hansen@bekk.no>
2018-10-11 10:19:51 +0200

    Set focus on added periode

--- src/app/util/focusUtils.ts
+++ src/app/util/focusUtils.ts
@@ -1,3 +1,5 @@
+import ReactDOM from 'react-dom';
+
 const elementSelector = [
     'a[href]',
     'select:not([disabled])',
@@ -17,6 +19,23 @@ function nodelistToArray(nodes: NodeList): HTMLElement[] {
 const tabEnabledSelectors = ["[tabIndex='0']"].concat(elementSelector);
 
 const allTabIndexEnabledSelectors = ['[tabIndex]'].concat(elementSelector);
+
+export function focusElement(el: any) {
+    if (!el) {
+        return;
+    }
+    if (typeof el === 'object') {
+        const node = ReactDOM.findDOMNode(el);
+        if (node) {
+            (node as HTMLElement).focus();
+        }
+    } else {
+        const domElement = document.getElementById(el);
+        if (domElement) {
+            domElement.focus();
+        }
+    }
+}
 
 export function getFocusableElements(el: Element, onlyTabReachable: boolean = true): HTMLElement[] | undefined {
     if (!el) {
