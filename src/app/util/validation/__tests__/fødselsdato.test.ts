e7de51f4dd18888c9cc60cae2f9127254c4e91b2
Frode Hansen <frode.hansen@bekk.no>
2018-10-11 10:41:28 +0200

    Update fødselsdato.test

--- src/app/util/validation/__tests__/fødselsdato.test.ts
+++ src/app/util/validation/__tests__/fødselsdato.test.ts
@@ -14,6 +14,7 @@ describe('Fødselsdato validation', () => {
         (commonRules as any).hasValueRule = jest.fn();
         (commonRules as any).dateIsNotInFutureRule = jest.fn();
         (commonRules as any).dateIs3YearsAgoOrLaterRule = jest.fn();
+        (commonRules as any).dateIs15YearsAnd3MonthsAgoOrLaterRule = jest.fn();
     });
 
     it('should call correct validators with given date and string', () => {
@@ -21,5 +22,12 @@ describe('Fødselsdato validation', () => {
         expect(commonRules.hasValueRule).toHaveBeenCalledWith(todaysDate, expect.any(String));
         expect(commonRules.dateIsNotInFutureRule).toHaveBeenCalledWith(todaysDate, expect.any(String));
         expect(commonRules.dateIs3YearsAgoOrLaterRule).toHaveBeenCalledWith(todaysDate, expect.any(String));
+    });
+
+    it('should call correct validators with given date, gjelderAdopsjon and string', () => {
+        getFødselsdatoRegler(todaysDate, true, intl);
+        expect(commonRules.hasValueRule).toHaveBeenCalledWith(todaysDate, expect.any(String));
+        expect(commonRules.dateIsNotInFutureRule).toHaveBeenCalledWith(todaysDate, expect.any(String));
+        expect(commonRules.dateIs15YearsAnd3MonthsAgoOrLaterRule).toHaveBeenCalledWith(todaysDate, expect.any(String));
     });
 });
