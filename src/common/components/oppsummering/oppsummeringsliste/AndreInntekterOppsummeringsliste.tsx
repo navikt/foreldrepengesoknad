acb482f099b218792b40ade1f07e3d9f3915fd38
Erlend Vige <erlend.vige@gmail.com>
2018-10-15 12:22:44 +0200

    AndreInntekterOppsummeringsliste
    Implement list-component for summarizing andreInntekterSiste10Mnd and add usage of this
    component in InntektOppsummering.
    
    
    Former-commit-id: 4db9a699faa20db66b77e0726968db12c89f7ea2

---
+++ src/common/components/oppsummering/oppsummeringsliste/AndreInntekterOppsummeringsliste.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
+import getMessage from 'common/util/i18nUtils';
+import { formatDate } from '../../../../app/util/dates/dates';
+import Oppsummeringsliste from 'common/components/oppsummering/oppsummeringsliste/Oppsummeringsliste';
+import { AnnenInntekt } from '../../../../app/types/søknad/AnnenInntekt';
+
+interface AndreInntekterOppsummeringslisteProps {
+    andreInntekter: AnnenInntekt[];
+}
+
+type Props = AndreInntekterOppsummeringslisteProps & InjectedIntlProps;
+
+const AndreInntekterOppsummeringsliste: React.StatelessComponent<Props> = ({ andreInntekter, intl }: Props) => {
+    return (
+        <Oppsummeringsliste
+            data={andreInntekter.map(({ type, tidsperiode, pågående }) => ({
+                venstrestiltTekst: getMessage(intl, `inntektstype.${type.toLowerCase()}`),
+                høyrestiltTekst: getMessage(intl, 'tidsintervall', {
+                    fom: formatDate(tidsperiode.fom),
+                    tom: pågående ? 'pågående' : formatDate(tidsperiode.tom)
+                })
+            }))}
+        />
+    );
+};
+export default injectIntl(AndreInntekterOppsummeringsliste);
