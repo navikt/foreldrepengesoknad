4f0f8bee6490dfbb98f183ae2ff9562d0817022f
Erlend Vige <erlend.vige@gmail.com>
2018-10-15 11:51:34 +0200

    Oppsummeringsliste rewrite
    Split UtenlandsoppholdSummaryList into two seperate components:
    - One component that now uses <List/>, that only takes two texts per element
    and makes sure layout for a list is correct. Component is called Oppsummeringsliste.
    - UtenlandsoppholdOppsummeringsliste, which takes InformasjonOmUtenlandsopphold as props
    and renders Oppsummeringsliste using data from there.
    - Also fixes inconsistent naming across components (summary vs oppsummering)
    - Also adds optional className property to <List/>
    - Also adds several fields to InntektOppsummering frilans-section
    
    
    Former-commit-id: 0867a1e5f758f9401215bbcd98e24dd55440ef71

---
+++ src/common/components/oppsummering/oppsummeringsliste/Oppsummeringsliste.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Element } from 'nav-frontend-typografi';
+import { guid } from 'nav-frontend-js-utils';
+import List from '../../../../app/components/list/List';
+import './oppsummeringsliste.less';
+
+interface OppsummeringslisteelementProps {
+    venstrestiltTekst: string;
+    høyrestiltTekst: string;
+}
+
+interface OppsummeringslisteProps {
+    data: OppsummeringslisteelementProps[];
+}
+
+const Oppsummeringsliste: React.StatelessComponent<OppsummeringslisteProps> = (props: OppsummeringslisteProps) => {
+    const { data } = props;
+    return (
+        <List
+            className="oppsummeringsliste"
+            data={data}
+            renderElement={(elementProps: OppsummeringslisteelementProps) => (
+                <Oppsummeringslisteelement {...elementProps} />
+            )}
+        />
+    );
+};
+
+const Oppsummeringslisteelement: React.StatelessComponent<OppsummeringslisteelementProps> = ({
+    venstrestiltTekst,
+    høyrestiltTekst
+}: OppsummeringslisteelementProps) => (
+    <li className="oppsummeringsliste__element" key={guid()}>
+        <Element>{venstrestiltTekst}</Element>
+        <Element>{høyrestiltTekst}</Element>
+    </li>
+);
+
+export default Oppsummeringsliste;
