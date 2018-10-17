3360b1d55670a7d27ecf72bd8c554cc279721ee4
Erlend Vige <erlend.vige@gmail.com>
2018-10-17 13:11:13 +0200

    OppsummeringAvDokumentasjon
    Implement component for summarizing attachments throughout foreldrepengesøknad.
    Remove oppsummeringUtils.
    
    
    Former-commit-id: 0af71d08191a545f65cc7ccdadf3d4f5f149ac41

--- src/common/components/kompleks-feltoppsummering/KompleksFeltoppsummering.tsx
+++ src/common/components/kompleks-feltoppsummering/KompleksFeltoppsummering.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import InnholdMedLedetekst from 'common/components/innhold-med-ledetekst/InnholdMedLedetekst';
 
 interface Props {
+    className?: string;
     ledetekst: string;
     children: JSX.Element | JSX.Element[];
 }
 
-const KompleksFeltoppsummering: React.StatelessComponent<Props> = ({ ledetekst, children }) => (
-    <InnholdMedLedetekst className="feltoppsummering" ledetekst={ledetekst} children={children} />
+const KompleksFeltoppsummering: React.StatelessComponent<Props> = ({ ledetekst, children, className }) => (
+    <InnholdMedLedetekst className={`feltoppsummering ${className}`} ledetekst={ledetekst} children={children} />
 );
 
 export default KompleksFeltoppsummering;
