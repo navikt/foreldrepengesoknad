ff0265476ae2ac5cf91241da22fa991b280e8e4d
Frode Hansen <frode.hansen@bekk.no>
2018-10-17 13:25:09 +0200

    Update

--- src/common/components/infoboks/Infoboks.tsx
+++ src/common/components/infoboks/Infoboks.tsx
@@ -5,6 +5,8 @@ import InfoIkon from 'common/components/ikoner/InfoIkon';
 import { Collapse } from 'react-collapse';
 const classNames = require('classnames');
 import './infoboks.less';
+import getMessage from 'common/util/i18nUtils';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
 
 interface InfoboksProps {
     tekst: string | React.ReactNode;
@@ -15,8 +17,10 @@ interface InfoboksState {
     isExpanded: boolean;
 }
 
-class Infoboks extends React.Component<InfoboksProps, InfoboksState> {
-    constructor(props: InfoboksProps) {
+type Props = InfoboksProps & InjectedIntlProps;
+
+class Infoboks extends React.Component<Props, InfoboksState> {
+    constructor(props: Props) {
         super(props);
 
         this.state = {
@@ -33,16 +37,15 @@ class Infoboks extends React.Component<InfoboksProps, InfoboksState> {
     }
 
     render() {
-        const { tekst, stil = 'info' } = this.props;
+        const { tekst, stil = 'info', intl } = this.props;
         const { isExpanded } = this.state;
 
         const ikon = isExpanded ? <LukkInfoIkon /> : <InfoIkon />;
-
         return (
             <React.Fragment>
                 <Sirkelknapp
                     stil={stil}
-                    ariaLabel={tekst}
+                    ariaLabel={typeof tekst === 'string' ? tekst : getMessage(intl, 'infoboks.sirkeltekst')}
                     onClick={this.toggleIsExpanded}
                     ikon={ikon}
                     toggle={{ pressed: isExpanded }}
@@ -54,11 +57,11 @@ class Infoboks extends React.Component<InfoboksProps, InfoboksState> {
                     })}
                     isOpened={isExpanded}
                     springConfig={{ stiffness: 250, damping: 30 }}>
-                    <div className="infoboks__wrapper">{tekst}</div>
+                    {isExpanded ? <div className="infoboks__wrapper">{tekst}</div> : <span />}
                 </Collapse>
             </React.Fragment>
         );
     }
 }
 
-export default Infoboks;
+export default injectIntl(Infoboks);
