7093a20f4cef1f82acbd825c37a1de90d8dd30b1
Erlend Vige <erlend.vige@gmail.com>
2018-10-18 14:35:04 +0200

    Margin-fix oppsmmeringsliste
    Add kompakt-property to Oppsummeringsliste+Oppsummeringslisteelement which allows
    for smaller/bigger margins for different usages.
    
    
    Former-commit-id: b0d0e87564faed18c4bfd4db9ddd2468dd4ca6f5

--- src/common/components/oppsummering/oppsummeringer/detaljer/Overføringsperiodedetaljer.tsx
+++ src/common/components/oppsummering/oppsummeringer/detaljer/Overføringsperiodedetaljer.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
-import { Periode } from '../../../../../app/types/uttaksplan/periodetyper';
+import { Overføringsperiode } from '../../../../../app/types/uttaksplan/periodetyper';
+import Feltoppsummering from 'common/components/feltoppsummering/Feltoppsummering';
+import OppsummeringAvDokumentasjon from 'common/components/oppsummering-av-dokumentasjon/OppsummeringAvDokumentasjon';
 
 interface OverføringsperiodedetaljerProps {
-    periode: Periode;
+    periode: Overføringsperiode;
 }
 
 type Props = OverføringsperiodedetaljerProps & InjectedIntlProps;
 
 const Overføringsperiodedetaljer: React.StatelessComponent<Props> = ({ periode, intl }) => {
-    return null;
+    const { årsak, vedlegg } = periode;
+    return (
+        <>
+            <Feltoppsummering feltnavn="Årsak" verdi={årsak} />
+            <OppsummeringAvDokumentasjon vedlegg={vedlegg || []} />
+        </>
+    );
 };
 
 export default injectIntl(Overføringsperiodedetaljer);
