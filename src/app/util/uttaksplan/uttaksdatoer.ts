b1cb563578d96aa5a60a5f3f78c7366ea6d23654
Frode Hansen <frode.hansen@bekk.no>
2018-10-22 10:45:28 +0200

    Own file for uttaksdatoer

---
+++ src/app/util/uttaksplan/uttaksdatoer.ts
@@ -0,0 +1,32 @@
+import moment from 'moment';
+import { Uttaksdagen } from './Uttaksdagen';
+import { getPermisjonsregler } from './permisjonsregler';
+
+const permisjonsregler = getPermisjonsregler();
+
+export const uttaksdatoer = (familiehendelsesdato: Date) => ({
+    førsteUttaksdagForeldrepengerFørFødsel: getFørsteUttaksdagForeldrepengerFørFødsel(familiehendelsesdato),
+    førsteUttaksdagPåEllerEtterFødsel: Uttaksdagen(familiehendelsesdato).denneEllerNeste(),
+    førsteMuligeUttaksdag: getFørsteMuligeUttaksdag(familiehendelsesdato),
+    sisteMuligeUttaksdag: getSisteMuligeUttaksdag(familiehendelsesdato)
+});
+
+export function getFørsteUttaksdagPåEllerEtterFødsel(familiehendelsesdato: Date) {
+    return Uttaksdagen(familiehendelsesdato).denneEllerNeste();
+}
+
+export function getFørsteUttaksdagForeldrepengerFørFødsel(familiehendelsesdato: Date): Date {
+    return Uttaksdagen(familiehendelsesdato).trekkFra(permisjonsregler.antallUkerForeldrepengerFørFødsel * 5);
+}
+
+export function getFørsteMuligeUttaksdag(familiehendelsesdato: Date): Date {
+    return Uttaksdagen(familiehendelsesdato).trekkFra(permisjonsregler.maksAntallUkerForeldrepengerFørFødsel * 5);
+}
+
+export function getSisteMuligeUttaksdag(familiehendelsesdato: Date): Date {
+    return Uttaksdagen(
+        moment(familiehendelsesdato)
+            .add(permisjonsregler.maksPermisjonslengdeIÅr, 'year')
+            .toDate()
+    ).denneEllerNeste();
+}
