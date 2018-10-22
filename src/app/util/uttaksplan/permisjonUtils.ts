cef9c1336699223b1919d38af8f8076e3159ed39
Frode Hansen <frode.hansen@bekk.no>
2018-10-22 08:05:28 +0200

    Delete unused code
    Former-commit-id: 3574d83e9dab0d4ad3ff85d595f960623586d087

--- src/app/util/uttaksplan/permisjonUtils.ts
+++ src/app/util/uttaksplan/permisjonUtils.ts
@@ -1,15 +1,6 @@
 import { Permisjonsregler } from '../../types/uttaksplan/permisjonsregler';
-import { Tidsperiode, Dekningsgrad, Forelder } from 'common/types';
-import { Utsettelsesperiode, UtsettelseÅrsakType } from '../../types/uttaksplan/periodetyper';
-import { getPakrevdMødrekvoteEtterTermin } from './forslag/util';
-import { Tidsperioden } from './Tidsperioden';
+import { Dekningsgrad } from 'common/types';
 import { Uttaksdagen } from './Uttaksdagen';
-
-/**
- *
- * Denne må nok modifiseres noe når de ulike søkertypene/rollene tas i bruk
- *
- */
 
 /**
  * Finner default startdato før termin (antallUkerForeldrepengerFørFødsel)
@@ -20,25 +11,6 @@ export function getDefaultPermisjonStartdato(familiehendelsedato: Date, permisjo
     return Uttaksdagen(
         familiehendelsedato // Siste uttaksdag i denne perioden er dagen før termin
     ).trekkFra(permisjonsregler.antallUkerForeldrepengerFørFødsel * 5);
-}
-
-/**
- * Henter ut gyldig tidsrom å legge inn en utsettelse
- * @param familiehendelsedato
- * @param dekningsgrad
- * @param permisjonsregler
- */
-export function getGyldigTidsromForUtsettelse(
-    familiehendelsedato: Date,
-    permisjonsregler: Permisjonsregler,
-    sisteRegistrertePermisjonsdag: Date
-): Tidsperiode {
-    const mødrekvoteEtterTermin = getPakrevdMødrekvoteEtterTermin(familiehendelsedato, permisjonsregler);
-    const fom = Uttaksdagen(mødrekvoteEtterTermin.tom).neste();
-    return {
-        fom,
-        tom: sisteRegistrertePermisjonsdag
-    };
 }
 
 /**
@@ -55,31 +27,4 @@ export function getAntallUkerFellesperiode(permisjonsregler: Permisjonsregler, d
         permisjonsregler.antallUkerFedrekvote -
         permisjonsregler.antallUkerForeldrepengerFørFødsel
     );
-}
-
-/**
- * Summerer antall uttaksdager som er registrert som ferie for en forelder
- * @param utsettelser
- * @param forelder
- */
-export const getAntallFeriedagerForForelder = (utsettelser: Utsettelsesperiode[], forelder: Forelder): number => {
-    const ferier = utsettelser.filter(
-        (utsettelse) => utsettelse.årsak === UtsettelseÅrsakType.Ferie && utsettelse.forelder === forelder
-    );
-    return ferier.length === 0
-        ? 0
-        : ferier.reduce(
-              (dager: number, periode: Utsettelsesperiode) =>
-                  dager + Tidsperioden(periode.tidsperiode).getAntallUttaksdager(),
-              0
-          );
-};
-
-/**
- * Finner totalt antall uker tilgjengelig
- * @param permisjonsregler
- * @param dekningsgrad
- */
-export function getAntallUkerTotalt(permisjonsregler: Permisjonsregler, dekningsgrad: Dekningsgrad) {
-    return dekningsgrad === '80' ? permisjonsregler.antallUkerTotalt80 : permisjonsregler.antallUkerTotalt100;
 }
