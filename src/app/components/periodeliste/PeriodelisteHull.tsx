f794abcb7d438f631db0ce66286493daa66966fb
Frode Hansen <frode.hansen@bekk.no>
2018-10-23 13:03:28 +0200

    Dom structure and aria-labels in uttaksplan

--- src/app/components/periodeliste/PeriodelisteHull.tsx
+++ src/app/components/periodeliste/PeriodelisteHull.tsx
@@ -3,21 +3,27 @@ import { periodelisteBem } from './Periodeliste';
 import { FormattedMessage, injectIntl, InjectedIntlProps } from 'react-intl';
 import { getVarighetString } from 'common/util/intlUtils';
 import AdvarselIkonÅpen from '../uttaksplan-ikon/ikoner/AdvarselIkon\u00C5pen';
+import { Normaltekst } from 'nav-frontend-typografi';
 
 export interface Props {
     antallDager: number;
 }
 
 const PeriodelisteHull: React.StatelessComponent<Props & InjectedIntlProps> = ({ antallDager, intl }) => (
-    <div className={periodelisteBem.element('hull')}>
-        <span className={periodelisteBem.element('hull__icon')}>
+    <article className={periodelisteBem.element('hull')}>
+        <h1 className="sr-only">
+            <FormattedMessage id="periodeliste.hullMellomPerioder.aria-label" />
+        </h1>
+        <span className={periodelisteBem.element('hull__icon')} role="presentation">
             <AdvarselIkonÅpen />
         </span>
-        <FormattedMessage
-            id="periodeliste.hullMellomPerioder"
-            values={{ dager: getVarighetString(antallDager, intl) }}
-        />
-    </div>
+        <Normaltekst tag="span">
+            <FormattedMessage
+                id="periodeliste.hullMellomPerioder"
+                values={{ dager: getVarighetString(antallDager, intl) }}
+            />
+        </Normaltekst>
+    </article>
 );
 
 export default injectIntl(PeriodelisteHull);
