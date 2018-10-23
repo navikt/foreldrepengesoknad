3c799d294b9825cc7e4d843348086c689d86211c
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-10-23 18:37:13 +0200

    Add document title for pages that is not a steg

--- src/app/connected-components/sider/feilsider/GenerellFeil.tsx
+++ src/app/connected-components/sider/feilsider/GenerellFeil.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
 import Applikasjonsside from '../Applikasjonsside';
 import DocumentTitle from 'react-document-title';
 import Feilsidemelding from 'common/components/feilsidemelding/Feilsidemelding';
+import getMessage from 'common/util/i18nUtils';
 
-class GenerellFeil extends React.Component {
-    render() {
-        return (
-            <Applikasjonsside visSpråkvelger={false}>
-                <DocumentTitle title="Søknad om foreldrepenger" />
-                <Feilsidemelding tittel="Det oppstod en feil" ingress="Litt mer informasjon om hva som gikk feil" />
-            </Applikasjonsside>
-        );
-    }
-}
-
-export default GenerellFeil;
+const GenerellFeil: React.StatelessComponent<InjectedIntlProps> = (props: InjectedIntlProps) => {
+    return (
+        <Applikasjonsside visSpråkvelger={false}>
+            <DocumentTitle title={getMessage(props.intl, 'feilside.dokument.tittel')} />
+            <Feilsidemelding tittel="Det oppstod en feil" ingress="Litt mer informasjon om hva som gikk feil" />
+        </Applikasjonsside>
+    );
+};
+export default injectIntl(GenerellFeil);
