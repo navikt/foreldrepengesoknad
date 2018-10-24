4eb0af9e561ec807ec9dbfb897a9f3575a40be56
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-10-24 10:51:48 +0200

    Add document title and text keys for all routes
    Former-commit-id: 7d65bab479202d30c963f21e05737d92f3fee3a0

--- src/app/components/loading-screen/LoadingScreen.tsx
+++ src/app/components/loading-screen/LoadingScreen.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
 import ApplicationSpinner from 'common/components/application-spinner/ApplicationSpinner';
 import Applikasjonsside from '../../connected-components/sider/Applikasjonsside';
+import DocumentTitle from 'react-document-title';
+import getMessage from 'common/util/i18nUtils';
 
-const LoadingScreen = () => (
-    <Applikasjonsside>
-        <ApplicationSpinner />
-    </Applikasjonsside>
-);
-
-export default LoadingScreen;
+type Props = InjectedIntlProps;
+const LoadingScreen: React.StatelessComponent<Props> = (props: Props) => {
+    return (
+        <Applikasjonsside>
+            <DocumentTitle title={getMessage(props.intl, 'dokument.tittel.loadingScreen')} />
+            <ApplicationSpinner />
+        </Applikasjonsside>
+    );
+};
+export default injectIntl(LoadingScreen);
