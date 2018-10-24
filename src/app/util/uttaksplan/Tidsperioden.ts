7a221c04c0f24940b481eef8b911e7e8cba59923
Frode Hansen <frode.hansen@bekk.no>
2018-10-24 13:22:56 +0200

    Tidsperiodene object

--- src/app/util/uttaksplan/Tidsperioden.ts
+++ src/app/util/uttaksplan/Tidsperioden.ts
@@ -6,20 +6,17 @@ import { Uttaksdagen } from './Uttaksdagen';
 import { InjectedIntl } from 'react-intl';
 import { formaterDatoUtenDag } from 'common/util/datoUtils';
 import getMessage from 'common/util/i18nUtils';
+import { dateIsSameOrBefore, dateIsSameOrAfter } from '../dates/dates';
 
-/**
- * Wrapper en Tidsperiode med uttaksdager-funksjonalitet
- * @param dato
- */
-
-export const Tidsperioden = (tidsperiode: Tidsperiode) => ({
+export const Tidsperioden = (tidsperiode: Partial<Tidsperiode>) => ({
     erLik: (tidsperiode2: Tidsperiode) => erTidsperioderLike(tidsperiode, tidsperiode2),
     erOmsluttetAv: (tidsperiode2: Tidsperiode) => erTidsperiodeOmsluttetAvTidsperiode(tidsperiode, tidsperiode2),
     erUtenfor: (tidsperiode2: Tidsperiode) => erTidsperiodeUtenforTidsperiode(tidsperiode, tidsperiode2),
     getAntallUttaksdager: (taBortFridager?: boolean) => getAntallUttaksdagerITidsperiode(tidsperiode, taBortFridager),
     getAntallFridager: () => getUttaksdagerSomErFridager(tidsperiode).length,
-    setStartdato: (fom: Date) => flyttTidsperiode(tidsperiode, fom),
-    setUttaksdager: (uttaksdager: number) => getTidsperiode(tidsperiode.fom, uttaksdager),
+    setStartdato: (fom: Date) => (isValidTidsperiode(tidsperiode) ? flyttTidsperiode(tidsperiode, fom) : tidsperiode),
+    setUttaksdager: (uttaksdager: number) =>
+        tidsperiode.fom ? getTidsperiode(tidsperiode.fom, uttaksdager) : tidsperiode,
     formaterString: (intl: InjectedIntl) => tidsperiodeToString(tidsperiode, intl),
     erFomEllerEtterDato: (dato: Date) => erTidsperiodeFomEllerEtterDato(tidsperiode, dato),
     erFørDato: (dato: Date) => erTidsperiodeFomEllerEtterDato(tidsperiode, dato) === false
@@ -53,11 +50,6 @@ export function getValidTidsperiode(tidsperiode: Partial<Tidsperiode> | undefine
     return undefined;
 }
 
-/**
- * Returnerer ny Tidsperiode gitt gyldig uttaksdag-fom og antall uttaksdager
- * @param fom
- * @param uttaksdager
- */
 export function getTidsperiode(fom: Date, uttaksdager: number): Tidsperiode {
     if (!Uttaksdagen(fom).erUttaksdag()) {
         throw new Error('FOM er ikke en uttaksdag');
@@ -68,37 +60,14 @@ export function getTidsperiode(fom: Date, uttaksdager: number): Tidsperiode {
     };
 }
 
-/**
- * Summerer antall uttaksdager som er i en eller flere perioder
- * @param tidsperioder
- */
-export function getAntallUttaksdagerITidsperioder(tidsperioder: Tidsperiode[], taBortFridager?: boolean): number {
-    return tidsperioder.reduce(
-        (dager: number, tidsperiode: Tidsperiode) =>
-            dager + getAntallUttaksdagerITidsperiode(tidsperiode, taBortFridager),
-        0
-    );
-}
-
-export function erSammeEllerTidligereDato(d1: Date, d2: Date) {
-    return moment(d1).isSameOrBefore(d2, 'day');
-}
-
-export function erSammeEllerSenereDato(d1: Date, d2: Date) {
-    return moment(d1).isSameOrAfter(d2, 'day');
-}
-
-/**
- * Summerer antall uttaksdager i angitt tidsperiode
- */
-function getAntallUttaksdagerITidsperiode(tidsperiode: Tidsperiode, taBortFridager?: boolean): number {
-    if (!tidsperiode.fom || !tidsperiode.tom) {
+function getAntallUttaksdagerITidsperiode(tidsperiode: Partial<Tidsperiode>, taBortFridager?: boolean): number {
+    if (isValidTidsperiode(tidsperiode) === false) {
         return 0;
     }
     const fom = moment(tidsperiode.fom);
     const tom = moment(tidsperiode.tom);
     if (fom.isAfter(tom, 'day')) {
-        return -1;
+        return 0;
     }
     let antall = 0;
     let fridager = 0;
@@ -114,61 +83,52 @@ function getAntallUttaksdagerITidsperiode(tidsperiode: Tidsperiode, taBortFridag
     return antall - fridager;
 }
 
-/**
- * Finner uttaksdager som er offentlig fridag
- */
-function getUttaksdagerSomErFridager(tidsperiode: Tidsperiode): Holiday[] {
-    return getOffentligeFridager(tidsperiode).filter((dag) => Uttaksdagen(dag.date).erUttaksdag());
+function getUttaksdagerSomErFridager(tidsperiode: Partial<Tidsperiode>): Holiday[] {
+    if (isValidTidsperiode(tidsperiode) === false) {
+        return [];
+    }
+    return getOffentligeFridager(tidsperiode as Tidsperiode).filter((dag) => Uttaksdagen(dag.date).erUttaksdag());
 }
 
-/**
- * Setter ny startdato og flytter sluttdato slik at antall
- * uttaksdager blir det samme
- * @param tidsperiode
- * @param fom
- */
 function flyttTidsperiode(tidsperiode: Tidsperiode, fom: Date): Tidsperiode {
     const uttaksdager = getAntallUttaksdagerITidsperiode(tidsperiode);
     return getTidsperiode(fom, uttaksdager);
 }
 
-/**
- * Sjekker om to tidsperioder har samme start- og sluttdato
- * @param t1 Sjekker
- * @param t2
- */
-function erTidsperioderLike(t1: Tidsperiode, t2: Tidsperiode) {
+function erTidsperioderLike(t1: Partial<Tidsperiode>, t2: Partial<Tidsperiode>) {
     return JSON.stringify(t1) === JSON.stringify(t2);
 }
 
-/**
- * Sjekker om tidsperiode1 er inne i tidsperiode2
- * @param tidsperiode1
- * @param tidsperiode2
- */
-function erTidsperiodeOmsluttetAvTidsperiode(tidsperiode1: Tidsperiode, tidsperiode2: Tidsperiode): boolean {
-    return (
-        erSammeEllerSenereDato(tidsperiode1.fom, tidsperiode2.fom) &&
-        erSammeEllerTidligereDato(tidsperiode1.tom, tidsperiode2.tom)
-    );
+function erTidsperiodeOmsluttetAvTidsperiode(
+    tidsperiode1: Partial<Tidsperiode>,
+    tidsperiode2: Partial<Tidsperiode>
+): boolean {
+    if (isValidTidsperiode(tidsperiode1) && isValidTidsperiode(tidsperiode2)) {
+        return (
+            dateIsSameOrAfter(tidsperiode1.fom, tidsperiode2.fom) &&
+            dateIsSameOrBefore(tidsperiode1.tom, tidsperiode2.tom)
+        );
+    }
+    return false;
 }
 
-/**
- * Sjekker om tidsperiode1 er helt utenfor tidsperiode2
- * @param tidsperiode1
- * @param tidsperiode2
- */
-function erTidsperiodeUtenforTidsperiode(tidsperiode1: Tidsperiode, tidsperiode2: Tidsperiode): boolean {
-    return (
-        moment(tidsperiode1.fom).isAfter(tidsperiode2.tom, 'day') ||
-        moment(tidsperiode1.tom).isBefore(tidsperiode2.fom, 'day')
-    );
+function erTidsperiodeUtenforTidsperiode(
+    tidsperiode1: Partial<Tidsperiode>,
+    tidsperiode2: Partial<Tidsperiode>
+): boolean {
+    if (isValidTidsperiode(tidsperiode1) && isValidTidsperiode(tidsperiode2)) {
+        return (
+            moment(tidsperiode1.fom).isAfter(tidsperiode2.tom, 'day') ||
+            moment(tidsperiode1.tom).isBefore(tidsperiode2.fom, 'day')
+        );
+    }
+    return false;
 }
 
-function tidsperiodeToString(tidsperiode: Tidsperiode, intl: InjectedIntl) {
+function tidsperiodeToString(tidsperiode: Partial<Tidsperiode>, intl: InjectedIntl) {
     return getMessage(intl, 'tidsperiode', {
-        fom: formaterDatoUtenDag(tidsperiode.fom),
-        tom: formaterDatoUtenDag(tidsperiode.tom)
+        fom: tidsperiode.fom ? formaterDatoUtenDag(tidsperiode.fom) : '',
+        tom: tidsperiode.tom ? formaterDatoUtenDag(tidsperiode.tom) : ''
     });
 }
 
