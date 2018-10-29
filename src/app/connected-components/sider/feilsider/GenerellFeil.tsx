3d4ee701f7a2e79aac9f72e6a454a4f70eb9875b
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-10-29 10:33:32 +0100

    Add better error message

--- src/app/connected-components/sider/feilsider/GenerellFeil.tsx
+++ src/app/connected-components/sider/feilsider/GenerellFeil.tsx
@@ -1,15 +1,31 @@
 import * as React from 'react';
-import { InjectedIntlProps, injectIntl } from 'react-intl';
+import { InjectedIntlProps, injectIntl, FormattedMessage } from 'react-intl';
 import Applikasjonsside from '../Applikasjonsside';
 import DocumentTitle from 'react-document-title';
 import Feilsidemelding from 'common/components/feilsidemelding/Feilsidemelding';
 import getMessage from 'common/util/i18nUtils';
+import Lenke from 'nav-frontend-lenker';
+import lenker from 'app/util/routing/lenker';
 
 const GenerellFeil: React.StatelessComponent<InjectedIntlProps> = (props: InjectedIntlProps) => {
     return (
         <Applikasjonsside visSpråkvelger={false}>
             <DocumentTitle title={getMessage(props.intl, 'dokument.tittel.feilside.generell')} />
-            <Feilsidemelding tittel="Det oppstod en feil" ingress="Litt mer informasjon om hva som gikk feil" />
+            <Feilsidemelding
+                tittel={getMessage(props.intl, 'feilside.tittel')}
+                ingress={
+                    <FormattedMessage
+                        id={'feilside.ingress'}
+                        values={{
+                            lenke: (
+                                <Lenke href={lenker.brukerstøtte}>
+                                    {getMessage(props.intl, 'feilside.ingress.lenke')}
+                                </Lenke>
+                            )
+                        }}
+                    />
+                }
+            />
         </Applikasjonsside>
     );
 };
