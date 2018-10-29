e0bd02f18c87073ae476dbeebd5368cd82ca484b
Frode Hansen <frode.hansen@bekk.no>
2018-10-29 11:42:26 +0100

    Basic lagForslag test
    Former-commit-id: 2b44d91e09aee68f639ffff8bdc175f61c8da41e

--- src/app/util/uttaksplan/forslag/lagUttaksplan.ts
+++ src/app/util/uttaksplan/forslag/lagUttaksplan.ts
@@ -1,29 +1,17 @@
-import { Barn, FødtBarn, UfødtBarn, Adopsjonsbarn, ForeldreansvarBarn } from '../../../types/søknad/Barn';
-import Søknad, { Søkersituasjon } from '../../../types/søknad/Søknad';
+import Søknad from '../../../types/søknad/Søknad';
 import { Periode, TilgjengeligStønadskonto } from '../../../types/uttaksplan/periodetyper';
 import { ikkeDeltUttak } from './ikkeDeltUttak';
 import { deltUttak } from './deltUttak';
 import { erFarEllerMedmor } from '../../domain/personUtil';
-import { DateValue } from '../../../types/common';
 import { getErDeltUttak } from './util';
-
-const getFamiliehendelsesdato = (barn: Barn, situasjon: Søkersituasjon): DateValue => {
-    if (situasjon === Søkersituasjon.FØDSEL) {
-        return barn.erBarnetFødt ? (barn as FødtBarn).fødselsdatoer[0] : (barn as UfødtBarn).termindato;
-    } else if (situasjon === Søkersituasjon.ADOPSJON) {
-        return (barn as Adopsjonsbarn).adopsjonsdato;
-    } else if (situasjon === Søkersituasjon.FORELDREANSVAR) {
-        return (barn as ForeldreansvarBarn).foreldreansvarsdato;
-    }
-    return undefined;
-};
+import { getFamiliehendelsedato } from '..';
 
 export const lagUttaksplan = (søknad: Søknad, tilgjengeligeStønadskontoer: TilgjengeligStønadskonto[]): Periode[] => {
     const { barn, situasjon, ekstrainfo, annenForelder } = søknad;
     const { erUfør } = annenForelder;
     const { uttaksplanSkjema } = ekstrainfo;
     const { startdatoPermisjon, fellesperiodeukerMor } = uttaksplanSkjema;
-    const famDato = getFamiliehendelsesdato(barn, situasjon);
+    const famDato = getFamiliehendelsedato(barn, situasjon);
     const erDeltUttak: boolean = getErDeltUttak(tilgjengeligeStønadskontoer);
 
     if (famDato) {
