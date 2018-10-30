e8be803850ef16dbc835e56cb2d6ffc2871ac6a5
Frode Hansen <frode.hansen@bekk.no>
2018-10-30 11:57:53 +0100

    Stripes in uttaksplan

--- src/app/components/periodeliste/Periodeliste.tsx
+++ src/app/components/periodeliste/Periodeliste.tsx
@@ -14,6 +14,7 @@ import PeriodelisteHull from './PeriodelisteHull';
 import { focusElement } from '../../util/focusUtils';
 import { Tidsperiode } from 'nav-datovelger/src/datovelger/types';
 import { Periodene } from '../../util/uttaksplan/Periodene';
+import PeriodelisteItemMarkør from './PeriodelisteItemMark\u00F8r';
 
 export interface OwnProps {
     perioder: Periode[];
@@ -100,6 +101,8 @@ class Periodeliste extends React.Component<Props> {
                                         ),
                                         periodelisteBem.element('item', `type-${periode.type}`)
                                     )}>
+                                    <PeriodelisteItemMarkør periode={periode} />
+                                    <div className={periodelisteBem.element('item__strek')} />
                                     {periode.type === Periodetype.Hull ? (
                                         <PeriodelisteHull
                                             key={periode.id}
