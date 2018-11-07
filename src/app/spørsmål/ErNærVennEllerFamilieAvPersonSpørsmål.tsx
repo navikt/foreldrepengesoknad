f3bbc4d6bd74c1a8480f20ea00a834c627563f54
Frode Hansen <frode.hansen@bekk.no>
2018-11-07 15:23:13 +0100

    Use JaNeiSpørsmål in questions

--- src/app/spørsmål/ErNærVennEllerFamilieAvPersonSpørsmål.tsx
+++ src/app/spørsmål/ErNærVennEllerFamilieAvPersonSpørsmål.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { injectIntl, InjectedIntlProps } from 'react-intl';
 import getMessage from 'common/util/i18nUtils';
-import RadioPanelGruppeResponsive from 'common/components/skjema/elements/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive';
-import { InputChangeEvent } from '../types/dom/Events';
+import JaNeiSpørsmål from '../components/ja-nei-sp\u00F8rsm\u00E5l/JaNeiSp\u00F8rsm\u00E5l';
 
 interface ErNærVennEllerFamilieAvPersonSpørsmålProps {
     erNærVennEllerFamilieAvPerson?: boolean;
@@ -11,39 +10,15 @@ interface ErNærVennEllerFamilieAvPersonSpørsmålProps {
 
 type Props = ErNærVennEllerFamilieAvPersonSpørsmålProps & InjectedIntlProps;
 
-enum ErNærVennEllerFamilieAvPerson {
-    'ER_NÆR_VENN_ELLER_FAMILIE_AV_PERSON' = 'erNærVennEllerFamilieAvPerson',
-    'ER_IKKE_NÆR_VENN_ELLER_FAMILIE_AV_PERSON' = 'erIkkeNærVennEllerFamilieAvPerson'
-}
-
 const ErNærVennEllerFamilieAvPersonSpørsmål = (props: Props) => {
     const { onChange, erNærVennEllerFamilieAvPerson, intl } = props;
 
-    let checked;
-    if (erNærVennEllerFamilieAvPerson === true) {
-        checked = ErNærVennEllerFamilieAvPerson.ER_NÆR_VENN_ELLER_FAMILIE_AV_PERSON;
-    } else if (erNærVennEllerFamilieAvPerson === false) {
-        checked = ErNærVennEllerFamilieAvPerson.ER_IKKE_NÆR_VENN_ELLER_FAMILIE_AV_PERSON;
-    }
-
     return (
-        <RadioPanelGruppeResponsive
-            checked={checked}
-            legend={getMessage(intl, 'næringsrelasjon.nærVennEllerFamilie')}
-            radios={[
-                {
-                    label: getMessage(intl, 'ja'),
-                    value: ErNærVennEllerFamilieAvPerson.ER_NÆR_VENN_ELLER_FAMILIE_AV_PERSON
-                },
-                {
-                    label: getMessage(intl, 'nei'),
-                    value: ErNærVennEllerFamilieAvPerson.ER_IKKE_NÆR_VENN_ELLER_FAMILIE_AV_PERSON
-                }
-            ]}
-            name="erNærVennEllerFamilieAvPerson"
-            onChange={(e: InputChangeEvent, v: ErNærVennEllerFamilieAvPerson) =>
-                onChange(v === ErNærVennEllerFamilieAvPerson.ER_NÆR_VENN_ELLER_FAMILIE_AV_PERSON)
-            }
+        <JaNeiSpørsmål
+            spørsmål={getMessage(intl, 'næringsrelasjon.nærVennEllerFamilie')}
+            navn="erNærVennEllerFamilieAvPerson"
+            valgtVerdi={erNærVennEllerFamilieAvPerson}
+            onChange={(verdi) => onChange(verdi)}
         />
     );
 };
--- src/app/spørsmål/HarDuJobbetSomFrilansSiste10MndSpørsmål.tsx
+++ src/app/spørsmål/HarDuJobbetSomFrilansSiste10MndSpørsmål.tsx
@@ -1,49 +1,24 @@
 import * as React from 'react';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
-import RadioPanelGruppeResponsive from 'common/components/skjema/elements/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive';
 import getMessage from 'common/util/i18nUtils';
-
-enum FrilansSiste10Mnd {
-    'HAR_JOBBET_FRILANS_SISTE_10_MND' = 'harJobbetFrilansSiste10Mnd',
-    'IKKE_JOBBET_FRILANS_SISTE_10_MND' = 'harIkkeJobbetFrilansSiste10Mnd'
-}
+import JaNeiSpørsmål from '../components/ja-nei-sp\u00F8rsm\u00E5l/JaNeiSp\u00F8rsm\u00E5l';
 
 interface HarDuJobbetSomFrilansSiste10MndSpørsmålProps {
     harJobbetSomFrilansSiste10Mnd: boolean;
-    onChange: (erFrilanser: boolean, e: React.ChangeEvent<HTMLInputElement>) => void;
+    onChange: (erFrilanser: boolean) => void;
 }
 
 type Props = HarDuJobbetSomFrilansSiste10MndSpørsmålProps & InjectedIntlProps;
 
 const HarDuJobbetSomFrilansSiste10MndSpørsmål = (props: Props) => {
-    const { onChange, harJobbetSomFrilansSiste10Mnd, intl, ...otherProps } = props;
-
-    let checked;
-    if (harJobbetSomFrilansSiste10Mnd === true) {
-        checked = FrilansSiste10Mnd.HAR_JOBBET_FRILANS_SISTE_10_MND;
-    } else if (harJobbetSomFrilansSiste10Mnd === false) {
-        checked = FrilansSiste10Mnd.IKKE_JOBBET_FRILANS_SISTE_10_MND;
-    }
+    const { onChange, harJobbetSomFrilansSiste10Mnd, intl } = props;
 
     return (
-        <RadioPanelGruppeResponsive
-            checked={checked}
-            legend={getMessage(intl, 'harDuJobbetSomFrilansSiste10Mnd.spørsmål')}
-            radios={[
-                {
-                    label: getMessage(intl, 'ja'),
-                    value: FrilansSiste10Mnd.HAR_JOBBET_FRILANS_SISTE_10_MND
-                },
-                {
-                    label: getMessage(intl, 'nei'),
-                    value: FrilansSiste10Mnd.IKKE_JOBBET_FRILANS_SISTE_10_MND
-                }
-            ]}
-            name="harJobbetSomFrilansSiste10Mnd"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>, v: FrilansSiste10Mnd) =>
-                onChange(v === FrilansSiste10Mnd.HAR_JOBBET_FRILANS_SISTE_10_MND, e)
-            }
-            {...otherProps}
+        <JaNeiSpørsmål
+            spørsmål={getMessage(intl, 'harDuJobbetSomFrilansSiste10Mnd.spørsmål')}
+            navn="harJobbetSomFrilansSiste10Mnd"
+            valgtVerdi={harJobbetSomFrilansSiste10Mnd}
+            onChange={(verdi) => onChange(verdi)}
         />
     );
 };
