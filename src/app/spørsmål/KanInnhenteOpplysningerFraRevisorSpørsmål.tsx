c78438ca08efdef4e711e0af862c680eab029e30
Frode Hansen <frode.hansen@bekk.no>
2018-11-07 15:37:55 +0100

    Update questions

--- src/app/spørsmål/KanInnhenteOpplysningerFraRevisorSpørsmål.tsx
+++ src/app/spørsmål/KanInnhenteOpplysningerFraRevisorSpørsmål.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { injectIntl, InjectedIntlProps } from 'react-intl';
 import getMessage from 'common/util/i18nUtils';
-import RadioPanelGruppeResponsive from 'common/components/skjema/elements/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive';
-import { InputChangeEvent } from '../types/dom/Events';
+import JaNeiSpørsmål from '../components/ja-nei-sp\u00F8rsm\u00E5l/JaNeiSp\u00F8rsm\u00E5l';
 
 interface InnhenteOpplsyningerOmRevisorSpørsmålProps {
     kanInnhenteOpplysningerFraRevisor?: boolean;
@@ -11,39 +10,15 @@ interface InnhenteOpplsyningerOmRevisorSpørsmålProps {
 
 type Props = InnhenteOpplsyningerOmRevisorSpørsmålProps & InjectedIntlProps;
 
-enum OpplysningerFraRevior {
-    'KAN_INNHENTE_OPPLYSNINGER_FRA_REVISOR' = 'hentOpplysningerFraRevisor',
-    'KAN_IKKE_INNHENTE_OPPLYSNINGER_FRA_REVISOR' = 'ikkeHentOpplysningerFraRevisor'
-}
-
 const KanInnhenteOpplysningerFraRevisorSpørsmål = (props: Props) => {
     const { onChange, kanInnhenteOpplysningerFraRevisor, intl } = props;
 
-    let checked;
-    if (kanInnhenteOpplysningerFraRevisor === true) {
-        checked = OpplysningerFraRevior.KAN_INNHENTE_OPPLYSNINGER_FRA_REVISOR;
-    } else if (kanInnhenteOpplysningerFraRevisor === false) {
-        checked = OpplysningerFraRevior.KAN_IKKE_INNHENTE_OPPLYSNINGER_FRA_REVISOR;
-    }
-
     return (
-        <RadioPanelGruppeResponsive
-            checked={checked}
-            legend={getMessage(intl, 'kanInnhenteOpplysningerFraRevisor.spørsmål')}
-            radios={[
-                {
-                    label: getMessage(intl, 'ja'),
-                    value: OpplysningerFraRevior.KAN_INNHENTE_OPPLYSNINGER_FRA_REVISOR
-                },
-                {
-                    label: getMessage(intl, 'nei'),
-                    value: OpplysningerFraRevior.KAN_IKKE_INNHENTE_OPPLYSNINGER_FRA_REVISOR
-                }
-            ]}
-            name="hentOpplysningerFraRevisor"
-            onChange={(e: InputChangeEvent, v: OpplysningerFraRevior) =>
-                onChange(v === OpplysningerFraRevior.KAN_INNHENTE_OPPLYSNINGER_FRA_REVISOR)
-            }
+        <JaNeiSpørsmål
+            spørsmål={getMessage(intl, 'kanInnhenteOpplysningerFraRevisor.spørsmål')}
+            navn="hentOpplysningerFraRevisor"
+            valgtVerdi={kanInnhenteOpplysningerFraRevisor}
+            onChange={(verdi) => onChange(verdi)}
         />
     );
 };
