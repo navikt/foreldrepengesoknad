40f01a416a75a54795714b766759efe01a59df87
Frode Hansen <frode.hansen@bekk.no>
2018-11-07 14:48:35 +0100

    Use JaNeiSpørsmål in questions AnnenForelder
    Former-commit-id: b08126934c1bb6ebe35af2fedb2aa40ea783ee64

--- src/app/spørsmål/ErAnnenForelderInformertSpørsmål.tsx
+++ src/app/spørsmål/ErAnnenForelderInformertSpørsmål.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
 import { InjectedIntlProps, injectIntl } from 'react-intl';
-import RadioPanelGruppeResponsive from 'common/components/skjema/elements/radio-panel-gruppe-responsive/RadioPanelGruppeResponsive';
 import getMessage from 'common/util/i18nUtils';
-
-enum AnnenForelder {
-    'ER_INFORMERT' = 'erInformert',
-    'IKKE_INFORMERT' = 'ikkeInformert'
-}
+import JaNeiSpørsmål from '../components/ja-nei-sp\u00F8rsm\u00E5l/JaNeiSp\u00F8rsm\u00E5l';
 
 interface ErAnnenForelderInformertSpørsmålProps {
     navn?: string;
@@ -17,36 +12,16 @@ interface ErAnnenForelderInformertSpørsmålProps {
 type Props = ErAnnenForelderInformertSpørsmålProps & InjectedIntlProps;
 
 const ErAnnenForelderInformertSpørsmål = (props: Props) => {
-    const { onChange, intl, erAnnenForelderInformert, navn, ...otherProps } = props;
-
-    let checked;
-    if (erAnnenForelderInformert === true) {
-        checked = AnnenForelder.ER_INFORMERT;
-    } else if (erAnnenForelderInformert === false) {
-        checked = AnnenForelder.IKKE_INFORMERT;
-    }
+    const { onChange, intl, erAnnenForelderInformert, navn } = props;
 
     return (
-        <RadioPanelGruppeResponsive
-            checked={checked}
-            legend={getMessage(intl, 'erAnnenForelderInformert.spørsmål', {
+        <JaNeiSpørsmål
+            spørsmål={getMessage(intl, 'erAnnenForelderInformert.spørsmål', {
                 navn
             })}
-            radios={[
-                {
-                    label: getMessage(intl, 'ja'),
-                    value: AnnenForelder.ER_INFORMERT
-                },
-                {
-                    label: getMessage(intl, 'nei'),
-                    value: AnnenForelder.IKKE_INFORMERT
-                }
-            ]}
-            name="erAnnenForelderInformert"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>, v: AnnenForelder) =>
-                onChange(v === AnnenForelder.ER_INFORMERT)
-            }
-            {...otherProps}
+            valgtVerdi={erAnnenForelderInformert}
+            navn="erAnnenForelderInformert"
+            onChange={(erInformert) => onChange(erInformert)}
         />
     );
 };
