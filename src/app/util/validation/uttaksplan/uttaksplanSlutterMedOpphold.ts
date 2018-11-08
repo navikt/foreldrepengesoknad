794c3047e288d12bbea08f55c8d8f35ad6115eb0
Andreas Dybdahl <andreas.dyb@gmail.com>
2018-11-08 14:54:20 +0100

    Fix array mutation
    Former-commit-id: 8dfd7ee67d02f91615eeddf65461ee90934f4f38

--- src/app/util/validation/uttaksplan/uttaksplanSlutterMedOpphold.ts
+++ src/app/util/validation/uttaksplan/uttaksplanSlutterMedOpphold.ts
@@ -1,5 +1,10 @@
 import { Periode, Periodetype } from '../../../types/uttaksplan/periodetyper';
 
 export const uttaksplanSlutterMedOpphold = (perioder: Periode[]): boolean => {
-    return perioder.reverse().findIndex((periode) => periode.type === Periodetype.Opphold) === 0;
+    return (
+        perioder
+            .slice()
+            .reverse()
+            .findIndex((periode) => periode.type === Periodetype.Opphold) === 0
+    );
 };
