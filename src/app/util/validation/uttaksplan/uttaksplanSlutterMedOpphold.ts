572679f47dc0d6f74e8135baad94700fe610d046
Andreas Dybdahl <andreas.dyb@gmail.com>
2018-11-08 14:42:00 +0100

    Added tests

--- src/app/util/validation/uttaksplan/uttaksplanSlutterMedOpphold.ts
+++ src/app/util/validation/uttaksplan/uttaksplanSlutterMedOpphold.ts
@@ -1,4 +1,4 @@
-import { Periode, Periodetype } from 'app/types/uttaksplan/periodetyper';
+import { Periode, Periodetype } from '../../../types/uttaksplan/periodetyper';
 
 export const uttaksplanSlutterMedOpphold = (perioder: Periode[]): boolean => {
     return perioder.reverse().findIndex((periode) => periode.type === Periodetype.Opphold) === 0;
--- src/app/util/validation/uttaksplan/uttaksplanStarterMedOpphold.ts
+++ src/app/util/validation/uttaksplan/uttaksplanStarterMedOpphold.ts
@@ -1,4 +1,4 @@
-import { Periode, Periodetype } from 'app/types/uttaksplan/periodetyper';
+import { Periode, Periodetype } from '../../../types/uttaksplan/periodetyper';
 
 export const uttaksplanStarterMedOpphold = (perioder: Periode[]): boolean => {
     return perioder.findIndex((periode) => periode.type === Periodetype.Opphold) === 0;
