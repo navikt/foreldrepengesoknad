e3966cbbff89d8b79081196ad90d37fe645d9f5d
Andreas Dybdahl <andreas.dyb@gmail.com>
2018-11-08 14:42:00 +0100

    Added tests
    Former-commit-id: 0063bd8598d98dad65e0905b393706831a3c7670

--- src/app/util/validation/uttaksplan/uttaksplanErBareOpphold.ts
+++ src/app/util/validation/uttaksplan/uttaksplanErBareOpphold.ts
@@ -1,5 +1,9 @@
-import { Periode, Periodetype } from 'app/types/uttaksplan/periodetyper';
+import { Periode, Periodetype } from '../../../types/uttaksplan/periodetyper';
 
 export const uttaksplanErBareOpphold = (perioder: Periode[]): boolean => {
+    if (perioder.length === 0) {
+        return false;
+    }
+
     return perioder.every((periode) => periode.type === Periodetype.Opphold);
 };
