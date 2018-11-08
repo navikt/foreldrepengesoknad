2f192a15ab8f080ef9927d35f6e9dc8b91c36f20
Frode Hansen <frode.hansen@bekk.no>
2018-11-08 14:31:25 +0100

    Show dekningsgrad on oppsummering
    Former-commit-id: fcc28622de7be3245301bcc0bf1a929a6264055b

--- src/common/components/oppsummering/oppsummeringer/UttaksplanOppsummering.tsx
+++ src/common/components/oppsummering/oppsummeringer/UttaksplanOppsummering.tsx
@@ -2,26 +2,43 @@ import * as React from 'react';
 import { Periode } from '../../../../app/types/uttaksplan/periodetyper';
 import Oppsummeringsseksjon from 'common/components/oppsummeringsseksjon/Oppsummeringsseksjon';
 import UttaksplanOppsummeringsliste from 'common/components/oppsummering/oppsummeringer/lister/UttaksplanOppsummeringsliste';
-import { NavnPåForeldre } from 'common/types';
+import { NavnPåForeldre, Dekningsgrad } from 'common/types';
 import Arbeidsforhold from '../../../../app/types/Arbeidsforhold';
 import { UttaksplanValideringState } from 'app/redux/reducers/uttaksplanValideringReducer';
+import Feltoppsummering from 'common/components/feltoppsummering/Feltoppsummering';
+import { injectIntl, InjectedIntlProps } from 'react-intl';
+import getMessage from 'common/util/i18nUtils';
+import { getPermisjonsregler } from 'app/util/uttaksplan/permisjonsregler';
 
-interface UttaksplanOppsummeringProps {
+interface OwnProps {
     perioder: Periode[];
     navnPåForeldre: NavnPåForeldre;
     erFarEllerMedmor: boolean;
     registrerteArbeidsforhold: Arbeidsforhold[];
     uttaksplanValidering: UttaksplanValideringState;
+    dekningsgrad: Dekningsgrad;
 }
 
-class UttaksplanOppsummering extends React.Component<UttaksplanOppsummeringProps> {
+type Props = OwnProps & InjectedIntlProps;
+
+class UttaksplanOppsummering extends React.Component<Props> {
     render() {
+        const { dekningsgrad, intl, ...rest } = this.props;
+        const permisjonsregler = getPermisjonsregler();
+        const antallUker =
+            dekningsgrad === '100' ? permisjonsregler.antallUkerTotalt100 : permisjonsregler.antallUkerTotalt80;
         return (
             <Oppsummeringsseksjon>
-                <UttaksplanOppsummeringsliste {...this.props} />
+                <Feltoppsummering
+                    feltnavn={getMessage(intl, 'oppsummering.dekningsgrad.label')}
+                    verdi={getMessage(intl, `oppsummering.dekningsgrad.verdi${dekningsgrad}`, {
+                        antallUker
+                    })}
+                />
+                <UttaksplanOppsummeringsliste {...rest} />
             </Oppsummeringsseksjon>
         );
     }
 }
 
-export default UttaksplanOppsummering;
+export default injectIntl(UttaksplanOppsummering);
