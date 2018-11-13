8c7bdf3ffa3e6b932ea637382b9c7c79f74aa04d
Frode Hansen <frode.hansen@bekk.no>
2018-11-13 10:13:37 +0100

    More failsafe BUILD handler
    Former-commit-id: 3c5fdf9d198833f79ed1896324dd3b7c697a6096

--- src/app/components/applicationInfo/ApplicationInfo.tsx
+++ src/app/components/applicationInfo/ApplicationInfo.tsx
@@ -4,14 +4,21 @@ import './applicationInfo.less';
 interface BUILDInfo {
     VERSION: string;
 }
+
 declare const BUILD: BUILDInfo;
 
 export interface Props {}
 
-const ApplicationInfo: React.StatelessComponent<Props> = (props) => (
-    <div className="applicationInfo" role="presentation" aria-hidden={true}>
-        {BUILD.VERSION}
-    </div>
-);
+const ApplicationInfo: React.StatelessComponent<Props> = (props) => {
+    try {
+        return (
+            <div className="applicationInfo" role="presentation" aria-hidden={true}>
+                {BUILD.VERSION}
+            </div>
+        );
+    } catch (e) {
+        return null;
+    }
+};
 
 export default ApplicationInfo;
