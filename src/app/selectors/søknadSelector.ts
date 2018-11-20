f142afea67334512877087c5c36e15e8eb80932a
Andreas Dybdahl <andreas.dyb@gmail.com>
2018-11-20 14:03:48 +0100

    Lagt forslag til regeloppsett

--- src/app/selectors/søknadSelector.ts
+++ src/app/selectors/søknadSelector.ts
@@ -1,114 +1,10 @@
 import { createSelector } from 'reselect';
 import { AppState } from '../redux/reducers';
-import Søknad, { SøkerRolle, Søkersituasjon } from '../types/søknad/Søknad';
-import { TilgjengeligStønadskonto } from '../types/uttaksplan/periodetyper';
-import { getFamiliehendelsedato } from '../util/uttaksplan';
-import { getErDeltUttak } from '../util/uttaksplan/forslag/util';
-import { getErSøkerFarEllerMedmor } from '../util/domain/personUtil';
-import { Søknadsinfo, Uttaksdatoer } from './types';
-import { getUttaksdatoer } from '../util/uttaksplan/uttaksdatoer';
+import Søknad, { Søkersituasjon } from '../types/søknad/Søknad';
 import { RecursivePartial } from '../types/Partial';
-import { Barn } from '../types/søknad/Barn';
-
-const søknadSelector = (state: AppState): RecursivePartial<Søknad> => state.søknad;
-
-const getBarn = createSelector([søknadSelector], (søknad) => søknad.barn);
-const getSituasjon = createSelector([søknadSelector], (søknad) => søknad.situasjon);
-const getDekningsgrad = createSelector([søknadSelector], (søknad) => søknad.dekningsgrad);
-const getSøker = createSelector([søknadSelector], (søknad) => søknad.søker);
-const getAnnenForelder = createSelector([søknadSelector], (søknad) => søknad.annenForelder);
-
-const getErAleneOmOmsorg = createSelector([getSøker], (søker = {}) => søker.erAleneOmOmsorg);
-const getRolle = createSelector([getSøker], (søker = {}) => søker.rolle);
-
-const getAntallBarn = createSelector([getBarn], (barn = {}) => barn.antallBarn);
-
-const getHarRettPåForeldrepenger = createSelector(
-    [getAnnenForelder],
-    (annenForelder = {}) => annenForelder.harRettPåForeldrepenger
-);
-const getKanIkkeOppgis = createSelector([getAnnenForelder], (annenForelder = {}) => annenForelder.kanIkkeOppgis);
-
-const tilgjengeligeSøknadskontoerSelector = (state: AppState): TilgjengeligStønadskonto[] | undefined =>
-    state.api.tilgjengeligeStønadskontoer.length > 0 ? state.api.tilgjengeligeStønadskontoer : undefined;
-
-const familiehendelsesdatoSelector = createSelector(
-    [getBarn, getSituasjon],
-    (barn, situasjon): Date | undefined =>
-        barnHasRequiredValues(barn) && søkersituasjonHasRequiredValues(situasjon)
-            ? getFamiliehendelsedato(barn, situasjon)
-            : undefined
-);
-
-export const uttaksdatoerSelector = createSelector(
-    familiehendelsesdatoSelector,
-    (familiehendelsesdato: Date | undefined): Uttaksdatoer | undefined => {
-        return familiehendelsesdato !== undefined ? getUttaksdatoer(familiehendelsesdato) : undefined;
-    }
-);
-export const søknadsinfoSelector = createSelector(
-    familiehendelsesdatoSelector,
-    uttaksdatoerSelector,
-    tilgjengeligeSøknadskontoerSelector,
-    getSituasjon,
-    getDekningsgrad,
-    getErAleneOmOmsorg,
-    getRolle,
-    getAntallBarn,
-    getHarRettPåForeldrepenger,
-    getKanIkkeOppgis,
-    (
-        familiehendelsesdato,
-        uttaksdatoer,
-        tilgjengeligeStønadskontoer,
-        situasjon,
-        dekningsgrad,
-        erAleneOmOmsorg,
-        rolle,
-        antallBarn,
-        harRettPåForeldrepenger,
-        kanIkkeOppgis
-    ): Søknadsinfo | undefined => {
-        if (
-            familiehendelsesdato !== undefined &&
-            uttaksdatoer !== undefined &&
-            tilgjengeligeStønadskontoer !== undefined &&
-            situasjon !== undefined &&
-            dekningsgrad !== undefined &&
-            erAleneOmOmsorg !== undefined &&
-            rolle !== undefined &&
-            antallBarn !== undefined &&
-            harRettPåForeldrepenger !== undefined &&
-            kanIkkeOppgis
-        ) {
-            const søkerErFarEllerMedmor = getErSøkerFarEllerMedmor(rolle!);
-            const søkerErMor = !søkerErFarEllerMedmor;
-            const morHarAleneomsorg = søkerErFarEllerMedmor === false && (erAleneOmOmsorg || kanIkkeOppgis);
-            const morHarRett = rolle === SøkerRolle.MOR || (søkerErFarEllerMedmor && harRettPåForeldrepenger === true);
-            const farEllerMedmorHarAleneomsorg = søkerErFarEllerMedmor && (erAleneOmOmsorg || kanIkkeOppgis);
-            const farEllerMedmorHarRett =
-                søkerErFarEllerMedmor || (rolle === SøkerRolle.MOR && harRettPåForeldrepenger === true);
-
-            return {
-                situasjon,
-                familiehendelsesdato,
-                antallBarn,
-                dekningsgrad,
-                farEllerMedmorHarAleneomsorg,
-                farEllerMedmorHarRett,
-                morHarAleneomsorg,
-                morHarRett,
-                søkerErMor,
-                søkerErFarEllerMedmor,
-                søkerErAleneOmOmsorgen: erAleneOmOmsorg,
-                erDeltUttak: getErDeltUttak(tilgjengeligeStønadskontoer),
-                uttaksdatoer,
-                tilgjengeligeStønadskontoer
-            };
-        }
-        return undefined;
-    }
-);
+import { ApiState } from 'app/redux/reducers/apiReducer';
+import Barn from 'app/types/søknad/Barn';
+import { getFamiliehendelsedato } from 'app/util/uttaksplan';
 
 const barnHasRequiredValues = (barn: RecursivePartial<Barn> | undefined): barn is Barn =>
     barn !== undefined && barn.antallBarn !== undefined;
@@ -116,3 +12,42 @@ const barnHasRequiredValues = (barn: RecursivePartial<Barn> | undefined): barn i
 const søkersituasjonHasRequiredValues = (
     situasjon: RecursivePartial<Søkersituasjon> | undefined
 ): situasjon is Søkersituasjon => situasjon !== undefined;
+
+const søknadSelector = (state: AppState): RecursivePartial<Søknad> => state.søknad;
+const apiSelector = (state: AppState): ApiState => state.api;
+
+// Søknad
+export const getBarn = createSelector([søknadSelector], (søknad = {}) => søknad.barn);
+export const getSituasjon = createSelector([søknadSelector], (søknad = {}) => søknad.situasjon);
+export const getDekningsgrad = createSelector([søknadSelector], (søknad = {}) => søknad.dekningsgrad);
+export const getSøker = createSelector([søknadSelector], (søknad = {}) => søknad.søker);
+export const getAnnenForelder = createSelector([søknadSelector], (søknad = {}) => søknad.annenForelder);
+
+// Søker
+export const getSøkerErAleneOmOmsorg = createSelector([getSøker], (søker = {}) => søker.erAleneOmOmsorg);
+export const getSøkerRolle = createSelector([getSøker], (søker = {}) => søker.rolle);
+
+// Barn
+export const getAntallBarn = createSelector([getBarn], (barn = {}) => barn.antallBarn);
+
+// Annen forelder
+export const getAnnenForelderHarRettPåForeldrepenger = createSelector(
+    [getAnnenForelder],
+    (annenForelder = {}) => annenForelder.harRettPåForeldrepenger
+);
+export const getAnnenForelderKanIkkeOppgis = createSelector(
+    [getAnnenForelder],
+    (annenForelder = {}) => annenForelder.kanIkkeOppgis
+);
+
+// API
+export const getTilgjengeligeStønadskontoer = createSelector([apiSelector], (api) => api.tilgjengeligeStønadskontoer);
+
+// Utledet data
+export const familiehendelsesdatoSelector = createSelector(
+    [getBarn, getSituasjon],
+    (barn, situasjon): Date | undefined =>
+        barnHasRequiredValues(barn) && søkersituasjonHasRequiredValues(situasjon)
+            ? getFamiliehendelsedato(barn, situasjon)
+            : undefined
+);
