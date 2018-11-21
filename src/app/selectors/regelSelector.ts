554c8cb070ecbb6123768f877c6d3320035c86cb
Andreas Dybdahl <andreas.dyb@gmail.com>
2018-11-21 12:14:43 +0100

    Stripped out all unused variables
    Former-commit-id: 0f86865c8749f234f7164b04034f439bbb9bec49

--- src/app/selectors/regelSelector.ts
+++ src/app/selectors/regelSelector.ts
@@ -1,39 +1,12 @@
-import {
-    getSituasjon,
-    getDekningsgrad,
-    getSøkerErAleneOmOmsorg,
-    getSøkerRolle,
-    getAntallBarn,
-    getAnnenForelderHarRettPåForeldrepenger,
-    getAnnenForelderKanIkkeOppgis,
-    getTilgjengeligeStønadskontoer
-} from './søknadSelector';
+import { getAntallBarn, getTilgjengeligeStønadskontoer } from './søknadSelector';
 import { createSelector } from 'reselect';
 import { getErDeltUttak } from 'app/util/uttaksplan/forslag/util';
 
 const getErFlerbarnsSøknad = (antallBarn: number | undefined): boolean => antallBarn !== undefined && antallBarn > 1;
 
 export const getRegler = createSelector(
-    [
-        getSituasjon,
-        getDekningsgrad,
-        getSøkerErAleneOmOmsorg,
-        getSøkerRolle,
-        getAntallBarn,
-        getAnnenForelderHarRettPåForeldrepenger,
-        getAnnenForelderKanIkkeOppgis,
-        getTilgjengeligeStønadskontoer
-    ],
-    (
-        situasjon,
-        dekningsgrad,
-        søkerErAleneOmOmsorg,
-        søkerRolle,
-        antallBarn,
-        annenForelderHarRettPåForeldrepenger,
-        annenForelderKanIkkeOppgis,
-        tilgjengeligeStønadskontoer
-    ) => {
+    [getAntallBarn, getTilgjengeligeStønadskontoer],
+    (antallBarn, tilgjengeligeStønadskontoer) => {
         return {
             erFlerbarnsSøknad: getErFlerbarnsSøknad(antallBarn),
             deltUttak: getErDeltUttak(tilgjengeligeStønadskontoer)
