062d073095b3ba87d186cb919c5d6529f839b186
Erlend Vige <erlend.vige@gmail.com>
2018-11-23 08:22:48 +0100

    Stillingsprosent validation in Uttaksplan
    Fixed an issue where user could advance with an invalid value in stillingsprosent-field
    in GradertUttakPart. Also removes old and unused references to stillingsprosent in SelvstendigNæringsdrivendeModal.

--- src/common/util/numberUtils.ts
+++ src/common/util/numberUtils.ts
@@ -4,7 +4,7 @@ const trimNumberValue = (value: string): string => {
     return value.replace(/,/g, '.').replace(/\s/g, '');
 };
 
-export const getFloatFromString = (value: string | undefined, decimals: number = 1): number | undefined => {
+export const getFloatFromString = (value: string | undefined): number | undefined => {
     if (value !== undefined && value !== null && value.length > 0 && numberRegExp.test(value)) {
         const trimmedValue = trimNumberValue(value);
         const numberOfPoints = (trimmedValue.match(/\./g) || []).length;
