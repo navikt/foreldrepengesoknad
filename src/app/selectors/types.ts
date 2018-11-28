efa676015ee44525af25d843f3a8e4baea2b9b46
Frode Hansen <frode.hansen@bekk.no>
2018-11-28 14:52:48 +0100

    Delete unused code

--- src/app/selectors/types.ts
+++ src/app/selectors/types.ts
@@ -1,7 +1,3 @@
-// import { Dekningsgrad } from 'common/types';
-// import { Søkersituasjon } from '../types/søknad/Søknad';
-// import { TilgjengeligStønadskonto } from '../types/uttaksplan/periodetyper';
-
 export interface Uttaksdatoer {
     førsteUttaksdag: Date;
     førFødsel: {
@@ -15,20 +11,3 @@ export interface Uttaksdatoer {
         sisteMuligeUttaksdag: Date;
     };
 }
-
-// export interface Søknadsinfo {
-//     situasjon: Søkersituasjon;
-//     familiehendelsesdato: Date;
-//     dekningsgrad: Dekningsgrad;
-//     antallBarn: number;
-//     morHarRett: boolean;
-//     morHarAleneomsorg: boolean;
-//     farEllerMedmorHarRett: boolean;
-//     farEllerMedmorHarAleneomsorg: boolean;
-//     erDeltUttak: boolean;
-//     søkerErMor: boolean;
-//     søkerErFarEllerMedmor: boolean;
-//     søkerErAleneOmOmsorgen: boolean;
-// }
-// // uttaksdatoer: Uttaksdatoer;
-// // tilgjengeligeStønadskontoer: TilgjengeligStønadskonto[];
