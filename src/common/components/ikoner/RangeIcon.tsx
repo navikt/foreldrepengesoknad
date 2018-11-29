610cdc69171bdf508dfb89085f8cf483edaa5292
Erlend Vige <erlend.vige@gmail.com>
2018-11-29 14:07:11 +0100

    RangeIcon refactor
    Remove state & background from react-component and into css.
    
    
    Former-commit-id: 53b53f006fbd272fb95d3fc08150fdbf112e341b

--- src/common/components/ikoner/RangeIcon.tsx
+++ src/common/components/ikoner/RangeIcon.tsx
@@ -1,40 +1,10 @@
 import * as React from 'react';
 
-interface RangeIconState {
-    isHovered: boolean;
-}
-
 interface RangeIconProps {
     type: 'plus' | 'minus';
 }
 
-type RangeIconBackgroundCode = '#0067C5' | '#005B82';
-
-export default class RangeIcon extends React.Component<RangeIconProps, RangeIconState> {
-    constructor(props: RangeIconProps) {
-        super(props);
-
-        this.state = {
-            isHovered: false
-        };
-
-        this.onMouseEnterHandler = this.onMouseEnterHandler.bind(this);
-        this.onMouseLeaveHandler = this.onMouseLeaveHandler.bind(this);
-        this.renderBackground = this.renderBackground.bind(this);
-    }
-
-    onMouseEnterHandler() {
-        this.setState({
-            isHovered: true
-        });
-    }
-
-    onMouseLeaveHandler() {
-        this.setState({
-            isHovered: false
-        });
-    }
-
+export default class RangeIcon extends React.Component<RangeIconProps> {
     renderPlusIcon() {
         return (
             <path
@@ -55,33 +25,13 @@ export default class RangeIcon extends React.Component<RangeIconProps, RangeIcon
         );
     }
 
-    renderBackground(background: RangeIconBackgroundCode) {
-        return (
-            <path
-                d="M20.1468152,3.884 C17.9787252,1.707 15.0946055,0.506 11.999477,0.5 C5.67021422,0.5 0.512000078,5.648 0.49999958,11.977 C0.493999331,15.049 1.68404874,17.939 3.85213875,20.116 C6.02022876,22.292 8.90534853,23.494 11.9784761,23.5 L12.000477,23.5 C18.3287398,23.5 23.4879539,18.351 23.4999544,12.021 C23.5059547,8.95 22.3149052,6.06 20.1468152,3.884 Z"
-                id="Background"
-                fill={background}
-            />
-        );
-    }
-
     render() {
         const { type } = this.props;
-        const { isHovered } = this.state;
-
-        const background = isHovered ? '#005B82' : '#0067C5';
         const icon = type === 'minus' ? this.renderMinusIcon() : this.renderPlusIcon();
 
         return (
-            <svg
-                width="24px"
-                height="24px"
-                viewBox="0 0 24 24"
-                className="rangeIcon"
-                onMouseEnter={this.onMouseEnterHandler}
-                onMouseLeave={this.onMouseLeaveHandler}>
+            <svg width="24px" height="24px" viewBox="0 0 24 24" className="rangeIcon">
                 <g id="Icons/subtract/default" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                    {this.renderBackground(background)}
                     {icon}
                 </g>
             </svg>
