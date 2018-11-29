487e8ca3d1a7af68efb1bd3c19fedebdc6ee3e6e
Erlend Vige <erlend.vige@gmail.com>
2018-11-29 13:51:28 +0100

    Validation of text lengths
    Adds validation for several fields in SelvstendigNæringsdrivendeModal that ensures they don't exceed max lengths.

--- src/app/util/stringUtils.ts
+++ src/app/util/stringUtils.ts
@@ -1,3 +1,7 @@
 export const removeSpacesFromString = (value: string) => {
     return value.replace(/\s/g, '');
 };
+
+export const maxLengthIsGreaterThanOrEqualToStringLength = (maxLength: number, value: string) => {
+    return value.length <= maxLength;
+};
