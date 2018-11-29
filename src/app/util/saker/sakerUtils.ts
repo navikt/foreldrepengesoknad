5cf36f38164a067ebddf6401f2a9e689248df79e
Frode Hansen <frode.hansen@bekk.no>
2018-11-29 10:38:55 +0100

    Sjekke om sak gjelder foreldrepengesøknad

--- src/app/util/saker/sakerUtils.ts
+++ src/app/util/saker/sakerUtils.ts
@@ -1,9 +1,29 @@
 import Sak from '../../types/søknad/Sak';
-import { BehandlingTema } from '../../types/søknad/Behandling';
+import Behandling, { BehandlingTema } from '../../types/søknad/Behandling';
+
+const getBehandling = (sak: Sak): Behandling | undefined => {
+    if (sak !== undefined && sak.behandlinger !== undefined && sak.behandlinger.length > 0) {
+        return sak.behandlinger[0];
+    }
+    return undefined;
+};
 
 export const sakGjelderAdopsjon = (sak: Sak): boolean => {
-    if (sak === undefined || sak.behandlinger === undefined || sak.behandlinger.length === 0) {
-        return false;
+    const behandling = getBehandling(sak);
+    return behandling !== undefined && behandling.tema === BehandlingTema.FORELDREPENGER_ADOPSJON;
+};
+
+export const gjelderSakForeldrepengesøknad = (sak: Sak): boolean => {
+    const behandling = getBehandling(sak);
+    if (behandling === undefined) {
+        return true;
+    } else {
+        const { tema } = behandling;
+        return (
+            tema === BehandlingTema.FORELDREPENGER ||
+            tema === BehandlingTema.FORELDREPENGER_ADOPSJON ||
+            tema === BehandlingTema.FORELDREPENGER_FØDSEL ||
+            tema === BehandlingTema.UDEFINERT
+        );
     }
-    return sak.behandlinger[0].tema === BehandlingTema.FORELDREPENGER_ADOPSJON;
 };
