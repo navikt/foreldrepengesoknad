a3b02fc1f6c320584c10e03864a1f4b0cdc0dbcd
Andreas Dybdahl <andreas.dyb@gmail.com>
2018-12-07 10:08:11 +0100

    Gjort tekst dynamisk

--- src/app/connected-components/steg/uttaksplan/OvertrukneDager.tsx
+++ src/app/connected-components/steg/uttaksplan/OvertrukneDager.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Stønadskontouttak } from 'app/components/uttaksoppsummering/Uttaksoppsummering';
 import Veilederinfo from 'common/components/veileder-info/Veilederinfo';
 import { getVarighetString } from 'common/util/intlUtils';
-import { injectIntl, InjectedIntl } from 'react-intl';
+import { injectIntl, InjectedIntl, FormattedMessage } from 'react-intl';
 import { guid } from 'nav-frontend-js-utils';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { NavnPåForeldre } from 'common/types';
@@ -18,10 +18,13 @@ const OvertrukneDager: React.SFC<OvertrukneDagerProps> = ({ overtrukneKontoer, n
     <Veilederinfo type="feil">
         {overtrukneKontoer.map((konto) => (
             <Normaltekst key={guid()}>
-                {`Du har tatt ${getVarighetString(
-                    Math.abs(konto.antallDager),
-                    intl
-                )} for mye uttak av ${getStønadskontoNavn(intl, konto.konto, navnPåForeldre)}`}
+                <FormattedMessage
+                    id="uttaksteg.overtruknedager.info"
+                    values={{
+                        varighet: getVarighetString(Math.abs(konto.antallDager), intl),
+                        konto: getStønadskontoNavn(intl, konto.konto, navnPåForeldre)
+                    }}
+                />
             </Normaltekst>
         ))}
     </Veilederinfo>
