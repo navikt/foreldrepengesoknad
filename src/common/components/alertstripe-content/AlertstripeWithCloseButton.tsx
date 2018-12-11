c5f185caed4bd5d12e0243919d762a6178aa07e3
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-12-11 09:55:30 +0100

    Handle multiselect file input
    Former-commit-id: 2c67f3e4ef5b0479bb572169cfd53ed7e9009fba

--- src/common/components/alertstripe-content/AlertstripeWithCloseButton.tsx
+++ src/common/components/alertstripe-content/AlertstripeWithCloseButton.tsx
@@ -1,26 +1,43 @@
 import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
+import AlertStripe from 'nav-frontend-alertstriper';
 import Lukknapp, { LukknappProps } from 'nav-frontend-lukknapp';
+import { Element, Normaltekst } from 'nav-frontend-typografi';
+import { guid } from 'nav-frontend-js-utils';
 
-import AlertStripe, { AlertStripeBaseProps } from 'nav-frontend-alertstriper';
+import BEMHelper from 'common/util/bem';
 
 import './alertstripeWithCloseButton.less';
 
 interface AlertstripeContentProps {
-    alertStripeProps: AlertStripeBaseProps;
     lukknappProps: LukknappProps;
     onClose?: () => void;
+    errorMessages: React.ReactNode[];
 }
 
-const AlertstripeWithCloseButton: React.StatelessComponent<AlertstripeContentProps> = ({
-    alertStripeProps,
-    lukknappProps,
-    onClose
-}) => {
+const AlertstripeWithCloseButton = ({ lukknappProps, onClose, errorMessages }: AlertstripeContentProps) => {
+    const cls = BEMHelper('alertStripeContent');
     return (
-        <AlertStripe {...alertStripeProps}>
-            <div className="alertStripeContent">
-                {alertStripeProps.children}
-                {onClose && <Lukknapp {...lukknappProps} onClick={onClose} />}
+        <AlertStripe type="advarsel" solid={true}>
+            <div className={cls.className}>
+                {errorMessages.length === 1 && (
+                    <Normaltekst className={cls.element('title')}>{errorMessages[0]}</Normaltekst>
+                )}
+                {errorMessages.length > 1 && (
+                    <div className={cls.element('error-list')}>
+                        <Element className={cls.element('title')}>
+                            <FormattedMessage id={'vedlegg.feilmelding.tittel.flereFeil'} />
+                        </Element>
+                        <ul>
+                            {errorMessages.map((message: React.ReactNode) => (
+                                <li className={cls.element('error-list-element')} key={guid()}>
+                                    <Normaltekst>{message}</Normaltekst>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+                {onClose && <Lukknapp className={cls.element('lukk-knapp')} {...lukknappProps} onClick={onClose} />}
             </div>
         </AlertStripe>
     );
