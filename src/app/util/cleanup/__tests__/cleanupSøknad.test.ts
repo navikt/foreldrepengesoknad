71135e50af679d85f428f1af530a3c70a83139a6
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-12-18 10:54:01 +0100

    Remove attachment with duplicate ids for perioder

--- src/app/util/cleanup/__tests__/cleanupSøknad.test.ts
+++ src/app/util/cleanup/__tests__/cleanupSøknad.test.ts
@@ -1,5 +1,11 @@
 import { Periode, Periodetype } from '../../../types/uttaksplan/periodetyper';
-import { removePeriodetypeHullFromUttaksplan } from '../cleanupSøknad';
+import {
+    removeDuplicateAttachmentIds,
+    removeDuplicateAttachments,
+    removePeriodetypeHullFromUttaksplan
+} from '../cleanupSøknad';
+import { Skjemanummer } from '../../../types/søknad/Søknad';
+import { AttachmentType } from 'common/storage/attachment/types/AttachmentType';
 
 const uttaksplan: Periode[] = [
     {
@@ -28,9 +34,63 @@ const uttaksplanMedHull: Periode[] = [
     }
 ] as Periode[];
 
+const uttaksplanMedLikeVedlegg: Periode[] = [
+    ...uttaksplan,
+    {
+        id: 'asd',
+        type: Periodetype.Utsettelse,
+        vedlegg: [
+            {
+                id: 'v123',
+                file: new File([''], 'mock.pdf'),
+                filesize: 1024,
+                filename: 'mockFile.pdf',
+                pending: false,
+                uploaded: true,
+                url: undefined,
+                skjemanummer: Skjemanummer.ANNET,
+                type: AttachmentType.OVERFØRING_KVOTE
+            }
+        ]
+    },
+    {
+        id: 'asd',
+        type: Periodetype.Utsettelse,
+        vedlegg: [
+            {
+                id: 'v123',
+                file: new File([''], 'mock.pdf'),
+                filesize: 1024,
+                filename: 'mockFile.pdf',
+                pending: false,
+                uploaded: true,
+                url: undefined,
+                skjemanummer: Skjemanummer.ANNET,
+                type: AttachmentType.OVERFØRING_KVOTE
+            },
+            {
+                id: 'v321',
+                file: new File([''], 'mock.pdf'),
+                filesize: 1024,
+                filename: 'mockFile.pdf',
+                pending: false,
+                uploaded: true,
+                url: undefined,
+                skjemanummer: Skjemanummer.ANNET,
+                type: AttachmentType.OVERFØRING_KVOTE
+            }
+        ]
+    }
+] as Periode[];
+
 describe('cleanupSøknad', () => {
     it('cleans uttaksplan', () => {
         const cleandUttaksplan = removePeriodetypeHullFromUttaksplan(uttaksplanMedHull);
         expect(JSON.stringify(cleandUttaksplan)).toEqual(JSON.stringify(uttaksplan));
+    });
+
+    it('Removes duplicate ids refs from perioder', () => {
+        removeDuplicateAttachments(uttaksplanMedLikeVedlegg);
+        expect(uttaksplanMedLikeVedlegg[uttaksplanMedLikeVedlegg.length - 1].vedlegg!.length).toEqual(1);
     });
 });
