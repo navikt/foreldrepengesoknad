d9c930e9aa559b84550a8857f44a865b2bd4a8da
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-01-21 12:53:50 +0100

    Type BehandlingsÅrsak for Behandling

--- src/app/types/søknad/Behandling.ts
+++ src/app/types/søknad/Behandling.ts
@@ -1,11 +1,15 @@
 export default interface Behandling {
+    opprettetTidspunkt: string;
+    endretTidspunkt: string;
     behandlendeEnhet: string;
     behandlendeEnhetNavn: string;
     id: any;
     status: BehandlingStatus;
     tema: string;
     type: string;
-    årsak: any;
+    årsak: BehandlingÅrsak;
+    behandlingResultatType: string;
+    inntektsmeldinger: string[];
 }
 
 export enum BehandlingStatus {
@@ -14,6 +18,37 @@ export enum BehandlingStatus {
     FATTER_VEDTAK = 'FVED',
     IVERKSETTER_VEDTAK = 'IVED',
     AVSLUTTET = 'AVSLU'
+}
+
+export enum BehandlingÅrsak {
+    HENDELSE_FØDSEL = 'RE-HENDELSE-FØDSEL',
+    MANGLENDE_FØDSEL = 'RE-MF',
+    MANGLENDE_FØDSEL_TERMIN = 'RE-MFIP',
+    ULIKT_ANTALL_BARN = 'RE-AVAB',
+    FEIL_LOVANVENDELSE = 'RE-LOV',
+    FEIL_REGELVERKSFORSTÅELSE = 'RE-RGLF',
+    FEIL_ELLER_ENDRET_FAKTA = 'RE-FEFAKTA',
+    PROSESSURELL_FEIL = 'RE-PRSSL',
+    ETTER_KLAGE = 'ETTER_KLAGE',
+    ENDRING_FRA_BRUKER = 'RE-END-FRA-BRUKER',
+    ENDRET_INNTEKTSMELDING = 'RE-END-INNTEKTSMELD',
+    KØET_BEHANDLING = 'KØET_BEHANDLING',
+    BERØRT_BEHANDLING = 'BERØRT_BEHANDLING',
+    REGISTER_OPPLYSNING = 'RE-REGISTEROPPL',
+    YTELSE = 'RE-YTELSE',
+    KLAGE = 'RE-KLAG',
+    MEDLEMSKAP = 'RE-MDL',
+    OPPTJENING = 'RE-OPTJ',
+    FORDELING_STØNADSPERIODE = 'RE-FRDLING',
+    INNTEKT = 'RE-INNTK',
+    DØD = 'RE-DØD',
+    RELASJON_TIL_BARNET = 'RE-SRTB',
+    SØKNADSFRIST = 'RE-FRIST',
+    BEREGNINGSGRUNNLAG = 'RE-BER-GRUN',
+    TILSTØTENDE_YTELSE_INNVILGET = 'RE-TILST-YT-INNVIL',
+    ENDRING_BEREGNINGSGRUNNLAG = 'RE-ENDR-BER-GRUN',
+    TILSTØTENDE_YTELSE_OPPHØRT = 'RE-TILST-YT-OPPH',
+    ANNET = 'RE-ANNET'
 }
 
 export enum BehandlingTema {
