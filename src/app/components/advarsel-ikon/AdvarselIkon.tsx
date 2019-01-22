21c8805776c074175219a13778a5ce689bbd132e
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2019-01-22 09:31:44 +0100

    PFP-5033: Expired session popup (#812)
    * Catch 401 responses from /storage endpoint and tag session as expired
    * Show a modal with options when user token expires

---
+++ src/app/components/advarsel-ikon/AdvarselIkon.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const AdvarselIkon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" viewBox="0 0 80 80">
+        <g fill="none" fillRule="evenodd">
+            <circle cx="40" cy="40" r="40" fill="#FFBD66" />
+            <path
+                fill="#3E3832"
+                d="M36 20a4 4 0 1 1 8 0v25.002a4 4 0 1 1-8 0V20zm4 44a4.5 4.5 0 1 1 0-9 4.5 4.5 0 0 1 0 9z"
+            />
+        </g>
+    </svg>
+);
+
+export default AdvarselIkon;
