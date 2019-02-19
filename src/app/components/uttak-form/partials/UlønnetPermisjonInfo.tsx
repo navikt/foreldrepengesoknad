8fa6d4cc3f4b35c925beeab9636e446752715a26
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-02-19 09:44:21 +0100

    Lagt til info om ulønnet permisjon i uttaksplanen
    Former-commit-id: 141b077026129abfafea684bfba24679e3c20cc5

---
+++ src/app/components/uttak-form/partials/UlønnetPermisjonInfo.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { injectIntl, FormattedHTMLMessage } from 'react-intl';
+import UtvidetInformasjon from 'app/components/utvidetinformasjon/UtvidetInformasjon';
+
+import './ulønnetPermisjonInfo.less';
+
+const UlønnetPermisjonInfo: React.SFC<{}> = () => {
+    return (
+        <UtvidetInformasjon apneLabel="Vi ønsker en periode med ulønnet permisjon">
+            <div className="ulonnetPermisjon">
+                <FormattedHTMLMessage id="uttaksplan.ulønnetPermisjonInfo.del1" />
+            </div>
+            <FormattedHTMLMessage id="uttaksplan.ulønnetPermisjonInfo.del2" />
+        </UtvidetInformasjon>
+    );
+};
+
+export default injectIntl(UlønnetPermisjonInfo);
