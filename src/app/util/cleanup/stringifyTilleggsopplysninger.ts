995b6fb61b4d265f37a268b30f989c98c7dc9818
Kjetil Svalestuen <kjetil.svalestuen@bekk.no>
2019-02-25 13:35:10 +0100

    Legg inn begrunnelse for sen utsettelse/uttak i oppsummering

--- src/app/util/cleanup/stringifyTilleggsopplysninger.ts
+++ src/app/util/cleanup/stringifyTilleggsopplysninger.ts
@@ -1,4 +1,4 @@
-import { Tilleggsopplysninger } from 'app/types/søknad/Søknad';
+import { Tilleggsopplysninger, Tilleggsopplysning, Opplysning } from 'app/types/søknad/Søknad';
 
 const tilSaksbehandling = {
     begrunnelseForSenEndring: 'Begrunnelse for å søke om utsettelse',
@@ -8,32 +8,51 @@ const tilSaksbehandling = {
         'på grunn av sykdom tilbake i tid og uttak mer enn tre måneder tilbake i tid'
 };
 
+export interface TilleggsopplysningMedBeskrivelse {
+    beskrivelse: string;
+    tekst: string;
+    ekstraInformasjon: string;
+}
+
+export const beskrivTilleggsopplysning = (
+    opplysning: Opplysning,
+    tilleggsopplysning: Tilleggsopplysning
+): TilleggsopplysningMedBeskrivelse => {
+    const { tekst, ekstraInformasjon } = tilleggsopplysning;
+
+    const beskrivelseAvOpplysning = tilSaksbehandling[opplysning] || 'Generell opplysning';
+    const ekstraInfoTilSaksbehandling = beskrivelseAvOpplysning
+        ? tilSaksbehandling[`${opplysning}.${ekstraInformasjon}`]
+        : undefined;
+
+    return {
+        beskrivelse: beskrivelseAvOpplysning,
+        ekstraInformasjon: ekstraInfoTilSaksbehandling,
+        tekst
+    };
+};
+
 const stringifyTilleggsopplysninger = (tilleggsopplysninger: Tilleggsopplysninger): string => {
-    const opplysninger = Object.keys(tilleggsopplysninger);
+    const opplysninger = Object.keys(tilleggsopplysninger) as Opplysning[];
     if (opplysninger.length === 0) {
         return '';
     }
 
     return opplysninger
-        .map((opplysningKey) => {
-            const { tekst, ekstraInformasjon } = tilleggsopplysninger[opplysningKey];
+        .map((opplysningstype) => {
+            const opplysning = tilleggsopplysninger[opplysningstype] as Tilleggsopplysning;
+            const medBeskrivelse = beskrivTilleggsopplysning(opplysningstype, opplysning);
 
-            if (tekst.length === 0) {
+            if (medBeskrivelse) {
+                const tilSaksbehandler =
+                    medBeskrivelse.beskrivelse + medBeskrivelse.ekstraInformasjon
+                        ? ', ' + medBeskrivelse.ekstraInformasjon
+                        : '';
+
+                return `${tilSaksbehandler}: ${medBeskrivelse.tekst}`;
+            } else {
                 return '';
             }
-
-            const beskrivelseAvOpplysning = tilSaksbehandling[opplysningKey];
-
-            let beskrivelseTilSaksbehandling = 'Generell opplysning';
-            if (beskrivelseAvOpplysning) {
-                const ekstraInfoTilSaksbehandling = tilSaksbehandling[`${opplysningKey}.${ekstraInformasjon}`];
-
-                beskrivelseTilSaksbehandling = ekstraInfoTilSaksbehandling
-                    ? `${beskrivelseAvOpplysning}, ${ekstraInfoTilSaksbehandling}`
-                    : beskrivelseAvOpplysning;
-            }
-
-            return `${beskrivelseTilSaksbehandling}: ${tekst}`;
         })
         .join('\n\n');
 };
