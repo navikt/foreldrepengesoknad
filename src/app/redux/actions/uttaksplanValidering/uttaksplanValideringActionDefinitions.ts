3cdc587fbb0f4ed0b8a10662228bf496cb9ecdc7
Frode Hansen <frode.hansen@bekk.no>
2019-03-19 17:28:24 +0100

    morHarSøktUgyldigUttakFørsteSeksUker

--- src/app/redux/actions/uttaksplanValidering/uttaksplanValideringActionDefinitions.ts
+++ src/app/redux/actions/uttaksplanValidering/uttaksplanValideringActionDefinitions.ts
@@ -14,7 +14,6 @@ export interface SetUttaksplanValidering {
     regelTestresultat: UttaksplanRegelTestresultat | undefined;
     validertePerioder: { [periodeId: string]: ValidertPeriode };
     inneholderPerioder: boolean;
-    morHarSøktUgyldigUtsettelseFørsteSeksUker: boolean;
     uttakErBareOpphold: boolean;
     uttaksplanStarterMedOpphold: boolean;
     uttaksplanSlutterMedOpphold: boolean;
