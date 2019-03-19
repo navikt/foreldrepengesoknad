c3e91fdc74cc53c57d41f7055d90ad248b7244b0
Frode Hansen <frode.hansen@bekk.no>
2019-03-19 09:40:56 +0100

    HarFarMedmorSøktUgyldigUttakFørsteSeksUkerRegel

--- src/app/regler/uttaksplanValidering/index.ts
+++ src/app/regler/uttaksplanValidering/index.ts
@@ -2,10 +2,12 @@ import { Regel } from './types';
 import { inneholderUttaksplanPerioderRegel } from './ulovlig/inneholderUttaksplanPerioderRegel';
 import { harMorSøktUgyldigUttakFørsteSeksUkerRegel } from './ulovlig/harMorHarSøktUgyldigUttakFørsteSeksUkerRegel';
 import { inneholderStønadskontoForMyeUttakRegel } from './ulovlig/inneholderStønadskontoForMyeUttak';
+import { harFarMedmorSøktUgyldigUttakFørsteSeksUkerRegel } from './ulovlig/harFarMedmorHarS\u00F8ktUgyldigUttakF\u00F8rsteSeksUkerRegel';
 
 const uttaksplanRegler: Regel[] = [
     inneholderUttaksplanPerioderRegel,
     harMorSøktUgyldigUttakFørsteSeksUkerRegel,
+    harFarMedmorSøktUgyldigUttakFørsteSeksUkerRegel,
     inneholderStønadskontoForMyeUttakRegel
 ];
 
