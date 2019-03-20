45c4bf480b82d0f91460956004180d27ee18d81d
Frode Hansen <frode.hansen@bekk.no>
2019-03-20 08:38:08 +0100

    Bugfix - erBegrunnelseForSenEndringGyldigTest

--- src/app/regler/uttaksplanValidering/tester/erBegrunnelseForSenEndringGyldigTest.ts
+++ src/app/regler/uttaksplanValidering/tester/erBegrunnelseForSenEndringGyldigTest.ts
@@ -1,10 +1,19 @@
 import { Regelgrunnlag, RegelTestresultat } from '../types';
-import { begrunnelseForSenEndringErGyldig } from '../../../util/validation/uttaksplan/begrunnelseForSenEndringValidation';
+import {
+    begrunnelseForSenEndringErGyldig,
+    begrunnelseSenEndringMaxLength
+} from '../../../util/validation/uttaksplan/begrunnelseForSenEndringValidation';
 
 export function erBegrunnelseForSenEndringGyldigTest(grunnlag: Regelgrunnlag): RegelTestresultat {
     const { begrunnelseForSenEndring } = grunnlag.tilleggsopplysninger;
 
     return begrunnelseForSenEndringErGyldig(begrunnelseForSenEndring ? begrunnelseForSenEndring.tekst : '')
-        ? { passerer: false }
-        : { passerer: true };
+        ? { passerer: true }
+        : {
+              passerer: false,
+              info: {
+                  intlKey: 'valideringsfeil.fritekst.kanIkkeVæreLengreEnn',
+                  values: { maxLength: begrunnelseSenEndringMaxLength }
+              }
+          };
 }
