6c840b27068c0e7a16c7efd04f0589a2a1bb4050
Frode Hansen <frode.hansen@bekk.no>
2019-03-20 07:49:16 +0100

    Bugfix - planenInneholderIngenPerioder

--- src/app/regler/uttaksplanValidering/tester/inneholderUttaksplanPerioderTest.ts
+++ src/app/regler/uttaksplanValidering/tester/inneholderUttaksplanPerioderTest.ts
@@ -2,5 +2,5 @@ import { Regelgrunnlag, RegelTest, RegelTestresultat } from '../types';
 import { isValidTidsperiode } from '../../../util/uttaksplan/Tidsperioden';
 
 export const inneholderUttaksplanPerioderTest: RegelTest = (grunnlag: Regelgrunnlag): RegelTestresultat => {
-    return { passerer: grunnlag.perioder.filter((periode) => isValidTidsperiode(periode)).length > 0 };
+    return { passerer: grunnlag.perioder.filter((periode) => isValidTidsperiode(periode.tidsperiode)).length > 0 };
 };
