4631c15a2bfe6d9c90173d2748179716e1534294
AndreasDybdahl <andreas.dyb@gmail.com>
2019-03-26 11:51:50 +0100

    Revert "Gradering skal nå avrundes per kvote i stedet for per periode"

--- src/app/util/uttaksPlanStatus.ts
+++ src/app/util/uttaksPlanStatus.ts
@@ -20,11 +20,11 @@ export const finnAntallDagerÅTrekke = (dager: number, p: Periode): number => {
         const periodeErSamtidigUttak = p.samtidigUttakProsent !== undefined;
 
         if (periodeErSamtidigUttak) {
-            return dager * (getFloatFromString(p.samtidigUttakProsent)! / 100);
+            return Math.floor(dager * (getFloatFromString(p.samtidigUttakProsent)! / 100));
         } else if (periodeErGradert) {
             const graderingsProsent = (100 - getFloatFromString(p.stillingsprosent)!) / 100;
 
-            return dager * graderingsProsent;
+            return Math.floor(dager * graderingsProsent);
         } else {
             return dager;
         }
@@ -66,8 +66,6 @@ export const beregnGjenståendeUttaksdager = (
                         : antallDager - finnAntallDagerÅTrekke(Perioden(p).getAntallUttaksdager(), p);
                 }
             });
-
-            antallDager = beregnDagerBrukt ? Math.floor(antallDager) : Math.ceil(antallDager);
         }
 
         return {
