c79c1335bc66a7ac320755579507939e5c576ff3
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-03-29 10:15:40 +0100

    Fjernet hardkodede permisjonsregler som må være dynamiske

--- src/app/util/uttaksplan/permisjonUtils.ts
+++ src/app/util/uttaksplan/permisjonUtils.ts
@@ -1,5 +1,4 @@
 import { Permisjonsregler } from '../../types/uttaksplan/permisjonsregler';
-import { Dekningsgrad } from 'common/types';
 import { Uttaksdagen } from './Uttaksdagen';
 
 /**
@@ -10,20 +9,4 @@ import { Uttaksdagen } from './Uttaksdagen';
 export function getDefaultPermisjonStartdato(familiehendelsedato: Date, permisjonsregler: Permisjonsregler): Date {
     const førsteUttaksdag = Uttaksdagen(familiehendelsedato).denneEllerNeste();
     return Uttaksdagen(førsteUttaksdag).trekkFra(permisjonsregler.antallUkerForeldrepengerFørFødsel * 5);
-}
-
-/**
- * Finner antall uker for fellesperiode ut fra dekningsgrad
- * @param permisjonsregler
- * @param dekningsgrad
- */
-export function getAntallUkerFellesperiode(permisjonsregler: Permisjonsregler, dekningsgrad: Dekningsgrad) {
-    const totaltAntallUker =
-        dekningsgrad === '80' ? permisjonsregler.antallUkerTotalt80 : permisjonsregler.antallUkerTotalt100;
-    return (
-        totaltAntallUker -
-        permisjonsregler.antallUkerMødrekvote -
-        permisjonsregler.antallUkerFedrekvote -
-        permisjonsregler.antallUkerForeldrepengerFørFødsel
-    );
 }
--- src/app/util/uttaksplan/permisjonsregler.ts
+++ src/app/util/uttaksplan/permisjonsregler.ts
@@ -1,14 +1,8 @@
 import { Permisjonsregler } from '../../types/uttaksplan/permisjonsregler';
 
 const reglerFomJuli2018: Permisjonsregler = {
-    antallUkerTotalt100: 49,
-    antallUkerFellesperiode100: 18,
-    antallUkerTotalt80: 59,
-    antallUkerFellesperiode80: 28,
     antallUkerForeldrepengerFørFødsel: 3,
     antallUkerMødrekvoteEtterFødsel: 6,
-    antallUkerFedrekvote: 15,
-    antallUkerMødrekvote: 15,
     maksAntallUkerForeldrepengerFørFødsel: 12,
     maksPermisjonslengdeIÅr: 3,
     maksFeriedagerEttÅr: 21,
