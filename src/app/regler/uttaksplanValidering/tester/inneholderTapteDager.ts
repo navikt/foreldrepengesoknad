a2040b165dedc008e94eb1037d4f10bb69f32c96
Frode Hansen <frode.hansen@bekk.no>
2019-03-29 12:50:40 +0100

    Planen inneholder tapte dager
    Former-commit-id: 5ec051ae04745a58dffc063313444d415bba0c8f

---
+++ src/app/regler/uttaksplanValidering/tester/inneholderTapteDager.ts
@@ -0,0 +1,34 @@
+import { Regelgrunnlag, RegelTestresultat } from '../types';
+import { Periodene } from '../../../util/uttaksplan/Periodene';
+import getInformasjonOmTaptUttakVedUttakEtterSeksUkerFarMedmor from '../../uttaksplan/getInformasjonOmTaptUttakVedUttakEtterSeksUkerFarMedmor';
+import { isUttaksperiode, Periodetype, StønadskontoType } from '../../../types/uttaksplan/periodetyper';
+
+export function inneholderTapteDagerTest(grunnlag: Regelgrunnlag): RegelTestresultat {
+    const {
+        perioder,
+        søknadsinfo: { søknaden, søker, mor }
+    } = grunnlag;
+    const infoOmTaptUttakVedUttakEtterSeksUkerFarMedmor = getInformasjonOmTaptUttakVedUttakEtterSeksUkerFarMedmor(
+        perioder,
+        søknaden.familiehendelsesdato,
+        søker.erFarEllerMedmor,
+        mor.harRett === false,
+        mor.erUfør
+    );
+
+    const planInneholderTapteDager =
+        Periodene(perioder).getHull().length > 0 || infoOmTaptUttakVedUttakEtterSeksUkerFarMedmor !== undefined;
+
+    const planInneholderAnnetEnnAktivitetsfriKvote = perioder
+        .filter((p) => p.type !== Periodetype.Hull)
+        .some((p) => (isUttaksperiode(p) && p.konto !== StønadskontoType.AktivitetsfriKvote) || !isUttaksperiode(p));
+
+    const passerer = (planInneholderTapteDager && planInneholderAnnetEnnAktivitetsfriKvote) === false;
+
+    return {
+        passerer,
+        info: {
+            intlKey: 'uttaksplan.veileder.planenInneholderHull'
+        }
+    };
+}
--- src/app/selectors/uttaksplanVeilederinfoSelector.ts
+++ src/app/selectors/uttaksplanVeilederinfoSelector.ts
@@ -4,13 +4,7 @@ import { Message } from 'app/components/veilederpanel-innhold/VeilederpanelInnho
 import { Periodene } from 'app/util/uttaksplan/Periodene';
 import { søknadSelector } from './søknadSelector';
 import Søknad from 'app/types/søknad/Søknad';
-import getInformasjonOmTaptUttakVedUttakEtterSeksUkerFarMedmor from 'app/regler/uttaksplan/getInformasjonOmTaptUttakVedUttakEtterSeksUkerFarMedmor';
-import {
-    Periodetype,
-    isUttaksperiode,
-    StønadskontoType,
-    isUtsettelsesperiode
-} from 'app/types/uttaksplan/periodetyper';
+import { isUtsettelsesperiode } from 'app/types/uttaksplan/periodetyper';
 import { formaterDato } from 'common/util/datoUtils';
 import { Uttaksdagen } from 'app/util/uttaksplan/Uttaksdagen';
 import { InjectedIntl } from 'react-intl';
@@ -19,30 +13,8 @@ export const selectUttaksplanVeilederinfo = (intl: InjectedIntl) =>
     createSelector([getSøknadsinfo, søknadSelector], (søknadsinfo, søknad) => {
         const messages: Message[] = [];
         const { uttaksplan } = søknad as Søknad;
-        const { søknaden, søker, mor } = søknadsinfo!;
 
-        const infoOmTaptUttakVedUttakEtterSeksUkerFarMedmor = getInformasjonOmTaptUttakVedUttakEtterSeksUkerFarMedmor(
-            uttaksplan,
-            søknaden.familiehendelsesdato,
-            søker.erFarEllerMedmor,
-            mor.harRett === false,
-            mor.erUfør
-        );
-        const planInneholderTapteDager =
-            Periodene(uttaksplan).getHull().length > 0 || infoOmTaptUttakVedUttakEtterSeksUkerFarMedmor !== undefined;
-        const planInneholderAnnetEnnAktivitetsfriKvote = uttaksplan
-            .filter((p) => p.type !== Periodetype.Hull)
-            .some(
-                (p) => (isUttaksperiode(p) && p.konto !== StønadskontoType.AktivitetsfriKvote) || !isUttaksperiode(p)
-            );
         const planErBareUtsettelser = !uttaksplan.some((p) => !isUtsettelsesperiode(p)) && uttaksplan.length > 0;
-        if (planInneholderTapteDager && planInneholderAnnetEnnAktivitetsfriKvote) {
-            messages.push({
-                title: 'uttaksplan.veileder.planenInneholderHull.tittel',
-                type: 'info',
-                contentIntlKey: 'uttaksplan.veileder.planenInneholderHull'
-            });
-        }
 
         if (planErBareUtsettelser) {
             const messageContent = søknad.erEndringssøknad
