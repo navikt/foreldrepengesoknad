a1fcf8f5cdede62d8e4f14bd4749fe83ad72dc6c
Frode Hansen <frode.hansen@bekk.no>
2019-05-13 10:53:38 +0200

    Filer tilknyttet oppsummering i endringssøknad

---
+++ src/common/components/sirkelmaske/Sirkelmaske.tsx
@@ -0,0 +1,32 @@
+import React, { CSSProperties } from 'react';
+import { UttaksplanHexFarge } from 'common/util/colors';
+import BEMHelper from 'common/util/bem';
+
+import './sirkelmaske.less';
+
+interface Props {
+    aktiv?: boolean;
+    farge?: UttaksplanHexFarge;
+    diameter: string;
+}
+
+const bem = BEMHelper('sirkelmaske');
+
+const Sirkelmaske: React.StatelessComponent<Props> = ({ farge, diameter, aktiv = true, children }) => {
+    const style: Partial<CSSProperties> = {
+        backgroundColor: farge
+    };
+    if (diameter) {
+        style.width = diameter;
+        style.height = diameter;
+    }
+    return (
+        <div
+            className={bem.classNames(bem.className, bem.modifierConditional('inaktiv', aktiv === false))}
+            style={style}>
+            <div className={bem.element('content')}>{children}</div>
+        </div>
+    );
+};
+
+export default Sirkelmaske;
---
+++ src/common/util/colors.ts
@@ -0,0 +1,9 @@
+export enum UttaksplanHexFarge {
+    lilla = '#634689',
+    blaa = '#3385d1',
+    gronn = '#06893a',
+    yellow = '#ff9100',
+    graa = '#7f756c',
+    rammeGraa = '#c6c2bf',
+    rod = '#BA3A26'
+}
