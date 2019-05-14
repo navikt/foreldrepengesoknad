6be7b0f7cde9798d53e7917152b9b6a8dfae79e3
Frode Hansen <frode.hansen@bekk.no>
2019-05-14 16:04:13 +0200

    Trekke dager fra avslått periode

--- src/app/util/uttaksPlanStatus.ts
+++ src/app/util/uttaksPlanStatus.ts
@@ -6,7 +6,8 @@ import {
     isUttaksperiode,
     Periodetype,
     Oppholdsperiode,
-    Stønadskontouttak
+    Stønadskontouttak,
+    isPeriodeHullAvslåttPeriode
 } from '../types/uttaksplan/periodetyper';
 import { Forelder } from 'common/types';
 import { Perioden } from './uttaksplan/Perioden';
@@ -56,9 +57,9 @@ export const beregnGjenståendeUttaksdager = (
         if (uttaksplanPerioder) {
             uttaksplanPerioder.forEach((p: Periode) => {
                 if (
-                    p.type !== Periodetype.Utsettelse &&
-                    p.type !== Periodetype.Opphold &&
-                    p.type !== Periodetype.Hull
+                    p.type === Periodetype.Uttak ||
+                    p.type === Periodetype.Overføring ||
+                    isPeriodeHullAvslåttPeriode(p)
                 ) {
                     antallDager = beregnDagerBrukt
                         ? antallDager + finnAntallDagerÅTrekke(Perioden(p).getAntallUttaksdager(), p)
