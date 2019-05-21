5ec9b90aa7926b9c4c3c00b813fc2672b6b46438
Frode Hansen <frode.hansen@bekk.no>
2019-05-21 11:55:35 +0200

    Slette oppholdsårsak fra Saksperiode
    Former-commit-id: 806e65ff0f35e001eaaa82ead3103a0020f52544

--- src/app/util/eksisterendeSak/mapSaksperioderTilUttaksperioder.ts
+++ src/app/util/eksisterendeSak/mapSaksperioderTilUttaksperioder.ts
@@ -100,9 +100,6 @@ const getOppholdÅrsakFromSaksperiode = (saksperiode: Saksperiode): OppholdÅrsa
     if (saksperiode.flerbarnsdager) {
         return OppholdÅrsakType.UttakFlerbarnsukerAnnenForelder;
     }
-    if (saksperiode.oppholdsårsak) {
-        return saksperiode.oppholdsårsak;
-    }
     switch (saksperiode.stønadskontotype) {
         case StønadskontoType.Fedrekvote:
             return OppholdÅrsakType.UttakFedrekvoteAnnenForelder;
