c945ffd90fbb1b68b8bdd8003f0a1fa53b77f243
Frode Hansen <frode.hansen@bekk.no>
2019-05-23 19:23:06 +0200

    Selector for perioder som skal søkes om
    Former-commit-id: d41998b3492bbaf45cce4b547e10e23f495f310a

--- src/app/util/uttaksplan/index.ts
+++ src/app/util/uttaksplan/index.ts
@@ -10,14 +10,13 @@ import {
     PeriodeInfoType
 } from '../../types/uttaksplan/periodetyper';
 import { InjectedIntl } from 'react-intl';
-import Søknad, { Søkersituasjon } from '../../types/søknad/Søknad';
+import { Søkersituasjon } from '../../types/søknad/Søknad';
 import { findOldestDate } from '../dates/dates';
 import { Barn, isFødtBarn, isUfødtBarn, isAdopsjonsbarn, isForeldreansvarsbarn } from '../../types/søknad/Barn';
 import { formaterNavn } from '../domain/personUtil';
 import getMessage from 'common/util/i18nUtils';
 import { Navn } from '../../types/common';
 import { getNavnGenitivEierform } from '../tekstUtils';
-import { getEndretUttaksplanForInnsending } from './uttaksplanEndringUtil';
 import { getStønadskontoFromOppholdsårsak } from './uttaksperiodeUtils';
 
 const isValidStillingsprosent = (pst: number | undefined): boolean => pst !== undefined && isNaN(pst) === false;
@@ -193,14 +192,4 @@ export const uttaksplanInneholderSelvstendignæringaktivitet = (uttaksplan: Peri
                 (arbeidsform: Arbeidsform) => arbeidsform === Arbeidsform.selvstendignæringsdrivende
             )
     );
-};
-
-export const getPerioderDetSøkesOm = (søknad: Søknad): Periode[] => {
-    if (søknad.erEndringssøknad) {
-        const { eksisterendeSak } = søknad.ekstrainfo;
-        if (eksisterendeSak && eksisterendeSak.uttaksplan) {
-            return getEndretUttaksplanForInnsending(eksisterendeSak.uttaksplan, søknad.uttaksplan) || [];
-        }
-    }
-    return søknad.uttaksplan;
 };
