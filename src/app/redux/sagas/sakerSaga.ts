36976f034aeaa4e8d4f12657fdbcddfc502a9aec
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-05-24 10:04:27 +0200

    Bugfix sak for endringssøknad logic

--- src/app/redux/sagas/sakerSaga.ts
+++ src/app/redux/sagas/sakerSaga.ts
@@ -3,7 +3,7 @@ import { ApiActionKeys } from '../actions/api/apiActionDefinitions';
 import Api from '../../api/api';
 import { default as apiActions } from '../actions/api/apiActionCreators';
 import Sak, { FagsakStatus } from '../../types/søknad/Sak';
-import { skalKunneSøkeOmEndring, harSakUnderBehandling } from '../../util/saker/sakerUtils';
+import { skalKunneSøkeOmEndring, harSakUnderBehandling, harEnAvsluttetBehandling } from '../../util/saker/sakerUtils';
 import { getEksisterendeSakFromDTO } from 'app/util/eksisterendeSak/eksisterendeSakUtils';
 
 function* getSaker() {
@@ -13,21 +13,24 @@ function* getSaker() {
         const response = yield call(Api.getSaker);
         const saker: Sak[] = response.data;
         const nyesteSakArray = saker.sort((a, b) => b.opprettet.localeCompare(a.opprettet));
-        const nyesteSak = nyesteSakArray.find((sak) => sak.status === FagsakStatus.LOPENDE);
+        const nyesteRelevanteSak = nyesteSakArray.find(
+            (sak) =>
+                sak.status === FagsakStatus.LOPENDE || (harSakUnderBehandling(sak) && harEnAvsluttetBehandling(sak))
+        );
 
-        if (nyesteSak !== undefined) {
-            if (skalKunneSøkeOmEndring(nyesteSak)) {
+        if (nyesteRelevanteSak !== undefined) {
+            if (skalKunneSøkeOmEndring(nyesteRelevanteSak)) {
                 yield put(
                     apiActions.updateApi({
-                        sakForEndringssøknad: nyesteSak
+                        sakForEndringssøknad: nyesteRelevanteSak
                     })
                 );
             }
 
-            if (harSakUnderBehandling(nyesteSak) && !skalKunneSøkeOmEndring(nyesteSak)) {
+            if (harSakUnderBehandling(nyesteRelevanteSak) && !skalKunneSøkeOmEndring(nyesteRelevanteSak)) {
                 yield put(
                     apiActions.updateApi({
-                        sakUnderBehandling: nyesteSak
+                        sakUnderBehandling: nyesteRelevanteSak
                     })
                 );
             }
