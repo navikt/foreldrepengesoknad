bdc8fd76f4d8d5c7cb47259b40e5684dc867cf9c
AndreasDybdahl <andreas.dyb@gmail.com>
2019-05-27 14:28:49 +0200

    Revert "Endret samtidig uttak og gradering til kun å bruke heltall"
    Former-commit-id: 58f5da6c25c4c730fb645b962f6bf92137f2a564

--- src/app/util/validation/uttaksplan/uttakGraderingValidation.ts
+++ src/app/util/validation/uttaksplan/uttakGraderingValidation.ts
@@ -6,7 +6,7 @@ export const gradertUttaksperiodeErUgyldig = (periode: UttakFormPeriodeType) => 
     if (periode.type === Periodetype.Uttak) {
         const { gradert, stillingsprosent } = periode;
         if (gradert === true) {
-            const regler = getStillingsprosentRegler(false, stillingsprosent || 0);
+            const regler = getStillingsprosentRegler(false, stillingsprosent || '');
             const results = regler.map((regel) => regel.test() === true);
             return results.some((erGyldig) => erGyldig === false);
         }
