62ba7db5d65c1f1c86bc8a3d1332cc13565531f2
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-05-28 14:06:48 +0200

    Filtrere ut duplikate perioder forårsaket av flere arbeidsforhold

--- src/build/scripts/envSettings.js
+++ src/build/scripts/envSettings.js
@@ -14,7 +14,8 @@ function createEnvSettingsFile(settingsFile) {
                 FEATURE_VIS_AVSLATT_PERIODE:  '${process.env.FEATURE_VIS_AVSLATT_PERIODE}',
                 FEATURE_MAP_OPPHOLD:  '${process.env.FEATURE_MAP_OPPHOLD}',
                 FEATURE_MAP_ANNEN_PART_TIL_INFO:  '${process.env.FEATURE_MAP_ANNEN_PART_TIL_INFO}',
-                FEATURE_VIS_INFOSKRIV:  '${process.env.FEATURE_VIS_INFOSKRIV}'
+                FEATURE_VIS_INFOSKRIV:  '${process.env.FEATURE_VIS_INFOSKRIV}',
+                FEATURE_MAP_FLERE_ARBEIDSFORHOLD: '${process.env.FEATURE_MAP_FLERE_ARBEIDSFORHOLD}'
             };`
         );
     });
