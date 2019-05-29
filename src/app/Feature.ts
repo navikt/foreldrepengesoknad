56f835f758c85de85c9359cef788fbe8b53ae751
Frode Hansen <frode.hansen@bekk.no>
2019-05-29 12:08:20 +0200

    Fjerne resterende endreSøknad featureToggles
    Former-commit-id: 08183b877087e189a1f3d2bf4f84263d16c1c7c9

--- src/app/Feature.ts
+++ src/app/Feature.ts
@@ -1,11 +1,8 @@
 import Environment from './Environment';
 
 export enum Feature {
-    mapOpphold = 'FEATURE_MAP_OPPHOLD',
-    mapAnnenPartTilInfo = 'FEATURE_MAP_ANNEN_PART_TIL_INFO',
     logging = 'FEATURE_LOGGING',
-    visInfoskriv = 'FEATURE_VIS_INFOSKRIV',
-    mapFlereArbeidsforhold = 'FEATURE_MAP_FLERE_ARBEIDSFORHOLD'
+    visInfoskriv = 'FEATURE_VIS_INFOSKRIV'
     /** Nye features må også registreres i ./Environment */
 }
 
