f71b33430cae87d94b861b4736ddefd1fc7fa302
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-06-25 11:38:36 +0200

    Use formatHTMLMessage instead of formatMessage

--- src/common/util/i18nUtils.ts
+++ src/common/util/i18nUtils.ts
@@ -2,6 +2,6 @@ import InjectedIntl = ReactIntl.InjectedIntl;
 import MessageValue = ReactIntl.MessageValue;
 
 const getMessage = (intl: InjectedIntl, id: string, value?: { [key: string]: MessageValue }): string =>
-    intl.formatMessage({ id }, value);
+    intl.formatHTMLMessage({ id }, value);
 
 export default getMessage;
