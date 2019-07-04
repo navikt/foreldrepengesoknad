dc602c44d74ecd5141cb4103cf8f4d3c7c396983
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-07-04 12:37:44 +0200

    Cleaup, bugfix and test endringssøknad logic
    Former-commit-id: 54b9f7cb98c7c8b016d2ab2d8dc85544aa29c35c

--- src/app/util/saker/sakerUtils.ts
+++ src/app/util/saker/sakerUtils.ts
@@ -1,5 +1,5 @@
 import Sak, { FagsakStatus, SakType } from '../../types/søknad/Sak';
-import Behandling, { BehandlingStatus, BehandlingTema } from '../../types/søknad/Behandling';
+import Behandling, { BehandlingStatus, BehandlingTema, BehandligType } from '../../types/søknad/Behandling';
 
 const getBehandling = (sak: Sak): Behandling | undefined => {
     if (sak !== undefined && sak.behandlinger !== undefined && sak.behandlinger.length > 0) {
@@ -15,17 +15,7 @@ export const sakGjelderAdopsjon = (sak: Sak): boolean => {
 
 export const gjelderSakForeldrepengesøknad = (sak: Sak): boolean => {
     const behandling = getBehandling(sak);
-    if (behandling === undefined) {
-        return true;
-    } else {
-        const { tema } = behandling;
-        return (
-            tema === BehandlingTema.FORELDREPENGER ||
-            tema === BehandlingTema.FORELDREPENGER_ADOPSJON ||
-            tema === BehandlingTema.FORELDREPENGER_FØDSEL ||
-            tema === BehandlingTema.UDEFINERT
-        );
-    }
+    return behandling ? behandling.type === BehandligType.FORELDREPENGESØKNAD : true;
 };
 
 export const erInfotrygdSak = (sak: Sak): boolean => {
@@ -38,33 +28,37 @@ export const harEnAvsluttetBehandling = (sak: Sak): boolean => {
         : false;
 };
 
-const harEnAktivBehandling = (nyesteSak: Sak): boolean => {
-    return nyesteSak.behandlinger
-        ? nyesteSak.behandlinger.some(
+const harEnAktivBehandling = (sak: Sak): boolean => {
+    return sak.behandlinger
+        ? sak.behandlinger.some(
               (behandling: Behandling) =>
                   behandling.status === BehandlingStatus.OPPRETTET || behandling.status === BehandlingStatus.UTREDES
           )
         : false;
 };
 
-export const harSakUnderBehandling = (nyesteSak: Sak): boolean => {
-    if (!gjelderSakForeldrepengesøknad(nyesteSak)) {
-        return false;
-    }
-
-    return (
-        (nyesteSak.status === FagsakStatus.OPPRETTET || nyesteSak.status === FagsakStatus.UNDER_BEHANDLING) &&
-        harEnAktivBehandling(nyesteSak)
-    );
+export const harSakUnderBehandling = (sak: Sak): boolean => {
+    return gjelderSakForeldrepengesøknad(sak)
+        ? sak.status === FagsakStatus.OPPRETTET ||
+              (sak.status === FagsakStatus.UNDER_BEHANDLING && harEnAktivBehandling(sak))
+        : false;
 };
 
-export const skalKunneSøkeOmEndring = (nyesteSak: Sak): boolean => {
-    if (!gjelderSakForeldrepengesøknad(nyesteSak)) {
-        return false;
-    }
-
-    return (
-        (nyesteSak.status !== FagsakStatus.AVSLUTTET && harEnAvsluttetBehandling(nyesteSak)) ||
-        erInfotrygdSak(nyesteSak)
-    );
+export const skalKunneSøkeOmEndring = (sak: Sak): boolean => {
+    return gjelderSakForeldrepengesøknad(sak)
+        ? (sak.status !== FagsakStatus.AVSLUTTET && harEnAvsluttetBehandling(sak)) || erInfotrygdSak(sak)
+        : false;
 };
+
+export const getSakUnderBehandlig = (saker: Sak[]): Sak | undefined => {
+    return saker
+        .filter(gjelderSakForeldrepengesøknad)
+        .filter((sak) => !harEnAvsluttetBehandling(sak) && harSakUnderBehandling(sak))
+        .sort(sakByDescendingOrder)[0];
+};
+
+export const getSakForEndringssøknad = (saker: Sak[]): Sak | undefined => {
+    return saker.filter(skalKunneSøkeOmEndring).sort(sakByDescendingOrder)[0];
+};
+
+const sakByDescendingOrder = (a: Sak, b: Sak) => b.opprettet.localeCompare(a.opprettet);
