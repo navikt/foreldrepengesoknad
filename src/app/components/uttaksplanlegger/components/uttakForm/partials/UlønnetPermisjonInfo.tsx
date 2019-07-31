43912572a9706bd43662a08c3611deffae02018b
Andreas Dybdahl <andreas.dyb@gmail.com>
2019-07-31 14:04:00 +0200

    Fjernet hardkodet tekst

--- src/app/components/uttaksplanlegger/components/uttakForm/partials/UlønnetPermisjonInfo.tsx
+++ src/app/components/uttaksplanlegger/components/uttakForm/partials/UlønnetPermisjonInfo.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { FormattedHTMLMessage } from 'react-intl';
+import { FormattedHTMLMessage, injectIntl, InjectedIntl } from 'react-intl';
 import UtvidetInformasjon from 'app/components/elementer/utvidetinformasjon/UtvidetInformasjon';
 import Block from 'common/components/block/Block';
 
-const UlønnetPermisjonInfo: React.SFC<{}> = () => {
+interface Props {
+    intl: InjectedIntl;
+}
+
+const UlønnetPermisjonInfo: React.FunctionComponent<Props> = ({ intl }) => {
     return (
-        <UtvidetInformasjon apneLabel="Vi ønsker en periode med ulønnet permisjon">
+        <UtvidetInformasjon apneLabel={intl.formatMessage({ id: 'uttaksplan.ulønnetPermisjonInfo.tittel' })}>
             <Block margin="xs">
                 <FormattedHTMLMessage id="uttaksplan.ulønnetPermisjonInfo.del1" />
             </Block>
@@ -14,4 +18,4 @@ const UlønnetPermisjonInfo: React.SFC<{}> = () => {
     );
 };
 
-export default UlønnetPermisjonInfo;
+export default injectIntl(UlønnetPermisjonInfo);
