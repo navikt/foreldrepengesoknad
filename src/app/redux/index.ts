7585e148ac76245afd6a4bf3e6997a2b921212c9
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-02-27 13:20:08 +0100

    Redux dev tools fix
    Former-commit-id: 28a8b40c4610c54a6057477a5ae7b40045f1632e

--- src/app/redux/index.ts
+++ src/app/redux/index.ts
@@ -3,9 +3,14 @@ import reducers from './reducers';
 import middleware, { sagaMiddleware } from './middleware';
 import rootSaga from './sagas/rootSaga';
 
+const composeEnhancer = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     combineReducers(reducers),
-    compose(middleware, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__())
+    composeEnhancer(
+        middleware,
+        (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 sagaMiddleware.run(rootSaga);
