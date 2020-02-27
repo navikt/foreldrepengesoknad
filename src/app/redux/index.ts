7262184e72ddec07febc66b381f6f449ade1e444
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-02-27 12:47:49 +0100

    Updated redux store creation
    Former-commit-id: 666af5a02549ba1c25fe236f038655fb585d3163

--- src/app/redux/index.ts
+++ src/app/redux/index.ts
@@ -1,12 +1,11 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, compose } from 'redux';
 import reducers from './reducers';
 import middleware, { sagaMiddleware } from './middleware';
 import rootSaga from './sagas/rootSaga';
 
 const store = createStore(
     combineReducers(reducers),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
-    middleware
+    compose(middleware, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 sagaMiddleware.run(rootSaga);
