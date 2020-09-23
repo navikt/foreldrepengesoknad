c82fe9ef33e7b33192edc012c16c9d01668b40aa
MagnusRom <magnus.skalleberg.rom@nav.no>
2020-09-23 14:04:06 +0200

    Fikset text og oppdatert søknadstittel

--- src/app/components/applikasjon/søknadstittel/Søknadstittel.tsx
+++ src/app/components/applikasjon/søknadstittel/Søknadstittel.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 import './søknadstittel.less';
-
 interface Props {
-    children: string;
+    children: React.ReactNode;
 }
-
-export default class Søknadstittel extends React.Component<Props> {
-    render() {
-        const { children } = this.props;
-
-        return (
-            <div className="søknadstittel">
-                <h1 className="typo-undertittel">{children}</h1>
-            </div>
-        );
-    }
-}
+const Søknadstittel: React.FunctionComponent<Props> = ({ children }) => {
+    return (
+        <div className="søknadstittel">
+            <h1 className="typo-undertittel">{children}</h1>
+        </div>
+    );
+};
+export default Søknadstittel;
