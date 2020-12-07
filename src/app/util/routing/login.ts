036beeb7fd685d925db5881413e55e24cb490f27
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-12-07 14:42:47 +0100

    Login URL i test
    Former-commit-id: 9cdf44102160a5dbbd0baa6824301de2e2a98f77

--- src/app/util/routing/login.ts
+++ src/app/util/routing/login.ts
@@ -1,5 +1,9 @@
 import Environment from '../../Environment';
 
 export const redirectToLogin = () => {
-    window.location.href = Environment.LOGIN_URL + '?redirect=' + window.location.origin;
+    if ((Environment.LOGIN_URL as string).includes('loginservice-q')) {
+        window.location.href = Environment.LOGIN_URL + '?acr=Level3&redirect=' + window.location.origin;
+    } else {
+        window.location.href = Environment.LOGIN_URL + '?redirect=' + window.location.origin;
+    }
 };
