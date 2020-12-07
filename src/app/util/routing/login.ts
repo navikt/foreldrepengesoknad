383064045e917666a07644409730c5b9f6cf1424
Andreas Dybdahl <andreas.dybdahl@nav.no>
2020-12-07 15:24:51 +0100

    Revert login changes

--- src/app/util/routing/login.ts
+++ src/app/util/routing/login.ts
@@ -1,9 +1,5 @@
 import Environment from '../../Environment';
 
 export const redirectToLogin = () => {
-    if ((Environment.LOGIN_URL as string).includes('loginservice-q')) {
-        window.location.href = Environment.LOGIN_URL + '?level=Level3&redirect=' + window.location.origin;
-    } else {
-        window.location.href = Environment.LOGIN_URL + '?redirect=' + window.location.origin;
-    }
+    window.location.href = Environment.LOGIN_URL + '?redirect=' + window.location.origin;
 };
