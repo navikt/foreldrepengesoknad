04e7e69e7d23c8c7f4d428bbfbd0410eb4f42379
Andreas Dybdahl <andreas.dybdahl@nav.no>
2021-03-11 10:53:17 +0100

    Updated plugin options for TerserPlugin

--- src/build/webpack/webpack.config.production.js
+++ src/build/webpack/webpack.config.production.js
@@ -15,7 +15,6 @@ webpackConfig.plugins.push(
 webpackConfig.optimization = {
     minimizer: [
         new TerserPlugin({
-            sourceMap: true,
             terserOptions: {
                 mangle: true, // Note `mangle.properties` is `false` by default.
                 ie8: true,
