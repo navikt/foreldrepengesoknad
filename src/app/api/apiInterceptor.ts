adfc59f503d8332795b4e0cad9cabfd32144827a
tor-nav <tor.roneid@nav.no>
2021-06-17 15:34:01 +0200

    div

--- src/app/api/apiInterceptor.ts
+++ src/app/api/apiInterceptor.ts
@@ -6,17 +6,15 @@ const apiBaseUrl = Environment.REST_API_URL;
 const AxiosInstance = axios.create({ baseURL: apiBaseUrl });
 
 const getAxiosInstance = (fnr?: string) => {
-    AxiosInstance.interceptors.request.use(
-        (config: AxiosRequestConfig): AxiosRequestConfig => {
-            config.withCredentials = true;
-            config.timeout = 60 * 1000;
+    AxiosInstance.interceptors.request.use((config: AxiosRequestConfig): AxiosRequestConfig => {
+        config.withCredentials = true;
+        config.timeout = 60 * 1000;
 
-            if (process.env.NODE_ENV !== 'development' && fnr) {
-                config.headers.fnr = fnr;
-            }
-            return config;
+        if (process.env.NODE_ENV !== 'development' && fnr) {
+            config.headers.fnr = fnr;
         }
-    );
+        return config;
+    });
 
     AxiosInstance.interceptors.response.use(
         (response: AxiosResponse) => {
