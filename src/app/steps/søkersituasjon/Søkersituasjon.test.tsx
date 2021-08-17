bd5f4d0c4828f5690b3d7880571c71db3b6ef27f
tor-nav <tor.roneid@nav.no>
2021-08-17 15:31:15 +0200

    test

---
+++ src/app/steps/søkersituasjon/Søkersituasjon.test.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { composeStories } from '@storybook/testing-react';
+import userEvent from '@testing-library/user-event';
+import * as stories from 'stories/steps/søkersituasjon/Søkersituasjon.stories';
+
+const { Default } = composeStories(stories);
+
+const GÅ_VIDERE_KNAPP = 'Gå videre';
+
+describe('<Søkersituasjon>', () => {
+    it('skal ha født og søke som mor', async () => {
+        render(<Default />);
+
+        expect(await screen.findByText('Velg det som gjelder for deg')).toBeInTheDocument();
+        userEvent.click(screen.getByText('Fødsel'));
+
+        expect(await screen.findByText('Hva søker du som?')).toBeInTheDocument();
+        userEvent.click(screen.getByText('Mor'));
+
+        expect(await screen.findByText(GÅ_VIDERE_KNAPP)).toBeInTheDocument();
+    });
+
+    it('skal ha født og søke som medmor', async () => {
+        render(<Default />);
+
+        expect(await screen.findByText('Velg det som gjelder for deg')).toBeInTheDocument();
+        userEvent.click(screen.getByText('Fødsel'));
+
+        expect(await screen.findByText('Hva søker du som?')).toBeInTheDocument();
+        userEvent.click(screen.getByText('Medmor'));
+
+        expect(await screen.findByText(GÅ_VIDERE_KNAPP)).toBeInTheDocument();
+    });
+
+    it('skal adoptere og søke som mor', async () => {
+        render(<Default />);
+
+        expect(await screen.findByText('Velg det som gjelder for deg')).toBeInTheDocument();
+        userEvent.click(screen.getByText('Adopsjon'));
+
+        expect(await screen.findByText('Hva søker du som?')).toBeInTheDocument();
+        userEvent.click(screen.getByText('Mor'));
+
+        expect(await screen.findByText(GÅ_VIDERE_KNAPP)).toBeInTheDocument();
+    });
+
+    it('skal adoptere og søke som medmor', async () => {
+        render(<Default />);
+
+        expect(await screen.findByText('Velg det som gjelder for deg')).toBeInTheDocument();
+        userEvent.click(screen.getByText('Adopsjon'));
+
+        expect(await screen.findByText('Hva søker du som?')).toBeInTheDocument();
+        userEvent.click(screen.getByText('Medmor'));
+
+        expect(await screen.findByText(GÅ_VIDERE_KNAPP)).toBeInTheDocument();
+    });
+});
