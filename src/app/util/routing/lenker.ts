6cd071030b9d28633425c043f519dc5f2352f185
Andreas Dybdahl <andreas.dybdahl@nav.no>
2021-10-15 19:29:10 +0200

    Oppdatert gamle linker til ny produktside

--- src/app/util/routing/lenker.ts
+++ src/app/util/routing/lenker.ts
@@ -1,21 +1,21 @@
 export default {
-    adoptere: 'https://familie.nav.no/om-foreldrepenger#adoptere',
+    adoptere: 'https://www.nav.no/foreldrepenger-etter-oktober#adoptere',
     rettOgPlikt: 'https://nav.no/rettOgPlikt',
-    fleksibeltuttak: 'https://familie.nav.no/om-foreldrepenger#hjemme-samtidig',
+    fleksibeltuttak: 'https://www.nav.no/foreldrepenger-etter-oktober#samtidig',
     papirsøknad: 'https://www.nav.no/soknader/nb/person/familie/foreldrepenger-og-engangsstonad',
-    viktigeFrister: 'https://familie.nav.no/om-foreldrepenger#nar-kan-du-soke',
-    nav_aktivitetskrav: 'https://familie.nav.no/om-foreldrepenger#hvor-lenge-kan-jeg-fa-foreldrepenger',
+    viktigeFrister: 'https://www.nav.no/foreldrepenger-etter-oktober#soknadsfrister',
+    nav_aktivitetskrav: 'https://www.nav.no/foreldrepenger-etter-oktober#hvor-lenge',
     brukerstøtte:
         'https://www.nav.no/no/NAV+og+samfunn/Kontakt+NAV/Teknisk+brukerstotte/hjelp-til-personbruker?kap=398749',
     innsyn: 'https://foreldrepenger.nav.no',
-    morsAktivitetskrav: 'https://familie.nav.no/om-foreldrepenger',
+    morsAktivitetskrav: 'https://www.nav.no/foreldrepenger-etter-oktober#hvor-lenge',
     frilanserInfoBoks:
         'https://www.skatteetaten.no/bedrift-og-organisasjon/arbeidsgiver/a-meldingen/veiledning/arbeidsforholdet/type-arbeidsforhold/frilanser-oppdragstaker-og-personer-som-mottar-honorarer/',
     NaringsdrivendeInfoBoks:
         'https://www.skatteetaten.no/bedrift-og-organisasjon/starte-og-drive/er-jeg-naringsdrivende/',
     brukerprofil: 'https://tjenester.nav.no/brukerprofil/',
     familie: 'https://familie.nav.no/',
-    graderingInfo: 'https://familie.nav.no/om-foreldrepenger#hvis-du-vil-jobbe',
+    graderingInfo: 'https://www.nav.no/foreldrepenger-etter-oktober#arbeid',
     hvordanSendeInntektsmelding:
         'https://www.nav.no/no/bedrift/tjenester-og-skjemaer/nav-og-altinn-tjenester/foreldrepenger-og-svangerskapspenger2',
     omLottOgHyre: 'https://www.skatteetaten.no/rettskilder/type/handboker/skatte-abc/2019/fiske/F-14.014/F-14.048/',
