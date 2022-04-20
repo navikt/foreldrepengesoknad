9d230a7596a382928e9c8bbec926515c2c1c6ead
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-04-20 15:51:16 +0200

    fiks feil med at tom plan med kun annen forelders uttaksplan kan sendes inn
    Signed-off-by: Gabriela Rutkowska <gabriela.rutkowska@nav.no>
    
    Former-commit-id: e2bc56a8254349fee3ae65e355e7fb16e4a410ab

--- src/uttaksplan/validering/tester/inneholderUttaksplanPerioderTest.ts
+++ src/uttaksplan/validering/tester/inneholderUttaksplanPerioderTest.ts
@@ -1,8 +1,12 @@
 import { RegelTestresultat, RegelTest } from 'shared/regler/regelTypes';
 import { isValidTidsperiode } from 'app/steps/uttaksplan-info/utils/Tidsperioden';
 import { Søknadsinfo } from '../utils/types/Søknadsinfo';
+import { isInfoPeriode } from 'uttaksplan/types/Periode';
 export const inneholderUttaksplanPerioderTest: RegelTest = (grunnlag: Søknadsinfo): RegelTestresultat => {
     return {
-        passerer: grunnlag.perioder.filter((periode) => isValidTidsperiode(periode.tidsperiode)).length > 0,
+        passerer:
+            grunnlag.perioder
+                .filter((p) => !isInfoPeriode(p))
+                .filter((periode) => isValidTidsperiode(periode.tidsperiode)).length > 0,
     };
 };
