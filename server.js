aded988441ed353ffce5d0ad0cd2613dfabe3778
Andreas Dybdahl <andreas.dybdahl@nav.no>
2022-04-22 15:19:22 +0200

    Sonarcloud

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.set('views', `${__dirname}/dist`);
 server.set('view engine', 'mustache');
 server.engine('html', mustacheExpress());
 
-server.use((req, res, next) => {
+server.use((_req, res, next) => {
     res.removeHeader('X-Powered-By');
     res.set('X-Frame-Options', 'SAMEORIGIN');
     res.set('X-XSS-Protection', '1; mode=block');
