58bbf4f09fb2866666e3c2da43dc68d52ab7a53c
Andreas Dybdahl <andreas.dybdahl@nav.no>
2022-05-04 11:37:37 +0200

    Dumpe mellomlagrede data om man er på en rute som ikke finnes i endringssøknad

---
+++ src/app/utils/mellomlagringUtils.test.ts
@@ -0,0 +1,82 @@
+import { ForeldrepengesøknadContextState } from 'app/context/ForeldrepengesøknadContextConfig';
+import SøknadRoutes from 'app/routes/routes';
+import { shouldApplyStorage } from './mellomlagringUtils';
+
+describe('Test av mellomlagring', () => {
+    it('Burde ikke bruke mellomlagrede data hvis versjon ikke er lik current version', () => {
+        const result = shouldApplyStorage({
+            version: 3,
+            søknad: {},
+        } as ForeldrepengesøknadContextState);
+
+        expect(result).toBe(false);
+    });
+
+    it('Burde bruke mellomlagrede data hvis versjon er lik current version', () => {
+        const result = shouldApplyStorage({ version: 4, søknad: {} } as ForeldrepengesøknadContextState);
+
+        expect(result).toBe(true);
+    });
+
+    it('Burde ikke bruke mellomlagrede data hvis currentRoute er en rute som ikke finnes for endringssøknad', () => {
+        let result = shouldApplyStorage({
+            version: 4,
+            currentRoute: SøknadRoutes.SØKERSITUASJON,
+            søknad: {
+                erEndringssøknad: true,
+            },
+        } as ForeldrepengesøknadContextState);
+
+        expect(result).toBe(false);
+
+        result = shouldApplyStorage({
+            version: 4,
+            currentRoute: SøknadRoutes.OM_BARNET,
+            søknad: {
+                erEndringssøknad: true,
+            },
+        } as ForeldrepengesøknadContextState);
+
+        expect(result).toBe(false);
+
+        result = shouldApplyStorage({
+            version: 4,
+            currentRoute: SøknadRoutes.ANNEN_FORELDER,
+            søknad: {
+                erEndringssøknad: true,
+            },
+        } as ForeldrepengesøknadContextState);
+
+        expect(result).toBe(false);
+
+        result = shouldApplyStorage({
+            version: 4,
+            currentRoute: SøknadRoutes.UTTAKSPLAN_INFO,
+            søknad: {
+                erEndringssøknad: true,
+            },
+        } as ForeldrepengesøknadContextState);
+
+        expect(result).toBe(false);
+
+        result = shouldApplyStorage({
+            version: 4,
+            currentRoute: SøknadRoutes.UTENLANDSOPPHOLD,
+            søknad: {
+                erEndringssøknad: true,
+            },
+        } as ForeldrepengesøknadContextState);
+
+        expect(result).toBe(false);
+
+        result = shouldApplyStorage({
+            version: 4,
+            currentRoute: SøknadRoutes.INNTEKTSINFORMASJON,
+            søknad: {
+                erEndringssøknad: true,
+            },
+        } as ForeldrepengesøknadContextState);
+
+        expect(result).toBe(false);
+    });
+});
