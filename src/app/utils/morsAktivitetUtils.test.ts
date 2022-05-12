9e3d7841816c1ea12c291ad6a0b21cd7952aebd1
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-05-12 13:31:29 +0200

    bugfix - sette mors aktivitet til TRENGER_HJELP hvis far svarer ja om at mor er syk eller innlagt
    Signed-off-by: Gabriela Rutkowska <gabriela.rutkowska@nav.no>
    
    Former-commit-id: 807f9728bcc51fcaf67b23182cc8e6a01c26e4ad

--- src/app/utils/morsAktivitetUtils.test.ts
+++ src/app/utils/morsAktivitetUtils.test.ts
@@ -1,7 +1,7 @@
 import AnnenForelder, { AnnenForelderOppgitt } from 'app/context/types/AnnenForelder';
 import { Skjemanummer } from 'app/types/Skjemanummer';
 import { MorsAktivitet } from 'uttaksplan/types/MorsAktivitet';
-import { aktivitetskravMorUtil, getMorsAktivitetSkjemanummer } from './morsAktivitetUtils';
+import { aktivitetskravMorUtil, getMorsAktivitet, getMorsAktivitetSkjemanummer } from './morsAktivitetUtils';
 
 describe('Aktivitetskrav ved utsettelse', () => {
     it('should be required if søkerErFarEllerMedmor and !annenForelderHarRettPåFP && !ufør', () => {
@@ -73,5 +73,23 @@ describe('getMorsAktivitetSkjemanummer', () => {
 
     it('should require Skjemanummer.BEKREFTELSE_FRA_STUDIESTED when morsAktivitet=Utdanning', () => {
         expect(getMorsAktivitetSkjemanummer(MorsAktivitet.Utdanning)).toBe(Skjemanummer.BEKREFTELSE_FRA_STUDIESTED);
+    });
+});
+
+describe('getMorsAktivitet', () => {
+    it('should return undefined for no input on morsaktivitet or no input on mor er for syk', () => {
+        expect(getMorsAktivitet(undefined, undefined)).toBeUndefined();
+    });
+
+    it('should return mors aktivitet if there is input on mors aktivitet', () => {
+        expect(getMorsAktivitet(MorsAktivitet.Innlagt, undefined)).toEqual(MorsAktivitet.Innlagt);
+    });
+
+    it('should return mors aktivitet = TRENGER HJELP for no input on mors aktivitet and input that mor er syk', () => {
+        expect(getMorsAktivitet(undefined, true)).toEqual(MorsAktivitet.TrengerHjelp);
+    });
+
+    it('should return undefined if no input on mors aktivitet and mor er syk is false', () => {
+        expect(getMorsAktivitet(undefined, false)).toBeUndefined();
     });
 });
