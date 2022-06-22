9539d0c93c02f119c4de5ac087dbb2b3d9a84707
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-06-22 17:56:40 +0200

    Oppdaterer infotekst med link

--- src/uttaksplan/validering/tester/farMedmorHarRettPåForeldrepengerUtenAktivitetskravTest.tsx
+++ src/uttaksplan/validering/tester/farMedmorHarRettPåForeldrepengerUtenAktivitetskravTest.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import { RegelTest, RegelTestresultat } from '../utils/types/regelTypes';
 import { Søknadsinfo } from '../utils/types/Søknadsinfo';
 import { erUttaksmengdeForFarMedmorForHøyTest } from './erUttaksmengdeForFarMedmorForHøyTest';
 import { andreAugust2022ReglerGjelder } from 'app/utils/dateUtils';
+import links from 'app/links/links';
+import { IntlShape } from 'react-intl';
 
 export const farMedmorHarRettPåForeldrepengerUtenAktivitetskravTest: RegelTest = (
     grunnlag: Søknadsinfo
@@ -20,13 +23,19 @@ export const farMedmorHarRettPåForeldrepengerUtenAktivitetskravTest: RegelTest 
         kontoUtenAktivitetskravUker = 8;
     }
 
-    console.log('kontoUtenAktivitetskravUker', kontoUtenAktivitetskravUker);
     return {
         passerer: kontoUtenAktivitetskravUker === 0,
         info: {
             intlKey: 'uttaksplan.validering.info.rettTilAktivitetsfriUttak',
+            renderAsHtml: true,
             values: {
                 antallUker: kontoUtenAktivitetskravUker,
+                a: (_intl: IntlShape) => (msg: any) =>
+                    (
+                        <a href={links.aktivitetsfriUttakInfo} className="lenke" rel="noreferrer" target="_blank">
+                            {msg}
+                        </a>
+                    ),
             },
         },
     };
