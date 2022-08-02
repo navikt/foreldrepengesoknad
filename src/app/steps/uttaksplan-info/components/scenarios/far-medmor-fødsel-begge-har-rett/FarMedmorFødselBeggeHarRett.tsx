9405666962e186755247b1d6b70cc9ca28856cfc
Andreas Dybdahl <andreas.dybdahl@nav.no>
2022-08-02 13:03:19 +0200

    Fiks

--- src/app/steps/uttaksplan-info/components/scenarios/far-medmor-fødsel-begge-har-rett/FarMedmorFødselBeggeHarRett.tsx
+++ src/app/steps/uttaksplan-info/components/scenarios/far-medmor-fødsel-begge-har-rett/FarMedmorFødselBeggeHarRett.tsx
@@ -107,7 +107,7 @@ const FarMedmorFødselFørsteganggsøknadBeggeHarRett: FunctionComponent<Props> 
                     },
                     bareFarMedmorHarRett: false,
                     termindato,
-                    harAktivitetskravIPeriodeUtenUttak: true,
+                    harAktivitetskravIPeriodeUtenUttak: false,
                 })
             ),
         ];
