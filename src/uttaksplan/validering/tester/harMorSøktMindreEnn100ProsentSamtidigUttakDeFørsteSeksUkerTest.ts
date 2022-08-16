8f26ad7435bb8d5ae574b1be5558244c639b94cb
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-08-16 11:58:04 +0200

    bugfix med validering som skal kun kjøres ved fødsel

--- src/uttaksplan/validering/tester/harMorSøktMindreEnn100ProsentSamtidigUttakDeFørsteSeksUkerTest.ts
+++ src/uttaksplan/validering/tester/harMorSøktMindreEnn100ProsentSamtidigUttakDeFørsteSeksUkerTest.ts
@@ -3,12 +3,13 @@ import { Søknadsinfo } from '../utils/types/Søknadsinfo';
 import dayjs from 'dayjs';
 import { RegelTest, RegelTestresultat } from '../utils/types/regelTypes';
 import { isUttaksperiode } from 'uttaksplan/types/Periode';
+import Søkersituasjon from 'app/steps/søkersituasjon/Søkersituasjon';
 
 export const harMorSøktMindreEnn100ProsentSamtidigUttakDeFørsteSeksUkerTest: RegelTest = (
     grunnlag: Søknadsinfo
 ): RegelTestresultat => {
     const søkerErMor = !grunnlag.søkerErFarEllerMedmor;
-    if (søkerErMor) {
+    if (søkerErMor && grunnlag.søkersituasjon.situasjon === 'fødsel') {
         const sisteUttaksdagEtterSeksUker = getSisteUttaksdag6UkerEtterFødsel(grunnlag.familiehendelsesdato);
         const perioderMedSamtidigUttakUnder100ProsentInnenFørsteSeksUker = grunnlag.perioder.filter(
             (p) =>
