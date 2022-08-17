afc2d12e8560e203b753b13df2629b838f51f046
Andreas Dybdahl <andreas.dybdahl@nav.no>
2022-08-17 11:20:37 +0200

    Storybook fiks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,68 +1,75 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
-  stories: ['../src/storybook/stories/**/*.stories.@(tsx)'],
-  addons: ['@storybook/addon-essentials', 'storybook-formik/register'],
-  webpackFinal: async (config, { configType }) => {
-    //Fjern default svg-loader
-    config.module.rules = config.module.rules.map( data => {
-      if (/svg\|/.test(String(data.test))) {
-        data.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
-      }
-      return data;
-    });
+    core: {
+        builder: 'webpack5',
+    },
+    features: {
+        storyStoreV7: true,
+    },
+    stories: ['../src/storybook/stories/**/*.stories.tsx'],
+    addons: ['@storybook/addon-essentials', 'storybook-formik/register'],
+    webpackFinal: async (config, { configType }) => {
+        //Fjern default svg-loader
+        config.module.rules = config.module.rules.map((data) => {
+            if (/svg\|/.test(String(data.test))) {
+                data.test = /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani)(\?.*)?$/;
+            }
+            return data;
+        });
 
-    config.devtool = 'source-map';
+        config.devtool = 'source-map';
 
-    // Make whatever fine-grained changes you need
-    config.module.rules = config.module.rules.concat({
-      test: /\.(tsx?|ts?)$/,
-      enforce: 'pre',
-      loader: 'eslint-loader',
-      options: {
-        failOnWarning: false,
-        failOnError: false,
-        configFile: path.resolve(__dirname, '../.eslintrc.js'),
-        fix: true,
-        cache: true,
-      },
-      include: [path.resolve(__dirname, '../src')],
-    }, {
-      test: /\.(ts|tsx|js)$/,
-      use: [{ loader: 'babel-loader' }],
-      exclude: /node_modules/,
-    }, {
-      test: /\.less$/,
-      use: [
-          {
-              loader: MiniCssExtractPlugin.loader,
-          },
-          'css-loader',
-          {
-              loader: 'less-loader',
-          },
-      ],
-    }, {
-      test: /\.svg$/,
-      use: { loader: 'svg-sprite-loader', options: {} },
-    });
+        // Make whatever fine-grained changes you need
+        config.module.rules = config.module.rules.concat(
+            {
+                test: /\.(ts|tsx|js)$/,
+                use: [{ loader: 'babel-loader' }],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    {
+                        loader: 'less-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: { loader: 'svg-sprite-loader', options: {} },
+            }
+        );
 
-    config.plugins.push(new MiniCssExtractPlugin({
-      filename: 'css/[name].css?[hash]-[chunkhash]-[contenthash]-[name]',
-    }));
-    
-    config.resolve.extensions.push('.ts', '.tsx', '.less');
-    config.resolve.alias = {
-        ...config.resolve.alias,
-        app: path.resolve(__dirname, './../src/app'),
-        uttaksplan: path.resolve(__dirname, './../src/uttaksplan'),
-    }
+        config.plugins.push(
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].css?[hash]-[chunkhash]-[contenthash]-[name]',
+            }),
+            new ESLintPlugin({
+                context: path.resolve(__dirname, '../src'),
+                extensions: ['tsx', 'ts'],
+                failOnWarning: false,
+                failOnError: false,
+                fix: true,
+                overrideConfigFile: path.resolve(__dirname, '../eslintrc.js'),
+                lintDirtyModulesOnly: true,
+            })
+        );
 
-    // Return the altered config
-    return config;
-  },
-}
+        config.resolve.extensions.push('.ts', '.tsx', '.less');
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            app: path.resolve(__dirname, './../src/app'),
+            uttaksplan: path.resolve(__dirname, './../src/uttaksplan'),
+        };
+
+        // Return the altered config
+        return config;
+    },
+};
