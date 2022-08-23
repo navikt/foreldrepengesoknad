e9860f8ab7409d089b43b929caff4aac40c54bae
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-08-23 16:50:37 +0200

    Legger til validering som foreslår gradering til bruker hvis mange vekslende perioder uten uttak og uttaksperioder
    Former-commit-id: 213e2a71be86f7f835a04bb044b72e087db07c69

--- src/uttaksplan/validering/tester/burdeKanskjeSøkeGraderingTest.tsx
+++ src/uttaksplan/validering/tester/burdeKanskjeSøkeGraderingTest.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { RegelTest, RegelTestresultat } from '../utils/types/regelTypes';
 import { IntlShape } from 'react-intl';
-import { isInfoPeriode, isUtsettelsePgaArbeid, isUttaksperiode, Periode } from 'uttaksplan/types/Periode';
+import {
+    isInfoPeriode,
+    isPeriodeUtenUttak,
+    isUtsettelsePgaArbeid,
+    isUttaksperiode,
+    Periode,
+} from 'uttaksplan/types/Periode';
 import links from 'app/links/links';
 import { Søknadsinfo } from '../utils/types/Søknadsinfo';
 
-const primitivGraderingTest = (kunSøkersPerioder: Periode[]): boolean => {
+const vekslendeArbeidOgUttakTest = (kunSøkersPerioder: Periode[]): boolean => {
     const firstArbeidsperiodeIndex = kunSøkersPerioder.findIndex((p) => isUtsettelsePgaArbeid(p));
 
     if (firstArbeidsperiodeIndex > -1) {
@@ -46,15 +52,68 @@ const primitivGraderingTest = (kunSøkersPerioder: Periode[]): boolean => {
     return true;
 };
 
+const vekslendePeriodeUtenUttakOgUttakTest = (kunSøkersPerioder: Periode[]): boolean => {
+    const firstPeriodeUtenUttakIndex = kunSøkersPerioder.findIndex((p) => isPeriodeUtenUttak(p));
+
+    if (firstPeriodeUtenUttakIndex > -1) {
+        const antallPerioderEtterFørstePeriodeUtenUttak = kunSøkersPerioder.length - 1 - firstPeriodeUtenUttakIndex;
+
+        if (antallPerioderEtterFørstePeriodeUtenUttak >= 5) {
+            // Hvis vi har en serie perioder av formatet:
+            // Uten Uttak -> Uttak ->  Uten Uttak -> Uttak ->  Uten Uttak -> Uttak => foreslå gradering
+            if (
+                isPeriodeUtenUttak(kunSøkersPerioder[firstPeriodeUtenUttakIndex]) &&
+                isUttaksperiode(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 1]) &&
+                isPeriodeUtenUttak(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 2]) &&
+                isUttaksperiode(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 3]) &&
+                isPeriodeUtenUttak(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 4]) &&
+                isUttaksperiode(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 5])
+            ) {
+                return false;
+            }
+        }
+        if (antallPerioderEtterFørstePeriodeUtenUttak === 4 && firstPeriodeUtenUttakIndex > 0) {
+            // Hvis vi har en serie perioder av formatet:
+            // Uttak ->  Uten Uttak -> Uttak ->  Uten Uttak -> Uttak ->  Uten Uttak => foreslå gradering
+            if (
+                firstPeriodeUtenUttakIndex > 0 &&
+                isUttaksperiode(kunSøkersPerioder[firstPeriodeUtenUttakIndex - 1]) &&
+                isPeriodeUtenUttak(kunSøkersPerioder[firstPeriodeUtenUttakIndex]) &&
+                isUttaksperiode(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 1]) &&
+                isPeriodeUtenUttak(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 2]) &&
+                isUttaksperiode(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 3]) &&
+                isPeriodeUtenUttak(kunSøkersPerioder[firstPeriodeUtenUttakIndex + 4])
+            ) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
+
 export const burdeKanskjeSøkeGraderingTest: RegelTest = (grunnlag: Søknadsinfo): RegelTestresultat => {
     const { perioder } = grunnlag;
     const kunSøkersPerioder = perioder.filter((p: Periode) => !isInfoPeriode(p));
 
     if (kunSøkersPerioder.length >= 6) {
+        const vekslerArbeidOgUttakTest = vekslendeArbeidOgUttakTest(kunSøkersPerioder);
+        const veklserPerioderUtenUttakOgUttaktest = vekslendePeriodeUtenUttakOgUttakTest(kunSøkersPerioder);
+        const passerer = vekslerArbeidOgUttakTest && veklserPerioderUtenUttakOgUttaktest;
+        let infoTekstKey = undefined;
+        if (!vekslerArbeidOgUttakTest) {
+            infoTekstKey =
+                'uttaksplan.validering.info.burdeKanskjeSøkeGraderingPgaVekslendeUtsettelserPgaArbeidOgUttak';
+        }
+
+        if (!veklserPerioderUtenUttakOgUttaktest) {
+            infoTekstKey = 'uttaksplan.validering.info.burdeKanskjeSøkeGraderingPgaVekslendePerioderUtenUttakOgUttak';
+        }
+
         return {
-            passerer: primitivGraderingTest(kunSøkersPerioder),
+            passerer: passerer,
             info: {
-                intlKey: 'uttaksplan.validering.info.burdeKanskjeSøkeGradering',
+                intlKey: infoTekstKey,
                 renderAsHtml: true,
                 values: {
                     a: (_intl: IntlShape) => (msg: any) =>
