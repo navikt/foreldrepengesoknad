0ed8b40268e062745d8f34885c0bb66048bf878f
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-09-12 11:26:18 +0200

    Redirecter tilbake til uttaksplan fra oppsummering hvis uttaksplan inneholder perioder uten konto
    Former-commit-id: 9e945933b7cdd84d91ed0b07df2cb39ab43c8b47

--- src/app/routes/isAvailable.test.tsx
+++ src/app/routes/isAvailable.test.tsx
@@ -1,6 +1,9 @@
 import SøknadRoutes from './routes';
 import isAvailable from './isAvailable';
 import { Søknad } from 'app/context/types/Søknad';
+import { Periodetype } from 'uttaksplan/types/Periode';
+import { StønadskontoType } from 'uttaksplan/types/StønadskontoType';
+import { Forelder } from 'app/types/Forelder';
 
 describe('<isAvailable>', () => {
     it('skal vere tilgjengelig når rute er SØKERSITUASJON og en har godkjent vilkår', () => {
@@ -19,11 +22,50 @@ describe('<isAvailable>', () => {
         expect(erTilgjengelig).toBe(false);
     });
 
-    it('skal ikke vere tilgjengelig når rute er noe annet enn SØKERSITUASJON', () => {
+    it('skal vere tilgjengelig når rute er OPPSUMMERING og søknad har uttaksplan der alle uttaksperioder har kontoer ', () => {
+        const søknad = {
+            harGodkjentVilkår: true,
+            uttaksplan: [
+                {
+                    type: Periodetype.Uttak,
+                    konto: StønadskontoType.Fedrekvote,
+                    forelder: Forelder.farMedmor,
+                },
+                {
+                    type: Periodetype.Uttak,
+                    konto: StønadskontoType.Fellesperiode,
+                    forelder: Forelder.farMedmor,
+                },
+            ],
+        } as Søknad;
+        const erTilgjengelig = isAvailable(SøknadRoutes.OPPSUMMERING, søknad);
+        expect(erTilgjengelig).toBe(true);
+    });
+    it('skal ikke vere tilgjengelig når rute er OPPSUMMERING og søknad har uttaksplan der noen  uttaksperioder ikke har kontoer ', () => {
+        const søknad = {
+            harGodkjentVilkår: true,
+            uttaksplan: [
+                {
+                    type: Periodetype.Uttak,
+                    konto: StønadskontoType.Mødrekvote,
+                    forelder: Forelder.farMedmor,
+                },
+                {
+                    type: Periodetype.Uttak,
+                    konto: undefined,
+                    forelder: Forelder.farMedmor,
+                },
+            ],
+        } as Søknad;
+        const erTilgjengelig = isAvailable(SøknadRoutes.OPPSUMMERING, søknad);
+        expect(erTilgjengelig).toBe(false);
+    });
+
+    it('skal vere tilgjengelig når rute er noe annet enn SØKERSITUASJON eller OPPSUMMERING', () => {
         const søknad = {
             harGodkjentVilkår: true,
         } as Søknad;
         const erTilgjengelig = isAvailable(SøknadRoutes.OM_BARNET, søknad);
-        expect(erTilgjengelig).toBe(false);
+        expect(erTilgjengelig).toBe(true);
     });
 });
--- src/app/routes/isAvailable.ts
+++ src/app/routes/isAvailable.ts
@@ -1,12 +1,15 @@
 import { Søknad } from 'app/context/types/Søknad';
+import { uttaksplanInneholderPerioderUtenKonto } from 'app/utils/uttaksplan/uttaksplanUtils';
 import SøknadRoutes from './routes';
 
 const isAvailable = (route: SøknadRoutes, søknad: Søknad): boolean => {
     switch (route) {
         case SøknadRoutes.SØKERSITUASJON:
             return søknad.harGodkjentVilkår === true;
+        case SøknadRoutes.OPPSUMMERING:
+            return uttaksplanInneholderPerioderUtenKonto(søknad.uttaksplan) === false;
         default:
-            return false;
+            return true;
     }
 };
 
