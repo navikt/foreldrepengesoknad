144ca5e90dce722f5bd22f8a6c31b2a480702620
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-11-22 19:21:30 +0100

    OppholdÅrsakType bugfix

--- src/uttaksplan/types/OppholdÅrsakType.ts
+++ src/uttaksplan/types/OppholdÅrsakType.ts
@@ -1,6 +1,6 @@
 export enum OppholdÅrsakType {
     UttakFellesperiodeAnnenForelder = 'FELLESPERIODE_ANNEN_FORELDER',
-    UttakFedrekvoteAnnenForelder = ' FEDREKVOTE_ANNEN_FORELDER',
+    UttakFedrekvoteAnnenForelder = 'FEDREKVOTE_ANNEN_FORELDER',
     UttakMødrekvoteAnnenForelder = 'MØDREKVOTE_ANNEN_FORELDER',
     UttakForeldrepengerAnnenForelder = 'FORELDREPENGER_ANNEN_FORELDER',
     UttakForelderpengerFørFødselAnnenForelder = 'FORELDREPENGER_FØR_FØDSEL_ANNEN_FORELDER',
