a0ba13dfe8c7ff06985b13f95c12106fe1466315
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-11-22 19:32:44 +0100

    utvidet MinsterettType med to tette

--- src/app/types/TilgjengeligeStønadskontoerDTO.ts
+++ src/app/types/TilgjengeligeStønadskontoerDTO.ts
@@ -3,6 +3,7 @@ import { MinsterettType } from 'uttaksplan/types/MinsterettType';
 export interface TilgjengeligeMinsterettskontoer {
     [MinsterettType.farRundtFødsel]: number;
     [MinsterettType.generellMinsterett]: number;
+    [MinsterettType.toTette]: number;
 }
 export interface TilgjengeligeStønadskontoerDTO {
     kontoer: {
