da489db0210982a685b756b6b9a027a2ea056aae
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-11-30 11:47:27 +0100

    sorterer valgt barn på fornavn

--- src/app/steps/om-barnet/components/ValgteRegistrerteBarn.tsx
+++ src/app/steps/om-barnet/components/ValgteRegistrerteBarn.tsx
@@ -8,10 +8,18 @@ interface Props {
     valgteBarn: RegistrertBarn[];
 }
 
+const sorterRegistrerteBarn = (b1: RegistrertBarn, b2: RegistrertBarn): number => {
+    if (b1.fornavn === b2.fornavn) {
+        return 0;
+    }
+    return b1.fornavn < b2.fornavn ? -1 : 1;
+};
+
 const ValgteRegistrerteBarn: React.FunctionComponent<Props> = ({ valgteBarn }: Props) => {
+    const sorterteRegistrerteBarn = valgteBarn.sort(sorterRegistrerteBarn);
     return (
         <div>
-            {valgteBarn.map((valgtBarn: RegistrertBarn, index: number) => (
+            {sorterteRegistrerteBarn.map((valgtBarn: RegistrertBarn, index: number) => (
                 <Block padBottom="l" key={index}>
                     <RegistrertePersonalia
                         person={valgtBarn}
