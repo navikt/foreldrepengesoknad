f29c2ce3bb3b50ef566e04e3e0ab936b188458c5
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2022-12-06 10:25:30 +0100

    fikser AppContainer test

--- src/app/AppContainer.test.tsx
+++ src/app/AppContainer.test.tsx
@@ -1,30 +1,29 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import Api from './api/api';
-// import AppContainer from './AppContainer';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Api from './api/api';
+import AppContainer from './AppContainer';
 
-// describe('<AppContainer>', () => {
-//     afterEach(() => {
-//         jest.clearAllMocks();
-//     });
+describe('<AppContainer>', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 
-//     it('skal returnere spinner når data blir hentet', () => {
-//         jest.spyOn(Api, 'useSøkerinfo').mockImplementationOnce(() => ({
-//             søkerinfoData: undefined,
-//             søkerinfoError: null,
-//         }));
-//         jest.spyOn(Api, 'useStoredAppState').mockImplementationOnce(() => ({
-//             storageData: undefined,
-//             storageError: null,
-//         }));
+    it('skal returnere spinner når data blir hentet', () => {
+        jest.spyOn(Api, 'useSøkerinfo').mockImplementationOnce(() => ({
+            søkerinfoData: undefined,
+            søkerinfoError: null,
+        }));
+        jest.spyOn(Api, 'useGetSaker').mockImplementationOnce(() => ({
+            sakerData: undefined,
+            sakerError: null,
+        }));
+        jest.spyOn(Api, 'useStoredAppState').mockImplementationOnce(() => ({
+            storageData: undefined,
+            storageError: null,
+        }));
 
-//         render(<AppContainer />);
+        render(<AppContainer />);
 
-//         expect(screen.getByText('Venter...')).toBeInTheDocument();
-//     });
-// });
-describe('Test', () => {
-    it('test', async () => {
-        expect(1).toEqual(1);
+        expect(screen.getByText('Venter...')).toBeInTheDocument();
     });
 });
