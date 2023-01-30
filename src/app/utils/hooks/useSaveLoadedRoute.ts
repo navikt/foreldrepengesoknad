65c390610b0c381860e5c22ef79fe53283630c70
Andreas Dybdahl <andreas.dybdahl@nav.no>
2023-01-30 16:08:51 +0100

    Hook fiks

--- src/app/utils/hooks/useSaveLoadedRoute.ts
+++ src/app/utils/hooks/useSaveLoadedRoute.ts
@@ -1,28 +1,25 @@
 import actionCreator from '../../context/action/actionCreator';
 import SøknadRoutes from '../../routes/routes';
 import { useForeldrepengesøknadContext } from '../../context/hooks/useForeldrepengesøknadContext';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { storeAppState } from '../submitUtils';
 import { redirectToLogin } from '../redirectToLogin';
 
 const useSaveLoadedRoute = (currentRoute: SøknadRoutes) => {
     const { dispatch, state } = useForeldrepengesøknadContext();
-    const [currentIsSet, setCurrentIsSet] = useState(false);
+    const routeHasBeenSaved = useRef(false);
 
     useEffect(() => {
-        if (!currentIsSet) {
+        if (!routeHasBeenSaved.current) {
+            routeHasBeenSaved.current = true;
             dispatch(actionCreator.updateCurrentRoute(currentRoute));
-            storeAppState(state)
-                .then(() => {
-                    setCurrentIsSet(true);
-                })
-                .catch((error) => {
-                    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-                        redirectToLogin();
-                    }
-                });
+            storeAppState({ ...state, currentRoute }).catch((error) => {
+                if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+                    redirectToLogin();
+                }
+            });
         }
-    }, [currentRoute, dispatch, state, currentIsSet]);
+    }, [currentRoute, dispatch, state]);
 };
 
 export default useSaveLoadedRoute;
