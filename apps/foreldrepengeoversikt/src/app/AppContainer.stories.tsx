3803b7ad79d3a73ee50cd3143db929e44fcfed76
tor-nav <tor.roneid@nav.no>
2023-03-28 10:43:27 +0200

    Lagt til storybook for foreldrepengeroversikt
    Former-commit-id: 39527031c13ec488472eecb8c65ca392c9939480

---
+++ apps/foreldrepengeoversikt/src/app/AppContainer.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Story } from '@storybook/react';
+import MockAdapter from 'axios-mock-adapter';
+
+import søkerinfo from '../../mock-api/mock_data/sokerinfo.json';
+import saker from '../../mock-api/mock_data/saker.json';
+import dokumenter from '../../mock-api/mock_data/dokumenter.json';
+import annenPartsVedtak from '../../mock-api/mock_data/annenPartsVedtak.json';
+import tidslinjeHendelser from '../../mock-api/mock_data/tidslinjeHendelser.json';
+import miniDialog from '../../mock-api/mock_data/miniDialog.json';
+import manglendeVedlegg from '../../mock-api/mock_data/manglendeVedlegg.json';
+
+import AppContainer from './AppContainer';
+import { AxiosInstance } from './api/apiInterceptor';
+
+import '@navikt/ds-css';
+
+export default {
+  title: 'AppContainer',
+  component: AppContainer,
+};
+
+const Template: Story<any> = () => {
+  const apiMock = new MockAdapter(AxiosInstance);
+  apiMock.onGet('/sokerinfo').reply(200, søkerinfo);
+  apiMock.onGet('/innsyn/v2/saker').reply(200, saker);
+  apiMock.onGet('/innsyn/v2/annenPartVedtak').reply(200, annenPartsVedtak);
+  apiMock.onGet('/dokument/alle').reply(200, dokumenter);
+  apiMock.onGet('/innsyn/tidslinje').reply(200, tidslinjeHendelser);
+  apiMock.onGet('/minidialog').reply(200, miniDialog);
+  apiMock.onGet('/historikk/vedlegg').reply(200, manglendeVedlegg);
+
+  apiMock.onPost('/soknad/ettersen').reply(200, {});
+
+  return (
+    <AppContainer />
+  )
+  };
+
+export const VisApp = Template.bind({});
