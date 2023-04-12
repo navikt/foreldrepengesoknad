2bd1722b13198c31fcc2fcb8ddc0204fbd528410
tor-nav <tor.roneid@nav.no>
2023-04-12 16:01:56 +0200

    Diverse storybook-fiksar
    Former-commit-id: c93b073f843c63bdec7cf5d6dfe796c77b3497d5

--- apps/foreldrepengesoknad/src/app/steps/uttaksplan-info/MorFarAdopsjon.stories.tsx
+++ apps/foreldrepengesoknad/src/app/steps/uttaksplan-info/MorFarAdopsjon.stories.tsx
@@ -24,7 +24,7 @@ import UttaksplanInfoTestData from './uttaksplanInfoTestData';
 import UttaksplanInfo from './UttaksplanInfo';
 
 const UTTAKSPLAN_ANNEN_URL = '/innsyn/v2/annenPartVedtak';
-const STØNADSKONTO_URL = '/uttak-url/konto';
+const STØNADSKONTO_URL = '/undefined/konto';
 
 const contextMorSøkerAdopsjon = _contextMorSøkerAdopsjon as any;
 const contextFarSøkerAdopsjon = _contextFarSøkerAdopsjon as any;
