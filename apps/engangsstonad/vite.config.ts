73bed47b42cd4ec0e00852df7a48b8bf0193c543
Tor Røneid <tor.roneid@nav.no>
2023-04-12 15:20:36 +0200

    Oppdatert DEPS + storybook 7 (#3875)

---
+++ apps/engangsstonad/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        react({
+            include: '**/*.{jsx,tsx}',
+        }),
+    ],
+    build: {
+        sourcemap: true,
+    },
+    resolve: {
+        alias: {
+            app: path.resolve(__dirname, './src/app'),
+            uttaksplan: path.resolve(__dirname, './src/uttaksplan'),
+            assets: path.resolve(__dirname, './src/app/assets/'),
+            components: path.resolve(__dirname, './src/app/components/'),
+            styles: path.resolve(__dirname, './src/app/styles/'),
+            util: path.resolve(__dirname, './src/app/util/'),
+            common: path.resolve(__dirname, './src/common/'),
+            intl: path.resolve(__dirname, './src/app/intl/'),
+        },
+    },
+    optimizeDeps: {
+        include: ['@navikt/fp-common'],
+    },
+    server: {
+        port: 8080,
+    },
+});
---
+++ apps/svangerskapspengesoknad/.storybook/main.ts
@@ -0,0 +1,13 @@
+const config = {
+    stories: ['../src/**/*.stories.@(ts|tsx)'],
+    framework: {
+      name: '@storybook/react-vite',
+      options: {},
+    },
+    docs: {
+      autodocs: false,
+    },
+  };
+  
+  export default config;
+  
