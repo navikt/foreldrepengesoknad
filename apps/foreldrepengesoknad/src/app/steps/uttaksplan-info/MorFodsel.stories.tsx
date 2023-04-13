d9074964f1cd2c09ac0dde3aa03a02d235691f99
tor-nav <tor.roneid@nav.no>
2023-04-13 09:39:21 +0200

    Fiks oppsett av environment i storybook
    Former-commit-id: c9d4cf3095ad802abbdd84fb11de91ecc287aa55

--- apps/foreldrepengesoknad/src/app/steps/uttaksplan-info/MorFodsel.stories.tsx
+++ apps/foreldrepengesoknad/src/app/steps/uttaksplan-info/MorFodsel.stories.tsx
@@ -26,7 +26,7 @@ import UttaksplanInfoTestData from './uttaksplanInfoTestData';
 import UttaksplanInfo from './UttaksplanInfo';
 
 const UTTAKSPLAN_ANNEN_URL = '/innsyn/v2/annenPartVedtak';
-const STØNADSKONTO_URL = '/undefined/konto';
+const STØNADSKONTO_URL = '/uttak-url/konto';
 
 const søkerinfo = _søkerinfo as any;
 const context = _context as any;
