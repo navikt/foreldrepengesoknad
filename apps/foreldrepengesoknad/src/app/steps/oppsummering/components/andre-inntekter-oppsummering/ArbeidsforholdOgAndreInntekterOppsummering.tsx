8a79ab7979390a2a6c912f30c2ed35d1a593cad9
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2023-04-17 12:59:34 +0200

    FPS - vise kun aktive arbeidsforhold i oppsummeringen

--- apps/foreldrepengesoknad/src/app/steps/oppsummering/components/andre-inntekter-oppsummering/ArbeidsforholdOgAndreInntekterOppsummering.tsx
+++ apps/foreldrepengesoknad/src/app/steps/oppsummering/components/andre-inntekter-oppsummering/ArbeidsforholdOgAndreInntekterOppsummering.tsx
@@ -8,17 +8,24 @@ import OppsummeringsPunkt from '../OppsummeringsPunkt';
 import AndreInntekterOppsummering from './AndreInntekterOppsummering';
 import FrilansOppsummering from './FrilansOppsummering';
 import SelvstendigNæringsdrivendeOppsummering from './SelvstendigNæringsdrivendeOppsummering';
+import useSøknad from 'app/utils/hooks/useSøknad';
+import { getFamiliehendelsedato } from 'app/utils/barnUtils';
+import { getAktiveArbeidsforhold } from 'app/utils/arbeidsforholdUtils';
+import { ISOStringToDate } from 'app/utils/dateUtils';
 
 const ArbeidsforholdOgAndreInntekterOppsummering: FunctionComponent = () => {
     const intl = useIntl();
     const { arbeidsforhold } = useSøkerinfo();
-    const harArbeidsforhold = arbeidsforhold !== undefined && arbeidsforhold.length > 0;
+    const { barn } = useSøknad();
+    const familiehendelsesdato = getFamiliehendelsedato(barn);
+    const aktiveArbeidsForhold = getAktiveArbeidsforhold(arbeidsforhold, ISOStringToDate(familiehendelsesdato));
+    const harArbeidsforhold = aktiveArbeidsForhold !== undefined && aktiveArbeidsForhold.length > 0;
 
     return (
         <>
             <OppsummeringsPunkt title={intlUtils(intl, 'oppsummering.inntekt.registrerteArbeidsforhold')}>
                 <HarIkkeArbeidsforhold harArbeidsforhold={harArbeidsforhold} />
-                <HarArbeidsforhold harArbeidsforhold={harArbeidsforhold} arbeidsforhold={arbeidsforhold} />
+                <HarArbeidsforhold harArbeidsforhold={harArbeidsforhold} arbeidsforhold={aktiveArbeidsForhold} />
             </OppsummeringsPunkt>
             <FrilansOppsummering />
             <SelvstendigNæringsdrivendeOppsummering />
