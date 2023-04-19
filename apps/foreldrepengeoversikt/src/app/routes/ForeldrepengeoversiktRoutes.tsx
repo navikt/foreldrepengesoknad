0a3ec8253f996248f4c27f05ac3a4bc35398c7cc
Andreas Dybdahl <andreas.dyb@gmail.com>
2023-04-19 16:13:06 +0200

    Kommentar
    Former-commit-id: a9e6364166372f75b599451a820f161bddd505a8

--- apps/foreldrepengeoversikt/src/app/routes/ForeldrepengeoversiktRoutes.tsx
+++ apps/foreldrepengeoversikt/src/app/routes/ForeldrepengeoversiktRoutes.tsx
@@ -63,6 +63,7 @@ const ForeldrepengeoversiktRoutes: React.FunctionComponent<Props> = ({
     const minidialogerIds = minidialogerData ? minidialogerData.map((oppgave) => oppgave.dialogId) : [];
     const grupperteSaker = grupperSakerPåBarn(søkerinfo.søker.barn, saker);
     const alleYtelser = getAlleYtelser(saker);
+    // Super spesifikt case for avslåtte papirsøknad for svangerskapspenger. Bør fjernes
     const avslåttSvangerskapspengesak =
         grupperteSaker.length === 0 && alleYtelser.length === 1 && saker.svangerskapspenger.length === 1
             ? saker.svangerskapspenger[0]
