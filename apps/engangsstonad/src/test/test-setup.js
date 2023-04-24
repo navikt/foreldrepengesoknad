1b7843cd64835f5b49d4082f566d3e4df6a003c0
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2023-04-24 14:26:21 +0200

    FPS og ES testconfig

--- apps/engangsstonad/src/test/test-setup.js
+++ apps/engangsstonad/src/test/test-setup.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import '@testing-library/jest-dom';
-
+global.React = React;
 // eslint-disable-next-line no-undef
 window.scrollTo = () => ({});
 // eslint-disable-next-line no-undef
--- apps/foreldrepengesoknad/jest/setup.ts
+++ apps/foreldrepengesoknad/jest/setup.ts
@@ -5,6 +5,9 @@ import '@formatjs/intl-datetimeformat/polyfill-force';
 import '@formatjs/intl-datetimeformat/locale-data/nb';
 import '@formatjs/intl-numberformat/polyfill-force';
 import '@formatjs/intl-numberformat/locale-data/nb';
+import React from 'react';
+
+global.React = React;
 
 jest.mock('./../src/app/Environment.ts', () => ({
     REST_API_URL: 'test',
