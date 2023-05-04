919ac2eaf76383e3afd3ff8d374cfde5b45d0d58
Andreas Dybdahl <andreas.dyb@gmail.com>
2023-05-04 18:26:59 +0200

    Added env variable to decorator
    Former-commit-id: 57b7ad324956ce4780862e9dcb2793789aab1a7c

--- apps/svangerskapspengesoknad/src/build/scripts/decorator.cjs
+++ apps/svangerskapspengesoknad/src/build/scripts/decorator.cjs
@@ -20,6 +20,11 @@ const getDecorator = () =>
                     NAV_STYLES: document.getElementById('styles')[prop],
                     NAV_HEADING: document.getElementById('header-withmenu')[prop],
                     NAV_FOOTER: document.getElementById('footer-withmenu')[prop],
+                    APP_SETTINGS: JSON.stringify({
+                        REST_API_URL: `${process.env.FORELDREPENGESOKNAD_API_URL}`,
+                        LOGIN_URL: `${process.env.LOGINSERVICE_URL}`,
+                        LOG_VALIDATION: `${process.env.LOG_VALIDATION}`,
+                    }),
                 };
                 resolve(data);
             } else {
