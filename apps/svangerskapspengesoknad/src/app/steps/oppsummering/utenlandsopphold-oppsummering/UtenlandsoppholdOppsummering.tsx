c7d926911921a080aea696d02872edb8c234e75d
Andreas Dybdahl <andreas.dyb@gmail.com>
2023-09-06 16:12:06 +0200

    Fikset på tekst og visning av utenlandsopphold i oppsummering

--- apps/svangerskapspengesoknad/src/app/steps/oppsummering/utenlandsopphold-oppsummering/UtenlandsoppholdOppsummering.tsx
+++ apps/svangerskapspengesoknad/src/app/steps/oppsummering/utenlandsopphold-oppsummering/UtenlandsoppholdOppsummering.tsx
@@ -8,58 +8,20 @@ interface Props {
     informasjonOmUtenlandsopphold: InformasjonOmUtenlandsopphold;
 }
 
-// const getErINorgePåFamiliehendelsedato = (
-//     familiehendelsedato: string,
-//     tidligereOpphold: Utenlandsopphold[],
-//     senereOpphold: Utenlandsopphold[]
-// ): boolean => {
-//     let erINorge = true;
-
-//     tidligereOpphold.forEach((tidOpphold) => {
-//         if (doesTidsperiodeContainDate(tidOpphold.tidsperiode, familiehendelsedato)) {
-//             erINorge = false;
-//         }
-//     });
-
-//     senereOpphold.forEach((senOpphold) => {
-//         if (doesTidsperiodeContainDate(senOpphold.tidsperiode, familiehendelsedato)) {
-//             erINorge = false;
-//         }
-//     });
-
-//     return erINorge;
-// };
-
 const UtenlandsoppholdOppsummering: FunctionComponent<Props> = ({ informasjonOmUtenlandsopphold }) => {
-    // const { senereOpphold, tidligereOpphold } = informasjonOmUtenlandsopphold;
-    // const familiehendelsedato = getFamiliehendelsedato(barn);
-    // const erINorgePåFamiliehendelsedato = getErINorgePåFamiliehendelsedato(
-    //     familiehendelsedato,
-    //     tidligereOpphold,
-    //     senereOpphold
-    // );
-
     return (
         <>
-            <Block margin="m" padBottom="l">
-                <BodyShort>
-                    {informasjonOmUtenlandsopphold.iNorgeSiste12Mnd
-                        ? 'Du har bodd i Norge de siste 12 månedene'
-                        : 'Du har oppholdt deg i utlandet i de siste 12 månedene'}
-                </BodyShort>
+            <Block padBottom="m">
+                <UtenlandsoppholdListe
+                    utenlandsopphold={informasjonOmUtenlandsopphold.tidligereOpphold}
+                    tidligereOpphold={true}
+                />
             </Block>
-            <Block padBottom="xl">
-                <UtenlandsoppholdListe utenlandsopphold={informasjonOmUtenlandsopphold.tidligereOpphold} />
-            </Block>
-            <Block padBottom="l">
-                <BodyShort>
-                    {informasjonOmUtenlandsopphold.iNorgeNeste12Mnd
-                        ? 'Du skal bo i Norge de neste 12 månedene'
-                        : 'Du skal oppholde deg i utlandet de neste 12 månedene'}
-                </BodyShort>
-            </Block>
-            <Block padBottom="xl">
-                <UtenlandsoppholdListe utenlandsopphold={informasjonOmUtenlandsopphold.senereOpphold} />
+            <Block padBottom="m">
+                <UtenlandsoppholdListe
+                    utenlandsopphold={informasjonOmUtenlandsopphold.senereOpphold}
+                    tidligereOpphold={false}
+                />
             </Block>
             <Block padBottom="xl">
                 <BodyShort>
--- apps/svangerskapspengesoknad/src/app/steps/oppsummering/utenlandsopphold-oppsummering/UtenlandsoppholdOppsummeringListe.tsx
+++ apps/svangerskapspengesoknad/src/app/steps/oppsummering/utenlandsopphold-oppsummering/UtenlandsoppholdOppsummeringListe.tsx
@@ -3,14 +3,16 @@ import countries from 'i18n-iso-countries';
 import { FunctionComponent } from 'react';
 import { BodyShort, Label } from '@navikt/ds-react';
 import { Utenlandsopphold } from 'app/types/InformasjonOmUtenlandsopphold';
+import { FormattedMessage } from 'react-intl';
 
 import './utenlandsoppholdOppsummeringListe.css';
 
 interface Props {
     utenlandsopphold: Utenlandsopphold[];
+    tidligereOpphold: boolean;
 }
 
-const UtenlandsoppholdOppsummeringListe: FunctionComponent<Props> = ({ utenlandsopphold }) => {
+const UtenlandsoppholdOppsummeringListe: FunctionComponent<Props> = ({ utenlandsopphold, tidligereOpphold }) => {
     const bem = bemUtils('utenlandsoppholdOppsummering');
 
     return (
@@ -20,7 +22,21 @@ const UtenlandsoppholdOppsummeringListe: FunctionComponent<Props> = ({ utenlands
                     <Block padBottom={index !== utenlandsopphold.length - 1 ? 'l' : 'none'}>
                         <div className={bem.block}>
                             <Block padBottom="m">
-                                <Label>{countries.getName(opphold.land, 'nb')}</Label>
+                                {tidligereOpphold ? (
+                                    <Label>
+                                        <FormattedMessage
+                                            id="oppsummering.utenlandsopphold.harBoddINorge.utenlands"
+                                            values={{ land: countries.getName(opphold.land, 'nb') }}
+                                        />
+                                    </Label>
+                                ) : (
+                                    <Label>
+                                        <FormattedMessage
+                                            id="oppsummering.utenlandsopphold.skalBoINorge.utenlands"
+                                            values={{ land: countries.getName(opphold.land, 'nb') }}
+                                        />
+                                    </Label>
+                                )}
                             </Block>
                             <BodyShort>
                                 {formatDate(opphold.tidsperiode.fom)} - {formatDate(opphold.tidsperiode.tom)}
