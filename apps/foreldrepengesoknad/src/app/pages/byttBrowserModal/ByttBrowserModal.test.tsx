c5a73ebe21782060af3a17c06d05fa08de9eab7b
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2023-09-27 15:03:46 +0200

    FPS - fikser test som feiler

--- apps/foreldrepengesoknad/src/app/pages/byttBrowserModal/ByttBrowserModal.test.tsx
+++ apps/foreldrepengesoknad/src/app/pages/byttBrowserModal/ByttBrowserModal.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './ByttBrowserModal.stories';
 
@@ -7,6 +7,6 @@ const { Default } = composeStories(stories);
 describe('<ByttBrowserModal>', () => {
     it('skal vise modal for browser bytte', () => {
         render(<Default />);
-        // expect(await screen.findByText(('Vi ser du bruker en utdatert nettleser')).toBeInTheDocument();
+        expect(screen.getByText('Du bruker en utdatert nettleser.')).toBeInTheDocument();
     });
 });
