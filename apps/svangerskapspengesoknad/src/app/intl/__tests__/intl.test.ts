678514fbf85aede36e3bbd9370c6bda7c1fd370b
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2023-10-20 15:51:26 +0200

    tester intl

--- apps/svangerskapspengesoknad/src/app/intl/__tests__/intl.test.ts
+++ apps/svangerskapspengesoknad/src/app/intl/__tests__/intl.test.ts
@@ -1,3 +1,5 @@
+import glob from 'fast-glob';
+import { extract } from '@formatjs/cli-lib';
 const nb = require('../nb_NO.json');
 const nn = require('../nn_NO.json');
 
@@ -10,5 +12,20 @@ describe('intl tests', () => {
 
         expect(missingKeysBokmål.length).toBe(0);
         expect(missingKeysNynorsk.length).toBe(0);
+    });
+
+    //TODO Denne sjekker ikke alle strings. Greier kun å henta en del fra kode, men ikke alle.
+    // Hadde ein henta alle så kunne ein sjekka begge vegar.
+    it('Check that i18n strings in code exists in nb_NO language file', async () => {
+        const files = await glob('src/**/*.{ts,tsx}');
+        const foundTranslations = await extract(files, {
+            idInterpolationPattern: '[sha512:contenthash:base64:6]',
+        });
+
+        const foundKeys = Object.keys(JSON.parse(foundTranslations));
+
+        const missingKeysBokmål = foundKeys.filter((key) => !Object.keys(nb).includes(key));
+        missingKeysBokmål.forEach((key) => console.log('key ' + key + ' not found in nn_NO.json.'));
+        expect(missingKeysBokmål.length).toBe(0);
     });
 });
