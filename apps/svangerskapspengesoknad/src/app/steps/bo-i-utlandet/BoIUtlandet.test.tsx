8741ffe8978735336017ddac36ee5c6f481f39e2
Marthe <marthe.hagen@nav.no>
2023-10-26 13:04:49 +0200

    laget tester til step BoIUtlandet

--- apps/svangerskapspengesoknad/src/app/steps/bo-i-utlandet/BoIUtlandet.test.tsx
+++ apps/svangerskapspengesoknad/src/app/steps/bo-i-utlandet/BoIUtlandet.test.tsx
@@ -2,12 +2,15 @@ import userEvent from '@testing-library/user-event';
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './BoIUtlandet.stories';
 import { render, screen } from '@testing-library/react';
+import dayjs from 'dayjs';
 
 const { OppgirIFortid, OppgirIFremtid } = composeStories(stories);
 
 const NESTE_STEG = 'Neste steg';
 const HVILKET_LAND_HAR_DU_BODD = 'Hvilket land har du bodd i?';
 const HVILKET_LAND_SKAL_DU_BO = 'Hvilket land skal du bo i?';
+const FOM = 'Fra og med';
+const TOM = 'Til og med';
 
 describe('<BoIUtlandet> - oppgir bosted i fortid', () => {
     const user = userEvent.setup();
@@ -28,13 +31,46 @@ describe('<BoIUtlandet> - oppgir bosted i fortid', () => {
         expect(await screen.getAllByText('Du må oppgi en til og med dato.')[0]).toBeInTheDocument();
     });
     it('skal vise feilmelding hvis fra og med dato på feil format', async () => {
-        //TODO
+        render(<OppgirIFortid />);
+
+        expect(await screen.findByText(FOM)).toBeInTheDocument();
+
+        const fraOgMedInput = screen.getByLabelText(FOM);
+        await user.type(fraOgMedInput, 'bla bla');
+        await user.click(screen.getByText(NESTE_STEG));
+
+        expect(
+            await screen.getAllByText('Fra og med dato må være en gyldig dato på formatet dd.mm.åååå.')[0],
+        ).toBeInTheDocument();
     });
     it('skal vise feilmelding hvis til og med dato på feil format', async () => {
-        //TODO
+        render(<OppgirIFortid />);
+
+        expect(await screen.findByText(TOM)).toBeInTheDocument();
+
+        const tilOgMedInput = screen.getByLabelText(TOM);
+        await user.type(tilOgMedInput, 'bla bla');
+        await user.click(screen.getByText(NESTE_STEG));
+
+        expect(
+            await screen.getAllByText('Til og med dato må være en gyldig dato på formatet dd.mm.åååå.')[0],
+        ).toBeInTheDocument();
     });
     it('skal vise feilmelding hvis fra og med dat er etter til og med dato på feil format', async () => {
-        //TODO
+        render(<OppgirIFortid />);
+
+        expect(await screen.findByText(FOM)).toBeInTheDocument();
+        expect(await screen.findByText(TOM)).toBeInTheDocument();
+
+        const fraOgMedInput = screen.getByLabelText(FOM);
+        const tilOgMedInput = screen.getByLabelText(TOM);
+        await user.type(fraOgMedInput, dayjs('2023-12-30').format('DD.MM.YYYY'));
+        await user.type(tilOgMedInput, dayjs('2023-11-30').format('DD.MM.YYYY'));
+        await user.click(screen.getByText(NESTE_STEG));
+
+        expect(
+            await screen.getAllByText('Fra og med dato kan ikke være etter til og med dato.')[0],
+        ).toBeInTheDocument();
     });
     it('skal vise feilmelding hvis man legger til overlappende opphold i utlandet', async () => {
         //TODO
