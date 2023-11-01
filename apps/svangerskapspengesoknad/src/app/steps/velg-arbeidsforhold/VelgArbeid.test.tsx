e54560efcc66524d9c7cbe5efcc0b1937f3f9ce5
Marthe <marthe.hagen@nav.no>
2023-11-01 14:59:15 +0100

    lagt til test på visning av guidepanel på velgArbeid step

--- apps/svangerskapspengesoknad/src/app/steps/velg-arbeidsforhold/VelgArbeid.test.tsx
+++ apps/svangerskapspengesoknad/src/app/steps/velg-arbeidsforhold/VelgArbeid.test.tsx
@@ -8,7 +8,9 @@ const { Default } = composeStories(stories);
 const SØKNAD_TITTEL = 'Søknad om svangerskapspenger';
 const HVOR_SKAL_DU_JOBBE = 'Hvor skal du jobbe mindre eller slutte å jobbe midlertidig på grunn av svangerskapet?';
 const SYKEHUSET = 'Sykehuset i Vestfold';
+const OMSORGSPARTNER = 'Omsorgspartner Vestfold AS';
 const RE = 'Re Kommune';
+const INFO = 'Du vil nå gå gjennom hvert arbeidsforhold og gjøre dette:';
 const NESTE_STEG = 'Neste steg';
 
 describe('<Velg arbeid>', () => {
@@ -45,5 +47,22 @@ describe('<Velg arbeid>', () => {
         render(<Default />);
 
         expect(await screen.queryByText(RE)).not.toBeInTheDocument();
+    });
+    it('skal ikke vise infoboks når kunn ett arbeidsforhold er valgt', async () => {
+        render(<Default />);
+        expect(await screen.findByText(SYKEHUSET)).toBeInTheDocument();
+
+        await user.click(screen.getByText(SYKEHUSET));
+
+        expect(await screen.queryByText(INFO)).not.toBeInTheDocument();
+    });
+    it('skal vise infoboks når mer enn ett arbeidsforhold er valgt', async () => {
+        render(<Default />);
+        expect(await screen.findByText(SYKEHUSET)).toBeInTheDocument();
+
+        await user.click(screen.getByText(SYKEHUSET));
+        await user.click(screen.getByText(OMSORGSPARTNER));
+
+        expect(await screen.findByText(INFO)).toBeInTheDocument();
     });
 });
