a06c9a649a898462247ed26d0880135ade168bb9
Marthe <marthe.hagen@nav.no>
2023-11-01 10:07:53 +0100

    laget tester på egenNæring step

---
+++ apps/svangerskapspengesoknad/src/app/steps/egen-næring/EgenNæring.stories.tsx
@@ -0,0 +1,44 @@
+import { StoryFn } from '@storybook/react';
+import withIntlProvider from 'storybook/decorators/withIntl';
+import withSvangerskapspengerContextProvider from 'storybook/decorators/withSvangerskapspengerContext';
+import withRouterProvider from 'storybook/decorators/withRouter';
+import EgenNæringStep from './EgenNæringStep';
+import SvangerskapspengerStateMock from 'storybook/utils/SvangerskapspengerStateMock';
+import _context from 'storybook/storydata/soknad/soknad.json';
+import { SvangerskapspengerContextState } from 'app/context/SvangerskapspengerContextConfig';
+
+const defaultExport = {
+    title: 'steps/EgenNæringStep',
+    component: EgenNæringStep,
+    decorators: [withIntlProvider, withSvangerskapspengerContextProvider, withRouterProvider],
+};
+
+export default defaultExport;
+
+interface EgenNæringStepStoryProps {
+    contextWithEgenNæring: SvangerskapspengerContextState;
+}
+
+const context = _context as any;
+const contextWithEgenNæring = {
+    ...context,
+    søknad: {
+        ...context.søknad,
+        søker: {
+            ...context.søknad.søker,
+            harJobbetSomSelvstendigNæringsdrivende: true,
+        },
+    },
+} as SvangerskapspengerContextState;
+
+const Template: StoryFn<EgenNæringStepStoryProps> = ({ contextWithEgenNæring }) => {
+    return (
+        <SvangerskapspengerStateMock context={contextWithEgenNæring}>
+            <EgenNæringStep />
+        </SvangerskapspengerStateMock>
+    );
+};
+export const Default = Template.bind({});
+Default.args = {
+    contextWithEgenNæring,
+};
