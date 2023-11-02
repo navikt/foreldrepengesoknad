d5ae119173a408bb023f3a052178a19d1575e028
tor-nav <tor.roneid@nav.no>
2023-11-02 16:41:05 +0100

    div

--- packages/utils-test/index.ts
+++ packages/utils-test/index.ts
@@ -1,2 +1,1 @@
 export { default as withIntlProvider, getIntlDecorator } from './src/storybook/decorators/withIntlProvider';
-export { default as withReactHookForm } from './src/storybook/decorators/withReactHookForm';
