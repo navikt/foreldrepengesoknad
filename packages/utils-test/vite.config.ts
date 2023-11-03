a43e6f23f8e3c281ac64e37926d1beb5132da02c
tor-nav <tor.roneid@nav.no>
2023-11-03 10:17:14 +0100

    div

---
+++ packages/utils-test/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Checker from 'vite-plugin-checker';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), Checker({ typescript: true })],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './vitest/setupTests.ts',
+        deps: {
+            inline: ['@navikt/ds-react'],
+        },
+        coverage: {
+            include: ['src/**/*'],
+            exclude: [],
+        },
+        testTimeout: 10000,
+    },
+});
