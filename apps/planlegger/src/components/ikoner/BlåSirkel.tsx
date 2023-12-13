1a46b1afcd813205a853f7c74d85a5f8eade8a3d
Marthe <marthe.hagen@nav.no>
2023-12-13 16:01:08 +0100

    lagt til ikoner

---
+++ apps/planlegger/src/components/ikoner/BlåSirkel.tsx
@@ -0,0 +1,7 @@
+const BlåSirkel = () => (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="8.19192" cy="8.00051" r="7.69192" fill="#66CBEC" />
+    </svg>
+);
+
+export default BlåSirkel;
