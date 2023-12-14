af0ed6f7f928a3b5bf7ef03bc2848bab7085bdb9
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2023-12-14 16:48:01 +0100

    SVP - padding fix

--- apps/svangerskapspengesoknad/src/app/steps/velg-arbeidsforhold/components/guidepanel/FlereArbeidsforholdGuidePanel.tsx
+++ apps/svangerskapspengesoknad/src/app/steps/velg-arbeidsforhold/components/guidepanel/FlereArbeidsforholdGuidePanel.tsx
@@ -1,11 +1,9 @@
 import { FormattedMessage, useIntl } from 'react-intl';
 import { BodyLong, GuidePanel } from '@navikt/ds-react';
-import Block from '@navikt/fp-common/src/common/components/block/Block';
-import { bemUtils, intlUtils, useDocumentTitle } from '@navikt/fp-common';
+import { Block, intlUtils, useDocumentTitle } from '@navikt/fp-common';
 import { PageKeys, logAmplitudeEvent } from 'app/amplitude/amplitude';
 
 const FlereArbeidsforholdGuidePanel: React.FunctionComponent = () => {
-    const bem = bemUtils('umyndig');
     const intl = useIntl();
     useDocumentTitle(intlUtils(intl, 'søknad.pageheading'));
 
@@ -15,23 +13,19 @@ const FlereArbeidsforholdGuidePanel: React.FunctionComponent = () => {
         pageKey: PageKeys.Umyndig,
     });
     return (
-        <>
-            <div className={bem.block}>
-                <GuidePanel>
-                    <Block>
-                        <BodyLong>
-                            <FormattedMessage id="velgArbeid.guidepanel.tekst" />
-                            <li>
-                                <FormattedMessage id="velgArbeid.guidepanel.liste.del1" />
-                            </li>
-                            <li>
-                                <FormattedMessage id="velgArbeid.guidepanel.liste.del2" />
-                            </li>
-                        </BodyLong>
-                    </Block>
-                </GuidePanel>
-            </div>
-        </>
+        <Block padBottom="xl">
+            <GuidePanel>
+                <BodyLong>
+                    <FormattedMessage id="velgArbeid.guidepanel.tekst" />
+                    <li>
+                        <FormattedMessage id="velgArbeid.guidepanel.liste.del1" />
+                    </li>
+                    <li>
+                        <FormattedMessage id="velgArbeid.guidepanel.liste.del2" />
+                    </li>
+                </BodyLong>
+            </GuidePanel>
+        </Block>
     );
 };
 
