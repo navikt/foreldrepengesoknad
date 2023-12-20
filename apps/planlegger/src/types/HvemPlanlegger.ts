4e2940affe9b438bc752f1cdb8f5f2d003a365b4
Marthe <marthe.hagen@nav.no>
2023-12-20 14:07:15 +0100

    WIP

--- apps/planlegger/src/types/HvemPlanlegger.ts
+++ apps/planlegger/src/types/HvemPlanlegger.ts
@@ -1,48 +1,48 @@
 import HvemPlanlegger from 'steps/hvem-planlegger/HvemPlanleggerSteg';
+import { SøkersituasjonEnum } from './Søkersituasjon';
 
-export type HvemPlanleggerEnum = MorOgFar | MorOgMedmor | FarOgFar | Mor | Far;
+export type HvemPlanlegger = MorOgFar | MorOgMedmor | FarOgFar | Mor | Far;
 
 export type MorOgFar = {
+    type: SøkersituasjonEnum.MOR_OG_FAR;
     navnPåMor: string;
     navnPåFar: string;
 };
 
 export type MorOgMedmor = {
+    type: SøkersituasjonEnum.MOR_OG_MEDMOR;
     navnPåMor: string;
     navnPåMedmor: string;
 };
 
 export type FarOgFar = {
+    type: SøkersituasjonEnum.FAR_OG_FAR;
     navnPåFar: string;
     navnPåMedfar: string;
 };
 
 export type Mor = {
-    navnPåMor: string;
+    type: SøkersituasjonEnum.MOR;
+    navnPåBareMor: string;
 };
 
 export type Far = {
-    navnPåFar: string;
+    type: SøkersituasjonEnum.FAR;
+    navnPåBareFar: string;
 };
 
-export type HvemPlanlegger = MorOgFar | MorOgMedmor | FarOgFar | Mor | Far;
-
-export const morOgFar = (hvemPlanlegger: HvemPlanlegger): hvemPlanlegger is MorOgFar => {
-    if ((hvemPlanlegger as MorOgFar).navnPåMor) {
-        return true;
-    }
-    if ((hvemPlanlegger as MorOgFar).navnPåFar) {
-        return true;
-    }
-    return false;
+export const isMorOgFar = (hvemPlanlegger: HvemPlanlegger): hvemPlanlegger is MorOgFar => {
+    return hvemPlanlegger.type === 'morOgFar';
 };
-
-export const morOgMedmor = (hvemPlanlegger: HvemPlanlegger): hvemPlanlegger is MorOgMedmor => {
-    if ((hvemPlanlegger as MorOgMedmor).navnPåMor) {
-        return true;
-    }
-    if ((hvemPlanlegger as MorOgMedmor).navnPåMedmor) {
-        return true;
-    }
-    return false;
+export const isMorOgMedmor = (hvemPlanlegger: HvemPlanlegger): hvemPlanlegger is MorOgMedmor => {
+    return hvemPlanlegger.type === 'morOgMedmor';
+};
+export const isFarOgFar = (hvemPlanlegger: HvemPlanlegger): hvemPlanlegger is FarOgFar => {
+    return hvemPlanlegger.type === 'farOgFar';
+};
+export const isMor = (hvemPlanlegger: HvemPlanlegger): hvemPlanlegger is Mor => {
+    return hvemPlanlegger.type === 'mor';
+};
+export const isFar = (hvemPlanlegger: HvemPlanlegger): hvemPlanlegger is Far => {
+    return hvemPlanlegger.type === 'far';
 };
