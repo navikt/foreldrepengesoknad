d45e19c1f61b97131826c5ea14255967decf65d4
Gabriela Rutkowska <gabriela.rutkowska@nav.no>
2024-01-04 11:24:35 +0100

    SVP - viser skjema i oppsummering

---
+++ apps/svangerskapspengesoknad/src/app/steps/oppsummering/vedlegg-oppsummering/VedleggOppsummering.tsx
@@ -0,0 +1,47 @@
+import { BodyShort, HStack, Link } from '@navikt/ds-react';
+import { bemUtils, guid, intlUtils } from '@navikt/fp-common';
+import Tilrettelegging, { ArbeidsforholdForTilrettelegging, Arbeidsforholdstype } from 'app/types/Tilrettelegging';
+import { FunctionComponent } from 'react';
+import { IntlShape, useIntl } from 'react-intl';
+import './vedlegg-oppsummering.css';
+import { FileIcon } from '@navikt/aksel-icons';
+
+interface Props {
+    tilrettelegging: Tilrettelegging[];
+}
+
+const getVedleggTittel = (arbeidsforhold: ArbeidsforholdForTilrettelegging, intl: IntlShape) => {
+    switch (arbeidsforhold.type) {
+        case Arbeidsforholdstype.FRILANSER:
+            return intlUtils(intl, 'skjema.legend.frilanser');
+        case Arbeidsforholdstype.SELVSTENDIG:
+            return intlUtils(intl, 'skjema.legend.frilanser');
+        default:
+            return `${intlUtils(intl, 'skjema.legend.virksomhet')} for ${arbeidsforhold.navn}`;
+    }
+};
+
+const VedleggOppsummering: FunctionComponent<Props> = ({ tilrettelegging }) => {
+    const intl = useIntl();
+    const bem = bemUtils('vedlegg-oppsummering');
+    return tilrettelegging.map((t) => {
+        const tittel = getVedleggTittel(t.arbeidsforhold, intl);
+        return (
+            <div className={bem.element('tilrettelegging')} key={t.id}>
+                <BodyShort className={bem.element('tittel')}>{tittel}</BodyShort>
+                {t.vedlegg.map((v) => (
+                    <div key={guid()}>
+                        <HStack>
+                            <Link className={bem.element('link')} href={v.url} target="_blank" icon>
+                                <FileIcon className={bem.element('icon')} title="Opplastet fil" />
+                                <BodyShort> {v.filename}</BodyShort>
+                            </Link>
+                        </HStack>
+                    </div>
+                ))}
+            </div>
+        );
+    });
+};
+
+export default VedleggOppsummering;
