454a5165d2008970402ff9aa3d6c5aba2281f015
Andreas Dybdahl <andreas.dyb@gmail.com>
2024-01-04 20:04:38 +0100

    Dokumentoppsummering uttaksperioder

--- apps/foreldrepengesoknad/src/app/steps/oppsummering/components/dokumentasjon-oppsummering/DokumentasjonOppsummering.tsx
+++ apps/foreldrepengesoknad/src/app/steps/oppsummering/components/dokumentasjon-oppsummering/DokumentasjonOppsummering.tsx
@@ -1,15 +1,23 @@
+import { AttachmentMetadataType } from '@navikt/fp-types/src/AttachmentMetadata';
 import { convertAttachmentsMapToArray } from 'app/api/apiUtils';
 import { VedleggDataType } from 'app/types/VedleggDataType';
 import { FunctionComponent } from 'react';
+import PeriodeDokumentasjonOppsummering from './components/PeriodeDokumentasjonOppsummering';
 
 interface Props {
     vedlegg: VedleggDataType;
 }
-
 const DokumentasjonOppsummering: FunctionComponent<Props> = ({ vedlegg }) => {
     const vedleggArray = convertAttachmentsMapToArray(vedlegg);
+    const perioderDokumentasjon = vedleggArray.filter(
+        (v) => v.dokumenterer && v.dokumenterer.type === AttachmentMetadataType.UTTAK,
+    );
 
-    return <>{vedleggArray.map((v) => v.beskrivelse)}</>;
+    return (
+        <>
+            <PeriodeDokumentasjonOppsummering dokumentasjon={perioderDokumentasjon} />
+        </>
+    );
 };
 
 export default DokumentasjonOppsummering;
--- apps/foreldrepengesoknad/src/app/steps/oppsummering/components/dokumentasjon-oppsummering/components/PeriodeDokumentasjonOppsummering.tsx
+++ apps/foreldrepengesoknad/src/app/steps/oppsummering/components/dokumentasjon-oppsummering/components/PeriodeDokumentasjonOppsummering.tsx
@@ -1,5 +1,18 @@
-const PeriodeDokumentasjonOppsummering = () => {
-    return <>Oppsummering for dokumentasjon av uttaksplanperioder</>;
+import { Heading } from '@navikt/ds-react';
+import { Attachment } from '@navikt/fp-types';
+import { FunctionComponent } from 'react';
+
+interface Props {
+    dokumentasjon: Attachment[];
+}
+
+const PeriodeDokumentasjonOppsummering: FunctionComponent<Props> = ({ dokumentasjon }) => {
+    return (
+        <>
+            <Heading size="xsmall">Dokumentasjon for perioder</Heading>
+            {dokumentasjon.map((d) => d.id)}
+        </>
+    );
 };
 
 export default PeriodeDokumentasjonOppsummering;
