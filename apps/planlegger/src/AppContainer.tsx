33b02eb1dd32664db193a178fc72cd2ea5ede652
Marthe <marthe.hagen@nav.no>
2024-01-12 08:40:40 +0100

    lagt til locale

--- apps/planlegger/src/AppContainer.tsx
+++ apps/planlegger/src/AppContainer.tsx
@@ -1,11 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
-import { ErrorBoundary } from '@navikt/fp-ui';
-import IntlProvider from './intl/IntlProvider';
+import { ErrorBoundary, IntlProvider } from '@navikt/fp-ui';
 import Planlegger from './Planlegger';
+import nnMessages from './intl/messages/nn_NO.json';
+import nbMessages from './intl/messages/nb_NO.json';
+import { useState } from 'react';
+import { LocaleNo } from '@navikt/fp-types';
+
+const MESSAGES_GROUPED_BY_LOCALE = {
+    nb: { ...nbMessages },
+    nn: { ...nnMessages },
+};
 
 const AppContainer = () => {
+    const [locale] = useState<LocaleNo>('nb');
+
     return (
-        <IntlProvider språkkode="nb">
+        <IntlProvider locale={locale} messagesGroupedByLocale={MESSAGES_GROUPED_BY_LOCALE}>
             <ErrorBoundary appName="Foreldrepengeplanlegger" retryCallback={() => undefined}>
                 <BrowserRouter>
                     <Planlegger />
