7b41c07138e41c77e471586bdc71a880ed2399da
Marthe <marthe.hagen@nav.no>
2024-01-30 15:46:37 +0100

    fikset typer

--- apps/planlegger/src/types/Barnet.ts
+++ apps/planlegger/src/types/Barnet.ts
@@ -1,9 +1,34 @@
-export enum BarnetEnum {
-    FØDSEL = 'fødsel',
-    ADOPSJON = 'adopsjon',
-}
+export type BarnetErFødt = {
+    erFødsel: boolean;
+    erBarnetFødt: boolean;
+    fødselsdato: string;
+};
 
-export type Barnet = {
-    barnet: BarnetEnum;
-    erFødt: boolean;
+export type BarnetErIkkeFødt = {
+    erFødsel: boolean;
+    erBarnetFødt: boolean;
+    termindato: string;
+};
+export type BarnetErAdoptert = {
+    erFødsel: boolean;
+    adopsjonsdato: string;
+    overtakelsesdato: string;
+};
+
+export type Fødsel = BarnetErFødt | BarnetErIkkeFødt;
+
+export type OmBarnet = Fødsel | BarnetErAdoptert;
+
+export const erBarnetIkkeFødt = (omBarnet: OmBarnet): omBarnet is BarnetErIkkeFødt => {
+    if ((omBarnet as BarnetErIkkeFødt).termindato) {
+        return true;
+    }
+    return false;
+};
+
+export const erBarnetFødt = (omBarnet: OmBarnet): omBarnet is BarnetErFødt => {
+    if ((omBarnet as BarnetErFødt).erBarnetFødt === true) {
+        return true;
+    }
+    return false;
 };
