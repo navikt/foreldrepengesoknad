036e8a6614ab5cc7c35ab97c05e3514fc1b63955
Marthe <marthe.hagen@nav.no>
2024-02-06 13:09:58 +0100

    lagt til info dersom det er under 3 mnd til termin

---
+++ apps/planlegger/src/steps/omBarnet/Foreldrepengeinfo.tsx
@@ -0,0 +1,48 @@
+import { BodyLong, Box, Heading, VStack } from '@navikt/ds-react';
+import { notEmpty } from '@navikt/fp-validation';
+import { ContextDataType, useContextGetData } from 'appData/PlanleggerDataContext';
+import { FunctionComponent } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { finnNavn } from 'steps/arbeidssituasjon/situasjon/FlereForsørgere';
+import { isAlene } from 'types/HvemPlanlegger';
+
+const Foreldrepengeinfo: FunctionComponent = () => {
+    const hvemPlanlegger = notEmpty(useContextGetData(ContextDataType.HVEM_PLANLEGGER));
+    const navn = finnNavn(hvemPlanlegger);
+
+    return (
+        <VStack gap="10">
+            <Box borderColor="border-alt-3" padding="4" borderWidth="2" borderRadius="xlarge">
+                <VStack gap="2">
+                    {isAlene(hvemPlanlegger) && (
+                        <VStack gap="2">
+                            <Heading size="small">
+                                <FormattedMessage id="barnet.foreldrepengerInfoDeg" />
+                            </Heading>
+
+                            <BodyLong>
+                                <FormattedMessage id="barnet.foreldrepengerInfoTekstDeg" />
+                            </BodyLong>
+                        </VStack>
+                    )}
+                    {!isAlene(hvemPlanlegger) && (
+                        <VStack gap="2">
+                            <Heading size="small">
+                                <FormattedMessage id="barnet.foreldrepengerInfo" />
+                            </Heading>
+
+                            <BodyLong>
+                                <FormattedMessage id="barnet.foreldrepengerInfoTekstMor" values={{ navn: navn[0] }} />
+                            </BodyLong>
+                            <BodyLong>
+                                <FormattedMessage id="barnet.foreldrepengerInfoTekstFar" />
+                            </BodyLong>
+                        </VStack>
+                    )}
+                </VStack>
+            </Box>
+        </VStack>
+    );
+};
+
+export default Foreldrepengeinfo;
