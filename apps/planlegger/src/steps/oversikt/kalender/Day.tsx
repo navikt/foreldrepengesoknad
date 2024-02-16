40cbd529f7e9eef9f588bafac952680a621807b2
tor-nav <tor.roneid@nav.no>
2024-02-16 11:25:07 +0100

    fargar i kalender

--- apps/planlegger/src/steps/oversikt/kalender/Day.tsx
+++ apps/planlegger/src/steps/oversikt/kalender/Day.tsx
@@ -3,7 +3,14 @@ import styles from './day.module.css';
 export enum PeriodType {
     INGEN = 'INGEN',
     FORELDREPENGER = 'FORELDREPENGER',
+    TERMINDATO = 'TERMINDATO',
 }
+
+const DAY_STYLE = {
+    [PeriodType.INGEN]: styles.none,
+    [PeriodType.FORELDREPENGER]: styles.foreldrepenger,
+    [PeriodType.TERMINDATO]: styles.termindato,
+};
 
 type Props = {
     day: number;
@@ -11,8 +18,6 @@ type Props = {
 };
 
 const Day: React.FunctionComponent<Props> = ({ day, periodType }) => {
-    return (
-        <div className={PeriodType.FORELDREPENGER === periodType ? styles.foreldrepenger : styles.none}>{day + 1}</div>
-    );
+    return <div className={DAY_STYLE[periodType]}>{day + 1}</div>;
 };
 export default Day;
