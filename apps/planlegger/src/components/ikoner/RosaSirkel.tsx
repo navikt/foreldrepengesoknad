ff6906f604a912e527eef60f1a070a46b78c132a
Marthe <marthe.hagen@nav.no>
2024-02-29 13:59:58 +0100

    endret farger

--- apps/planlegger/src/components/ikoner/RosaSirkel.tsx
+++ apps/planlegger/src/components/ikoner/RosaSirkel.tsx
@@ -1,6 +1,6 @@
 const RosaSirkel = () => (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="8.19192" cy="8.00051" r="7.69192" fill="#FFC2C2" />
+        <circle cx="8.19192" cy="8.00051" r="7.69192" fill="#CCF1D6" stroke="#66c786" strokeWidth="2px" />
     </svg>
 );
 
