4293db9a4c6f455fce7c4fa28d683487c7030e5b
Marthe <marthe.hagen@nav.no>
2024-02-29 09:50:02 +0100

    .

---
+++ apps/planlegger/src/components/Infoboks.tsx
@@ -0,0 +1,32 @@
+import { BabyWrappedIcon } from '@navikt/aksel-icons';
+import classnames from 'classnames';
+import React from 'react';
+
+import { Box, HStack, Heading, VStack } from '@navikt/ds-react';
+
+import styles from './Infoboks.module.css';
+
+interface Props {
+    header: string | React.ReactNode;
+    children: React.ReactNode;
+}
+const Infoboks: React.FC<Props> = ({ header, children }) => {
+    return (
+        <Box background="surface-alt-3-subtle" padding="4" borderRadius="large">
+            <VStack gap="2">
+                <HStack justify="space-between">
+                    <Heading size="small" className={styles.header}>
+                        {header}
+                    </Heading>
+                    <div className={classnames(styles.circle, styles.circle__info)}>
+                        <BabyWrappedIcon height={24} width={24} color="#236B7D" fontSize="1.5rem" />
+                    </div>
+                </HStack>
+
+                {children}
+            </VStack>
+        </Box>
+    );
+};
+
+export default Infoboks;
