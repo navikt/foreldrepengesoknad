c69c947fb4fccaef24c7b5763f147f835163bfab
Marthe <marthe.hagen@nav.no>
2024-02-29 16:40:08 +0100

    lagt inn perioder i kalenderen

--- apps/planlegger/src/steps/oversikt/kalender/Day.tsx
+++ apps/planlegger/src/steps/oversikt/kalender/Day.tsx
@@ -2,22 +2,45 @@ import styles from './day.module.css';
 
 export enum PeriodType {
     INGEN = 'INGEN',
-    FORELDREPENGER = 'FORELDREPENGER',
+    FORELDREPENGER_MOR = 'FORELDREPENGER_MOR',
+    FORELDREPENGER_FAR = 'FORELDREPENGER_FAR',
+    HELGEDAG = 'HELGEDAG',
     TERMINDATO = 'TERMINDATO',
+}
+
+export enum DagIPeriode {
+    FØRSTE_DAG = 'FØRSTE_DAG',
+    SISTE_DAG = 'SISTE_DAG',
+    FØRSTE_OG_SISTE_DAG = 'FØRSTE_OG_SISTE_DAG',
+    DAG_MELLOM = 'DAG_MELLOM',
 }
 
 const DAY_STYLE = {
     [PeriodType.INGEN]: styles.none,
-    [PeriodType.FORELDREPENGER]: styles.foreldrepenger,
+    [PeriodType.FORELDREPENGER_MOR]: styles.foreldrepengerMor,
+    [PeriodType.FORELDREPENGER_FAR]: styles.foreldrepengerFar,
+    [PeriodType.HELGEDAG]: styles.helgedag,
     [PeriodType.TERMINDATO]: styles.termindato,
 };
 
 type Props = {
     day: number;
     periodType: PeriodType;
+    startEllerSlutt?: DagIPeriode;
+    isFirstDay?: boolean;
+    isLastDay?: boolean;
 };
 
-const Day: React.FunctionComponent<Props> = ({ day, periodType }) => {
-    return <div className={DAY_STYLE[periodType]}>{day + 1}</div>;
+const Day: React.FunctionComponent<Props> = ({ day, periodType, startEllerSlutt }) => {
+    const erStart = startEllerSlutt === DagIPeriode.FØRSTE_DAG;
+    const erSlutt = startEllerSlutt === DagIPeriode.SISTE_DAG;
+    const erStartOgSlutt = startEllerSlutt === DagIPeriode.FØRSTE_OG_SISTE_DAG;
+    return (
+        <div
+            className={`${styles.days} ${DAY_STYLE[periodType]} ${erStart && styles.firstDay} ${erSlutt && styles.lastDay} ${erStartOgSlutt && styles.firstAndLastDay}`}
+        >
+            {day + 1}
+        </div>
+    );
 };
 export default Day;
