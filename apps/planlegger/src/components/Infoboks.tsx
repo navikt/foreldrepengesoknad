d1b6315568aa659978991d4d670143ab68e13c96
Marthe <marthe.hagen@nav.no>
2024-03-07 11:01:59 +0100

    infoboks og ikoner

--- apps/planlegger/src/components/Infoboks.tsx
+++ apps/planlegger/src/components/Infoboks.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { Box, HStack, Heading, VStack } from '@navikt/ds-react';
 
 import styles from './Infoboks.module.css';
+import ikonStyles from './ikoner/Ikon.module.css';
 
 interface Props {
     header: string | React.ReactNode;
@@ -18,7 +19,7 @@ const Infoboks: React.FC<Props> = ({ header, children, icon }) => {
                     <Heading size="small" className={styles.header}>
                         {header}
                     </Heading>
-                    <div className={classnames(styles.circle, styles.circle__info)}>{icon}</div>
+                    <div className={classnames(ikonStyles.circle, ikonStyles.circle__info)}>{icon}</div>
                 </HStack>
 
                 {children}
