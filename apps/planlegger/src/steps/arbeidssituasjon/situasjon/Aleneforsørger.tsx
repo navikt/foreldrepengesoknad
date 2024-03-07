1f4d75f88fba2a6c4a699b72d7ec7205b63caf96
tor-nav <tor.roneid@nav.no>
2024-03-07 13:46:53 +0100

    diverse arbeidssitasjon

--- apps/planlegger/src/steps/arbeidssituasjon/situasjon/Aleneforsørger.tsx
+++ apps/planlegger/src/steps/arbeidssituasjon/situasjon/Aleneforsørger.tsx
@@ -1,103 +1,69 @@
-import GreenPanel from 'components/GreenPanel';
 import Infoboks from 'components/Infoboks';
 import { FunctionComponent } from 'react';
-import { useForm } from 'react-hook-form';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { useFormContext } from 'react-hook-form';
+import { FormattedMessage } from 'react-intl';
 import { Arbeidssituasjon, ArbeidssituasjonEnum } from 'types/Arbeidssituasjon';
 
-import { BodyLong, Link, Radio, VStack } from '@navikt/ds-react';
-
-import { Form, RadioGroup } from '@navikt/fp-form-hooks';
-import { isRequired } from '@navikt/fp-validation';
+import { BodyLong, Link } from '@navikt/ds-react';
 
 import { HVOR_LENGE_LENKE, VEIVISER_LENKE } from '../ArbeidssituasjonSteg';
 
 const Aleneforsørger: FunctionComponent = () => {
-    const intl = useIntl();
-    const formMethods = useForm<Arbeidssituasjon>();
-    const arbeidssituasjon = formMethods.watch('arbeidssituasjonAlene');
+    const formMethods = useFormContext<Arbeidssituasjon>();
+
+    const arbeidssituasjon = formMethods.watch('arbeidssituasjon');
 
     return (
-        <Form formMethods={formMethods}>
-            <VStack gap="10">
-                <GreenPanel>
-                    <RadioGroup
-                        label={<FormattedMessage id="barnet.hvaGjelderDeg" />}
-                        name="arbeidssituasjonAlene"
-                        validate={[
-                            isRequired(
-                                intl.formatMessage({
-                                    id: 'feilmelding.arbeidssituasjonAlene.duMåOppgi',
-                                }),
-                            ),
-                        ]}
-                    >
-                        <Radio value={ArbeidssituasjonEnum.JOBBER}>
-                            <FormattedMessage id="arbeid.jobber" />
-                        </Radio>
-                        <Radio value={ArbeidssituasjonEnum.UFØR}>
-                            <FormattedMessage id="arbeid.ufør" />
-                        </Radio>
-                        <Radio value={ArbeidssituasjonEnum.INGEN}>
-                            <FormattedMessage id="arbeid.ingen" />
-                        </Radio>
-                    </RadioGroup>
-                </GreenPanel>
-                {arbeidssituasjon === ArbeidssituasjonEnum.JOBBER && (
-                    <Infoboks header={<FormattedMessage id="arbeid.jobber.infoboksDeg" />}>
-                        <BodyLong>
-                            <FormattedMessage id="arbeid.jobber.infoboks.beskrivelseDeg" />
-                        </BodyLong>
-                    </Infoboks>
-                )}
-                {arbeidssituasjon === ArbeidssituasjonEnum.UFØR && (
-                    <Infoboks header={<FormattedMessage id="arbeid.infoboksDeg" />}>
-                        <BodyLong>
-                            <FormattedMessage id="arbeid.infoboks.aktivitet" />
-                        </BodyLong>
-                        <BodyLong>
-                            <FormattedMessage
-                                id="arbeid.ufør.infoboks.beskrivelseDel3Deg"
-                                values={{
-                                    a: (msg: any) => (
-                                        <Link
-                                            href={HVOR_LENGE_LENKE}
-                                            className="lenke"
-                                            rel="noreferrer"
-                                            target="_blank"
-                                        >
-                                            {msg}
-                                        </Link>
-                                    ),
-                                }}
-                            />
-                        </BodyLong>
-                    </Infoboks>
-                )}
-                {arbeidssituasjon === ArbeidssituasjonEnum.INGEN && (
-                    <Infoboks header={<FormattedMessage id="arbeid.infoboksDeg" />}>
-                        <BodyLong>
-                            <FormattedMessage id="arbeid.ingen.infoboks.beskrivelseDeg" />
-                        </BodyLong>
-                        <BodyLong>
-                            <FormattedMessage id="arbeid.infoboks.aktivitet" />
-                        </BodyLong>
-                        <BodyLong>
-                            <FormattedMessage
-                                id="arbeid.ingen.infoboks.beskrivelseDel3Deg"
-                                values={{
-                                    a: (msg: any) => (
-                                        <Link href={VEIVISER_LENKE} className="lenke" rel="noreferrer" target="_blank">
-                                            {msg}
-                                        </Link>
-                                    ),
-                                }}
-                            />
-                        </BodyLong>
-                    </Infoboks>
-                )}
-            </VStack>
-        </Form>
+        <>
+            {arbeidssituasjon === ArbeidssituasjonEnum.JOBBER && (
+                <Infoboks header={<FormattedMessage id="arbeid.jobber.infoboksDeg" />}>
+                    <BodyLong>
+                        <FormattedMessage id="arbeid.jobber.infoboks.beskrivelseDeg" />
+                    </BodyLong>
+                </Infoboks>
+            )}
+            {arbeidssituasjon === ArbeidssituasjonEnum.UFØR && (
+                <Infoboks header={<FormattedMessage id="arbeid.infoboksDeg" />}>
+                    <BodyLong>
+                        <FormattedMessage id="arbeid.infoboks.aktivitet" />
+                    </BodyLong>
+                    <BodyLong>
+                        <FormattedMessage
+                            id="arbeid.ufør.infoboks.beskrivelseDel3Deg"
+                            values={{
+                                a: (msg: any) => (
+                                    <Link href={HVOR_LENGE_LENKE} className="lenke" rel="noreferrer" target="_blank">
+                                        {msg}
+                                    </Link>
+                                ),
+                            }}
+                        />
+                    </BodyLong>
+                </Infoboks>
+            )}
+            {arbeidssituasjon === ArbeidssituasjonEnum.INGEN && (
+                <Infoboks header={<FormattedMessage id="arbeid.infoboksDeg" />}>
+                    <BodyLong>
+                        <FormattedMessage id="arbeid.ingen.infoboks.beskrivelseDeg" />
+                    </BodyLong>
+                    <BodyLong>
+                        <FormattedMessage id="arbeid.infoboks.aktivitet" />
+                    </BodyLong>
+                    <BodyLong>
+                        <FormattedMessage
+                            id="arbeid.ingen.infoboks.beskrivelseDel3Deg"
+                            values={{
+                                a: (msg: any) => (
+                                    <Link href={VEIVISER_LENKE} className="lenke" rel="noreferrer" target="_blank">
+                                        {msg}
+                                    </Link>
+                                ),
+                            }}
+                        />
+                    </BodyLong>
+                </Infoboks>
+            )}
+        </>
     );
 };
 
