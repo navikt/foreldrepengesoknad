b9d873e8a84e7074cf9dc40c508a2bbe48cf6c01
Marthe <marthe.hagen@nav.no>
2024-03-11 16:31:53 +0100

    har lagt til infobokser med ikoner og fjernet 'hvorfor spør nav om dette'

--- apps/planlegger/src/steps/arbeidssituasjon/situasjon/FlereForsørgere.tsx
+++ apps/planlegger/src/steps/arbeidssituasjon/situasjon/FlereForsørgere.tsx
@@ -1,3 +1,4 @@
+import { CheckmarkIcon, XMarkIcon } from '@navikt/aksel-icons';
 import Infoboks from 'components/Infoboks';
 import GreenRadioGroup from 'components/formWrappers/GreenRadioGroup';
 import { FunctionComponent } from 'react';
@@ -14,7 +15,7 @@ import {
     isMorOgMedmor,
 } from 'types/HvemPlanlegger';
 
-import { BodyLong, Link, Radio } from '@navikt/ds-react';
+import { BodyLong, Link, Radio, VStack } from '@navikt/ds-react';
 
 import { isRequired } from '@navikt/fp-validation';
 
@@ -40,27 +41,47 @@ const FlereForsørgere: FunctionComponent<Props> = ({ hvemPlanlegger }) => {
     const formMethods = useFormContext<Arbeidssituasjon>();
 
     const arbeidssituasjon = formMethods.watch('arbeidssituasjon');
+    const arbeidssituasjonAnnenPart = formMethods.watch('arbeidssituasjonAnnenPart');
 
     const fornavnSøker = getFornavnPåSøker(hvemPlanlegger);
     const fornavnAnnenPart = getFornavnPåAnnenPart(hvemPlanlegger);
 
     return (
-        <>
+        <VStack gap="5">
             {arbeidssituasjon === ArbeidssituasjonEnum.JOBBER && (
-                <Infoboks header={<FormattedMessage id="arbeid.jobber.infoboks" values={{ navn: fornavnSøker }} />}>
+                <Infoboks
+                    header={
+                        <FormattedMessage
+                            id="arbeid.jobber.infoboks.harRettTilForeldrepenger"
+                            values={{ navn: fornavnSøker }}
+                        />
+                    }
+                    icon={<CheckmarkIcon height={28} width={28} color="#020C1CAD" fontSize="1.5rem" />}
+                >
                     <BodyLong>
-                        <FormattedMessage id="arbeid.jobber.infoboks.beskrivelse" values={{ navn: fornavnSøker }} />
+                        <FormattedMessage
+                            id="arbeid.jobber.infoboks.harJobbetSeksAvTiMnd"
+                            values={{ navn: fornavnSøker }}
+                        />
                     </BodyLong>
                 </Infoboks>
             )}
             {arbeidssituasjon === ArbeidssituasjonEnum.UFØR && (
-                <Infoboks header={<FormattedMessage id="arbeid.infoboks" values={{ navn: fornavnSøker }} />}>
+                <Infoboks
+                    header={
+                        <FormattedMessage
+                            id="arbeid.infoboks.harIkkeRettTilForeldrepenger"
+                            values={{ navn: fornavnSøker }}
+                        />
+                    }
+                    icon={<XMarkIcon height={28} width={28} color="#020C1CAD" fontSize="1.5rem" />}
+                >
                     <BodyLong>
-                        <FormattedMessage id="arbeid.infoboks.aktivitet" />
+                        <FormattedMessage id="arbeid.ufør.infoboks.erUfør" />
                     </BodyLong>
                     <BodyLong>
                         <FormattedMessage
-                            id="arbeid.ufør.infoboks.beskrivelseDel3"
+                            id="arbeid.ufør.infoboks.lesMer"
                             values={{
                                 a: (msg: any) => (
                                     <Link href={HVOR_LENGE_LENKE} className="lenke" rel="noreferrer" target="_blank">
@@ -74,16 +95,21 @@ const FlereForsørgere: FunctionComponent<Props> = ({ hvemPlanlegger }) => {
                 </Infoboks>
             )}
             {arbeidssituasjon === ArbeidssituasjonEnum.INGEN && (
-                <Infoboks header={<FormattedMessage id="arbeid.infoboks" values={{ navn: fornavnSøker }} />}>
+                <Infoboks
+                    header={
+                        <FormattedMessage
+                            id="arbeid.infoboks.harIkkeRettTilForeldrepenger"
+                            values={{ navn: fornavnSøker }}
+                        />
+                    }
+                    icon={<XMarkIcon height={28} width={28} color="#020C1CAD" fontSize="1.5rem" />}
+                >
                     <BodyLong>
-                        <FormattedMessage id="arbeid.ingen.infoboks.beskrivelse" values={{ navn: fornavnSøker }} />
-                    </BodyLong>
-                    <BodyLong>
-                        <FormattedMessage id="arbeid.infoboks.aktivitet" />
+                        <FormattedMessage id="arbeid.ingen.infoboks.manHarIkkeRett" values={{ navn: fornavnSøker }} />
                     </BodyLong>
                     <BodyLong>
                         <FormattedMessage
-                            id="arbeid.ingen.infoboks.beskrivelseDel3"
+                            id="arbeid.ingen.infoboks.engangsstønad"
                             values={{
                                 a: (msg: any) => (
                                     <Link href={VEIVISER_LENKE} className="lenke" rel="noreferrer" target="_blank">
@@ -97,29 +123,61 @@ const FlereForsørgere: FunctionComponent<Props> = ({ hvemPlanlegger }) => {
                 </Infoboks>
             )}
             {arbeidssituasjon && (
-                <GreenRadioGroup
-                    name="arbeidssituasjonAnnenPart"
-                    label={<FormattedMessage id="arbeid.andreForelder" values={{ navn: fornavnAnnenPart }} />}
-                    validate={[
-                        isRequired(
-                            intl.formatMessage(
-                                { id: 'feilmelding.arbeidssituasjonFlere.duMåOppgi' },
-                                {
-                                    hvem: finnAnnenPartTekst(intl, hvemPlanlegger),
-                                },
+                <VStack gap="5">
+                    <GreenRadioGroup
+                        name="arbeidssituasjonAnnenPart"
+                        label={<FormattedMessage id="arbeid.andreForelder" values={{ navn: fornavnAnnenPart }} />}
+                        validate={[
+                            isRequired(
+                                intl.formatMessage(
+                                    { id: 'feilmelding.arbeidssituasjonFlere.duMåOppgi' },
+                                    {
+                                        hvem: finnAnnenPartTekst(intl, hvemPlanlegger),
+                                    },
+                                ),
                             ),
-                        ),
-                    ]}
-                >
-                    <Radio value={true} autoFocus>
-                        <FormattedMessage id="ja" />
-                    </Radio>
-                    <Radio value={false}>
-                        <FormattedMessage id="nei" />
-                    </Radio>
-                </GreenRadioGroup>
+                        ]}
+                    >
+                        <Radio value={true} autoFocus>
+                            <FormattedMessage id="ja" />
+                        </Radio>
+                        <Radio value={false}>
+                            <FormattedMessage id="nei" />
+                        </Radio>
+                    </GreenRadioGroup>
+                    {arbeidssituasjonAnnenPart === true && (
+                        <Infoboks
+                            header={
+                                <FormattedMessage
+                                    id="arbeid.jobber.infoboks.harRettTilForeldrepenger"
+                                    values={{ navn: fornavnAnnenPart }}
+                                />
+                            }
+                            icon={<CheckmarkIcon height={28} width={28} color="#020C1CAD" fontSize="1.5rem" />}
+                        >
+                            <BodyLong>
+                                <FormattedMessage id="arbeid.jobber.infoboks.harJobbetSeksAvTiMnd" />
+                            </BodyLong>
+                        </Infoboks>
+                    )}
+                    {arbeidssituasjonAnnenPart === false && (
+                        <Infoboks
+                            header={
+                                <FormattedMessage
+                                    id="arbeid.infoboks.harIkkeRettTilForeldrepenger"
+                                    values={{ navn: fornavnAnnenPart }}
+                                />
+                            }
+                            icon={<XMarkIcon height={28} width={28} color="#020C1CAD" fontSize="1.5rem" />}
+                        >
+                            <BodyLong>
+                                <FormattedMessage id="arbeid.ingen.infoboks.manHarIkkeRett" />
+                            </BodyLong>
+                        </Infoboks>
+                    )}
+                </VStack>
             )}
-        </>
+        </VStack>
     );
 };
 
