f8a666d55b05afa6c2b3a3eee82d8528376e0af0
Tor Røneid <tor.roneid@nav.no>
2024-03-18 09:30:03 +0100

    Oppdatert DEPS (#4382)

--- packages/fp-common/src/common/components/block/Block.stories.tsx
+++ packages/fp-common/src/common/components/block/Block.stories.tsx
@@ -1,12 +1,13 @@
-import { ComponentMeta, Story } from '@storybook/react';
+import { StoryFn } from '@storybook/react';
+
 import Block, { BlockProps } from './Block';
 
 export default {
     title: 'components/Block',
     component: Block,
-} as ComponentMeta<typeof Block>;
+};
 
-const Template: Story<BlockProps> = (args) => <Block {...args} />;
+const Template: StoryFn<BlockProps> = (args) => <Block {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
