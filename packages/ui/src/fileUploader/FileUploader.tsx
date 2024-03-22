52e1568372c3e32cbaa6f4c2d179484c26d85888
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2024-03-22 12:44:49 +0100

    Vedlegg størrelse sjekkes baseres på Bytes og ikke bits og alle meldinger ut til bruker endres fra bits til Bytes (#4396)

--- packages/ui/src/fileUploader/FileUploader.tsx
+++ packages/ui/src/fileUploader/FileUploader.tsx
@@ -12,8 +12,8 @@ import FailedAttachmentList from './liste/FailedAttachmentList';
 import { FileUploadError } from './typer/FileUploadError';
 
 const VALID_EXTENSIONS = ['.pdf', '.jpeg', '.jpg', '.png'];
-const MAX_FIL_STØRRELSE_KB = 16384;
-const KILOBYTES_IN_BYTE = 0.0009765625;
+const MAX_FIL_STØRRELSE_MB = 16;
+const MAX_FIL_STØRRELSE_BYTES = MAX_FIL_STØRRELSE_MB * 1024 * 1024;
 
 // TODO Fjern any her utan å måtte dra inn axios i denne pakka
 type SaveAttachment = (attachment: Attachment) => Promise<any>;
@@ -38,8 +38,7 @@ const fileExtensionIsValid = (filename: string): boolean => {
 };
 
 const fileSizeIsValid = (filesizeInB: number): boolean => {
-    const filesizeInKb = filesizeInB * KILOBYTES_IN_BYTE;
-    return filesizeInKb <= MAX_FIL_STØRRELSE_KB;
+    return filesizeInB <= MAX_FIL_STØRRELSE_BYTES;
 };
 
 const uploadAttachment = async (attachment: Attachment, saveAttachment: SaveAttachment): Promise<void> => {
