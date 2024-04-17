34bb59f8ff14339a737752e177d262fecf570761
Andreas Dybdahl <andreas.dyb@gmail.com>
2024-04-17 16:25:47 +0200

    Ikke valider på annen parts uttak om fellesperiode om søker ikke har søkt om fellesperiode

--- packages/uttaksplan/src/validering/tester/erUttaksmengdeForFarMedmorForHøyTest.ts
+++ packages/uttaksplan/src/validering/tester/erUttaksmengdeForFarMedmorForHøyTest.ts
@@ -1,19 +1,54 @@
-import { RegelTest, RegelTestresultat } from '../utils/types/regelTypes';
 import {
+    Forelder,
+    OppholdÅrsakType,
     Periode,
+    PeriodeInfoType,
+    StønadskontoType,
     StønadskontoUttak,
     Søknadsinfo,
     TilgjengeligStønadskonto,
     beregnGjenståendeUttaksdager,
+    isInfoPeriodeAnnenPart,
+    isUttaksperiode,
 } from '@navikt/fp-common';
+
+import { RegelTest, RegelTestresultat } from '../utils/types/regelTypes';
+
+const harSøktOmFellesperiode = (periode: Periode) => {
+    if (isUttaksperiode(periode)) {
+        if (periode.forelder === Forelder.farMedmor && periode.konto === StønadskontoType.Fellesperiode) {
+            return true;
+        }
+    }
+
+    return false;
+};
 
 const erUttaksmengdeForFarMedmorForHøy = (
     uttaksplan: Periode[],
     tilgjengeligeStønadskontoer: TilgjengeligStønadskonto[],
     farEllerMedmor: boolean,
 ): boolean => {
+    const harFarMedmorSøktOmFellesperiode = uttaksplan.find((p) => harSøktOmFellesperiode(p)) !== undefined;
+
+    const perioderSomSkalBrukes = uttaksplan.filter((p) => {
+        if (harFarMedmorSøktOmFellesperiode) {
+            return true;
+        }
+
+        if (
+            isInfoPeriodeAnnenPart(p) &&
+            p.infotype === PeriodeInfoType.uttakAnnenPart &&
+            p.årsak === OppholdÅrsakType.UttakFellesperiodeAnnenForelder
+        ) {
+            return false;
+        }
+
+        return true;
+    });
+
     if (farEllerMedmor === true) {
-        const kontoUttak = beregnGjenståendeUttaksdager(tilgjengeligeStønadskontoer, uttaksplan, false);
+        const kontoUttak = beregnGjenståendeUttaksdager(tilgjengeligeStønadskontoer, perioderSomSkalBrukes, false);
         return kontoUttak.some((konto: StønadskontoUttak) => konto.dager < 0);
     } else {
         return false;
