0c98388cac25253450a9117b9b027a7778faa8c1
Andreas Dybdahl <andreas.dyb@gmail.com>
2024-04-25 10:13:17 +0200

    More changes

--- packages/ny-uttaksplan/src/components/periode-liste-header/PeriodeListeHeader.tsx
+++ packages/ny-uttaksplan/src/components/periode-liste-header/PeriodeListeHeader.tsx
@@ -1,15 +1,36 @@
-import { bemUtils } from '@navikt/fp-common';
+import { BabyWrappedFillIcon, PersonPregnantFillIcon } from '@navikt/aksel-icons';
+import classNames from 'classnames';
+
+import { bemUtils, formatDateShortMonth } from '@navikt/fp-common';
 
 import './periode-liste-header.css';
 
 const PeriodeListeHeader = () => {
     const bem = bemUtils('periode-liste-header');
 
+    const getIkon = (førTermindato: boolean) => {
+        if (førTermindato) {
+            return <PersonPregnantFillIcon className={bem.modifier('farge-blaa')} width={24} height={24} />;
+        }
+
+        return <BabyWrappedFillIcon className={bem.modifier('farge-gronn')} width={24} height={24} />;
+    };
+
     return (
         <div className={bem.block}>
-            <div className={bem.element('dato')}>21. jan - 3. mars</div>
+            <div className={bem.element('dato')}>
+                {formatDateShortMonth(new Date('2024-11-01'))} - {formatDateShortMonth(new Date('2024-12-05'))}
+            </div>
             <div className={bem.element('uker')}>3 uker</div>
-            <div className={bem.element('ikon')}>Ikon</div>
+            <div
+                className={classNames(
+                    bem.element('hendelse'),
+                    true ? bem.modifier('farge-bg-lysblaa') : bem.modifier('farge-bg-gronn'),
+                )}
+            >
+                <div>Du i permisjon</div>
+                {getIkon(true)}
+            </div>
         </div>
     );
 };
--- packages/ny-uttaksplan/src/components/periode-liste-item/PeriodeListeItem.tsx
+++ packages/ny-uttaksplan/src/components/periode-liste-item/PeriodeListeItem.tsx
@@ -2,19 +2,22 @@ import { Accordion } from '@navikt/ds-react';
 
 import { bemUtils } from '@navikt/fp-common';
 
-import PeriodeListeHeader from './../periode-liste-header/PeriodeListeHeader';
+import PeriodeListeContent from '../periode-liste-content/PeriodeListeContent';
+import PeriodeListeHeader from '../periode-liste-header/PeriodeListeHeader';
 import './periode-liste-item.css';
 
 const PeriodeListeItem = () => {
     const bem = bemUtils('periode-liste-item');
 
     return (
-        <Accordion>
+        <Accordion className={bem.element('item')}>
             <Accordion.Item>
                 <Accordion.Header className={bem.element('header')}>
                     <PeriodeListeHeader />
                 </Accordion.Header>
-                <Accordion.Content>Innhold</Accordion.Content>
+                <Accordion.Content>
+                    <PeriodeListeContent />
+                </Accordion.Content>
             </Accordion.Item>
         </Accordion>
     );
