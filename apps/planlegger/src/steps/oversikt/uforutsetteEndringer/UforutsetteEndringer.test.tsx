fcc82f530ab9b92847fe3a50174048997efc6e05
tor-nav <tor.roneid@nav.no>
2024-04-29 13:13:51 +0200

    FIX

--- apps/planlegger/src/steps/oversikt/uforutsetteEndringer/UforutsetteEndringer.test.tsx
+++ apps/planlegger/src/steps/oversikt/uforutsetteEndringer/UforutsetteEndringer.test.tsx
@@ -3,12 +3,12 @@ import { render, screen } from '@testing-library/react';
 
 import * as stories from './UforutsetteEndringer.stories';
 
-const { AdopsjonMorOgFarBeggeHarRett, FødselMorOgFarKunFarHarRett } = composeStories(stories);
+const { AdopsjonMorOgFarBeggeHarRettToBarn, FødselMorOgFarKunFarHarRett } = composeStories(stories);
 
 describe('<UforutsetteEndringer>', () => {
     //MorOgFar
     it('skal vise info for mor og far adopsjon hvor begge har rett', async () => {
-        render(<AdopsjonMorOgFarBeggeHarRett />);
+        render(<AdopsjonMorOgFarBeggeHarRettToBarn />);
 
         expect(await screen.findByText('Uforutsette endringer')).toBeInTheDocument();
 
@@ -16,6 +16,7 @@ describe('<UforutsetteEndringer>', () => {
         expect(screen.getByText('Hvis mor blir syk i de første seks ukene med foreldrepenger')).toBeInTheDocument();
         expect(screen.getByText('Hvis man får et nytt barn før det har gått tre år')).toBeInTheDocument();
     });
+
     it('skal vise info for mor og far fødsel hvor kun far har rett', async () => {
         render(<FødselMorOgFarKunFarHarRett />);
 
