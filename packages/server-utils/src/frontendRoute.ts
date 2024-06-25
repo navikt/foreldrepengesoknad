a2b23ba770ebef5c783193b550fcc9f2b05b8fa4
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2024-06-25 16:44:27 +0200

    Legger til fiks av skjermdeling ved å tillate kall på assets uinnlogget (#4546)
    * Fjern auth fra css
    
    * Logging på no token
    
    * Flytter static ut også
    
    * ignore path login if assets
    
    * update resten av pakkene med samme oppsettt
    
    * fjern/logging
    
    ---------
    
    Co-authored-by: Andreas Dybdahl <andreas.dyb@gmail.com>

--- packages/server-utils/src/frontendRoute.ts
+++ packages/server-utils/src/frontendRoute.ts
@@ -1,12 +1,10 @@
 import { injectDecoratorServerSide } from '@navikt/nav-dekoratoren-moduler/ssr/index.js';
-import express, { Express } from 'express';
+import { Express } from 'express';
 import path from 'node:path';
 
 import config from './config.js';
 
-export const setupStaticRoutes = async (app: Express) => {
-    app.use(express.static('./public', { index: false }));
-
+export const setupAndServeHtml = async (app: Express) => {
     // When deployed, the built frontend is copied into the public directory. If running BFF locally the index.html will not exist.
     const spaFilePath = path.resolve('./public', 'index.html');
 
@@ -22,7 +20,6 @@ export const setupStaticRoutes = async (app: Express) => {
         }),
     );
 
-    app.use(`${config.app.publicPath}/assets`, express.static(path.resolve(path.resolve('public'), 'assets')));
     app.get(/^\/(?!.*dist).*$/, async (_request, response) => {
         return response.send(renderedHtml);
     });
