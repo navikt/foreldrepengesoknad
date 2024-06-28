95911ec79de9b193e919b22bab4c47a1a89e8d38
Andreas Dybdahl <andreas.dyb@gmail.com>
2024-06-28 14:59:32 +0200

    Noe design

--- packages/ny-uttaksplan/src/components/periode-liste-content/PeriodeListeContent.tsx
+++ packages/ny-uttaksplan/src/components/periode-liste-content/PeriodeListeContent.tsx
@@ -1,10 +1,10 @@
+import { CalendarIcon } from '@navikt/aksel-icons';
 import { FunctionComponent } from 'react';
 
 import { BodyShort } from '@navikt/ds-react';
 
-import { Periode, isUttaksperiode } from '@navikt/fp-common';
-
-import { formatDate } from '../../../../utils';
+import { Periode, Uttaksperiode, isUttaksperiode } from '@navikt/fp-common';
+import { formatDate, formatDateExtended } from '@navikt/fp-utils';
 
 interface Props {
     periode: Periode;
@@ -23,8 +23,32 @@ const renderContent = (periode: Periode, erFamiliehendelse: boolean) => {
     return `${periode.type} - ${formatDate(periode.tidsperiode.fom)} - ${formatDate(periode.tidsperiode.tom)}`;
 };
 
+const renderPeriodeUtenSamtidigUttak = (periode: Uttaksperiode) => {
+    return (
+        <>
+            <div style={{ margin: '0 1rem', display: 'flex', alignItems: 'center', gap: '1rem' }}>
+                <CalendarIcon width={24} height={24} />
+                <BodyShort weight="semibold">
+                    {formatDateExtended(periode.tidsperiode.fom)} - {formatDateExtended(periode.tidsperiode.tom)}
+                </BodyShort>
+            </div>
+            <div style={{ margin: '1rem' }}>
+                <BodyShort>Foreldrepenger fra Petters kvote</BodyShort>
+            </div>
+        </>
+    );
+};
+
 const PeriodeListeContent: FunctionComponent<Props> = ({ periode, erFamiliehendelse }) => {
-    return <div>{renderContent(periode, erFamiliehendelse)}</div>;
+    const erUttakUtenSamtidigUttak = isUttaksperiode(periode) && periode.ønskerSamtidigUttak !== true;
+
+    return (
+        <div>
+            {erUttakUtenSamtidigUttak
+                ? renderPeriodeUtenSamtidigUttak(periode)
+                : renderContent(periode, erFamiliehendelse)}
+        </div>
+    );
 };
 
 export default PeriodeListeContent;
