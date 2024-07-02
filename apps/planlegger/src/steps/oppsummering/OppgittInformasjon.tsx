3782c7ddba62a6f367ca9d3e6e914d55c8414a5b
Marthe <marthe.hagen@nav.no>
2024-07-02 16:00:23 +0200

    endring i ordlyd og spacing. finner riktig antall uker

--- apps/planlegger/src/steps/oppsummering/OppgittInformasjon.tsx
+++ apps/planlegger/src/steps/oppsummering/OppgittInformasjon.tsx
@@ -4,7 +4,7 @@ import { FormattedMessage, useIntl } from 'react-intl';
 import { Arbeidssituasjon } from 'types/Arbeidssituasjon';
 import { OmBarnet } from 'types/Barnet';
 import { Fordeling } from 'types/Fordeling';
-import { HvemPlanlegger, Situasjon } from 'types/HvemPlanlegger';
+import { HvemPlanlegger } from 'types/HvemPlanlegger';
 import { HvorLangPeriode } from 'types/HvorLangPeriode';
 import { erAlenesøker as erAlene, erFarOgFar, getFornavnPåSøker1, getFornavnPåSøker2 } from 'utils/HvemPlanleggerUtils';
 import { erBarnetAdoptert, erBarnetFødt } from 'utils/barnetUtils';
@@ -84,6 +84,7 @@ const OppgittInformasjon: FunctionComponent<Props> = ({
     const hvemHarRett = utledHvemSomHarRett(arbeidssituasjon);
     const valgtStønadskonto = stønadskontoer[hvorLangPeriode.dekningsgrad];
 
+<<<<<<< Updated upstream
     const uttaksdata = finnUttaksdata(
         hvemHarRett,
         hvemPlanlegger,
@@ -94,15 +95,29 @@ const OppgittInformasjon: FunctionComponent<Props> = ({
 
     const antallUkerFellesperiode = getAntallUkerFellesperiode(valgtStønadskonto);
     const antallUker = finnAntallUkerMedForeldrepenger(uttaksdata);
+=======
+    const antallUkerOgDagerFellesperiode = getAntallUkerOgDagerFellesperiode(valgtStønadskonto);
+    const antallUker = getAntallUker(valgtStønadskonto);
+    console.log('antallUker', antallUker);
+>>>>>>> Stashed changes
 
     // const antallUkerAdopsjon = erAdoptert
     //     ? getAntallUker(valgtStønadskonto) - getAntallUkerForeldrepengerFørFødsel(valgtStønadskonto)
     //     : getAntallUker(valgtStønadskonto);
 
+<<<<<<< Updated upstream
     const antallUkerFellesperiodeSøker1 = fordeling ? fordeling.antallUkerSøker1 : '';
     const antallUkerFellesperiodeSøker2 = fordeling ? antallUkerFellesperiode - fordeling.antallUkerSøker1 : '';
 
     const erFarOgFarFødsel = hvemPlanlegger.type === Situasjon.FAR_OG_FAR && !erAdoptert;
+=======
+    const antallUkerOgDagerFellesperiodeSøker1 = fordeling ? getUkerOgDager(fordeling.antallDagerSøker1) : undefined;
+    const antallUkerOgDagerFellesperiodeSøker2 = fordeling
+        ? getUkerOgDager(antallUkerOgDagerFellesperiode.totaltAntallDager - fordeling.antallDagerSøker1)
+        : undefined;
+
+    const erFarOgFarFødsel = erFedre && !erAdoptert;
+>>>>>>> Stashed changes
 
     const minsteInntekt = formatCurrencyWithKr(finnSisteGrunnbeløp(satser) / 2);
 
@@ -121,6 +136,7 @@ const OppgittInformasjon: FunctionComponent<Props> = ({
                 </ExpansionCard.Header>
                 <ExpansionCard.Content>
                     <VStack gap="2">
+<<<<<<< Updated upstream
                         <GreenPanel>
                     <VStack gap="10">
                         <BluePanel>
