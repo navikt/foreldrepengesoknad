9bed6a8d0000742289307ce3172bcfa542b0f9b3
Tor Røneid <tor.roneid@nav.no>
2024-07-02 11:59:54 +0200

    PL Endre url på framsida (#4553)

--- apps/planlegger/src/PlanleggerRouter.tsx
+++ apps/planlegger/src/PlanleggerRouter.tsx
@@ -1,6 +1,6 @@
 import { PlanleggerRoutes } from 'appData/routes';
-import { FunctionComponent, useEffect, useState } from 'react';
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { FunctionComponent } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import ArbeidssituasjonSteg from 'steps/arbeidssituasjon/ArbeidssituasjonSteg';
 import FordelingSteg from 'steps/fordeling/FordelingSteg';
 import HvemPlanleggerSteg from 'steps/hvemPlanlegger/HvemPlanleggerSteg';
@@ -22,25 +22,9 @@ interface Props {
 }
 
 const PlanleggerRouter: FunctionComponent<Props> = ({ locale, changeLocale, stønadskontoer, satser }) => {
-    const navigate = useNavigate();
-    const [isFirstLoad, setIsFirstLoad] = useState(true);
-
-    useEffect(() => {
-        setIsFirstLoad(false);
-        navigate(PlanleggerRoutes.OM_PLANLEGGEREN);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    if (isFirstLoad) {
-        return null;
-    }
-
     return (
         <Routes>
-            <Route
-                path={PlanleggerRoutes.OM_PLANLEGGEREN}
-                element={<OmPlanleggerenSteg locale={locale} changeLocale={changeLocale} />}
-            />
+            <Route path="/" element={<OmPlanleggerenSteg locale={locale} changeLocale={changeLocale} />} />
             <Route path={PlanleggerRoutes.HVEM_PLANLEGGER} element={<HvemPlanleggerSteg />} />
             <Route path={PlanleggerRoutes.OM_BARNET} element={<OmBarnetSteg />} />
             <Route path={PlanleggerRoutes.ARBEIDSSITUASJON} element={<ArbeidssituasjonSteg satser={satser} />} />
@@ -60,7 +44,7 @@ const PlanleggerRouter: FunctionComponent<Props> = ({ locale, changeLocale, stø
                 path={PlanleggerRoutes.OPPSUMMERING}
                 element={<OppsummeringSteg stønadskontoer={stønadskontoer} satser={satser} />}
             />
-            <Route path="*" element={<Navigate to={PlanleggerRoutes.OM_PLANLEGGEREN} />} />
+            <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
 };
