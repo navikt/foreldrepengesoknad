e2c081c481075d51f4f178893df91c0801881256
Tor Røneid <tor.roneid@nav.no>
2024-07-03 14:49:48 +0200

    Bug/planlegger1 (#4559)
    * PL FIX Visning av antall veker i oppsummering + diverse andre ting

--- apps/planlegger/src/steps/oppsummering/OppsummeringHarRett.tsx
+++ apps/planlegger/src/steps/oppsummering/OppsummeringHarRett.tsx
@@ -88,7 +88,8 @@ const OppsummeringHarRett: FunctionComponent<Props> = ({
                             id="OppsummeringSteg.DereValgte"
                             values={{
                                 prosent: hvorLangPeriode.dekningsgrad,
-                                antallUker: getAntallUker(valgtStønadskonto),
+                                antallUker: getAntallUkerOgDager(valgtStønadskonto).uker,
+                                antallDager: getAntallUkerOgDager(valgtStønadskonto).dager,
                                 hvem: getFornavnPåSøker1(hvemPlanlegger, intl),
                                 hvem2: getFornavnPåSøker2(hvemPlanlegger, intl),
                                 uker: antallUkerOgDagerFellesperiodeSøker1?.uker,
@@ -195,7 +196,8 @@ const OppsummeringHarRett: FunctionComponent<Props> = ({
                                         antallUkerOgDagerAktivitetsfriKvote.dager,
                                     hvem: fornavnSøker1,
                                     prosent: hvorLangPeriode.dekningsgrad,
-                                    antallUker: getAntallUker(valgtStønadskonto),
+                                    antallUker: getAntallUkerOgDager(valgtStønadskonto).uker,
+                                    dager: getAntallUkerOgDager(valgtStønadskonto).dager,
                                 }}
                             />
                         </BodyShort>
