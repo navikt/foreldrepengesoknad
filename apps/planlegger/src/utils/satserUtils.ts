717879e32829b7ac699400083892c04b90b261c0
tor-nav <tor.roneid@nav.no>
2024-07-03 14:28:46 +0200

    FIX

--- apps/planlegger/src/utils/satserUtils.ts
+++ apps/planlegger/src/utils/satserUtils.ts
@@ -9,7 +9,10 @@ dayjs.extend(isSameOrAfter);
 export const finnGrunnbeløp = (satser: Satser, dato: string | Dayjs) => {
     const { grunnbeløp } = satser;
     const aktueltGrunnbeløp = grunnbeløp.find((grunnbeløpData) => dayjs(dato).isSameOrAfter(grunnbeløpData.fom));
-    return notEmpty(aktueltGrunnbeløp).verdi;
+    //TODO fjern dette når rar ios-feil er retta
+    console.log(aktueltGrunnbeløp);
+    console.log(grunnbeløp[0]);
+    return notEmpty(grunnbeløp[0]).verdi;
 };
 
 export const finnEngangsstønad = (satser: Satser, dato: string | Dayjs) => {
@@ -17,5 +20,8 @@ export const finnEngangsstønad = (satser: Satser, dato: string | Dayjs) => {
     const aktuellEngangsstønad = engangstønad.find((engangsstønadData) =>
         dayjs(dato).isSameOrAfter(engangsstønadData.fom),
     );
-    return notEmpty(aktuellEngangsstønad).verdi;
+    //TODO fjern dette når rar ios-feil er retta
+    console.log(aktuellEngangsstønad);
+    console.log(engangstønad[0]);
+    return notEmpty(engangstønad[0]).verdi;
 };
