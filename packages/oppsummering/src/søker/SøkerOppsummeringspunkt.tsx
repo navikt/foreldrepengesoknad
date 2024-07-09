0200f6b2afdaf51c1657edd1d1b3dbe0deab345b
johannbm <johannes.moskvil@gmail.com>
2024-07-09 11:15:47 +0200

    fiks intl SøkerOppsummeringspunkt.tsx

--- packages/oppsummering/src/søker/SøkerOppsummeringspunkt.tsx
+++ packages/oppsummering/src/søker/SøkerOppsummeringspunkt.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 import { FormSummary } from '@navikt/ds-react';
 
@@ -13,19 +12,19 @@ interface Props {
     søker: Søker;
 }
 
-const SøkerOppsummeringspunkt: React.FunctionComponent<Props> = ({ søker }) => {
-    const intl = useIntl();
-
+const SøkerOppsummeringspunkt = ({ søker }: Props) => {
     return (
         <FormSummary>
             <FormSummary.Header>
                 <FormSummary.Heading level="2">
-                    {intl.formatMessage({ id: 'DegOppsummeringspunkt.OmDeg' })}
+                    <FormattedMessage id="DegOppsummeringspunkt.tittel" />
                 </FormSummary.Heading>
             </FormSummary.Header>
             <FormSummary.Answers>
                 <FormSummary.Answer>
-                    <FormSummary.Label>Navn og fødselsnummer</FormSummary.Label>
+                    <FormSummary.Label>
+                        <FormattedMessage id="DegOppsummeringspunkt.info" />
+                    </FormSummary.Label>
                     <FormSummary.Value>
                         {fullNameFormat(søker.fornavn, søker.etternavn, søker.mellomnavn)}, {søker.fnr}
                     </FormSummary.Value>
