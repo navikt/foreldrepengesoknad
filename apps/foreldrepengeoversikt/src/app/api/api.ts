488233344924d5ea7e349f788e5173734ab0cdaa
johannbm <johannes.moskvil@gmail.com>
2024-07-12 14:51:30 +0200

    Fiks content type bug

--- apps/foreldrepengeoversikt/src/app/api/api.ts
+++ apps/foreldrepengeoversikt/src/app/api/api.ts
@@ -51,9 +51,20 @@ type AnnenPartsVedtakRequestBody = {
 export const hentAnnenPartsVedtakOptions = (body: AnnenPartsVedtakRequestBody) =>
     queryOptions({
         queryKey: ['ANNEN_PARTS_VEDTAK', body],
-        queryFn: () => {
+        queryFn: async () => {
             try {
-                return ky.post('/rest/innsyn/v2/annenPartVedtak', { json: body }).json<AnnenPartVedtakDTO>();
+                // Det funker ikke å bruke ky.post() her.
+                // Det virker som at siden måten Adrum wrapper alle requests på, gjør at det skjer noe funny-business på et eller annet punkt som fjerner content-type...
+                // Undersøke videre senere, gjør det slik for nå for å rette feil.
+                const response = await fetch(`/rest/innsyn/v2/annenPartVedtak`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(body),
+                });
+
+                return JSON.parse(await response.json()) as AnnenPartVedtakDTO;
             } catch (error: any) {
                 // NOTE: inkluderer denne sjekken fordi den fantes før Tanstack refactor. Revurder om den behøves?
                 if (error.name === 'HTTPError') {
