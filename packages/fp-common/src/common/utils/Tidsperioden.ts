8db0a0e7dc98dfd804b37f97ed9fdbeb3afa4b3d
johannbm <johannes.moskvil@gmail.com>
2024-07-18 12:07:31 +0200

    redundant sjekk

--- packages/fp-common/src/common/utils/Tidsperioden.ts
+++ packages/fp-common/src/common/utils/Tidsperioden.ts
@@ -153,9 +153,9 @@ function erTidsperiodeUtenforTidsperiode(tidsperiode1: TidsperiodeDate, tidsperi
 }
 
 function tidsperiodeToString(tidsperiode: TidsperiodeDate, intl: IntlShape) {
-    const { fom, tom } = tidsperiode; // TODO: ifølge TS så er verken fom eller tom optional, forenkle?
+    const { fom, tom } = tidsperiode;
     if (fom && tom && dayjs(fom).isSame(tom, 'day')) {
-        return formaterDatoUtenDag(fom || tom);
+        return formaterDatoUtenDag(fom);
     }
     return intl.formatMessage(
         { id: 'tidsperiode' },
@@ -169,7 +169,7 @@ function tidsperiodeToString(tidsperiode: TidsperiodeDate, intl: IntlShape) {
 function tidsperiodeToStringKort(tidsperiode: TidsperiodeDate, intl: IntlShape) {
     const { fom, tom } = tidsperiode;
     if (fom && tom && dayjs(fom).isSame(tom, 'day')) {
-        return formaterDatoUtenDag(fom || tom);
+        return formaterDatoUtenDag(fom);
     }
     return intl.formatMessage(
         { id: 'tidsperiode.kort' },
