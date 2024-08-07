4f04f0c340132fc910f4376673c5d75786653b78
Tor Røneid <tor.roneid@nav.no>
2024-08-07 10:31:37 +0200

    Fpelleres testar (#4601)
    * VV Skriv testar for veivisar fpEllerEs

--- apps/veivisere/src/veivisere/fpEllerEs/situasjon/SituasjonSide.tsx
+++ apps/veivisere/src/veivisere/fpEllerEs/situasjon/SituasjonSide.tsx
@@ -49,13 +49,15 @@ const SituasjonSide: FunctionComponent<Props> = ({ satser, fpEllerEsSituasjon, s
         shouldUnregister: true,
     });
 
-    const situasjon = formMethods.watch('situasjon');
-    const erIArbeid = formMethods.watch('erIArbeid');
-    const harHattInntekt = formMethods.watch('harHattInntekt');
-    const lønnPerMåned = formMethods.watch('lønnPerMåned');
-    const borDuINorge = formMethods.watch('borDuINorge');
-    const harHattAndreInntekter = formMethods.watch('harHattAndreInntekter');
-    const erDuMedlemAvFolketrygden = formMethods.watch('erDuMedlemAvFolketrygden');
+    const {
+        situasjon,
+        erIArbeid,
+        harHattInntekt,
+        lønnPerMåned,
+        borDuINorge,
+        harHattAndreInntekter,
+        erDuMedlemAvFolketrygden,
+    } = formMethods.watch();
 
     const onSubmit = (formValues: FpEllerEsSituasjon) => {
         setFpEllerEsSituasjon(formValues);
@@ -152,7 +154,6 @@ const SituasjonSide: FunctionComponent<Props> = ({ satser, fpEllerEsSituasjon, s
                             </BodyShort>
                         </Infobox>
                     )}
-
                     {harHattInntekt && (
                         <VStack gap="4">
                             <BluePanel isDarkBlue={lønnPerMåned === undefined} shouldFadeIn>
