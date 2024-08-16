b7a973a801b1322796af406a24e9b1a7f4e491c1
tor-nav <tor.roneid@nav.no>
2024-08-16 09:55:54 +0200

    FIX

--- apps/veivisere/src/utils/satserUtils.ts
+++ apps/veivisere/src/utils/satserUtils.ts
@@ -2,7 +2,7 @@ import { Satser } from '@navikt/fp-types';
 import { notEmpty } from '@navikt/fp-validation';
 
 export const finnSisteGrunnbeløp = (satser: Satser): number => {
-    return notEmpty(satser.grunnbeløp[34340]).verdi;
+    return notEmpty(satser.grunnbeløp[0]).verdi;
 };
 
 export const finnSisteEngangsstønad = (satser: Satser): number => {
