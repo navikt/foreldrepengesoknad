2d057396ff447b81eab46bc840bfb72eee7d14f9
tor-nav <tor.roneid@nav.no>
2024-08-22 14:11:16 +0200

    FIX sonar warning

--- packages/ui/src/boxes/BlueHeading.tsx
+++ packages/ui/src/boxes/BlueHeading.tsx
@@ -1,14 +1,12 @@
 import { Box } from '@navikt/ds-react';
 
 interface Props {
-    isDarkBlue?: boolean;
     children: React.ReactElement | React.ReactElement[];
 }
 
-const BlueHeading: React.FunctionComponent<Props> = ({ children, isDarkBlue = false }) => (
+const BlueHeading: React.FunctionComponent<Props> = ({ children }) => (
     <Box
-        //TODO Fiks korrekt farge for dark blue
-        background={isDarkBlue ? 'surface-alt-3-subtle' : 'surface-alt-3-subtle'}
+        background="surface-alt-3-subtle"
         borderRadius="medium medium 0 0"
         padding="6"
         borderWidth="0 0 2"
