0282e58070580cab5a59db3cc514f5bdc84a919d
johannbm <johannes.moskvil@gmail.com>
2024-09-18 20:48:14 +0200

    Fiks

--- apps/foreldrepengeoversikt/src/app/bootstrap.tsx
+++ apps/foreldrepengeoversikt/src/app/bootstrap.tsx
@@ -7,9 +7,11 @@ import 'dayjs/locale/nn.js';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+// Viktig at index.css importeres før AppContainer. Det gjør at Aksel sin css laster før vår i de tilfellene vi vil overskrive.
+import '@navikt/ds-css';
+
 import AppContainer from './AppContainer';
 import { initAmplitude } from './amplitude/amplitude';
-// Viktig at index.css importeres før AppContainer. Det gjør at Aksel sin css laster før vår i de tilfellene vi vil overskrive.
 import './index.css';
 
 dayjs.locale('nb');
