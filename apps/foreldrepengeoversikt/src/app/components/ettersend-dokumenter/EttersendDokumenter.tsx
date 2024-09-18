9fe9ddca7abb7658a7344ba1441390d5fd6536f7
johannbm <johannes.moskvil@gmail.com>
2024-09-18 20:59:57 +0200

    dokument lenker

--- apps/foreldrepengeoversikt/src/app/components/ettersend-dokumenter/EttersendDokumenter.tsx
+++ apps/foreldrepengeoversikt/src/app/components/ettersend-dokumenter/EttersendDokumenter.tsx
@@ -3,26 +3,27 @@ import { Link } from 'react-router-dom';
 
 import { HStack, LinkPanel } from '@navikt/ds-react';
 
-import { bemUtils } from '@navikt/fp-utils';
-
 import OversiktRoutes from 'app/routes/routes';
 
 import './ettersend-dokumenter.css';
 
 const EttersendDokumenter = () => {
-    const bem = bemUtils('ettersend-dokumenter');
     return (
         <LinkPanel
             as={Link}
             to={`${OversiktRoutes.DOKUMENTER}/${OversiktRoutes.ETTERSEND}`}
             border={false}
-            className={bem.element('linkPanel')}
+            className="rounded-lg"
         >
-            <HStack gap="1">
-                <FilesIcon className={bem.element('icon')} width={52} height={52} color="#0056B4" aria-hidden={true} />
-                <LinkPanel.Title as="h2" className={bem.element('link')}>
-                    Ettersend dokumentasjon
-                </LinkPanel.Title>
+            <HStack gap="4" align="center">
+                <FilesIcon
+                    className="p-2 rounded-[50%] bg-deepblue-100"
+                    width={52}
+                    height={52}
+                    color="#0056B4"
+                    aria-hidden={true}
+                />
+                <LinkPanel.Title as="h2">Ettersend dokumentasjon</LinkPanel.Title>
             </HStack>
         </LinkPanel>
     );
--- apps/foreldrepengeoversikt/src/app/components/se-dokumenter/SeDokumenter.tsx
+++ apps/foreldrepengeoversikt/src/app/components/se-dokumenter/SeDokumenter.tsx
@@ -3,28 +3,20 @@ import { Link } from 'react-router-dom';
 
 import { HStack, LinkPanel } from '@navikt/ds-react';
 
-import { bemUtils } from '@navikt/fp-utils';
-
 import OversiktRoutes from 'app/routes/routes';
 
-import './se-dokumenter.css';
-
 const SeDokumenter = () => {
-    const bem = bemUtils('se-dokumenter');
-
     return (
-        <LinkPanel as={Link} to={OversiktRoutes.DOKUMENTER} border={false} className={bem.element('linkPanel')}>
-            <HStack gap="1">
+        <LinkPanel as={Link} to={OversiktRoutes.DOKUMENTER} border={false} className="rounded-lg">
+            <HStack gap="4" align="center">
                 <FolderFileIcon
-                    className={bem.element('icon')}
+                    className="p-2 rounded-[50%] bg-deepblue-100"
                     width={52}
                     height={52}
                     color="#0056B4"
                     aria-hidden={true}
                 />
-                <LinkPanel.Title className={bem.element('link')} as="h2">
-                    <div className={bem.block}>Dokumenter</div>
-                </LinkPanel.Title>
+                <LinkPanel.Title as="h2">Dokumenter</LinkPanel.Title>
             </HStack>
         </LinkPanel>
     );
