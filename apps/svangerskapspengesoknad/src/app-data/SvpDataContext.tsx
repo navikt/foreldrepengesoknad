c86a6321d493067b569bc3c303ba3a4c790aa451
tor-nav <tor.roneid@nav.no>
2024-09-18 15:23:40 +0200

    fix

--- apps/svangerskapspengesoknad/src/app-data/SvpDataContext.tsx
+++ apps/svangerskapspengesoknad/src/app-data/SvpDataContext.tsx
@@ -3,10 +3,13 @@ import { ArbeidIUtlandet } from 'types/ArbeidIUtlandet';
 import { Barn } from 'types/Barn';
 import Tilrettelegging from 'types/Tilrettelegging';
 
-import { ArbeidsforholdOgInntektSvp } from '@navikt/fp-steg-arbeidsforhold-og-inntekt';
-import { EgenNæring } from '@navikt/fp-steg-egen-naering';
-import { Frilans } from '@navikt/fp-steg-frilans';
-import { Utenlandsopphold, UtenlandsoppholdPeriode } from '@navikt/fp-types';
+import {
+    ArbeidsforholdOgInntektSvp,
+    EgenNæring,
+    Frilans,
+    Utenlandsopphold,
+    UtenlandsoppholdPeriode,
+} from '@navikt/fp-types';
 
 import SøknadRoutes from './routes';
 
