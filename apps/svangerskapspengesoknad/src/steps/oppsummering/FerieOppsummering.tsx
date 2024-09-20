4311a4d15c8497d8219487fcf76e9952d888a0e2
johannbm <johannes.moskvil@gmail.com>
2024-09-20 14:55:13 +0200

    Fiks FerieOppsummering.tsx

--- apps/svangerskapspengesoknad/src/steps/oppsummering/FerieOppsummering.tsx
+++ apps/svangerskapspengesoknad/src/steps/oppsummering/FerieOppsummering.tsx
@@ -1,18 +1,21 @@
 import { ContextDataType, useContextGetData } from 'appData/SvpDataContext';
 import { FormattedMessage } from 'react-intl';
+import { AvtaltFerie } from 'types/AvtaltFerie';
 
 import { FormSummary } from '@navikt/ds-react';
 
 import { JaNeiTekst } from '@navikt/fp-steg-oppsummering';
-import { formatDate } from '@navikt/fp-utils';
+import { capitalizeFirstLetterInEveryWordOnly, formatDate } from '@navikt/fp-utils';
+
+import { notEmpty } from '../../../../../packages/validation';
 
 export function FerieOppsummering({ onVilEndreSvar }: { readonly onVilEndreSvar: () => void }) {
     const ferie = useContextGetData(ContextDataType.FERIE);
+    const tilrettelegginger = notEmpty(useContextGetData(ContextDataType.TILRETTELEGGINGER));
 
     if (!ferie) {
         return null;
     }
-    console.log(ferie);
 
     return (
         <FormSummary>
@@ -23,35 +26,67 @@ export function FerieOppsummering({ onVilEndreSvar }: { readonly onVilEndreSvar:
                 </FormSummary.EditLink>
             </FormSummary.Header>
             <FormSummary.Answers>
-                <FormSummary.Answer>
-                    <FormSummary.Label>
-                        Har du planlagt ferie i perioden du skal ha svangerskapspenger?
-                    </FormSummary.Label>
-                    <JaNeiTekst ja={ferie.length > 0} />
-                </FormSummary.Answer>
-
-                {ferie.length > 0 && (
-                    <>
-                        <FormSummary.Answer>
-                            <FormSummary.Label>Hvor mange perioder med ferie skal du ha?</FormSummary.Label>
-                            <FormSummary.Value>{ferie.length}</FormSummary.Value>
-                        </FormSummary.Answer>
-
-                        <FormSummary.Answer>
-                            <FormSummary.Label>Periode med ferie</FormSummary.Label>
-                            <FormSummary.Value>
-                                <ul>
-                                    {ferie.map((feriePeriode) => (
-                                        <li key={`${feriePeriode.fom}-${feriePeriode.tom}`}>
-                                            {formatDate(feriePeriode.fom)} - {formatDate(feriePeriode.tom)}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </FormSummary.Value>
-                        </FormSummary.Answer>
-                    </>
+                {tilrettelegginger.length > 1 ? (
+                    <FlereArbeidsgivereFerieOppsummering avtaltFerie={ferie} />
+                ) : (
+                    <FeriePeriodeOppsummering avtaltFerie={ferie} />
                 )}
             </FormSummary.Answers>
         </FormSummary>
     );
 }
+
+const FeriePeriodeOppsummering = ({ avtaltFerie }: { avtaltFerie: AvtaltFerie[] }) => {
+    return (
+        <>
+            <FormSummary.Answer>
+                <FormSummary.Label>Har du planlagt ferie i perioden du skal ha svangerskapspenger?</FormSummary.Label>
+                <JaNeiTekst ja={avtaltFerie.length > 0} />
+            </FormSummary.Answer>
+
+            {avtaltFerie.length > 0 && (
+                <>
+                    <FormSummary.Answer>
+                        <FormSummary.Label>Hvor mange perioder med ferie skal du ha?</FormSummary.Label>
+                        <FormSummary.Value>{avtaltFerie.length}</FormSummary.Value>
+                    </FormSummary.Answer>
+
+                    <FormSummary.Answer>
+                        <FormSummary.Label>Periode med ferie</FormSummary.Label>
+                        <FormSummary.Value>
+                            <ul>
+                                {avtaltFerie.map((feriePeriode) => (
+                                    <li key={`${feriePeriode.fom}-${feriePeriode.tom}`}>
+                                        {formatDate(feriePeriode.fom)} - {formatDate(feriePeriode.tom)}
+                                    </li>
+                                ))}
+                            </ul>
+                        </FormSummary.Value>
+                    </FormSummary.Answer>
+                </>
+            )}
+        </>
+    );
+};
+
+const FlereArbeidsgivereFerieOppsummering = ({ avtaltFerie }: { avtaltFerie: AvtaltFerie[] }) => {
+    const tilrettelegginger = notEmpty(useContextGetData(ContextDataType.TILRETTELEGGINGER));
+
+    return tilrettelegginger.map((tilrettelegging) => {
+        const perioderForDenneTilretteleggingen = avtaltFerie.filter(
+            (periode) => periode.arbeidsforhold.id === tilrettelegging.arbeidsforhold.arbeidsgiverId,
+        );
+        return (
+            <FormSummary.Answer key={tilrettelegging.id}>
+                <FormSummary.Label>
+                    {capitalizeFirstLetterInEveryWordOnly(tilrettelegging.arbeidsforhold.navn)}
+                </FormSummary.Label>
+                <FormSummary.Value>
+                    <FormSummary.Answers>
+                        <FeriePeriodeOppsummering avtaltFerie={perioderForDenneTilretteleggingen} />
+                    </FormSummary.Answers>
+                </FormSummary.Value>
+            </FormSummary.Answer>
+        );
+    });
+};
