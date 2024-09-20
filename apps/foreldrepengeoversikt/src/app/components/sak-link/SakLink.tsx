51c7e08136d3d59a21a081aae4fd91afcdb9c8af
johannbm <johannes.moskvil@gmail.com>
2024-09-20 21:21:30 +0200

    Fiks SakLink

--- apps/foreldrepengeoversikt/src/app/components/sak-link/SakLink.tsx
+++ apps/foreldrepengeoversikt/src/app/components/sak-link/SakLink.tsx
@@ -1,7 +1,3 @@
-import { Link } from 'react-router-dom';
-
-import { Heading, LinkPanel, VStack } from '@navikt/ds-react';
-
 import { formatDate } from '@navikt/fp-utils';
 
 import { LenkePanel } from 'app/components/lenke-panel/LenkePanel';
@@ -9,7 +5,6 @@ import { Sak } from 'app/types/Sak';
 import { Ytelse } from 'app/types/Ytelse';
 
 import StatusTag from '../status-tag/StatusTag';
-import styles from './sak-link.module.css';
 
 interface Props {
     sak: Sak;
@@ -28,24 +23,12 @@ const getHeading = (ytelse: Ytelse) => {
 
 const SakLink: React.FunctionComponent<Props> = ({ sak }) => {
     return (
-        <>
-            <LenkePanel
-                tag={<StatusTag sak={sak} />}
-                undertittel={`Sist oppdatert ${formatDate(sak.oppdatertTidspunkt)}`}
-                tittel={getHeading(sak.ytelse)}
-                to={`/sak/${sak.saksnummer}`}
-            />
-
-            <LinkPanel as={Link} to={`/sak/${sak.saksnummer}`} className={styles.sakLink}>
-                <VStack gap="1">
-                    <Heading level="3" size="medium">
-                        {getHeading(sak.ytelse)}
-                    </Heading>
-                    <span>Sist oppdatert {formatDate(sak.oppdatertTidspunkt)}</span>
-                </VStack>
-                <StatusTag sak={sak} />
-            </LinkPanel>
-        </>
+        <LenkePanel
+            tag={<StatusTag sak={sak} />}
+            undertittel={`Sist oppdatert ${formatDate(sak.oppdatertTidspunkt)}`}
+            tittel={getHeading(sak.ytelse)}
+            to={`/sak/${sak.saksnummer}`}
+        />
     );
 };
 
