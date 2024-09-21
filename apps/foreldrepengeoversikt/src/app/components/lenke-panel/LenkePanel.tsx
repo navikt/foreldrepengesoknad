b4ebf49b267d1f55149dd18321c6074738f15e1d
Johannes Moskvil <johannbm@users.noreply.github.com>
2024-09-21 12:11:54 +0200

    Bruk nytt lenkepanel paa forside (#4690)

--- apps/foreldrepengeoversikt/src/app/components/lenke-panel/LenkePanel.tsx
+++ apps/foreldrepengeoversikt/src/app/components/lenke-panel/LenkePanel.tsx
@@ -1,5 +1,6 @@
 import { ArrowRightIcon } from '@navikt/aksel-icons';
 import classNames from 'classnames';
+import { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 
 import { BodyShort, HStack, VStack } from '@navikt/ds-react';
@@ -12,29 +13,33 @@ type Props = {
     undertittel?: string;
     className?: string;
     Ikon?: typeof ArrowRightIcon; // TODO: hacky?
+    tag?: ReactNode;
 };
-export const LenkePanel = ({ to, tittel, undertittel, Ikon, className }: Props) => {
+export const LenkePanel = ({ to, tittel, undertittel, Ikon, className, tag }: Props) => {
     return (
         <Link to={to} className={classNames(styles.LenkePanel, className)}>
-            <HStack gap="4" align="center">
-                {Ikon && (
-                    <Ikon
-                        className="p-1 rounded-[50%] bg-deepblue-100 text-icon-info"
-                        width={32}
-                        height={32}
-                        aria-hidden
-                    />
-                )}
-                <VStack gap="0">
-                    <BodyShort size="medium" weight="semibold">
-                        {tittel}
-                    </BodyShort>
-                    {undertittel && (
-                        <BodyShort size="small" textColor="subtle">
-                            {undertittel}
-                        </BodyShort>
+            <HStack gap="4" align="center" justify="space-between">
+                <HStack gap="4" align="center">
+                    {Ikon && (
+                        <Ikon
+                            className="p-1 rounded-[50%] bg-deepblue-100 text-icon-info"
+                            width={32}
+                            height={32}
+                            aria-hidden
+                        />
                     )}
-                </VStack>
+                    <VStack gap="0">
+                        <BodyShort size="medium" weight="semibold">
+                            {tittel}
+                        </BodyShort>
+                        {undertittel && (
+                            <BodyShort size="small" textColor="subtle">
+                                {undertittel}
+                            </BodyShort>
+                        )}
+                    </VStack>
+                </HStack>
+                {tag}
             </HStack>
             <ArrowRightIcon className="text-icon-info" fontSize="24" aria-hidden />
         </Link>
--- apps/foreldrepengeoversikt/src/app/components/sak-link/SakLink.tsx
+++ apps/foreldrepengeoversikt/src/app/components/sak-link/SakLink.tsx
@@ -1,14 +1,10 @@
-import { Link } from 'react-router-dom';
-
-import { Heading, LinkPanel, VStack } from '@navikt/ds-react';
-
 import { formatDate } from '@navikt/fp-utils';
 
+import { LenkePanel } from 'app/components/lenke-panel/LenkePanel';
 import { Sak } from 'app/types/Sak';
 import { Ytelse } from 'app/types/Ytelse';
 
 import StatusTag from '../status-tag/StatusTag';
-import styles from './sak-link.module.css';
 
 interface Props {
     sak: Sak;
@@ -25,18 +21,13 @@ const getHeading = (ytelse: Ytelse) => {
     }
 };
 
-const SakLink: React.FunctionComponent<Props> = ({ sak }) => {
+export const SakLink: React.FunctionComponent<Props> = ({ sak }) => {
     return (
-        <LinkPanel as={Link} to={`/sak/${sak.saksnummer}`} className={styles.sakLink}>
-            <VStack gap="1">
-                <Heading level="3" size="medium">
-                    {getHeading(sak.ytelse)}
-                </Heading>
-                <span>Sist oppdatert {formatDate(sak.oppdatertTidspunkt)}</span>
-            </VStack>
-            <StatusTag sak={sak} />
-        </LinkPanel>
+        <LenkePanel
+            tag={<StatusTag sak={sak} />}
+            undertittel={`Sist oppdatert ${formatDate(sak.oppdatertTidspunkt)}`}
+            tittel={getHeading(sak.ytelse)}
+            to={`/sak/${sak.saksnummer}`}
+        />
     );
 };
-
-export default SakLink;
