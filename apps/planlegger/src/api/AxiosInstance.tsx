62cb177a0fc0e203bff1368e492336f138e279d0
tor-nav <tor.roneid@nav.no>
2024-10-01 11:35:46 +0200

    PL Bytt frå Axios til Tanstack

--- apps/planlegger/src/api/AxiosInstance.tsx
+++
@@ -1,5 +0,0 @@
-import Environment from 'appData/Environment';
-
-import { getAxiosInstance } from '@navikt/fp-api';
-
-export const AxiosInstanceAPI = () => getAxiosInstance({ baseUrl: Environment.PUBLIC_PATH });
