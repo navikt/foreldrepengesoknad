e8e22874d6293012f2065b1fbd57edd9123aa267
johannbm <johannes.moskvil@gmail.com>
2024-10-17 20:53:42 +0200

    forenkle oversiktsside

--- apps/foreldrepengeoversikt/src/pages/inntektsmelding-page/InntektsmeldingOversiktPage.tsx
+++ apps/foreldrepengeoversikt/src/pages/inntektsmelding-page/InntektsmeldingOversiktPage.tsx
@@ -1,6 +1,5 @@
 import { Buildings3Icon } from '@navikt/aksel-icons';
 import { useQuery } from '@tanstack/react-query';
-import { groupBy } from 'lodash';
 import { Navigate, useParams } from 'react-router-dom';
 
 import { Tag, VStack } from '@navikt/ds-react';
@@ -32,29 +31,25 @@ export const InntektsmeldingOversiktPage = () => {
         );
     }
 
-    const imPerArbeidsgiver = groupBy(aktiveInntektsmeldinger, (im) => im.arbeidsgiverNavn);
-
     return (
         <PageRouteLayout header={<InntektsmeldingOversiktHeader />}>
             <VStack gap="4">
-                {Object.values(imPerArbeidsgiver).map((ims) =>
-                    ims.map((im) => (
-                        <LenkePanel
-                            key={im.journalpostId}
-                            tittel={im.arbeidsgiverNavn}
-                            undertittel={`Sendt ${formaterDatoUtenDag(im.mottattTidspunkt)}`}
-                            tag={
-                                ims.length > 1 && im.stillingsprosent ? (
-                                    <Tag size="small" variant="success">
-                                        {im.stillingsprosent}% stilling
-                                    </Tag>
-                                ) : null
-                            }
-                            to={im.journalpostId}
-                            Ikon={Buildings3Icon}
-                        />
-                    )),
-                )}
+                {aktiveInntektsmeldinger.map((im) => (
+                    <LenkePanel
+                        key={im.journalpostId}
+                        tittel={im.arbeidsgiverNavn}
+                        undertittel={`Sendt ${formaterDatoUtenDag(im.mottattTidspunkt)}`}
+                        tag={
+                            im.stillingsprosent ? (
+                                <Tag size="small" variant="success">
+                                    {im.stillingsprosent}% stilling
+                                </Tag>
+                            ) : null
+                        }
+                        to={im.journalpostId}
+                        Ikon={Buildings3Icon}
+                    />
+                ))}
             </VStack>
         </PageRouteLayout>
     );
