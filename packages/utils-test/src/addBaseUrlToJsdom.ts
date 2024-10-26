df285f22f1656cf7d1044802a8826c9dd9376adb
johannbm <johannes.moskvil@gmail.com>
2024-10-26 18:56:06 +0200

    forklarende kommentar

--- packages/utils-test/src/addBaseUrlToJsdom.ts
+++ packages/utils-test/src/addBaseUrlToJsdom.ts
@@ -1,13 +1,16 @@
-// TODO: legg til forklaring til hvorfor dette trengs
+/**
+ * Vitest kjører i jsdom, og ikke i browser. Som følge av det så finnes ikke en location.
+ * Det gjør at new URL() kall vil feile.
+ * Denne funksjonen overskriver new URL() konstruktøren til å legge på en base hvis den ikke finnes.
+ */
 export const addBaseUrlToJsdom = () => {
     const originalURL = global.URL;
 
     // @ts-expect-error
     global.URL = class extends originalURL {
         constructor(url: string, base: string) {
-            // Supply your own base URL if the base is not provided
             if (!base) {
-                base = 'http://localhost:3000'; // Your custom base URL
+                base = 'http://localhost:3000'; // Ikke så farlig hva denne er. Poenet er at det må finnes en baseUrl
             }
             super(url, base);
         }
