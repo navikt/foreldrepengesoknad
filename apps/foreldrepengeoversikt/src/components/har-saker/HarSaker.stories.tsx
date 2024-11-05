4ca57d9033d25ca23f94d567a323b24e3677bc4d
Tor Røneid <tor.roneid@nav.no>
2024-11-05 08:24:11 +0100

    FO Lagt til lenker til til infosider for ytelsane + oppdatert tekst n… (#4758)
    FO Lagt til lenker til til infosider for ytelsane + oppdatert tekst når ein ikkje har saker

--- apps/foreldrepengeoversikt/src/components/har-saker/HarSaker.stories.tsx
+++ apps/foreldrepengeoversikt/src/components/har-saker/HarSaker.stories.tsx
@@ -3,7 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import saker from 'storybookData/saker/saker.json';
 
 import { Ytelse } from './../../types/Ytelse';
-import HarSaker from './HarSaker';
+import { HarSaker } from './HarSaker';
 
 const meta = {
     title: 'HarSaker',
---
+++ apps/foreldrepengeoversikt/src/components/søkelenker/SøkelenkerPanel.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react';
+import { useIntl } from 'react-intl';
+
+import { Bleed, HGrid } from '@navikt/ds-react';
+
+import { links } from '@navikt/fp-constants';
+
+import { LenkePanel } from '../../components/lenke-panel/LenkePanel';
+import { LayoutWrapper } from '../../sections/LayoutWrapper';
+
+const PanelBleed = ({ children, doBleed }: { children: ReactNode; doBleed: boolean }) => {
+    return doBleed ? (
+        <div className="pt-12">
+            <Bleed marginInline="full" className="bg-surface-neutral-subtle">
+                <LayoutWrapper className="pt-1 pb-1 pl-4 pr-4">{children}</LayoutWrapper>
+            </Bleed>
+        </div>
+    ) : (
+        children
+    );
+};
+
+interface Props {
+    doBleed?: boolean;
+}
+
+export const SøkelenkerPanel = ({ doBleed = false }: Props) => {
+    const intl = useIntl();
+    return (
+        <PanelBleed doBleed={doBleed}>
+            <HGrid gap="4" columns={{ sm: 1, md: 2 }} className={doBleed ? 'mt-12 mb-12' : undefined}>
+                <LenkePanel
+                    tittel={intl.formatMessage({ id: 'SøkelenkerPanel.HarRett' })}
+                    undertittel={intl.formatMessage({ id: 'SøkelenkerPanel.VenterBarn' })}
+                    to={links.rettOgPlikt}
+                />
+                <LenkePanel tittel={intl.formatMessage({ id: 'SøkelenkerPanel.SokFp' })} to={links.foreldrepenger} />
+                <LenkePanel
+                    tittel={intl.formatMessage({ id: 'SøkelenkerPanel.SokSvp' })}
+                    to={links.svangerskapspenger}
+                />
+                <LenkePanel tittel={intl.formatMessage({ id: 'SøkelenkerPanel.SokEs' })} to={links.engangsstonad} />
+            </HGrid>
+        </PanelBleed>
+    );
+};
