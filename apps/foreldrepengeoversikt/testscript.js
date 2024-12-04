419d1144325a56f26c20eede3162a937b66376ab
johannbm <johannes.moskvil@gmail.com>
2024-12-04 14:49:09 +0100

    prøv denne

---
+++ apps/foreldrepengeoversikt/testscript.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+
+const parseViteOutput = (filePath) => {
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const lines = fileContent.split('\n');
+    const result = [];
+
+    for (const line of lines) {
+        if (line.startsWith('dist')) {
+            const parts = line.split(' ').filter(Boolean);
+            result.push({
+                name: parts[0],
+                size: Number(parts[1].replace(',', '')),
+            });
+        }
+    }
+
+    return [{ name: 'Total Size', size: result.reduce((prev, info) => prev + info.size, 0) }];
+};
+
+const outputPath = './build-output.log';
+const size = parseViteOutput(outputPath);
+console.log(JSON.stringify(size));
