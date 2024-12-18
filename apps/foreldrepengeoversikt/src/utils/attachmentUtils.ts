f79404e4e9e39613d48e6a7c15d6dfb8dba059fb
johannbm <johannes.moskvil@gmail.com>
2024-12-18 11:59:08 +0100

    fjern bytes

--- apps/foreldrepengeoversikt/src/utils/attachmentUtils.ts
+++ apps/foreldrepengeoversikt/src/utils/attachmentUtils.ts
@@ -1,41 +1,4 @@
-import Bytes from 'bytes';
-
-import { AttachmentType, InnsendingsType, Skjemanummer } from '@navikt/fp-constants';
 import { Attachment } from '@navikt/fp-types';
-
-import { guid } from './guid';
-
-const generateAttachmentId = () => 'V'.concat(guid().replace(/-/g, ''));
-
-export const mapFilTilVedlegg = (
-    file: File,
-    type: AttachmentType,
-    skjemanummer: Skjemanummer,
-    innsendingsType?: InnsendingsType,
-): Attachment => ({
-    id: generateAttachmentId(),
-    file,
-    filename: file.name,
-    filesize: file.size,
-    uploaded: false,
-    pending: false,
-    type,
-    skjemanummer,
-    innsendingsType,
-});
-
-export const bytesString = (bytes: number): string => {
-    return Bytes(bytes, {
-        unitSeparator: ' ',
-        thousandsSeparator: ' ',
-        decimalPlaces: 1,
-        fixedDecimals: false,
-    });
-};
-
-export const deleteAttachment = (attachments: Attachment[], deleted: Attachment): Attachment[] => {
-    return attachments.filter((att) => att !== deleted);
-};
 
 export const isAttachmentWithError = ({ pending, uploaded, filesize }: Attachment) =>
     (pending === false && uploaded === false) || filesize === 0;
