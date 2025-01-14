09a763862e9733b5bae62eb7af484db12608e906
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-01-14 11:31:01 +0100

    Oppdater confirmationpanel til å være i tråd med aksels skriveregler for punktliste  (#4807)

--- apps/foreldrepengesoknad/src/pages/forside/dine-plikter/DinePlikter.tsx
+++ apps/foreldrepengesoknad/src/pages/forside/dine-plikter/DinePlikter.tsx
@@ -19,6 +19,7 @@ export const DinePlikter = () => {
                             ),
                         }}
                     />
+                    .
                 </BodyLong>
             </li>
             <li>
