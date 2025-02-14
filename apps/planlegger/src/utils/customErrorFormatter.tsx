9a763fad0134f1e8464639f180d8654a75affa50
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-02-14 11:49:23 +0100

    refactor: Remove unnecessary padding from custom error formatter

--- apps/planlegger/src/utils/customErrorFormatter.tsx
+++ apps/planlegger/src/utils/customErrorFormatter.tsx
@@ -3,10 +3,6 @@ export const formatError = (error?: string) => {
         <span
             role="alert"
             style={{
-                paddingLeft: 5,
-                paddingRight: 5,
-                paddingBottom: 5,
-                paddingTop: 5,
                 borderRadius: 4,
                 width: '100%',
             }}
