cf9e28cbcb55cff27869e0f30c999a54591e4baf
johannbm <johannes.moskvil@gmail.com>
2025-02-26 08:43:58 +0100

    legg til komponent for svpSak

---
+++ apps/foreldrepengeoversikt/src/components/svangerskapspenger/Svangerskapspenger.tsx
@@ -0,0 +1,8 @@
+import { SvangerskapspengeSak } from '../../types/SvangerskapspengeSak';
+
+type SvangerskapspengerProps = {
+    svpSak: SvangerskapspengeSak;
+};
+export const Svangerskapspenger = ({ svpSak }: SvangerskapspengerProps) => {
+    return 'SVP';
+};
