ed8a142cd454bd5a5180d17e463185a757810924
johannbm <johannes.moskvil@gmail.com>
2025-03-13 08:28:54 +0100

    skriv tester

--- apps/foreldrepengeoversikt/src/pages/saksoversikt/Saksoversikt.test.tsx
+++ apps/foreldrepengeoversikt/src/pages/saksoversikt/Saksoversikt.test.tsx
@@ -4,7 +4,7 @@ import { applyRequestHandlers } from 'msw-storybook-addon';
 
 import * as stories from './Saksoversikt.stories';
 
-const { Engangsstønad } = composeStories(stories);
+const { Engangsstønad, Foreldrepenger, Svangerskapspenger } = composeStories(stories);
 
 describe('<Saksoversikt>', () => {
     it('skal vise hvor mye engangsstønad en har rett på', async () => {
@@ -16,5 +16,35 @@ describe('<Saksoversikt>', () => {
         expect(screen.getByText('Engangsstønad på 92 648 kr')).toBeInTheDocument();
         expect(screen.getByText('Utbetales til kontonummer 23232323 hvis søknaden innvilges')).toBeInTheDocument();
         expect(screen.getByText('Endre kontonummer')).toBeInTheDocument();
+    });
+
+    it('skal kun vise Endre plan lenke for foreldrepenger', async () => {
+        await applyRequestHandlers(Foreldrepenger.parameters.msw);
+        render(<Foreldrepenger />);
+        expect(await screen.findByText('Endre planen din')).toBeInTheDocument();
+    });
+
+    it('skal IKKE vise Endre plan lenke for svangerskapspenger', async () => {
+        await applyRequestHandlers(Svangerskapspenger.parameters.msw);
+        render(<Svangerskapspenger />);
+
+        // Sjekk at tittel er på plass sånn at vi vet siden har lastet ...
+        expect(await screen.findByText('Din sak')).toBeInTheDocument();
+        expect((await screen.findAllByText('svangerskapspenger')).length).toBeGreaterThan(0);
+
+        // ... når vi senere skal sjekke for at noe ikke eksisterer.
+        expect(screen.queryByText('Endre planen din')).toBeNull();
+    });
+
+    it('skal IKKE vise Endre plan lenke for engangsstønad', async () => {
+        await applyRequestHandlers(Engangsstønad.parameters.msw);
+        render(<Engangsstønad />);
+
+        // Sjekk at tittel er på plass sånn at vi vet siden har lastet ...
+        expect(await screen.findByText('Din sak')).toBeInTheDocument();
+        expect((await screen.findAllByText('engangsstønad')).length).toBeGreaterThan(0);
+
+        // ... når vi senere skal sjekke for at noe ikke eksisterer.
+        expect(screen.queryByText('Endre planen din')).toBeNull();
     });
 });
