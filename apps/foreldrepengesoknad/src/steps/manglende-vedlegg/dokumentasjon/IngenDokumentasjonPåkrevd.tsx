a8ef5bbd50560d570f2080bed6c95ce434a76428
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-03-26 10:41:32 +0100

    Legg til meldinger for situasjonen der dokumentasjon ikke er påkrevd (#4871)

---
+++ apps/foreldrepengesoknad/src/steps/manglende-vedlegg/dokumentasjon/IngenDokumentasjonPåkrevd.tsx
@@ -0,0 +1,24 @@
+import { FormattedMessage } from 'react-intl';
+
+import { Alert, BodyLong, Heading } from '@navikt/ds-react';
+
+export const IngenDokumentasjonPåkrevd = () => {
+    return (
+        <>
+            <BodyLong>
+                <FormattedMessage id="dokumentasjon.ikke.påkrevd.tittel" />
+            </BodyLong>
+            <Alert variant="info">
+                <Heading level="2" size="small">
+                    <FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.tittel" />
+                </Heading>
+                <BodyLong spacing>
+                    <FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.beskrivelse.del1"></FormattedMessage>
+                </BodyLong>
+                <BodyLong>
+                    <FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.beskrivelse.del2"></FormattedMessage>
+                </BodyLong>
+            </Alert>
+        </>
+    );
+};
