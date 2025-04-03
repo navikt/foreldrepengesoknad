d2735c39f51e0c7d2962951a9026cb7b28ea28f0
johannbm <johannes.moskvil@gmail.com>
2025-04-03 13:36:53 +0200

    wip tekst fra møtet

--- apps/foreldrepengesoknad/src/steps/manglende-vedlegg/dokumentasjon/IngenDokumentasjonPåkrevd.tsx
+++ apps/foreldrepengesoknad/src/steps/manglende-vedlegg/dokumentasjon/IngenDokumentasjonPåkrevd.tsx
@@ -1,15 +1,20 @@
 import { FormattedMessage } from 'react-intl';
 
-import { BodyLong, Label, VStack } from '@navikt/ds-react';
+import { Alert, BodyLong, BodyShort, Heading, Label, VStack } from '@navikt/ds-react';
 
 export const IngenDokumentasjonPåkrevd = () => {
     return (
         <VStack gap="2">
-            <Label>
-                <FormattedMessage id="dokumentasjon.ikke.påkrevd.tittel" />
-            </Label>
-            <BodyLong className="text-text-subtle">
-                <FormattedMessage id="dokumentasjon.ikke.påkrevd.beskrivelse" />
+            <Label>Dokumentasjon på at mor er i arbeid</Label>
+            <BodyLong>
+                Du trenger ikke sende inn dokumentasjon. Vi innhenter opplysninger om mors arbeid fra arbeidsgiver og
+                arbeidstakerregisteret. Mor vil bli informert når søknaden blir sendt.
+                <br />
+                <br />
+                Vi tar kontakt med deg dersom vi trenger mer informasjon.
+                {/*<FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.tittel" />*/}
+                {/*<FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.beskrivelse.del1" />*/}
+                {/*<FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.beskrivelse.del2" />*/}
             </BodyLong>
         </VStack>
     );
