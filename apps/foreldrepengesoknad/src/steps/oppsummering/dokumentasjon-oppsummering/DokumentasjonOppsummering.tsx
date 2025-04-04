250494d041a7c70c17ee95277cb721d8538d8075
johannbm <johannes.moskvil@gmail.com>
2025-04-04 12:56:52 +0200

    Refaktorer og forenkle sending av useState til DokumentasjonOppsummering.tsx

--- apps/foreldrepengesoknad/src/steps/oppsummering/dokumentasjon-oppsummering/DokumentasjonOppsummering.tsx
+++ apps/foreldrepengesoknad/src/steps/oppsummering/dokumentasjon-oppsummering/DokumentasjonOppsummering.tsx
@@ -1,8 +1,7 @@
-import { useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { VedleggDataType } from 'types/VedleggDataType';
 
-import { FormSummary, Link, VStack } from '@navikt/ds-react';
+import { Alert, BodyLong, FormSummary, Heading, Link, VStack } from '@navikt/ds-react';
 
 import { NavnPåForeldre, Periode } from '@navikt/fp-common';
 import { InnsendingsType } from '@navikt/fp-constants';
@@ -22,7 +21,6 @@ interface Props {
 export const DokumentasjonOppsummering = ({
     alleVedlegg,
     onVilEndreSvar,
-    setManglerDokumentasjon,
     erSøkerFarEllerMedmor,
     navnPåForeldre,
     uttaksperioderSomManglerVedlegg,
@@ -35,61 +33,71 @@ export const DokumentasjonOppsummering = ({
             .flatMap((vedlegg) => vedlegg)
             .find((v) => v.innsendingsType === InnsendingsType.SEND_SENERE);
 
-    useEffect(() => {
-        if (harSendSenereDokument) {
-            setManglerDokumentasjon(true);
-        }
-    }, []);
-
     if (!harVedlegg) {
         return null;
     }
 
     return (
-        <FormSummary>
-            <FormSummary.Header>
-                <FormSummary.Heading level="2">
-                    {harSendSenereDokument ? (
-                        <FormattedMessage id="oppsummering.manglerDokumentasjon" />
-                    ) : (
-                        <FormattedMessage id="DokumentasjonOppsummering.Tittel" />
-                    )}
-                </FormSummary.Heading>
-                <FormSummary.EditLink onClick={onVilEndreSvar}>
-                    <FormattedMessage id="Oppsummering.EndreSvar" />
-                </FormSummary.EditLink>
-            </FormSummary.Header>
-            <FormSummary.Answers>
-                {Object.entries(alleVedlegg)
-                    .filter((idOgVedlegg) => idOgVedlegg[1].length > 0)
-                    .map((idOgVedlegg) => (
-                        <FormSummary.Answer key={idOgVedlegg[1][0].id}>
-                            <FormSummary.Label>
-                                {idOgVedlegg[1][0].innsendingsType === InnsendingsType.SEND_SENERE ? (
-                                    <DokumentasjonSendSenereLabel
-                                        attachment={idOgVedlegg[1][0]}
-                                        erFarEllerMedmor={erSøkerFarEllerMedmor}
-                                        navnPåForeldre={navnPåForeldre}
-                                        uttaksperioderSomManglerVedlegg={uttaksperioderSomManglerVedlegg}
-                                    />
-                                ) : (
-                                    <DokumentasjonLastetOppLabel attachment={idOgVedlegg[1][0]} />
-                                )}
-                            </FormSummary.Label>
-                            <FormSummary.Value>
-                                <VStack>
-                                    {idOgVedlegg[1]
-                                        .filter((vedlegg) => vedlegg.innsendingsType !== InnsendingsType.SEND_SENERE)
-                                        .map((vedlegg) => (
-                                            <Link key={vedlegg.id} href={vedlegg.url} target="_blank">
-                                                {vedlegg.filename}
-                                            </Link>
-                                        ))}
-                                </VStack>
-                            </FormSummary.Value>
-                        </FormSummary.Answer>
-                    ))}
-            </FormSummary.Answers>
-        </FormSummary>
+        <>
+            <FormSummary>
+                <FormSummary.Header>
+                    <FormSummary.Heading level="2">
+                        {harSendSenereDokument ? (
+                            <FormattedMessage id="oppsummering.manglerDokumentasjon" />
+                        ) : (
+                            <FormattedMessage id="DokumentasjonOppsummering.Tittel" />
+                        )}
+                    </FormSummary.Heading>
+                    <FormSummary.EditLink onClick={onVilEndreSvar}>
+                        <FormattedMessage id="Oppsummering.EndreSvar" />
+                    </FormSummary.EditLink>
+                </FormSummary.Header>
+                <FormSummary.Answers>
+                    {Object.entries(alleVedlegg)
+                        .filter((idOgVedlegg) => idOgVedlegg[1].length > 0)
+                        .map((idOgVedlegg) => (
+                            <FormSummary.Answer key={idOgVedlegg[1][0].id}>
+                                <FormSummary.Label>
+                                    {idOgVedlegg[1][0].innsendingsType === InnsendingsType.SEND_SENERE ? (
+                                        <DokumentasjonSendSenereLabel
+                                            attachment={idOgVedlegg[1][0]}
+                                            erFarEllerMedmor={erSøkerFarEllerMedmor}
+                                            navnPåForeldre={navnPåForeldre}
+                                            uttaksperioderSomManglerVedlegg={uttaksperioderSomManglerVedlegg}
+                                        />
+                                    ) : (
+                                        <DokumentasjonLastetOppLabel attachment={idOgVedlegg[1][0]} />
+                                    )}
+                                </FormSummary.Label>
+                                <FormSummary.Value>
+                                    <VStack>
+                                        {idOgVedlegg[1]
+                                            .filter(
+                                                (vedlegg) => vedlegg.innsendingsType !== InnsendingsType.SEND_SENERE,
+                                            )
+                                            .map((vedlegg) => (
+                                                <Link key={vedlegg.id} href={vedlegg.url} target="_blank">
+                                                    {vedlegg.filename}
+                                                </Link>
+                                            ))}
+                                    </VStack>
+                                </FormSummary.Value>
+                            </FormSummary.Answer>
+                        ))}
+                </FormSummary.Answers>
+            </FormSummary>
+            {harSendSenereDokument && (
+                <Alert variant="info">
+                    <VStack gap="2">
+                        <Heading size="small" level="2">
+                            <FormattedMessage id="oppsummering.manglerDokumentasjon.heading" />
+                        </Heading>
+                        <BodyLong>
+                            <FormattedMessage id="oppsummering.manglerDokumentasjon.content" />
+                        </BodyLong>
+                    </VStack>
+                </Alert>
+            )}
+        </>
     );
 };
