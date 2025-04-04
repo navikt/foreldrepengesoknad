778ac33792564011999e2ce7336562d75b6d377c
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-04-04 10:46:56 +0200

    Refactor documentation strings and update translations for "Ingen dokumentasjon påkrevd" section

--- apps/foreldrepengesoknad/src/steps/manglende-vedlegg/dokumentasjon/IngenDokumentasjonPåkrevd.tsx
+++ apps/foreldrepengesoknad/src/steps/manglende-vedlegg/dokumentasjon/IngenDokumentasjonPåkrevd.tsx
@@ -1,20 +1,18 @@
 import { FormattedMessage } from 'react-intl';
 
-import { Alert, BodyLong, BodyShort, Heading, Label, VStack } from '@navikt/ds-react';
+import { BodyLong, Label, VStack } from '@navikt/ds-react';
 
 export const IngenDokumentasjonPåkrevd = () => {
     return (
         <VStack gap="2">
-            <Label>Dokumentasjon på at mor er i arbeid</Label>
-            <BodyLong>
-                Du trenger ikke sende inn dokumentasjon. Vi innhenter opplysninger om mors arbeid fra arbeidsgiver og
-                arbeidstakerregisteret. Mor vil bli informert når søknaden blir sendt.
-                <br />
-                <br />
-                Vi tar kontakt med deg dersom vi trenger mer informasjon.
-                {/*<FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.tittel" />*/}
-                {/*<FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.beskrivelse.del1" />*/}
-                {/*<FormattedMessage id="dokumentasjon.ikke.påkrevd.alert.beskrivelse.del2" />*/}
+            <Label>
+                <FormattedMessage id="dokumentasjon.ikke.påkrevd.label" />
+            </Label>
+            <BodyLong className="text-text-subtle">
+                <FormattedMessage id="dokumentasjon.ikke.påkrevd.beskrivelse.1" />
+            </BodyLong>
+            <BodyLong className="text-text-subtle">
+                <FormattedMessage id="dokumentasjon.ikke.påkrevd.beskrivelse.2" />
             </BodyLong>
         </VStack>
     );
