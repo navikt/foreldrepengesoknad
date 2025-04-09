650551ae9f03653af20a6e0800ccc6f5bd5a1f0f
johannbm <johannes.moskvil@gmail.com>
2025-04-09 15:19:24 +0200

    wip

--- apps/svangerskapspengesoknad/src/utils/endresøknadUtils.ts
+++ apps/svangerskapspengesoknad/src/utils/endresøknadUtils.ts
@@ -10,7 +10,7 @@ import {
     Tilretteleggingstype,
 } from 'types/Tilrettelegging';
 
-import { ArbeidsforholdOgInntektSvp, SvpSak } from '../../../../packages/types';
+import { ArbeidsforholdOgInntektSvp, EGEN_NÆRING_ID, FRILANS_ID, SvpArbeidsforhold, SvpSak } from '@navikt/fp-types';
 
 export const tilSkjematilstandFraEksisterendeSak = (svpSak: SvpSak) => {
     const valgteArbeidsforhold = svpSak.gjeldendeVedtak?.arbeidsforhold
@@ -45,9 +45,8 @@ const tilBarnetSkjema = (svpSak: SvpSak) => {
 
 const tilFerie = (svpSak: SvpSak) => {
     const avtalteFeriePerArbeidsgiver = svpSak.gjeldendeVedtak?.arbeidsforhold.reduce((acc, a) => {
-        const arbeidsgiverId = a.aktivitet.arbeidsgiver?.id;
-        // TODO: dette caset
-        if (!arbeidsgiverId) {
+        const nøkkel = finnTilretteleggingsnøkkel(a.aktivitet);
+        if (!nøkkel) {
             return acc;
         }
         const b = a.oppholdsperioder
@@ -55,15 +54,15 @@ const tilFerie = (svpSak: SvpSak) => {
             .map((ferie) => {
                 return {
                     arbeidsforhold: {
-                        type: Arbeidsforholdstype.VIRKSOMHET, // TODO
-                        id: arbeidsgiverId,
+                        type: arbeidsforholdTypeUnionTilEnum(a.aktivitet),
+                        id: nøkkel,
                     },
                     fom: ferie.fom,
                     tom: ferie.tom,
                 };
             });
 
-        acc[arbeidsgiverId] = {
+        acc[nøkkel] = {
             skalHaFerie: b.length > 0,
             feriePerioder: b,
         };
@@ -112,4 +111,35 @@ const tilTilrettelegginger = (svpSak: SvpSak) => {
     );
 
     return tilrettelegginer;
+};
+
+const finnTilretteleggingsnøkkel = (aktivitet: SvpArbeidsforhold['aktivitet']) => {
+    switch (aktivitet.type) {
+        case 'ORDINÆRT_ARBEID':
+            return aktivitet.arbeidsgiver?.id;
+        case 'SELVSTENDIG_NÆRINGSDRIVENDE':
+            return EGEN_NÆRING_ID;
+        case 'FRILANS':
+            return FRILANS_ID;
+        case 'ANNET':
+            throw new Error('Fant ANNET som aktivitetstype.');
+        default:
+            throw new Error(`Fant ukjent aktivitetstype ${aktivitet.type}`);
+    }
+};
+
+// TODO: rotete
+const arbeidsforholdTypeUnionTilEnum = (aktivitet: SvpArbeidsforhold['aktivitet']) => {
+    switch (aktivitet.type) {
+        case 'ORDINÆRT_ARBEID':
+            return Arbeidsforholdstype.VIRKSOMHET;
+        case 'SELVSTENDIG_NÆRINGSDRIVENDE':
+            return Arbeidsforholdstype.PRIVAT;
+        case 'FRILANS':
+            return Arbeidsforholdstype.FRILANSER;
+        case 'ANNET':
+            throw new Error('Fant ANNET som aktivitetstype.');
+        default:
+            throw new Error(`Fant ukjent aktivitetstype ${aktivitet.type}`);
+    }
 };
