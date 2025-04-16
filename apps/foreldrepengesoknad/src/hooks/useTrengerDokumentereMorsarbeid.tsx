21c32e63131ab74085c476f53a214c6360fdcf3f
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-04-16 10:07:45 +0200

    Lag hook

---
+++ apps/foreldrepengesoknad/src/hooks/useTrengerDokumentereMorsarbeid.tsx
@@ -0,0 +1,52 @@
+// apps/foreldrepengesoknad/src/hooks/useTrengerDokumentereMorsArbeid.ts
+import { useQuery } from '@tanstack/react-query';
+import { ContextDataType, useContextGetData } from 'appData/FpDataContext';
+import { DokumentereMorsArbeidParams, trengerDokumentereMorsArbeidOptions } from 'appData/api';
+
+import { AnnenForelder, Barn, Periode } from '@navikt/fp-common';
+import { isFødtBarn } from '@navikt/fp-types';
+import { getFamiliehendelsedato } from '@navikt/fp-utils';
+import { notEmpty } from '@navikt/fp-validation';
+
+const getDokumentereMorsArbeidParams = (
+    uttaksplan: Periode[],
+    barn: Barn,
+    annenPartFødselsnummer?: string,
+): DokumentereMorsArbeidParams | undefined => {
+    if (!annenPartFødselsnummer) {
+        return undefined;
+    }
+
+    const perioderMedAktivitetskrav = uttaksplan.filter(
+        (p): p is Periode & { morsAktivitetIPerioden: unknown } => 'morsAktivitetIPerioden' in p, // Sjekker om p har morsAktivitetIPerioden
+    );
+
+    const barnFødselsnummer =
+        isFødtBarn(barn) && barn.fnr !== undefined && barn.fnr.length > 0 ? barn.fnr[0] : undefined;
+
+    return {
+        annenPartFødselsnummer,
+        barnFødselsnummer,
+        familiehendelse: getFamiliehendelsedato(barn),
+        perioder: perioderMedAktivitetskrav.map((p) => ({
+            fom: p.tidsperiode.fom.toISOString(),
+            tom: p.tidsperiode.tom.toISOString(),
+        })),
+    };
+};
+
+export const useTrengerDokumentereMorsArbeid = () => {
+    const annenForelder = useContextGetData(ContextDataType.ANNEN_FORELDER) as NonNullable<AnnenForelder | undefined>;
+    const uttaksplan = notEmpty(useContextGetData(ContextDataType.UTTAKSPLAN));
+    const barn = notEmpty(useContextGetData(ContextDataType.OM_BARNET));
+
+    const annenPartFødselsnummer = annenForelder && 'fnr' in annenForelder ? annenForelder.fnr : undefined;
+    const dokumentereMorsArbeidParams = getDokumentereMorsArbeidParams(uttaksplan, barn, annenPartFødselsnummer);
+
+    const { data: trengerDokumentereMorsArbeid = true } = useQuery({
+        ...trengerDokumentereMorsArbeidOptions(dokumentereMorsArbeidParams!),
+        enabled: !!dokumentereMorsArbeidParams,
+    });
+
+    return trengerDokumentereMorsArbeid;
+};
