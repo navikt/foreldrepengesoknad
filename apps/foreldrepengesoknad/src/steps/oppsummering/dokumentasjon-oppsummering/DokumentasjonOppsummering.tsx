a51be85173301a7075b41dd35cde8b3b5b49ca8a
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-04-22 14:23:03 +0200

    Fiks: Forbedre filtrering av vedlegg i DokumentasjonOppsummering for å håndtere "Send senere" korrekt

--- apps/foreldrepengesoknad/src/steps/oppsummering/dokumentasjon-oppsummering/DokumentasjonOppsummering.tsx
+++ apps/foreldrepengesoknad/src/steps/oppsummering/dokumentasjon-oppsummering/DokumentasjonOppsummering.tsx
@@ -5,8 +5,7 @@ import { VedleggDataType } from 'types/VedleggDataType';
 import { Alert, BodyLong, FormSummary, Heading, Link, VStack } from '@navikt/ds-react';
 
 import { NavnPåForeldre, Periode } from '@navikt/fp-common';
-import { AttachmentType, InnsendingsType } from '@navikt/fp-constants';
-import { Skjemanummer } from '@navikt/fp-constants';
+import { AttachmentType, InnsendingsType, Skjemanummer } from '@navikt/fp-constants';
 
 import { useTrengerDokumentereMorsArbeid } from '../../../hooks/useTrengerDokumentereMorsarbeid';
 import { DokumentasjonLastetOppLabel } from './DokumentasjonLastetOppLabel';
@@ -28,16 +27,23 @@ export const DokumentasjonOppsummering = ({
     uttaksperioderSomManglerVedlegg,
 }: Props) => {
     const trengerDokumentereMorsArbeid = useTrengerDokumentereMorsArbeid(); // Bruk hooken
-    console.log(trengerDokumentereMorsArbeid);
+
     const harVedlegg = alleVedlegg && Object.values(alleVedlegg).some((v) => v.length > 0);
-    console.log(alleVedlegg);
+
     const harSendSenereDokument =
         harVedlegg &&
         Object.values(alleVedlegg)
             .flatMap((vedlegg) => vedlegg)
-            .find((v) => v.innsendingsType === InnsendingsType.SEND_SENERE);
+            .find(
+                (v) =>
+                    v.innsendingsType === InnsendingsType.SEND_SENERE &&
+                    v.type !== AttachmentType.MORS_AKTIVITET_DOKUMENTASJON,
+            );
 
     if (!harVedlegg) {
+        return null;
+    }
+    if (!harSendSenereDokument && (trengerDokumentereMorsArbeid ?? false)) {
         return null;
     }
 
@@ -57,56 +63,58 @@ export const DokumentasjonOppsummering = ({
                     </FormSummary.EditLink>
                 </FormSummary.Header>
                 <FormSummary.Answers>
-                    {Object.entries(alleVedlegg)
-                        .filter((idOgVedlegg) => {
-                            // Filtrere ut vedlegg der:
-                            // 1. Vedlegglisten er tom
-                            if (idOgVedlegg[1].length === 0) {
-                                return false;
-                            }
+                    {alleVedlegg &&
+                        Object.entries(alleVedlegg)
+                            .filter((idOgVedlegg) => {
+                                // 1. Vedlegglisten er tom
+                                if (idOgVedlegg[1].length === 0) {
+                                    return false;
+                                }
 
-                            // 2. Vedlegget er "Send senere" og det er dokumentasjon for mors arbeid,
-                            // men vi trenger ikke dokumentere mors arbeid
-                            const vedlegg = idOgVedlegg[1][0];
-                            if (
-                                vedlegg.innsendingsType === InnsendingsType.SEND_SENERE &&
-                                vedlegg.type === AttachmentType.MORS_AKTIVITET_DOKUMENTASJON &&
-                                vedlegg.skjemanummer === Skjemanummer.DOK_ARBEID_MOR &&
-                                !trengerDokumentereMorsArbeid
-                            ) {
-                                return false;
-                            }
-                            return true;
-                        })
-                        .map((idOgVedlegg) => (
-                            <FormSummary.Answer key={idOgVedlegg[1][0].id}>
-                                <FormSummary.Label>
-                                    {idOgVedlegg[1][0].innsendingsType === InnsendingsType.SEND_SENERE ? (
-                                        <DokumentasjonSendSenereLabel
-                                            attachment={idOgVedlegg[1][0]}
-                                            erFarEllerMedmor={erSøkerFarEllerMedmor}
-                                            navnPåForeldre={navnPåForeldre}
-                                            uttaksperioderSomManglerVedlegg={uttaksperioderSomManglerVedlegg}
-                                        />
-                                    ) : (
-                                        <DokumentasjonLastetOppLabel attachment={idOgVedlegg[1][0]} />
-                                    )}
-                                </FormSummary.Label>
-                                <FormSummary.Value>
-                                    <VStack>
-                                        {idOgVedlegg[1]
-                                            .filter(
-                                                (vedlegg) => vedlegg.innsendingsType !== InnsendingsType.SEND_SENERE,
-                                            )
-                                            .map((vedlegg) => (
-                                                <Link key={vedlegg.id} href={vedlegg.url} target="_blank">
-                                                    {vedlegg.filename}
-                                                </Link>
-                                            ))}
-                                    </VStack>
-                                </FormSummary.Value>
-                            </FormSummary.Answer>
-                        ))}
+                                // 2. Vedlegget er "Send senere" og det er dokumentasjon for mors arbeid,
+                                // men vi trenger ikke dokumentere mors arbeid og svar på hook viser at mor ikke må dokumntere
+                                // sitt arbeid
+                                const vedlegg = idOgVedlegg[1][0];
+                                if (
+                                    vedlegg.innsendingsType === InnsendingsType.SEND_SENERE &&
+                                    vedlegg.type === AttachmentType.MORS_AKTIVITET_DOKUMENTASJON &&
+                                    vedlegg.skjemanummer === Skjemanummer.DOK_ARBEID_MOR &&
+                                    !(trengerDokumentereMorsArbeid ?? false)
+                                ) {
+                                    return false;
+                                }
+                                return true;
+                            })
+                            .map((idOgVedlegg) => (
+                                <FormSummary.Answer key={idOgVedlegg[1][0].id}>
+                                    <FormSummary.Label>
+                                        {idOgVedlegg[1][0].innsendingsType === InnsendingsType.SEND_SENERE ? (
+                                            <DokumentasjonSendSenereLabel
+                                                attachment={idOgVedlegg[1][0]}
+                                                erFarEllerMedmor={erSøkerFarEllerMedmor}
+                                                navnPåForeldre={navnPåForeldre}
+                                                uttaksperioderSomManglerVedlegg={uttaksperioderSomManglerVedlegg}
+                                            />
+                                        ) : (
+                                            <DokumentasjonLastetOppLabel attachment={idOgVedlegg[1][0]} />
+                                        )}
+                                    </FormSummary.Label>
+                                    <FormSummary.Value>
+                                        <VStack>
+                                            {idOgVedlegg[1]
+                                                .filter(
+                                                    (vedlegg) =>
+                                                        vedlegg.innsendingsType !== InnsendingsType.SEND_SENERE,
+                                                )
+                                                .map((vedlegg) => (
+                                                    <Link key={vedlegg.id} href={vedlegg.url} target="_blank">
+                                                        {vedlegg.filename}
+                                                    </Link>
+                                                ))}
+                                        </VStack>
+                                    </FormSummary.Value>
+                                </FormSummary.Answer>
+                            ))}
                 </FormSummary.Answers>
             </FormSummary>
             {harSendSenereDokument && (
