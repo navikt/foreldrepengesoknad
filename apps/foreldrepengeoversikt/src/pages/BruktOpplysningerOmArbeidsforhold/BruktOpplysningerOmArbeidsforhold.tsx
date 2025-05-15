a472ac5628a649daf58741ee2c37620f01a44446
johannbm <johannes.moskvil@gmail.com>
2025-05-15 14:23:08 +0200

    med layoutwrapper

--- apps/foreldrepengeoversikt/src/pages/BruktOpplysningerOmArbeidsforhold/BruktOpplysningerOmArbeidsforhold.tsx
+++ apps/foreldrepengeoversikt/src/pages/BruktOpplysningerOmArbeidsforhold/BruktOpplysningerOmArbeidsforhold.tsx
@@ -4,37 +4,38 @@ import { Alert, BodyLong, Heading, Link, VStack } from '@navikt/ds-react';
 
 import { links } from '@navikt/fp-constants';
 
-import { ForsideHeader } from '../../components/header/Header';
-import { PageRouteLayout } from '../../routes/ForeldrepengeoversiktRoutes';
+import { LayoutWrapper } from '../../sections/LayoutWrapper.tsx';
 
 export const BruktOpplysniungerOmArbeidsforhold = () => {
     return (
-        <PageRouteLayout header={<ForsideHeader />}>
-            <Alert variant="info">
-                <VStack gap="2">
-                    <Heading size="small" level="2">
-                        <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tittel" />
-                    </Heading>
-                    <BodyLong>
-                        <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tekst.1" />
-                    </BodyLong>
-                    <BodyLong>
-                        <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tekst.2" />
-                    </BodyLong>
-                    <BodyLong>
-                        <FormattedMessage
-                            id="BruktOpplysningerOmArbeidsforhold.tekst.3"
-                            values={{
-                                a: (msg) => (
-                                    <Link href={links.personvernerklæringOm} rel="noreferrer" target="_blank">
-                                        {msg}
-                                    </Link>
-                                ),
-                            }}
-                        />
-                    </BodyLong>
-                </VStack>
-            </Alert>
-        </PageRouteLayout>
+        <>
+            <LayoutWrapper className="md:pb-28 p-4">
+                <Alert variant="info">
+                    <VStack gap="2">
+                        <Heading size="small" level="2">
+                            <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tittel" />
+                        </Heading>
+                        <BodyLong>
+                            <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tekst.1" />
+                        </BodyLong>
+                        <BodyLong>
+                            <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tekst.2" />
+                        </BodyLong>
+                        <BodyLong>
+                            <FormattedMessage
+                                id="BruktOpplysningerOmArbeidsforhold.tekst.3"
+                                values={{
+                                    a: (msg) => (
+                                        <Link href={links.personvernerklæringOm} rel="noreferrer" target="_blank">
+                                            {msg}
+                                        </Link>
+                                    ),
+                                }}
+                            />
+                        </BodyLong>
+                    </VStack>
+                </Alert>
+            </LayoutWrapper>
+        </>
     );
 };
