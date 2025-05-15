415b5ca57d42177f4d9fd4b2ec3167cc221e6660
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-05-15 22:59:11 +0200

    Ikke bruk alert

--- apps/foreldrepengeoversikt/src/pages/BruktOpplysningerOmArbeidsforhold/BruktOpplysningerOmArbeidsforhold.tsx
+++ apps/foreldrepengeoversikt/src/pages/BruktOpplysningerOmArbeidsforhold/BruktOpplysningerOmArbeidsforhold.tsx
@@ -10,31 +10,29 @@ export const BruktOpplysniungerOmArbeidsforhold = () => {
     return (
         <>
             <LayoutWrapper className="md:pb-28 p-4">
-                <Alert variant="info">
-                    <VStack gap="2">
-                        <Heading size="small" level="2">
-                            <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tittel" />
-                        </Heading>
-                        <BodyLong>
-                            <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tekst.1" />
-                        </BodyLong>
-                        <BodyLong>
-                            <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tekst.2" />
-                        </BodyLong>
-                        <BodyLong>
-                            <FormattedMessage
-                                id="BruktOpplysningerOmArbeidsforhold.tekst.3"
-                                values={{
-                                    a: (msg) => (
-                                        <Link href={links.personvernerklæringOm} rel="noreferrer" target="_blank">
-                                            {msg}
-                                        </Link>
-                                    ),
-                                }}
-                            />
-                        </BodyLong>
-                    </VStack>
-                </Alert>
+                <VStack className="bg-white p-4 rounded-2xl border-2 shadow border-deepblue-200" gap="2">
+                    <Heading size="small" level="2">
+                        <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tittel" />
+                    </Heading>
+                    <BodyLong>
+                        <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tekst.1" />
+                    </BodyLong>
+                    <BodyLong>
+                        <FormattedMessage id="BruktOpplysningerOmArbeidsforhold.tekst.2" />
+                    </BodyLong>
+                    <BodyLong>
+                        <FormattedMessage
+                            id="BruktOpplysningerOmArbeidsforhold.tekst.3"
+                            values={{
+                                a: (msg) => (
+                                    <Link href={links.personvernerklæringOm} rel="noreferrer" target="_blank">
+                                        {msg}
+                                    </Link>
+                                ),
+                            }}
+                        />
+                    </BodyLong>
+                </VStack>
             </LayoutWrapper>
         </>
     );
