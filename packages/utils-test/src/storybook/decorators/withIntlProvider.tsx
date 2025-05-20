788014e8569c292bf9774af1a57f351c6518862c
tor-nav <tor.roneid@nav.no>
2025-05-20 10:36:20 +0200

    FIX rull tilbake til || frå ?? da default språk ikkje blir satt korrekt i stories

--- packages/utils-test/src/storybook/decorators/withIntlProvider.tsx
+++ packages/utils-test/src/storybook/decorators/withIntlProvider.tsx
@@ -2,7 +2,7 @@ import { RawIntlProvider, createIntl } from 'react-intl';
 
 export const getIntlDecorator = (messagesForAllLanguages: Record<string, Record<string, string>>) => {
     const withIntlProvider = (story: any, context: any) => {
-        const locale = context.globals.locale ?? 'nb';
+        const locale = context.globals.locale || 'nb';
         const messages = messagesForAllLanguages[locale];
         return <RawIntlProvider value={createIntl({ locale, messages })}>{story()}</RawIntlProvider>;
     };
