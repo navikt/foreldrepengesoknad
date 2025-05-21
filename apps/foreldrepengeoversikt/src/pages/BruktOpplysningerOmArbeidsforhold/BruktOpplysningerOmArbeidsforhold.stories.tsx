f295cf856badb90735b6cd8e9653d7e30604629e
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-05-21 09:50:12 +0200

    Fjern render-funksjon fra Default story for BruktOpplysniungerOmArbeidsforhold

--- apps/foreldrepengeoversikt/src/pages/BruktOpplysningerOmArbeidsforhold/BruktOpplysningerOmArbeidsforhold.stories.tsx
+++ apps/foreldrepengeoversikt/src/pages/BruktOpplysningerOmArbeidsforhold/BruktOpplysningerOmArbeidsforhold.stories.tsx
@@ -10,6 +10,4 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-    render: () => <BruktOpplysniungerOmArbeidsforhold />,
-};
+export const Default: Story = {};
