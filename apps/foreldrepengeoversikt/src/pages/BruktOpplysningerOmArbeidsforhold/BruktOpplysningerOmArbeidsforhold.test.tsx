569b72337a9545acd9164e2050773a562755414c
espenwaaga <espen.waaga@nav.no>
2025-06-03 13:53:05 +0200

    Fiks lint/test

--- apps/foreldrepengeoversikt/src/pages/BruktOpplysningerOmArbeidsforhold/BruktOpplysningerOmArbeidsforhold.test.tsx
+++ apps/foreldrepengeoversikt/src/pages/BruktOpplysningerOmArbeidsforhold/BruktOpplysningerOmArbeidsforhold.test.tsx
@@ -10,6 +10,7 @@ describe('<BruktOpplysningerOmArbeidsforhold>', () => {
         render(<Default />);
 
         expect(await screen.findByText('Nav har hentet opplysninger om arbeidsforholdet ditt')).toBeInTheDocument();
+
         expect(
             screen.getByText(
                 'Den andre forelderen har søkt om foreldrepenger. For at den andre forelderen skal kunne få ' +
@@ -17,24 +18,32 @@ describe('<BruktOpplysningerOmArbeidsforhold>', () => {
                     'for eksempel i arbeid. I søknaden har den andre forelderen oppgitt at du er i arbeid i en eller flere av disse periodene.',
             ),
         ).toBeInTheDocument();
+
         expect(
             screen.getByText(
-                /Nav har derfor hentet informasjon om arbeidsforholdene dine fra Arbeidsgiver\- og arbeidstakerregisteret \(Aa-registeret\)\. Vi bruker opplysningene når vi behandler søknaden. Vi kan også bruke opplysningene for andre perioder i nye søknader\. Du kan selv sjekke hva som er registrert på deg i/i,
+                /Nav har derfor hentet informasjon om arbeidsforholdene dine fra Arbeidsgiver- og arbeidstakerregisteret \(Aa-registeret\)\./i,
             ),
         ).toBeInTheDocument();
-        expect(screen.getByText(/Dette har vi gjort med hjemmel i folketrygdloven § 21-4\./)).toBeInTheDocument();
-        expect(screen.getByText(/Regelverket for aktivitetskrav står i folketrygdloven § 14-13\./)).toBeInTheDocument();
-
+        expect(
+            screen.getByText(
+                /Vi bruker opplysningene når vi behandler søknaden. Vi kan også bruke opplysningene for andre perioder i nye søknader\./i,
+            ),
+        ).toBeInTheDocument();
+        expect(screen.getByText(/Du kan selv sjekke hva som er registrert på deg i/i)).toBeInTheDocument();
         expect(screen.getByText('Aa-registeret (åpner i en ny fane)')).toBeInTheDocument();
         expect(screen.getByText('Aa-registeret (åpner i en ny fane)')).toHaveAttribute(
             'href',
             'https://www.nav.no/person/personopplysninger/nb/#arbeidsforhold',
         );
+
+        expect(screen.getByText(/Dette har vi gjort med hjemmel i folketrygdloven § 21-4\./)).toBeInTheDocument();
         expect(screen.getByText('om behandling av personvernopplysninger (åpner i en ny fane)')).toBeInTheDocument();
         expect(screen.getByText('om behandling av personvernopplysninger (åpner i en ny fane)')).toHaveAttribute(
             'href',
             'https://www.nav.no/personvernerklaering#om',
         );
+
+        expect(screen.getByText(/Regelverket for aktivitetskrav står i folketrygdloven § 14-13\./)).toBeInTheDocument();
         expect(screen.getByText('nav.no/foreldrepenger (åpner i en ny fane)')).toBeInTheDocument();
         expect(screen.getByText('nav.no/foreldrepenger (åpner i en ny fane)')).toHaveAttribute(
             'href',
