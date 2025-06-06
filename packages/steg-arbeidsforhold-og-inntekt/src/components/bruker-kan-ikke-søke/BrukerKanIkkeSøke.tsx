23888d61e2a9e00b82f06f7fd01e6be6a71c264b
johannbm <johannes.moskvil@gmail.com>
2025-06-06 14:44:04 +0200

    oppdater tekster

--- packages/steg-arbeidsforhold-og-inntekt/src/components/bruker-kan-ikke-søke/BrukerKanIkkeSøke.tsx
+++ packages/steg-arbeidsforhold-og-inntekt/src/components/bruker-kan-ikke-søke/BrukerKanIkkeSøke.tsx
@@ -1,43 +1,16 @@
 import { FormattedMessage } from 'react-intl';
 
-import { Alert, Link, VStack } from '@navikt/ds-react';
+import { Alert, BodyLong, Heading, Link } from '@navikt/ds-react';
 
 export const BrukerKanIkkeSøke = () => {
     return (
         <Alert variant="warning">
-            <VStack gap="2">
-                <FormattedMessage
-                    id="inntektsinformasjon.alert.ingenArbeidsforhold.tittel"
-                    values={{
-                        b: (msg: any) => <b>{msg}</b>,
-                    }}
-                />
-                <div>
-                    <FormattedMessage
-                        id="inntektsinformasjon.alert.ingenArbeidsforhold"
-                        values={{
-                            a: (msg: any) => (
-                                <Link rel="noopener noreferrer" href="https://familie.nav.no/om-svangerskapspenger">
-                                    {msg}
-                                </Link>
-                            ),
-                        }}
-                    />
-                    <FormattedMessage
-                        id="inntektsinformasjon.alert.ingenArbeidsforhold.forsettelse"
-                        values={{
-                            a: (msg: any) => (
-                                <Link
-                                    rel="noopener noreferrer"
-                                    href="https://www.nav.no/no/Nav+og+samfunn/Kontakt+Nav/Relatert+informasjon/chat-med-oss-om-foreldrepenger"
-                                >
-                                    {msg}
-                                </Link>
-                            ),
-                        }}
-                    />
-                </div>
-            </VStack>
+            <Heading spacing size="small" level="3">
+                <FormattedMessage id="inntektsinformasjon.alert.ingenArbeidsforhold.tittel" />
+            </Heading>
+            <BodyLong spacing>
+                <FormattedMessage id="inntektsinformasjon.alert.ingenArbeidsforhold" />
+            </BodyLong>
         </Alert>
     );
 };
