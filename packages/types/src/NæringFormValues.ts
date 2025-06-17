4b7db6708e6ad30f49883e5f449f2f1e3cb35d42
johannbm <johannes.moskvil@gmail.com>
2025-06-17 12:49:19 +0200

    forklaring

--- packages/types/src/NæringFormValues.ts
+++ packages/types/src/NæringFormValues.ts
@@ -1,3 +1,7 @@
 import { NæringDto } from './apiDtoGenerert';
 
+/**
+ * Helst ville vi brukt NæringDto direkte, men i skjema er det nyttig å ha en ekstra "pågående" sjekkboks.
+ * I Dto til backend er pågående det samme som at tom ikke er satt
+ */
 export type NæringFormValues = NæringDto & { pågående: boolean };
