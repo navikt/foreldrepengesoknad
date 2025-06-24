1db25979e8b0a66373fcc5951fb96eafc788ae00
tor-nav <tor.roneid@nav.no>
2025-06-24 14:18:04 +0200

    storybook play

--- apps/engangsstonad/src/velkommen/Velkommen.stories.tsx
+++ apps/engangsstonad/src/velkommen/Velkommen.stories.tsx
@@ -1,16 +1,11 @@
 import { Meta, StoryObj } from '@storybook/react-vite';
-import { Action, EsDataContext } from 'appData/EsDataContext';
+import { Action, ContextDataType, EsDataContext } from 'appData/EsDataContext';
 import { Path } from 'appData/paths';
 import { ComponentProps } from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { action } from 'storybook/actions';
+import { expect, fn, within } from 'storybook/test';
 
 import { Velkommen } from './Velkommen';
-
-const promiseAction = () => (): Promise<void> => {
-    action('button-click')();
-    return Promise.resolve();
-};
 
 type StoryArgs = {
     gåTilNesteSide?: (action: Action) => void;
@@ -18,11 +13,11 @@ type StoryArgs = {
 
 const meta = {
     component: Velkommen,
-    render: ({ gåTilNesteSide = action('button-click'), ...rest }) => {
+    render: (props) => {
         return (
             <MemoryRouter initialEntries={[Path.VELKOMMEN]}>
-                <EsDataContext onDispatch={gåTilNesteSide}>
-                    <Velkommen {...rest} />
+                <EsDataContext onDispatch={props.gåTilNesteSide}>
+                    <Velkommen {...props} />
                 </EsDataContext>
             </MemoryRouter>
         );
@@ -34,8 +29,33 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
     args: {
-        startSøknad: action('button-click'),
-        mellomlagreOgNaviger: promiseAction(),
+        startSøknad: fn(),
+        mellomlagreOgNaviger: fn(),
+        gåTilNesteSide: fn(),
         erVelkommen: false,
+    },
+    play: async ({ args, canvasElement, userEvent }) => {
+        const canvas = within(canvasElement);
+
+        expect(await canvas.findByText('Søknad om engangsstønad')).toBeInTheDocument();
+
+        await userEvent.click(canvas.getByText('Start søknaden'));
+
+        expect(canvas.getByText('Du må bekrefte at du har lest og forstått')).toBeInTheDocument();
+
+        await userEvent.click(canvas.getByText('Ja, jeg har forstått mine plikter.'));
+
+        await userEvent.click(canvas.getByText('Start søknaden'));
+
+        expect(args.startSøknad).toHaveBeenCalledTimes(1);
+        expect(args.startSøknad).toHaveBeenNthCalledWith(1, true);
+
+        expect(args.gåTilNesteSide).toHaveBeenNthCalledWith(1, {
+            data: Path.SØKERSITUASJON,
+            key: ContextDataType.CURRENT_PATH,
+            type: 'update',
+        });
+
+        expect(args.mellomlagreOgNaviger).toHaveBeenCalledOnce();
     },
 };
