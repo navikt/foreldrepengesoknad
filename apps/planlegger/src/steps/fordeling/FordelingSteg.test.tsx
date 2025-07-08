705926a84ffec1822bafdb2dace9b53df8a59ae2
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-07-08 15:00:52 +0200

    Legg til test for oppdatert infotekst

--- apps/planlegger/src/steps/fordeling/FordelingSteg.test.tsx
+++ apps/planlegger/src/steps/fordeling/FordelingSteg.test.tsx
@@ -57,4 +57,46 @@ describe('<FordelingSteg>', () => {
         expect(navigateMock).toHaveBeenCalledTimes(1);
         expect(navigateMock).toHaveBeenCalledWith(expect.stringMatching(PlanleggerRoutes.PLANEN_DERES));
     });
+
+    it('Skal velge 16 uker til Mor, test av tekst i infoboks', async () => {
+        const navigateMock = vi.fn();
+        useNavigateMock.mockReturnValue(navigateMock);
+
+        const gåTilNesteSide = vi.fn();
+
+        const utils = render(<FlereForsørgereEttBarn gåTilNesteSide={gåTilNesteSide} />);
+
+        expect(await screen.findAllByText('Fordeling')).toHaveLength(2);
+
+        await userEvent.click(screen.getByText('Neste'));
+
+        expect(
+            screen.getByText('Du må svare på hvordan dere vil fordele fellesperioden før dere går videre.'),
+        ).toBeInTheDocument();
+
+        await userEvent.selectOptions(
+            utils.getByLabelText('Hvordan vil dere fordele 16 uker med fellesperiode?'),
+            '80',
+        );
+        expect(
+            screen.getByText(
+                'Dette er regnet ut fra at barnet blir født på termin og om dere tar sammenhengende permisjon fra tre uker før termin.',
+            ),
+        ).toBeInTheDocument();
+        expect(screen.getByText('Permisjonen kan se sånn ut med fordelingen dere valgte:')).toBeInTheDocument();
+        expect(screen.getByText('Klara: 11. des. 2023 – 2. aug. 2024')).toBeInTheDocument();
+        expect(screen.getByText('Espen: 5. aug. 2024 – 15. nov. 2024')).toBeInTheDocument();
+        await userEvent.click(screen.getByText('Neste'));
+
+        expect(gåTilNesteSide).toHaveBeenNthCalledWith(1, {
+            data: {
+                antallDagerSøker1: '80',
+            },
+            key: ContextDataType.FORDELING,
+            type: 'update',
+        });
+
+        expect(navigateMock).toHaveBeenCalledTimes(1);
+        expect(navigateMock).toHaveBeenCalledWith(expect.stringMatching(PlanleggerRoutes.PLANEN_DERES));
+    });
 });
