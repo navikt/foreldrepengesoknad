4446ce3f2f28cd10eb730c8c74168eb1397dca18
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-07-16 14:47:55 +0200

    fiks test

--- apps/planlegger/src/components/labels/CalendarLabels.stories.tsx
+++ apps/planlegger/src/components/labels/CalendarLabels.stories.tsx
@@ -2,7 +2,7 @@ import { Meta, StoryObj } from '@storybook/react-vite';
 import dayjs from 'dayjs';
 
 import { BarnType, Forelder, StønadskontoType } from '@navikt/fp-constants';
-import { SaksperiodeNy, UtsettelseÅrsakType } from '@navikt/fp-types';
+import { SaksperiodeNy } from '@navikt/fp-types';
 import { UttaksplanKalender } from '@navikt/fp-uttaksplan-kalender-ny';
 
 const meta = {
@@ -13,66 +13,99 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-// Todo: Kalle: Her må man sette opp slik at helgedatoer blir tatt høyde for
+// Beregn fødselsdato og juster for helgedager
+const fødselsdato = dayjs().subtract(1, 'day');
+const erHelgedag = fødselsdato.day() === 0 || fødselsdato.day() === 6; // Søndag eller lørdag
+
+// Beregn startdatoer basert på om fødselsdato er helgedag
+const startForeldrepengerFørFødsel = erHelgedag
+    ? fødselsdato.subtract(3, 'weeks').day(1) // Mandag 3 uker før hvis helgedag
+    : fødselsdato.subtract(3, 'weeks');
+
+const startMødrekvote = erHelgedag
+    ? fødselsdato.add(1, 'week').day(1) // Mandag etter fødsel hvis helgedag
+    : fødselsdato.add(1, 'day'); // Dagen etter fødsel hvis ukedag
+
+const startFellesperiode = startMødrekvote.add(15, 'weeks').add(1, 'day');
+const startFedrekvote = startFellesperiode.add(16, 'weeks').add(1, 'day');
 
 export const MorSøkerMedTapteDager: Story = {
     args: {
         søkersPerioder: [
             {
-                fom: dayjs().subtract(1, 'day').subtract(3, 'weeks').format('YYYY-MM-DD'), // 3 uker før fødsel
-                tom: dayjs().subtract(1, 'day').subtract(1, 'day').format('YYYY-MM-DD'), // til dagen før fødsel
+                fom: startForeldrepengerFørFødsel.format('YYYY-MM-DD'),
+                tom: fødselsdato.subtract(1, 'day').format('YYYY-MM-DD'),
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.ForeldrepengerFørFødsel,
             },
             {
-                fom: dayjs().subtract(1, 'day').add(2, 'days').format('YYYY-MM-DD'), // 2 dager etter fødsel
-                tom: dayjs().subtract(1, 'day').add(15, 'weeks').format('YYYY-MM-DD'), // 15 uker senere
+                // legger inn en tapt dag mellom fødsel og mødrekvote
+                fom: startMødrekvote.add(1, 'day').format('YYYY-MM-DD'),
+                tom: startMødrekvote.add(15, 'weeks').format('YYYY-MM-DD'),
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.Mødrekvote,
             },
             {
-                fom: dayjs().subtract(1, 'day').add(15, 'weeks').add(3, 'days').format('YYYY-MM-DD'), // 3 dager etter mødrekvote
-                tom: dayjs().subtract(1, 'day').add(32, 'weeks').format('YYYY-MM-DD'), // 17 uker senere
+                fom: startFellesperiode.format('YYYY-MM-DD'),
+                tom: startFellesperiode.add(16, 'weeks').format('YYYY-MM-DD'),
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.Fellesperiode,
             },
             {
-                fom: dayjs().subtract(1, 'day').add(32, 'weeks').add(3, 'days').format('YYYY-MM-DD'), // 3 dager etter fellesperiode
-                tom: dayjs().subtract(1, 'day').add(46, 'weeks').format('YYYY-MM-DD'), // 14 uker senere
+                fom: startFedrekvote.format('YYYY-MM-DD'),
+                tom: startFedrekvote.add(15, 'weeks').format('YYYY-MM-DD'),
                 forelder: Forelder.farMedmor,
                 kontoType: StønadskontoType.Fedrekvote,
             },
         ] satisfies SaksperiodeNy[],
         barn: {
             type: BarnType.FØDT,
-            fødselsdatoer: [dayjs().subtract(1, 'day').format('YYYY-MM-DD')], // I dag minus 1
+            fødselsdatoer: [fødselsdato.format('YYYY-MM-DD')],
             antallBarn: 1,
         },
         erFarEllerMedmor: false,
         harAktivitetskravIPeriodeUtenUttak: false,
         bareFarMedmorHarRett: false,
-        navnAnnenPart: 'Hanne',
+        navnAnnenPart: 'Ola',
     },
 };
-// lag en story, standard uttaksplan men uten tapte dager
+
 export const MorSøkerUtenTapteDager: Story = {
     args: {
         søkersPerioder: [
             {
-                fom: dayjs().subtract(1, 'day').format('YYYY-MM-DD'),
-                tom: dayjs().add(14, 'days').format('YYYY-MM-DD'),
+                fom: startForeldrepengerFørFødsel.format('YYYY-MM-DD'),
+                tom: fødselsdato.subtract(1, 'day').format('YYYY-MM-DD'),
                 forelder: Forelder.mor,
-                kontoType: StønadskontoType.Foreldrepenger,
+                kontoType: StønadskontoType.ForeldrepengerFørFødsel,
+            },
+            {
+                fom: startMødrekvote.format('YYYY-MM-DD'),
+                tom: startMødrekvote.add(15, 'weeks').format('YYYY-MM-DD'),
+                forelder: Forelder.mor,
+                kontoType: StønadskontoType.Mødrekvote,
+            },
+            {
+                fom: startFellesperiode.format('YYYY-MM-DD'),
+                tom: startFellesperiode.add(16, 'weeks').format('YYYY-MM-DD'),
+                forelder: Forelder.mor,
+                kontoType: StønadskontoType.Fellesperiode,
+            },
+            {
+                fom: startFedrekvote.format('YYYY-MM-DD'),
+                tom: startFedrekvote.add(15, 'weeks').format('YYYY-MM-DD'),
+                forelder: Forelder.farMedmor,
+                kontoType: StønadskontoType.Fedrekvote,
             },
         ] satisfies SaksperiodeNy[],
         barn: {
             type: BarnType.FØDT,
-            fødselsdatoer: [dayjs().subtract(1, 'day').format('YYYY-MM-DD')], // I dag minus 1
+            fødselsdatoer: [fødselsdato.format('YYYY-MM-DD')],
             antallBarn: 1,
         },
         erFarEllerMedmor: false,
         harAktivitetskravIPeriodeUtenUttak: false,
         bareFarMedmorHarRett: false,
-        navnAnnenPart: 'Hanne',
+        navnAnnenPart: 'Ola',
     },
 };
