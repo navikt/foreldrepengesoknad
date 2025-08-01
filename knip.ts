a0ac5e9f5f56de489b013ccd458055ff5b653e9e
johannbm <johannes.moskvil@gmail.com>
2025-08-01 14:24:56 +0200

    oppdater config

--- knip.ts
+++ knip.ts
@@ -3,7 +3,15 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
     ignore: ['server/**'], //bootstrap files
     ignoreBinaries: ['docker-compose'],
-    ignoreDependencies: [],
+    ignoreDependencies: [
+        "@navikt/ds-css",
+        "@navikt/ds-tailwind",
+        "@tailwindcss/vite",
+        "@storybook/addon-actions",
+        "@storybook/cli",
+        "tailwindcss",
+        "playwright"
+    ],
 };
 
 export default config;
