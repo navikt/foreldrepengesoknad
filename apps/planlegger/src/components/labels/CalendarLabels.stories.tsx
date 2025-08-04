819b21b43e69589fc1b62a2a5865084180b27f31
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2025-08-04 23:44:43 +0200

    Endre til at det CalendarLabels-componentet som testes isolert

--- apps/planlegger/src/components/labels/CalendarLabels.stories.tsx
+++ apps/planlegger/src/components/labels/CalendarLabels.stories.tsx
@@ -1,111 +1,105 @@
 import { Meta, StoryObj } from '@storybook/react-vite';
-import dayjs from 'dayjs';
 
-import { BarnType, Forelder, StønadskontoType } from '@navikt/fp-constants';
-import { SaksperiodeNy } from '@navikt/fp-types';
-import { UttaksplanKalender } from '@navikt/fp-uttaksplan-kalender-ny';
+import { Forelder, StønadskontoType } from '@navikt/fp-constants';
+import { HvemPlanleggerType, SaksperiodeNy } from '@navikt/fp-types';
+
+import { CalendarLabels } from './CalendarLabels';
 
 const meta = {
-    title: 'UttaksplanKalender',
-    component: UttaksplanKalender,
-} satisfies Meta<typeof UttaksplanKalender>;
+    title: 'CalendarLabels',
+    component: CalendarLabels,
+} satisfies Meta<typeof CalendarLabels>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-// Beregn fødselsdato og juster for helgedager
-const fødselsdato = dayjs().subtract(1, 'day');
-const erHelgedag = fødselsdato.day() === 0 || fødselsdato.day() === 6; // Søndag eller lørdag
-
-// Beregn startdatoer basert på om fødselsdato er helgedag
-const startForeldrepengerFørFødsel = erHelgedag
-    ? fødselsdato.subtract(3, 'weeks').day(1) // Mandag 3 uker før hvis helgedag
-    : fødselsdato.subtract(3, 'weeks');
-
-const startMødrekvote = erHelgedag
-    ? fødselsdato.add(1, 'week').day(1) // Mandag etter fødsel hvis helgedag
-    : fødselsdato.add(1, 'day'); // Dagen etter fødsel hvis ukedag
-
-const startFellesperiode = startMødrekvote.add(15, 'weeks').add(1, 'day');
-const startFedrekvote = startFellesperiode.add(16, 'weeks').add(1, 'day');
-
 export const MorSøkerMedTapteDager: Story = {
     args: {
-        søkersPerioder: [
+        uttaksplan: [
             {
-                fom: startForeldrepengerFørFødsel.format('YYYY-MM-DD'),
-                tom: fødselsdato.subtract(1, 'day').format('YYYY-MM-DD'),
+                fom: '2025-07-14',
+                tom: '2025-08-02',
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.ForeldrepengerFørFødsel,
             },
+            // legger inn tapt dag mellom termindato og første dag av mødrekvote
             {
-                // legger inn en tapt dag mellom fødsel og mødrekvote
-                fom: startMødrekvote.add(1, 'day').format('YYYY-MM-DD'),
-                tom: startMødrekvote.add(15, 'weeks').format('YYYY-MM-DD'),
+                fom: '2025-08-05',
+                tom: '2025-11-17',
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.Mødrekvote,
             },
             {
-                fom: startFellesperiode.format('YYYY-MM-DD'),
-                tom: startFellesperiode.add(16, 'weeks').format('YYYY-MM-DD'),
+                fom: '2025-11-18',
+                tom: '2026-03-10',
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.Fellesperiode,
             },
             {
-                fom: startFedrekvote.format('YYYY-MM-DD'),
-                tom: startFedrekvote.add(15, 'weeks').format('YYYY-MM-DD'),
+                fom: '2026-03-11',
+                tom: '2026-06-24',
                 forelder: Forelder.farMedmor,
                 kontoType: StønadskontoType.Fedrekvote,
             },
         ] satisfies SaksperiodeNy[],
-        barn: {
-            type: BarnType.FØDT,
-            fødselsdatoer: [fødselsdato.format('YYYY-MM-DD')],
-            antallBarn: 1,
+        barnet: {
+            erFødsel: true,
+            antallBarn: '1',
+            erBarnetFødt: true,
+            fødselsdato: '2025-08-03',
+            termindato: '2025-08-03',
         },
-        erFarEllerMedmor: false,
-        harAktivitetskravIPeriodeUtenUttak: false,
-        bareFarMedmorHarRett: false,
-        navnAnnenPart: 'Ola',
+        hvemHarRett: 'beggeHarRett',
+        hvemPlanlegger: {
+            type: HvemPlanleggerType.MOR_OG_FAR,
+            navnPåMor: 'Kari',
+            navnPåFar: 'Ola',
+        },
+        inneholderTapteDager: true,
     },
 };
 
 export const MorSøkerUtenTapteDager: Story = {
     args: {
-        søkersPerioder: [
+        uttaksplan: [
             {
-                fom: startForeldrepengerFørFødsel.format('YYYY-MM-DD'),
-                tom: fødselsdato.subtract(1, 'day').format('YYYY-MM-DD'),
+                fom: '2025-07-14',
+                tom: '2025-08-02',
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.ForeldrepengerFørFødsel,
             },
             {
-                fom: startMødrekvote.format('YYYY-MM-DD'),
-                tom: startMødrekvote.add(15, 'weeks').format('YYYY-MM-DD'),
+                fom: '2025-08-04',
+                tom: '2025-11-17',
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.Mødrekvote,
             },
             {
-                fom: startFellesperiode.format('YYYY-MM-DD'),
-                tom: startFellesperiode.add(16, 'weeks').format('YYYY-MM-DD'),
+                fom: '2025-11-18',
+                tom: '2026-03-10',
                 forelder: Forelder.mor,
                 kontoType: StønadskontoType.Fellesperiode,
             },
             {
-                fom: startFedrekvote.format('YYYY-MM-DD'),
-                tom: startFedrekvote.add(15, 'weeks').format('YYYY-MM-DD'),
+                fom: '2026-03-11',
+                tom: '2026-06-24',
                 forelder: Forelder.farMedmor,
                 kontoType: StønadskontoType.Fedrekvote,
             },
         ] satisfies SaksperiodeNy[],
-        barn: {
-            type: BarnType.FØDT,
-            fødselsdatoer: [fødselsdato.format('YYYY-MM-DD')],
-            antallBarn: 1,
+        barnet: {
+            erFødsel: true,
+            antallBarn: '1',
+            erBarnetFødt: true,
+            fødselsdato: '2025-08-04',
+            termindato: '2025-08-04',
         },
-        erFarEllerMedmor: false,
-        harAktivitetskravIPeriodeUtenUttak: false,
-        bareFarMedmorHarRett: false,
-        navnAnnenPart: 'Ola',
+        hvemHarRett: 'beggeHarRett',
+        hvemPlanlegger: {
+            type: HvemPlanleggerType.MOR_OG_FAR,
+            navnPåMor: 'Kari',
+            navnPåFar: 'Ola',
+        },
+        inneholderTapteDager: false,
     },
 };
