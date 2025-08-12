1919e2a4eba341affce1e7cf6d5e6c8c576db1ff
johannbm <johannes.moskvil@gmail.com>
2025-08-12 11:44:02 +0200

    multiline

--- packages/ny-uttaksplan/src/builder/uttaksplanbuilderUtils.ts
+++ packages/ny-uttaksplan/src/builder/uttaksplanbuilderUtils.ts
@@ -347,9 +347,11 @@ export const finnOgSettInnHull = (
 
 // @ts-expect-error ubrukt parameter
 const beregnSamtidiguttaksprosent = (p: Planperiode, overlappendePeriode: Planperiode) => {
-    // Når man lager planen ønsker vi å automatisk endre på den andre parten sin uttaksprosent så man slipper gjøre to sett med endringer.
-    // Men den må utledes utifra maks tillat prosent. Dette avhenger av hvilke kontoer som benyttes, eller om det er flerbarnsdager osv.
-    // For nå gjør vi det enkelt slik at det blir rett i innsyn for eksisterende planer.
+    /*
+     Når man lager planen ønsker vi å automatisk endre på den andre parten sin uttaksprosent så man slipper gjøre to sett med endringer.
+     Men den må utledes utifra maks tillat prosent. Dette avhenger av hvilke kontoer som benyttes, eller om det er flerbarnsdager osv.
+     For nå gjør vi det enkelt slik at det blir rett i innsyn for eksisterende planer.
+    */
     return overlappendePeriode.samtidigUttak ?? 100;
 };
 
