b34f8fdd2aae13d7801f5da0756d469930dfa215
johannbm <johannes.moskvil@gmail.com>
2025-10-06 15:09:41 +0200

    es eller fp

---
+++ apps/veiviser-fp-eller-es/src/app-data/queries.ts
@@ -0,0 +1,19 @@
+import { queryOptions } from '@tanstack/react-query';
+import ky from 'ky';
+
+import { DEFAULT_SATSER } from '@navikt/fp-constants';
+import { Satser } from '@navikt/fp-types';
+
+export const urlPrefiks = import.meta.env.BASE_URL;
+
+export const API_URLS = {
+    satser: `${urlPrefiks}/rest/satser`,
+} as const;
+
+export const hentSatserOptions = () =>
+    queryOptions({
+        queryKey: ['SATSER'],
+        queryFn: () => ky.get(API_URLS.satser).json<Satser>(),
+        staleTime: Infinity,
+        initialData: DEFAULT_SATSER,
+    });
