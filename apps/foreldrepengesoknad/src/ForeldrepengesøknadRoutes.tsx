93c26558fac5892214c34c54086027c47db45f22
johannbm <johannes.moskvil@gmail.com>
2025-10-21 10:07:33 +0200

    legg til kvittering for endringssøknad

--- apps/foreldrepengesoknad/src/ForeldrepengesøknadRoutes.tsx
+++ apps/foreldrepengesoknad/src/ForeldrepengesøknadRoutes.tsx
@@ -83,6 +83,7 @@ const renderSøknadRoutes = (
                         />
                     }
                 />
+                <Route path={SøknadRoutes.KVITTERING} element={<KvitteringPage />} />
             </>
         );
     }
