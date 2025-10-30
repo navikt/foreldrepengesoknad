91024ddcc35ce51388e8813ca3345f591032de1d
tor røneid <tor.roneid@nav.no>
2025-10-30 15:46:55 +0100

    labels

--- packages/ui/src/calendar/label/CalendarLabel.tsx
+++ packages/ui/src/calendar/label/CalendarLabel.tsx
@@ -5,15 +5,14 @@ import styles from './calendarLabel.module.css';
 
 interface Props {
     children: React.ReactElement | React.ReactElement[];
-    color: CalendarPeriodColor;
-    onClick?: () => void;
+    iconType: PeriodeColor;
 }
 
-export const CalendarLabel = ({ children, color, onClick }: Props) => {
-    if (color !== 'DARKBLUE') {
+export const CalendarLabel = ({ children, iconType }: Props) => {
+    if (iconType !== PeriodeColor.DARKBLUE) {
         return (
-            <HStack className={styles.calendarLabel} gap="space-8" align="center" onClick={onClick}>
-                {getRect(color)}
+            <HStack gap="space-8" align="center">
+                {getRect(iconType)}
                 {children}
             </HStack>
         );
@@ -21,93 +20,37 @@ export const CalendarLabel = ({ children, color, onClick }: Props) => {
     return null;
 };
 
-const getRect = (color: CalendarPeriodColor) => {
+const getRect = (color: PeriodeColor) => {
     switch (color) {
         case PeriodeColor.BLUE:
-            return <div className={styles.blueCircle} />;
+            return <div className={styles.blueRect} />;
         case PeriodeColor.GREEN:
-            return <div className={styles.greenCircle} />;
+            return <div className={styles.greenRect} />;
         case PeriodeColor.LIGHTBLUE:
         case PeriodeColor.BLUEOUTLINE:
-            return <div className={styles.blueOutlineCircle} />;
+            return <div className={styles.blueOutlineRect} />;
         case PeriodeColor.BLACKOUTLINE:
-            return <div className={styles.blackOutlineCircle} />;
+            return <div className={styles.blackOutlineRect} />;
         case PeriodeColor.GREENOUTLINE:
         case PeriodeColor.LIGHTGREEN:
-            return <div className={styles.greenOutlineCircle} />;
+            return <div className={styles.greenOutlineRect} />;
         case PeriodeColor.BLACK:
-            return <div className={styles.blackCircle} />;
+            return <div className={styles.blackRect} />;
         case PeriodeColor.LIGHTBLUEGREEN:
-            return <div className={styles.lightblueGreenCircle} />;
+            return <div className={styles.lightblueGreenRect} />;
         case PeriodeColor.LIGHTGREENBLUE:
-            return <div className={styles.lightgreenBlueCircle} />;
+            return <div className={styles.lightgreenBlueRect} />;
         case PeriodeColor.BLUESTRIPED:
-            return <div className={styles.blueStripedCircle} />;
+            return <div className={styles.blueStripedRect} />;
         case PeriodeColor.GREENSTRIPED:
-            return <div className={styles.greenStripedCircle} />;
+            return <div className={styles.greenStripedRect} />;
         case PeriodeColor.GRAY:
-            return <div className={styles.grayCircle} />;
+            return <div className={styles.grayRect} />;
         case PeriodeColor.PURPLE:
-            return <div className={styles.purpleCircle} />;
+            return <div className={styles.purpleRect} />;
+        case PeriodeColor.PINK:
+            return <div className={styles.pinkRect} />;
         default:
             return null;
     }
-};
-
-const PANEL_STYLE = {
-    [PeriodeColor.NONE]: styles.none,
-    [PeriodeColor.BLUE]: styles.bluePanel,
-    [PeriodeColor.LIGHTBLUE]: styles.bluePanel,
-    [PeriodeColor.BLUESTRIPED]: styles.bluePanel,
-    [PeriodeColor.LIGHTGREENBLUE]: styles.bluePanel,
-    [PeriodeColor.LIGHTBLUEGREEN]: styles.greenPanel,
-    [PeriodeColor.GREEN]: styles.greenPanel,
-    [PeriodeColor.LIGHTGREEN]: styles.greenPanel,
-    [PeriodeColor.GREENSTRIPED]: styles.greenPanel,
-    [PeriodeColor.GRAY]: styles.grayPanel,
-    [PeriodeColor.PINK]: styles.pinkPanel,
-    [PeriodeColor.PURPLE]: styles.purlpePanel,
-    [PeriodeColor.BLACK]: styles.grayPanel,
-    [PeriodeColor.GREENOUTLINE]: styles.greenOutlinePanel,
-    [PeriodeColor.BLUEOUTLINE]: styles.blueOutlinePanel,
-    [PeriodeColor.BLACKOUTLINE]: styles.blackOutlinePanel,
-};
-
-interface Props {
-    children: React.ReactElement | React.ReactElement[];
-    iconType: PeriodeColor;
-}
-
-export const CalendarLabel = ({ children, iconType }: Props) => {
-    if (iconType === PeriodeColor.PINK) {
-        return (
-            <div className={styles.pinkPanel}>
-                <HStack gap="space-8" align="center">
-                    {children}
-                    <HeartFillIcon color="var(--ax-danger-500)" aria-hidden />
-                </HStack>
-            </div>
-        );
-    }
-    if (iconType === PeriodeColor.BLACK) {
-        return (
-            <div className={styles.grayPanel}>
-                <HStack gap="space-8" align="center">
-                    <ExclamationmarkTriangleFillIcon style={{ color: 'var(--ax-bg-warning-strong)' }} />
-                    {children}
-                    <div className={styles.margin}>{getSirkel(iconType)}</div>
-                </HStack>
-            </div>
-        );
-    } else if (iconType !== PeriodeColor.DARKBLUE) {
-        return (
-            <div className={`${PANEL_STYLE[iconType]}`}>
-                <HStack gap="space-8" align="end" wrap={false}>
-                    {children}
-                    <div className={styles.margin}>{getSirkel(iconType)}</div>
-                </HStack>
-            </div>
-        );
-    }
-    return null;
 };
