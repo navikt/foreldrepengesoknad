a2f0e6038915f54eefeb8ba235cbb3251db8f625
Johannes Moskvil <johannbm@users.noreply.github.com>
2025-11-03 17:02:22 +0100

    Bruk uttak periode fpoversikt (#5096)

--- packages/fp-common/src/common/types/PeriodeResultatDTO.ts
+++
@@ -1,8 +0,0 @@
-import { UttakPeriodeResultatÅrsak_fpoversikt } from '@navikt/fp-types';
-
-export interface PeriodeResultatDTO {
-    innvilget: boolean;
-    trekkerMinsterett: boolean;
-    trekkerDager: boolean;
-    årsak: UttakPeriodeResultatÅrsak_fpoversikt;
-}
