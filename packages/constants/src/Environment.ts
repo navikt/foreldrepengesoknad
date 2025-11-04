7873bbe0a9a88b3486f28af6c664d750021c3e97
johannbm <johannes.moskvil@gmail.com>
2025-11-04 17:51:32 +0100

    fiks Sentry release tag

--- packages/constants/src/Environment.ts
+++ packages/constants/src/Environment.ts
@@ -1,3 +1,13 @@
+/**
+ * Image navnet er på følgende format:
+ * europe-north1-docker.pkg.dev/nais-management-233d/teamforeldrepenger/navikt/foreldrepengesoknad/svangerskapspengesoknad:2025.11.04.144040-5887ba6
+ * Sentry støtter ikke "/". Stripper derfor alt frem til siste /
+ */
+const formatAppVersion = (appVersion: string) => {
+    const lastSlashIndex = appVersion.lastIndexOf('/');
+    return lastSlashIndex !== -1 ? appVersion.substring(lastSlashIndex + 1) : appVersion;
+};
+
 const initEnvironment = () => {
     const settingsNode = document.getElementById('nav:appSettings');
 
@@ -8,7 +18,7 @@ const initEnvironment = () => {
     const appSettings = JSON.parse((settingsNode as HTMLScriptElement).text);
 
     return {
-        APP_VERSION: appSettings.APP_VERSION,
+        APP_VERSION: formatAppVersion(appSettings.APP_VERSION),
         INNSYN: appSettings.INNSYN,
         FEATURE_TEST_1JULI2024_REGLER: appSettings.FEATURE_TEST_1JULI2024_REGLER,
         LOG_VALIDATION: appSettings.LOG_VALIDATION,
