3bd46ae2e1c3db9629bf05ec03c43dc5fbbeab49
johannbm <johannes.moskvil@gmail.com>
2025-11-04 09:50:00 +0100

    rydd

--- packages/types/src/SaksperiodeNy.ts
+++ packages/types/src/SaksperiodeNy.ts
@@ -1,26 +1,3 @@
-import { PeriodeResultat } from './PeriodeResultat';
-import {
-    BrukerRolleSak_fpoversikt,
-    Gradering_fpoversikt,
-    KontoTypeUttak_fpoversikt,
-    MorsAktivitet_fpoversikt,
-    UttakOppholdÅrsak_fpoversikt,
-    UttakOverføringÅrsak_fpoversikt,
-    UttakUtsettelseÅrsak_fpoversikt,
-} from './fpoversiktDtoGenerert';
+import { UttakPeriode_fpoversikt } from './fpoversiktDtoGenerert';
 
-export interface SaksperiodeNy {
-    fom: string;
-    tom: string;
-    forelder?: BrukerRolleSak_fpoversikt;
-    resultat?: PeriodeResultat;
-    flerbarnsdager?: boolean;
-    kontoType?: KontoTypeUttak_fpoversikt;
-    gradering?: Gradering_fpoversikt;
-    oppholdÅrsak?: UttakOppholdÅrsak_fpoversikt;
-    utsettelseÅrsak?: UttakUtsettelseÅrsak_fpoversikt;
-    overføringÅrsak?: UttakOverføringÅrsak_fpoversikt;
-    samtidigUttak?: number;
-    morsAktivitet?: MorsAktivitet_fpoversikt;
-    trekkdager?: number;
-}
+export type SaksperiodeNy = UttakPeriode_fpoversikt;
