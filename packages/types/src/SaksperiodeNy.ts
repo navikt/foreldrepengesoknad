deca588fd02397ef9a8a00df93cd7ec9b8e6d536
Johannes Moskvil <johannbm@users.noreply.github.com>
2025-11-07 10:40:49 +0100

    Fiks visning av aktivitetsfrikvote i oppsummering fra saksperioder (#5113)

--- packages/types/src/SaksperiodeNy.ts
+++ packages/types/src/SaksperiodeNy.ts
@@ -15,6 +15,12 @@ export interface SaksperiodeNy {
     forelder?: BrukerRolleSak_fpoversikt;
     resultat?: UttakPeriodeResultat_fpoversikt;
     flerbarnsdager?: boolean;
+    /**
+     * Denne er litt upresis når det kommer til AKTIVIETETSFRI_KVOTE.
+     * I planlegger så brukes kontotype fra fpgrunndata. Da kan den inneholde "AKTIVIETETSFRI_KVOTE"
+     * Men når perioden kommer fra søknad eller vedtak vil ikke AKTIVIETETSFRI_KVOTE finnes.
+     * I de tilfellene er det kontoType="FORELDREPENGER" + morsAktivitet="IKKE_OPPGITT" som tilsier at det går den aktivitetsfri kvoten.
+     */
     kontoType?: KontoTypeUttak_fpoversikt;
     gradering?: Gradering_fpoversikt;
     oppholdÅrsak?: UttakOppholdÅrsak_fpoversikt;
