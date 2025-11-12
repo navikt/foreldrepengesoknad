e39d9cf3554e50e37190a3d7665b4b04edf8ebd5
johannbm <johannes.moskvil@gmail.com>
2025-11-12 12:50:35 +0100

    add test

---
+++ packages/ui/src/file-uploader/FileUploader.test.tsx
@@ -0,0 +1,51 @@
+import { composeStories } from '@storybook/react-vite';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { mswWrapper } from '@navikt/fp-utils-test';
+
+import * as stories from './FileUploader.stories';
+
+const { Default, OpplastningTimeout } = composeStories(stories);
+
+describe('FileUploader', () => {
+    it(
+        'skal laste opp en fil',
+        mswWrapper(async ({ setHandlers }) => {
+            setHandlers(Default.parameters.msw);
+
+            render(<Default />);
+
+            expect(await screen.findByText('Last opp fil')).toBeInTheDocument();
+
+            const file = new File(['hello'], 'test-document.pdf', { type: 'application/pdf' });
+
+            const fileInput = screen.getByLabelText('Last opp fil');
+            await userEvent.upload(fileInput, file);
+
+            expect(await screen.findByText('test-document.pdf')).toBeInTheDocument();
+            expect(await screen.queryByText('Laster opp...')).not.toBeInTheDocument();
+        }),
+    );
+
+    it(
+        'skal få timeout ved opplasting',
+        mswWrapper(async ({ setHandlers }) => {
+            setHandlers(OpplastningTimeout.parameters.msw);
+
+            render(<OpplastningTimeout />);
+
+            expect(await screen.findByText('Last opp fil')).toBeInTheDocument();
+
+            const file = new File(['hello'], 'test-document.pdf', { type: 'application/pdf' });
+
+            const fileInput = screen.getByLabelText('Last opp fil');
+            await userEvent.upload(fileInput, file);
+
+            expect(await screen.findByText('test-document.pdf')).toBeInTheDocument();
+            expect(
+                await screen.findByText('Det tok for lang tid å laste opp dokumentet ditt.', { exact: false }),
+            ).toBeInTheDocument();
+        }),
+    );
+});
