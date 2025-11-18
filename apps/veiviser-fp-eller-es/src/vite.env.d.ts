904c0cfc6d31a36efbc18e77064e2fe30f36c5be
johannbm <johannes.moskvil@gmail.com>
2025-11-18 10:53:29 +0100

    fiks veiviser fp

---
+++ apps/veiviser-fp-eller-es/src/vite.env.d.ts
@@ -0,0 +1,14 @@
+interface ViteTypeOptions {
+    // By adding this line, you can make the type of ImportMetaEnv strict
+    // to disallow unknown keys.
+    strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+    readonly VITE_SENTRY_RELEASE: string;
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
