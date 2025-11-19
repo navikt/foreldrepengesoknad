b285d4388913582307f58796fdcfea001a24a5d6
Tor Røneid <tor.roneid@nav.no>
2025-11-19 15:45:17 +0100

    diverse fiksar på uttaksplan-kalender (#5145)
    * diverse fiksar på uttaksplan-kalender

--- packages/ny-uttaksplan/src/kalender/redigering/EksisterendeValgtePerioder.tsx
+++ packages/ny-uttaksplan/src/kalender/redigering/EksisterendeValgtePerioder.tsx
@@ -1,13 +1,17 @@
-import { PersonGroupIcon, PersonPregnantFillIcon, PersonSuitFillIcon, TrashIcon } from '@navikt/aksel-icons';
+import {
+    BriefcaseFillIcon,
+    ParasolBeachIcon,
+    PersonGroupIcon,
+    PersonPregnantFillIcon,
+    PersonSuitFillIcon,
+    TrashIcon,
+} from '@navikt/aksel-icons';
 import dayjs from 'dayjs';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import { BodyShort, HStack, Heading, Spacer, VStack } from '@navikt/ds-react';
 
-import { useUttaksplanData } from '../../context/UttaksplanDataContext';
 import { Planperiode } from '../../types/Planperiode';
-import { getFomPeriodeUtenUttakValidator } from '../../utils/dateFomValidators';
-import { getTomPeriodeUtenUttakValidator } from '../../utils/dateTomValidators';
 
 export type PlanperiodeMedAntallDager = Planperiode & { overlappendeDager: number };
 
@@ -18,19 +22,17 @@ interface Props {
 
 export const EksisterendeValgtePerioder = ({ perioder, slettPeriode }: Props) => {
     const intl = useIntl();
-    const { familiehendelsedato, familiesituasjon } = useUttaksplanData();
-
-    const periodeUtenUttakFomValidatorer = getFomPeriodeUtenUttakValidator(intl, familiehendelsedato, familiesituasjon);
-    const periodeUtenUttakTomValidatorer = getTomPeriodeUtenUttakValidator(intl, familiehendelsedato, familiesituasjon);
 
     return (
         <VStack gap="space-12">
             {perioder.map((p) => {
-                const erPeriodeSlettbar =
-                    periodeUtenUttakFomValidatorer(p.fom) === null && periodeUtenUttakTomValidatorer(p.tom) === null;
-
                 return (
-                    <HStack gap="space-4" key={p.id} wrap={false}>
+                    <HStack
+                        gap="space-8"
+                        key={p.id}
+                        wrap={false}
+                        data-testid={`eksisterende-periode-${p.fom}-${p.tom}`}
+                    >
                         {(p.kontoType === 'FORELDREPENGER_FØR_FØDSEL' || p.kontoType === 'MØDREKVOTE') && (
                             <PersonPregnantFillIcon
                                 title={intl.formatMessage({ id: 'RedigeringPanel.Mor' })}
@@ -58,6 +60,24 @@ export const EksisterendeValgtePerioder = ({ perioder, slettPeriode }: Props) =>
                                 color="var(--ax-bg-success-strong)"
                             />
                         )}
+                        {!p.erAnnenPartEøs && p.utsettelseÅrsak === 'LOVBESTEMT_FERIE' && (
+                            <ParasolBeachIcon
+                                title={intl.formatMessage({ id: 'RedigeringPanel.Ferie' })}
+                                fontSize="1.5rem"
+                                height="35px"
+                                width="35px"
+                                color="var(--ax-bg-warning-strong)"
+                            />
+                        )}
+                        {!p.erAnnenPartEøs && p.utsettelseÅrsak === 'ARBEID' && (
+                            <BriefcaseFillIcon
+                                title={intl.formatMessage({ id: 'RedigeringPanel.Arbeid' })}
+                                fontSize="1.5rem"
+                                height="35px"
+                                width="35px"
+                                color="var(--ax-bg-info-strong)"
+                            />
+                        )}
                         <VStack gap="space-0">
                             <Heading size="xsmall">
                                 {(p.kontoType === 'FORELDREPENGER_FØR_FØDSEL' || p.kontoType === 'MØDREKVOTE') && (
@@ -65,9 +85,14 @@ export const EksisterendeValgtePerioder = ({ perioder, slettPeriode }: Props) =>
                                 )}
                                 {p.kontoType === 'FEDREKVOTE' && <FormattedMessage id="RedigeringPanel.Far" />}
                                 {p.kontoType === 'FELLESPERIODE' && <FormattedMessage id="RedigeringPanel.Felles" />}
-                                {!p.erAnnenPartEøs && p.utsettelseÅrsak === 'LOVBESTEMT_FERIE' && (
-                                    <FormattedMessage id="RedigeringPanel.Ferie" />
-                                )}
+                                {!p.erAnnenPartEøs &&
+                                    p.kontoType === undefined &&
+                                    p.utsettelseÅrsak !== undefined &&
+                                    p.forelder === 'MOR' && <FormattedMessage id="RedigeringPanel.Mor" />}
+                                {!p.erAnnenPartEøs &&
+                                    p.kontoType === undefined &&
+                                    p.utsettelseÅrsak !== undefined &&
+                                    p.forelder === 'FAR_MEDMOR' && <FormattedMessage id="RedigeringPanel.Far" />}
                             </Heading>
                             <BodyShort>
                                 {p.kontoType === 'FORELDREPENGER_FØR_FØDSEL' && (
@@ -79,6 +104,12 @@ export const EksisterendeValgtePerioder = ({ perioder, slettPeriode }: Props) =>
                                 {p.kontoType === 'FELLESPERIODE' && (
                                     <FormattedMessage id="RedigeringPanel.Fellesperiode" />
                                 )}
+                                {!p.erAnnenPartEøs && p.utsettelseÅrsak === 'LOVBESTEMT_FERIE' && (
+                                    <FormattedMessage id="RedigeringPanel.Ferie" />
+                                )}
+                                {!p.erAnnenPartEøs && p.utsettelseÅrsak === 'ARBEID' && (
+                                    <FormattedMessage id="RedigeringPanel.Arbeid" />
+                                )}
                             </BodyShort>
                             <BodyShort>
                                 <FormattedMessage
@@ -88,14 +119,12 @@ export const EksisterendeValgtePerioder = ({ perioder, slettPeriode }: Props) =>
                             </BodyShort>
                         </VStack>
                         <Spacer />
-                        {erPeriodeSlettbar && (
-                            <TrashIcon
-                                title={intl.formatMessage({ id: 'RedigeringPanel.SlettPeriode' })}
-                                fontSize="1.5rem"
-                                className="cursor-pointer hover:opacity-70"
-                                onClick={() => slettPeriode(p)}
-                            />
-                        )}
+                        <TrashIcon
+                            title={intl.formatMessage({ id: 'RedigeringPanel.SlettPeriode' })}
+                            fontSize="1.5rem"
+                            className="cursor-pointer hover:opacity-70"
+                            onClick={() => slettPeriode(p)}
+                        />
                     </HStack>
                 );
             })}
