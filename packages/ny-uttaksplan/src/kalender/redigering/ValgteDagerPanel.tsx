5f59edf903f174117148e76dd16572d97bcb403b
tor røneid <tor.roneid@nav.no>
2025-11-20 08:16:28 +0100

    Fjern slett-alle-knapp i kalender i ny-uttakplan

--- packages/ny-uttaksplan/src/kalender/redigering/ValgteDagerPanel.tsx
+++ packages/ny-uttaksplan/src/kalender/redigering/ValgteDagerPanel.tsx
@@ -1,44 +1,23 @@
 import { uniqueId } from 'lodash';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 import { Button, HStack, VStack } from '@navikt/ds-react';
 
 import { useUttaksplanData } from '../../context/UttaksplanDataContext';
-import { PeriodeHullType, Planperiode } from '../../types/Planperiode';
-import { getFomPeriodeUtenUttakValidator } from '../../utils/dateFomValidators';
-import { getTomPeriodeUtenUttakValidator } from '../../utils/dateTomValidators';
+import { Planperiode } from '../../types/Planperiode';
 import { RedigeringPanel } from './RedigeringPanel';
 import { useKalenderRedigeringContext } from './context/KalenderRedigeringContext';
 
 export const ValgteDagerPanel = () => {
-    const intl = useIntl();
-
-    const { erFarEllerMedmor, familiehendelsedato, familiesituasjon } = useUttaksplanData();
+    const { erFarEllerMedmor } = useUttaksplanData();
 
     const {
         sammenslåtteValgtePerioder,
         erKunEnHelEksisterendePeriodeValgt,
-        eksisterendePerioderSomErValgt,
         oppdaterUttaksplan,
         setValgtePerioder,
         setErIRedigeringsmodus,
     } = useKalenderRedigeringContext();
-
-    const slettAllePerioder = () => {
-        const planperioder = sammenslåtteValgtePerioder.map<Planperiode>((p) => ({
-            erAnnenPartEøs: false,
-            forelder: erFarEllerMedmor ? 'FAR_MEDMOR' : 'MOR',
-            periodeHullÅrsak: PeriodeHullType.PERIODE_UTEN_UTTAK,
-            fom: p.fom,
-            tom: p.tom,
-            readOnly: false,
-            id: uniqueId(),
-        }));
-
-        oppdaterUttaksplan(planperioder);
-
-        setValgtePerioder([]);
-    };
 
     const leggTilFerie = () => {
         oppdaterUttaksplan(
@@ -56,13 +35,6 @@ export const ValgteDagerPanel = () => {
         setValgtePerioder([]);
     };
 
-    const periodeUtenUttakValidatorerFom = getFomPeriodeUtenUttakValidator(intl, familiehendelsedato, familiesituasjon);
-    const periodeUtenUttakValidatorerTom = getTomPeriodeUtenUttakValidator(intl, familiehendelsedato, familiesituasjon);
-
-    const erSlettValgbart = sammenslåtteValgtePerioder.some(
-        (p) => periodeUtenUttakValidatorerFom(p.fom) === null && periodeUtenUttakValidatorerTom(p.tom) === null,
-    );
-
     return (
         <RedigeringPanel>
             <VStack gap="space-12">
@@ -77,21 +49,10 @@ export const ValgteDagerPanel = () => {
                     <Button variant="secondary" size="small" onClick={leggTilFerie} type="button">
                         <FormattedMessage id="RedigeringPanel.LeggInnFerie" />
                     </Button>
-                    {eksisterendePerioderSomErValgt.length > 0 && erSlettValgbart && (
-                        <Button variant="tertiary" size="small" onClick={slettAllePerioder} type="button">
-                            {eksisterendePerioderSomErValgt.length === 1 ? (
-                                <FormattedMessage id="RedigeringPanel.Slett" />
-                            ) : (
-                                <FormattedMessage id="RedigeringPanel.SlettAlle" />
-                            )}
-                        </Button>
-                    )}
-                </HStack>
-                <div>
-                    <Button type="button" variant="secondary" size="small" onClick={() => setValgtePerioder([])}>
+                    <Button type="button" variant="tertiary" size="small" onClick={() => setValgtePerioder([])}>
                         <FormattedMessage id="RedigeringPanel.LukkRedigeringsmodus" />
                     </Button>
-                </div>
+                </HStack>
             </VStack>
         </RedigeringPanel>
     );
