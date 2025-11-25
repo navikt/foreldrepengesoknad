3662bce1bf7feed0a9d5f24e4f3714ffa37cb557
Petter Ekern <petter.ekern@nav.no>
2025-11-25 08:36:00 +0100

    Oppdaterte typer

--- packages/types/src/fpsoknadDtoGenerert.ts
+++ packages/types/src/fpsoknadDtoGenerert.ts
@@ -758,17 +758,6 @@ export type EttersendelseDto = {
 
 export type YtelseType = 'FORELDREPENGER' | 'SVANGERSKAPSPENGER' | 'ENGANGSSTØNAD';
 
-export type no_nav_foreldrepenger_soknad_innsending_StatusInnsendingTjeneste_ForsendelseStatus = {
-    saksnummer?: string;
-    status: no_nav_foreldrepenger_soknad_innsending_StatusInnsendingTjeneste_ForsendelseStatus_Status;
-};
-
-export type no_nav_foreldrepenger_soknad_innsending_StatusInnsendingTjeneste_ForsendelseStatus_Status =
-    | 'PENDING'
-    | 'MIDLERTIDIG'
-    | 'ENDELIG'
-    | 'FORSENDELSE_FINNES_IKKE';
-
 export type no_nav_foreldrepenger_soknad_mellomlagring_YtelseMellomlagringType =
     | 'FORELDREPENGER'
     | 'SVANGERSKAPSPENGER'
