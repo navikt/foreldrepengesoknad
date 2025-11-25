e2bf697913bfef79105385c33b3703887d8d1848
Petter Ekern <petter.ekern@nav.no>
2025-11-25 09:26:49 +0100

    Statuspanel

--- apps/foreldrepengeoversikt/src/pages/beregning-page/BeregningPage.tsx
+++ apps/foreldrepengeoversikt/src/pages/beregning-page/BeregningPage.tsx
@@ -1,7 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
-import { Alert, BodyShort, Loader } from '@navikt/ds-react';
+import { Alert, BodyShort, Loader, VStack } from '@navikt/ds-react';
+
+import { BeregningV1_fpoversikt } from '@navikt/fp-types';
+import { formatDate } from '@navikt/fp-utils';
 
 import { hentBeregningOptions } from '../../api/queries.ts';
 import { BeregningHeader } from '../../components/header/Header.tsx';
@@ -29,9 +32,41 @@ export const BeregningPage = () => {
             </PageRouteLayout>
         );
     }
+    const beregning = beregningQuery.data;
     return (
         <PageRouteLayout header={<BeregningHeader />}>
-            <p>{beregningQuery.data.skjæringsTidspunkt}</p>
+            <BeregningStatuser beregning={beregning} />
+            <p>Dette er bestemt utifra hvilke aktiviteter du hadde {formatDate(beregning.skjæringsTidspunkt)}</p>
         </PageRouteLayout>
+    );
+};
+
+const BeregningStatuser = ({ beregning }: { beregning: BeregningV1_fpoversikt }) => {
+    const statuser = beregning.beregningAktivitetStatuser.map((status) => {
+        switch (status.aktivitetStatus) {
+            case 'AT':
+                return 'arbeidstaker';
+            case 'AT_FL':
+                return 'kombinert arbeidstaker og frilanser';
+            default:
+                return '';
+            // TODO mere greier
+        }
+    });
+    const hjemler = beregning.beregningAktivitetStatuser.map((status) => {
+        switch (status.hjemmel) {
+            case 'F_14_7_8_40':
+                return 'folketrygdloven kapittel 14 paragraf 7 og kapittel 8 paragraf 40';
+            default:
+                return 'regler';
+            // TODO mere greier
+        }
+    });
+
+    return (
+        <VStack>
+            <BodyShort>Du er blitt beregnet som {statuser.join(', ')}</BodyShort>
+            <BodyShort>Dette er hjemlet i {hjemler.join(', ')}</BodyShort>
+        </VStack>
     );
 };
