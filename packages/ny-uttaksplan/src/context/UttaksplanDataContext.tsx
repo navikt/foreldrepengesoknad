d419413f81c3b84f558f68ae67f5dae5932603a3
tor røneid <tor.roneid@nav.no>
2025-11-25 10:26:50 +0100

    FIX feil i uttaksplanDataContext

--- packages/ny-uttaksplan/src/context/UttaksplanDataContext.tsx
+++ packages/ny-uttaksplan/src/context/UttaksplanDataContext.tsx
@@ -27,7 +27,7 @@ type Props = {
     bareFarMedmorHarRett: boolean;
     erDeltUttak: boolean;
     saksperioder: Array<UttakPeriode_fpoversikt | UttakPeriodeAnnenpartEøs_fpoversikt>;
-    erFlereUttaksplanversjoner: boolean;
+    erFlereUttaksplanversjoner?: boolean;
     children: React.ReactNode;
 };
 
@@ -41,7 +41,7 @@ type ContextValues = Omit<Props, 'children'> & {
 const UttaksplanDataContext = createContext<ContextValues | null>(null);
 
 export const UttaksplanDataProvider = (props: Props) => {
-    const { children, ...otherProps } = props;
+    const { children, erFlereUttaksplanversjoner, ...otherProps } = props;
 
     const value = useMemo(() => {
         const familiehendelsedato = getFamiliehendelsedato(otherProps.barn);
@@ -61,6 +61,7 @@ export const UttaksplanDataProvider = (props: Props) => {
             ...otherProps,
             familiehendelsedato,
             familiesituasjon,
+            erFlereUttaksplanversjoner: erFlereUttaksplanversjoner || false,
             rettighetType: utledRettighetType(otherProps.erDeltUttak, otherProps.aleneOmOmsorg),
             uttaksplan: utledKomplettPlan({
                 familiehendelsedato,
@@ -75,7 +76,7 @@ export const UttaksplanDataProvider = (props: Props) => {
                 modus: otherProps.modus,
             }),
         };
-    }, [otherProps]);
+    }, [otherProps, erFlereUttaksplanversjoner]);
 
     return <UttaksplanDataContext value={value}>{children}</UttaksplanDataContext>;
 };
