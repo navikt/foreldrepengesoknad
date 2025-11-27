091ca98d810010f121139609d473144594a3b1e1
AndreasDybdahl <andreas.dyb@gmail.com>
2025-11-27 15:23:46 +0100

    Testfiks

--- packages/ny-uttaksplan/src/Uttaksplan.test.tsx
+++ packages/ny-uttaksplan/src/Uttaksplan.test.tsx
@@ -48,7 +48,6 @@ describe('Uttaksplan', () => {
             },
             {
                 fom: '2025-06-30',
-                forelder: 'MOR',
                 tom: '2025-08-28',
                 utsettelseÅrsak: 'LOVBESTEMT_FERIE',
             },
