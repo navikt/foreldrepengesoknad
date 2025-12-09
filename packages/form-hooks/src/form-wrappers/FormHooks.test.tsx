60634808dea91935468236e3fdcf0fecb46625e2
Johannes Moskvil <johannbm@users.noreply.github.com>
2025-12-09 08:41:36 +0100

    Tfp 6510 mellomrom tallinput (#5188)

--- packages/form-hooks/src/form-wrappers/FormHooks.test.tsx
+++ packages/form-hooks/src/form-wrappers/FormHooks.test.tsx
@@ -20,4 +20,44 @@ describe('<FormHooks>', () => {
         await userEvent.type(datofelt, '01.02.2020');
         fireEvent.blur(datofelt);
     });
+
+    it('skal sjekke at formatterTallTextField fungerer som forventet', async () => {
+        const utils = render(<VisFormkomponenter />);
+
+        const norskMinusTegn = '\u2212';
+        const norskMellomRom = '\u00A0';
+
+        const inputfelt = utils.getByLabelText('Tekstfelt for formattert tall');
+
+        await userEvent.type(inputfelt, '1');
+        expect(inputfelt).toHaveValue('1');
+        await userEvent.type(inputfelt, '0');
+        expect(inputfelt).toHaveValue('10');
+        await userEvent.type(inputfelt, '0');
+        expect(inputfelt).toHaveValue('100');
+        await userEvent.type(inputfelt, '0');
+        expect(inputfelt).toHaveValue(`1${norskMellomRom}000`);
+        await userEvent.type(inputfelt, '000');
+        expect(inputfelt).toHaveValue(`1${norskMellomRom}000${norskMellomRom}000`);
+
+        // Skal ikke kunne skrive punktum
+        await userEvent.clear(inputfelt);
+        await userEvent.type(inputfelt, '100.');
+        expect(inputfelt).toHaveValue('100');
+
+        // Skal kun kunne skrive to desimaler
+        await userEvent.clear(inputfelt);
+        await userEvent.type(inputfelt, '100,123');
+        expect(inputfelt).toHaveValue('100,12');
+
+        // Skal skrive minustall
+        await userEvent.clear(inputfelt);
+        await userEvent.type(inputfelt, '-1000,123');
+        expect(inputfelt).toHaveValue(`${norskMinusTegn}1${norskMellomRom}000,12`);
+
+        // Skal ignorere mellomrom
+        await userEvent.clear(inputfelt);
+        await userEvent.type(inputfelt, '1 00');
+        expect(inputfelt).toHaveValue(`100`);
+    });
 });
