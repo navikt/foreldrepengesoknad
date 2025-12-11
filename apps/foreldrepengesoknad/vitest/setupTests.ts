d09dbcba413a9d66916cfa66ad1e9ddb0239a130
Johannes Moskvil <johannbm@users.noreply.github.com>
2025-12-11 10:46:21 +0100

    Oppgrader ky og noen til (#5190)

--- apps/foreldrepengesoknad/vitest/setupTests.ts
+++ apps/foreldrepengesoknad/vitest/setupTests.ts
@@ -14,4 +14,15 @@ expect.extend(matchers);
 
 if (import.meta.env['TEST_MODE'] === 'jsdom-mode') {
     globalThis.scrollTo = () => undefined;
+
+    if (globalThis.ReadableStream) {
+        const RS = globalThis.ReadableStream.prototype;
+
+        // Denne var tricky å finne ut av. Den feiler ikke i jsdom, men den blir stående å vente/henge på et eller annet uvisst.
+        // Resolver den derfor umiddelbart. Det er oppførsel (retries, opploadprogress, etc.) som ikke er relevant å teste i jsdom.
+        // Om det i fremtiden skal testes bør det skje i ekte browser - ikke jsdom
+        RS.cancel = function () {
+            return Promise.resolve();
+        };
+    }
 }
