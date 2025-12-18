5640930ec004ac4817e06d9da38a4f2a1282cadf
johannbm <johannes.moskvil@gmail.com>
2025-12-18 13:42:48 +0100

    skriv enkel test

---
+++ apps/foreldrepengeoversikt/src/pages/beregning-page/BeregningPage.test.tsx
@@ -0,0 +1,26 @@
+import { composeStories } from '@storybook/react-vite';
+import { render, screen } from '@testing-library/react';
+
+import { mswWrapper } from '@navikt/fp-utils-test';
+
+import * as stories from './BeregningPage.stories.tsx';
+
+const { Beregning } = composeStories(stories);
+
+describe('<BeregningPage>', () => {
+    it(
+        'Default Beregning',
+        mswWrapper(async ({ setHandlers }) => {
+            setHandlers(Beregning.parameters.msw);
+            render(<Beregning />);
+
+            expect(await screen.findByText('Beregning av din ytelse')).toBeInTheDocument();
+            expect(await screen.findByText('Dagsats: 3 004 kr')).toBeInTheDocument();
+
+            (await screen.findByText('Beregning av foreldrepenger')).click();
+
+            expect(await screen.findByText('Bedrift - 992257822')).toBeInTheDocument();
+            expect(await screen.findByText('Frilans')).toBeInTheDocument();
+        }),
+    );
+});
