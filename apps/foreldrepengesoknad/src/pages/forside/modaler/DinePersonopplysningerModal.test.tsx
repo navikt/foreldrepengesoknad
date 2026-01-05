fad7ed5639e602279788ec7902c391dbfb957d61
johannbm <johannes.moskvil@gmail.com>
2026-01-05 19:24:16 +0100

    fiks test

--- apps/foreldrepengesoknad/src/pages/forside/modaler/DinePersonopplysningerModal.test.tsx
+++ apps/foreldrepengesoknad/src/pages/forside/modaler/DinePersonopplysningerModal.test.tsx
@@ -1,5 +1,6 @@
 import { composeStories } from '@storybook/react-vite';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event/dist/cjs/index.js';
 
 import * as stories from './DinePersonopplysningerModal.stories';
 
@@ -8,6 +9,7 @@ const { Default } = composeStories(stories);
 describe('<DinePersonopplysningerModal>', () => {
     it('skal vise modal for dine personopplysninger', async () => {
         render(<Default />);
+        await userEvent.click(screen.getByText('Les om hvordan Nav behandler personopplysningene dine'));
         expect(await screen.findByText('Slik behandler Nav personopplysningene dine')).toBeInTheDocument();
     });
 });
