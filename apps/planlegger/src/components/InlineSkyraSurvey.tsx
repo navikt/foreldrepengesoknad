2ff86295698d8d3d3a2fe8e08937db309f109821
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2026-01-21 12:00:44 +0100

    legg til skelleton

--- apps/planlegger/src/components/InlineSkyraSurvey.tsx
+++ apps/planlegger/src/components/InlineSkyraSurvey.tsx
@@ -1,4 +1,6 @@
-import { Box } from '@navikt/ds-react';
+import { useEffect, useState } from 'react';
+
+import { Box, HStack, Heading, Skeleton } from '@navikt/ds-react';
 
 import './InlineSkyraSurvey.module.css';
 
@@ -13,11 +15,36 @@ declare global {
 }
 
 export const InlineSkyraSurvey = () => {
+    const [showSkeleton, setShowSkeleton] = useState(true);
+
+    useEffect(() => {
+        // Vis skeleton i 2 sekunder, deretter vis surveyen
+        const timer = setTimeout(() => {
+            setShowSkeleton(false);
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <Box padding="space-16" className="border-ax-neutral-500 border mb-2 ax-border-default rounded-xl">
+        <Box
+            padding="space-16"
+            className="border-ax-neutral-500 border mb-6 ax-border-default rounded-xl ax-drop-shadow-ax-accent-500"
+        >
+            {showSkeleton && (
+                <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                    <Heading as={Skeleton} size="large">
+                        Vi ønsker din tilbakemelding
+                    </Heading>
+                    <Skeleton variant="rounded" width="100%" height={30} />
+                    <HStack justify={'end'}>
+                        <Skeleton variant="rounded" width="30%" height={50} />
+                    </HStack>
+                </div>
+            )}
             {/* @ts-expect-error - skyra-survey er et custom element */}
             <skyra-survey
-                className=""
+                style={{ display: showSkeleton ? 'none' : 'block' }}
                 slug="arbeids-og-velferdsetaten-nav/planlegg-foreldrepenger-inline"
             ></skyra-survey>
         </Box>
