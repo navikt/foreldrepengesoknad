a76fd40e29caacbb171f59624f447cdf74e8d0b9
Marthe <marthe.hagen@nav.no>
2026-02-06 11:32:43 +0100

    lagt til nye punkter

---
+++ apps/planlegger/src/steps/planen-deres/uforutsette-endringer/HvisBarnetErInnlagt.tsx
@@ -0,0 +1,44 @@
+import { StethoscopeIcon } from '@navikt/aksel-icons';
+import { FormattedMessage } from 'react-intl';
+import { Arbeidssituasjon } from 'types/Arbeidssituasjon';
+import { utledHvemSomHarRett } from 'utils/hvemHarRettUtils';
+
+import { BodyLong, HStack, Heading } from '@navikt/ds-react';
+
+import { IconCircleWrapper } from '@navikt/fp-ui';
+
+interface Props {
+    arbeidssituasjon: Arbeidssituasjon;
+}
+
+export const HvisBarnetErInnlagt = ({ arbeidssituasjon }: Props) => {
+    const hvemHarRett = utledHvemSomHarRett(arbeidssituasjon);
+    const kunEnPartSkalHa = hvemHarRett !== 'beggeHarRett';
+
+    return (
+        <HStack gap="space-20" wrap={false}>
+            <div>
+                <IconCircleWrapper color="lightBlue" size="medium">
+                    <StethoscopeIcon
+                        height={22}
+                        width={22}
+                        fontSize="1.5rem"
+                        color="var(--ax-bg-accent-strong)"
+                        aria-hidden
+                    />
+                </IconCircleWrapper>
+            </div>
+            <div>
+                <Heading size="small" level="4">
+                    <FormattedMessage id="UforutsetteEndringer.UforutsetteEndringer.HvisBarnetErInnlagt" />
+                </Heading>
+                <BodyLong>
+                    <FormattedMessage
+                        id="UforutsetteEndringer.UforutsetteEndringer.HvisBarnetErInnlagt.Tekst"
+                        values={{ erAleneforsørger: kunEnPartSkalHa }}
+                    />
+                </BodyLong>
+            </div>
+        </HStack>
+    );
+};
