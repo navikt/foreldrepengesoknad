403b986d25962202aedd54150ec7454d4571ca92
Tor Røneid <tor.roneid@nav.no>
2026-02-25 13:52:23 +0100

    Har justert bredden i planlegger og fp-søknad sånn at ein får to kolo… (#5319)
    Har justert bredden i planlegger og fp-søknad sånn at ein får to kolonner i uttaksplan-redigeringsmodus

--- packages/ui/src/page/Page.tsx
+++ packages/ui/src/page/Page.tsx
@@ -5,16 +5,17 @@ interface Props {
     children: React.ReactElement | React.ReactElement[];
 }
 
+// TODO (TOR) Ta i bruk Page-komponenten til Aksel (Evt SkjemaRotLayout)
 export const Page = ({ header, children }: Props) => {
     return (
-        <div className="bg-ax-neutral-200 pt-4 pb-4 max-[768px]:flex max-[768px]:flex-1 max-[768px]:flex-col max-[768px]:pt-0">
-            <div className="bg-ax-bg-default mx-auto max-w-[704px] rounded-t-[8px] max-[768px]:m-0 max-[768px]:w-full max-[768px]:max-w-none">
+        <div className="bg-ax-neutral-200 pt-4 pb-4 max-[1024px]:flex max-[1024px]:flex-1 max-[1024px]:flex-col max-[1024px]:pt-0">
+            <div className="bg-ax-bg-default mx-auto max-w-[1004px] rounded-t-[8px] max-[1024px]:m-0 max-[1024px]:w-full max-[1024px]:max-w-none">
                 {header}
             </div>
             <div
                 className={
-                    'bg-ax-bg-default mx-auto max-w-[704px] rounded-b-[8px] p-8 max-[768px]:m-0 max-[768px]:flex max-[768px]:max-w-none ' +
-                    'max-[768px]:flex-1 max-[768px]:flex-col max-[768px]:rounded-none max-[768px]:px-[1rem] max-[768px]:py-[1.5rem]'
+                    'bg-ax-bg-default mx-auto max-w-[1004px] rounded-b-[8px] p-8 max-[1024px]:m-0 max-[1024px]:flex max-[1024px]:max-w-none ' +
+                    'max-[1024px]:flex-1 max-[1024px]:flex-col max-[1024px]:rounded-none max-[1024px]:px-[1rem] max-[1024px]:py-[1.5rem]'
                 }
             >
                 {children}
