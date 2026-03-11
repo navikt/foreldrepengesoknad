43b6df4af0866953fdffe3ce482428fb3fd07038
johannbm <johannes.moskvil@gmail.com>
2026-03-11 12:28:35 +0100

    wip bedre filtrering av sentry events

--- packages/observability/src/initSentry.ts
+++ packages/observability/src/initSentry.ts
@@ -9,9 +9,10 @@ type InitSentryOptions = {
 const FEIL_VI_VIL_LUKE_BORT = ['personbruker/decorator-next'];
 
 export const initSentry = ({ dsn }: InitSentryOptions) => {
-    if (import.meta.env.MODE === 'development') {
-        return;
-    }
+    // if (import.meta.env.MODE === 'development') {
+    //     return;
+    // }
+    console.log('init sentry');
 
     Sentry.init({
         dsn,
@@ -19,21 +20,51 @@ export const initSentry = ({ dsn }: InitSentryOptions) => {
         environment: globalThis.location.hostname,
         integrations: [Sentry.breadcrumbsIntegration({ console: false })],
         beforeSend: (event) => {
-            const harStacktraceMedOpprinnelseIVårKode = (event.exception?.values ?? []).some((ex) => {
-                const assetsFrames =
-                    ex.stacktrace?.frames?.filter(
-                        (frame) => frame.filename && /\/assets\/.*\.js$/.test(frame.filename),
-                    ) ?? [];
+            console.log('event', event);
+            if (feilVarSomFølgeAvEn401Handling(event)) {
+                return null;
+            }
 
-                if (assetsFrames.length === 0) {
+            if (feilUtenOpprinnelseIVårKode(event)) {
+                return null;
+            }
+
+            return event;
+        },
+    });
+};
+
+/**
+ * Sjekker først om stacktrace kommer fra en assets fil. Feks "/foreldrepenger/soknad/assets/index-66kDgVTH.js"
+ * Disse kan også matche
+ */
+const feilUtenOpprinnelseIVårKode = (event: Sentry.ErrorEvent) => {
+    return (event.exception?.values ?? []).some((ex) => {
+        return ex.stacktrace?.frames?.some((frame) => {
+            const assetFrame = frame.filename && /\/assets\/.*\.js$/.test(frame.filename);
+
+            if (assetFrame) {
+                const erUønsketAssetFrame = FEIL_VI_VIL_LUKE_BORT.some((feil) => frame.filename?.includes(feil));
+                if (erUønsketAssetFrame) {
                     return false;
                 }
 
-                return assetsFrames.some(
-                    (frame) => !FEIL_VI_VIL_LUKE_BORT.some((feil) => frame.abs_path?.includes(feil)),
-                );
-            });
-            return harStacktraceMedOpprinnelseIVårKode ? event : null;
-        },
+                return true;
+            }
+            return false;
+        });
     });
+};
+
+/**
+ * 401 skaper mye støy da det er naturlig at folk sine sesjoner utløper. De blir da automatisk redirected til login, og ser ikke feilen engang
+ */
+const feilVarSomFølgeAvEn401Handling = (event: Sentry.ErrorEvent) => {
+    const sisteBreadcrumb = event.breadcrumbs?.at(-1)?.data ?? {};
+
+    if ('status_code' in sisteBreadcrumb) {
+        return sisteBreadcrumb.status_code === 401;
+    }
+
+    return false;
 };
---
+++ packages/observability/vitest/setupTests.ts
@@ -0,0 +1,1 @@
+// Minimal setup for observability tests
