35189292da42ad0603a228ecf5d1b0e88d1ea472
johannbm <johannes.moskvil@gmail.com>
2026-03-17 13:10:45 +0100

    sett opp for authenticated apps

--- apps/svangerskapspengesoknad/vite.config.ts
+++ apps/svangerskapspengesoknad/vite.config.ts
@@ -10,6 +10,9 @@ const setupFileDirName = path.resolve(__dirname, './vitest/setupTests.ts');
 // eslint-disable-next-line import/no-default-export
 export default mergeConfig(createSharedConfigWithCrossorgin(setupFileDirName), {
     base: '/svangerskapspenger/soknad',
+    server: {
+        port: 9112,
+    },
     plugins: [
         // Put the Sentry vite plugin after all other plugins
         sentryVitePlugin({
