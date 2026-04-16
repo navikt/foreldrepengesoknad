eef2f712eaeac40df7dc0b77025c2a79508b525b
johannbm <johannes.moskvil@gmail.com>
2026-04-16 12:48:21 +0200

    fix !=

--- apps/svangerskapspengesoknad/src/app-data/SvpDataContext.tsx
+++ apps/svangerskapspengesoknad/src/app-data/SvpDataContext.tsx
@@ -33,7 +33,6 @@ export enum ContextDataType {
 }
 
 export type ContextDataMap = {
-    // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
     [ContextDataType.APP_ROUTE]?: SøknadRoute | string;
     [ContextDataType.OM_BARNET]?: Barn;
     [ContextDataType.UTENLANDSOPPHOLD]?: Utenlandsopphold;
