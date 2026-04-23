e816c44178089b285ddbeae23e7e08f837fe128a
johannbm <johannes.moskvil@gmail.com>
2026-04-23 13:32:40 +0200

    kun set context

--- packages/observability/src/captureApiError.ts
+++ packages/observability/src/captureApiError.ts
@@ -4,14 +4,8 @@ import type { ProblemDetails } from '@navikt/fp-types';
 
 export const captureApiError = (message: string, apiError?: ProblemDetails) => {
     withScope((scope) => {
-        if (apiError?.callId) {
-            scope.setTag('callId', apiError.callId);
-        }
-        if (apiError?.feilkode) {
-            scope.setTag('feilkode', apiError.feilkode);
-        }
         if (apiError) {
-            scope.setContext('apiError', { ...apiError });
+            scope.setContext('apiError', apiError);
         }
         captureMessage(message);
     });
