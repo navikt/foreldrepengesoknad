4106c98c24675c3d341b50517cc7d560e0772bf8
AndreasDybdahl <andreas.dyb@gmail.com>
2026-05-06 15:25:29 +0200

    Sjekk også på submitted status

--- apps/foreldrepengesoknad/src/steps/annen-forelder/OppgiPersonalia.tsx
+++ apps/foreldrepengesoknad/src/steps/annen-forelder/OppgiPersonalia.tsx
@@ -87,7 +87,7 @@ export const OppgiPersonalia = ({ søkersFødselsnummer, rolle, barn }: Props) =
                         <RhfCheckbox
                             name="utenlandskFnr"
                             control={formMethods.control}
-                            onChange={() => void formMethods.trigger()}
+                            onChange={() => formMethods.formState.isSubmitted && void formMethods.trigger()}
                             label={intl.formatMessage({ id: 'annenForelder.spørsmål.utenlandskFnr' })}
                         />
                     </div>
