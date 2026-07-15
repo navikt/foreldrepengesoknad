abb368cd5ca6e547bf58273faa5c1e87aaf0c4a2
johannbm <johannes.moskvil@gmail.com>
2026-07-15 08:48:02 +0200

    wip

--- packages/steg-arbeidsforhold-og-inntekt/index.ts
+++ packages/steg-arbeidsforhold-og-inntekt/index.ts
@@ -9,3 +9,14 @@ export const arbeidsforholdOgInntektMessages = {
 };
 
 export { ArbeidsforholdOgInntektPanel } from './src/ArbeidsforholdOgInntektPanel';
+export { AndreInntektskilderFieldArray } from './src/components/andre-inntekter/AndreInntektskilderFieldArray';
+export { AndreInntektskilderModal } from './src/components/andre-inntekter/AndreInntektskilderModal';
+export { HarArbeidsforhold } from './src/components/arbeidsforhold-informasjon/HarArbeidsforhold';
+export { HarIkkeArbeidsforhold } from './src/components/arbeidsforhold-informasjon/HarIkkeArbeidsforhold';
+export {
+    AnnenInntektType,
+    type AndreInntektskilder,
+    type AndreInntektskilderUtkast,
+    type AndreInntekterFormValues,
+    erFerdigUtfylt,
+} from './src/types/AndreInntektskilder';
