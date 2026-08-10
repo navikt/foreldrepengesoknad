c7298dbb7b011be0ba802d293f7e8999b2af725a
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2026-08-10 11:55:21 +0200

    Fiks test

--- apps/foreldrepengesoknad/src/steps/periode-med-foreldrepenger/PeriodeMedForeldrepengerSteg.test.tsx
+++ apps/foreldrepengesoknad/src/steps/periode-med-foreldrepenger/PeriodeMedForeldrepengerSteg.test.tsx
@@ -249,7 +249,7 @@ describe('<PeriodeMedForeldrepengerSteg>', () => {
         ).toBeInTheDocument();
         expect(
             screen.getByText(
-                'Perioden blir forlenget med 17 uker hvis dere velger 100 % foreldrepenger eller 21 uker hvis dere velger 80% foreldrepenger.',
+                'Perioden blir forlenget med 17 uker hvis dere velger 100 % foreldrepenger eller 21 uker hvis dere velger 80 % foreldrepenger.',
             ),
         ).toBeInTheDocument();
 
@@ -291,7 +291,7 @@ describe('<PeriodeMedForeldrepengerSteg>', () => {
         ).toBeInTheDocument();
         expect(
             screen.getByText(
-                'Perioden blir forlenget med 46 uker hvis du velger 100 % foreldrepenger eller 56 uker hvis du velger 80% foreldrepenger.',
+                'Perioden blir forlenget med 46 uker hvis du velger 100 % foreldrepenger eller 56 uker hvis du velger 80 % foreldrepenger.',
             ),
         ).toBeInTheDocument();
 
