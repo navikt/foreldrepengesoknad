f06276a09be64217da48c43599b9f4a2d9ad5de7
Johannes Moskvil <johannbm@users.noreply.github.com>
2026-08-20 13:18:18 +0200

    Revert navigasjonsblokk (#5788)
    * Revert "fix(foreldrepengesoknad): hald URL og APP_ROUTE synkronisert (#5598)"
    
    This reverts commit cbc83f9e
    
    * Revert "fix(foreldrepengesoknad): unngå Step-krasj «Ingen valgte steg funnet» ved betinget stegsynlighet (#5597)"
    
    This reverts commit 8398b3ef
    
    * Revert "fix(foreldrepengesoknad): snapp URL til APP_ROUTE under render, ikkje… (#5609)"
    
    This reverts commit 6f2742df
    
    * Fiks

--- apps/foreldrepengesoknad/src/app-data/useStepConfig.ts
+++ apps/foreldrepengesoknad/src/app-data/useStepConfig.ts
@@ -39,37 +39,52 @@ const getPathToLabelMap = (intl: IntlShape) =>
         [SøknadRoutes.IKKE_MYNDIG]: '',
     }) satisfies Record<SøknadRoutes, string>;
 
+const isAfterStep = (previousStepPath: SøknadRoutes, currentStepPath: SøknadRoutes): boolean => {
+    return ROUTES_ORDER.indexOf(currentStepPath) > ROUTES_ORDER.indexOf(previousStepPath);
+};
+
 const showUtenlandsoppholdStep = (
     path: SøknadRoutes,
+    currentPath: SøknadRoutes,
     getData: <TYPE extends ContextDataType>(key: TYPE) => ContextDataMap[TYPE],
 ) => {
     if (path === SøknadRoutes.TIDLIGERE_UTENLANDSOPPHOLD) {
-        const erValgt = getData(ContextDataType.UTENLANDSOPPHOLD)?.harBoddUtenforNorgeSiste12Mnd === true;
-        return erValgt || !!getData(ContextDataType.UTENLANDSOPPHOLD_TIDLIGERE);
+        const erValgtOgEtterSteg =
+            getData(ContextDataType.UTENLANDSOPPHOLD)?.harBoddUtenforNorgeSiste12Mnd === true &&
+            isAfterStep(SøknadRoutes.UTENLANDSOPPHOLD, currentPath);
+        return erValgtOgEtterSteg || !!getData(ContextDataType.UTENLANDSOPPHOLD_TIDLIGERE);
     }
     if (path === SøknadRoutes.SENERE_UTENLANDSOPPHOLD) {
-        const erValgt = getData(ContextDataType.UTENLANDSOPPHOLD)?.skalBoUtenforNorgeNeste12Mnd === true;
-        return erValgt || !!getData(ContextDataType.UTENLANDSOPPHOLD_SENERE);
+        const erValgtOgEtterSteg =
+            getData(ContextDataType.UTENLANDSOPPHOLD)?.skalBoUtenforNorgeNeste12Mnd === true &&
+            isAfterStep(SøknadRoutes.UTENLANDSOPPHOLD, currentPath);
+        return erValgtOgEtterSteg || !!getData(ContextDataType.UTENLANDSOPPHOLD_SENERE);
     }
     return false;
 };
 
 const showFrilansOgEgenNæringOgAndreInntekter = (
     path: SøknadRoutes,
+    currentPath: SøknadRoutes,
     getData: <TYPE extends ContextDataType>(key: TYPE) => ContextDataMap[TYPE],
 ) => {
     if (path === SøknadRoutes.FRILANS) {
-        const erValgt = getData(ContextDataType.ARBEIDSFORHOLD_OG_INNTEKT)?.harJobbetSomFrilans === true;
-        return erValgt || !!getData(ContextDataType.FRILANS);
+        const erValgtOgEtterSteg =
+            getData(ContextDataType.ARBEIDSFORHOLD_OG_INNTEKT)?.harJobbetSomFrilans === true &&
+            isAfterStep(SøknadRoutes.ARBEID_OG_INNTEKT, currentPath);
+        return erValgtOgEtterSteg || !!getData(ContextDataType.FRILANS);
     }
     if (path === SøknadRoutes.EGEN_NÆRING) {
-        const erValgt =
-            getData(ContextDataType.ARBEIDSFORHOLD_OG_INNTEKT)?.harJobbetSomSelvstendigNæringsdrivende === true;
-        return erValgt || !!getData(ContextDataType.EGEN_NÆRING);
+        const erValgtOgEtterSteg =
+            getData(ContextDataType.ARBEIDSFORHOLD_OG_INNTEKT)?.harJobbetSomSelvstendigNæringsdrivende === true &&
+            isAfterStep(SøknadRoutes.ARBEID_OG_INNTEKT, currentPath);
+        return erValgtOgEtterSteg || !!getData(ContextDataType.EGEN_NÆRING);
     }
     if (path === SøknadRoutes.ANDRE_INNTEKTER) {
-        const erValgt = getData(ContextDataType.ARBEIDSFORHOLD_OG_INNTEKT)?.harHattAndreInntektskilder === true;
-        return erValgt || !!getData(ContextDataType.ANDRE_INNTEKTSKILDER);
+        const erValgtOgEtterSteg =
+            getData(ContextDataType.ARBEIDSFORHOLD_OG_INNTEKT)?.harHattAndreInntektskilder === true &&
+            isAfterStep(SøknadRoutes.ARBEID_OG_INNTEKT, currentPath);
+        return erValgtOgEtterSteg || !!getData(ContextDataType.ANDRE_INNTEKTSKILDER);
     }
     return false;
 };
@@ -176,13 +191,13 @@ export const useStepConfig = (
         () =>
             ROUTES_ORDER.flatMap((path) =>
                 (requiredSteps.includes(path) && skalViseFordelingSteg(path, getStateData)) ||
-                showUtenlandsoppholdStep(path, getStateData) ||
+                showUtenlandsoppholdStep(path, currentPath, getStateData) ||
                 showManglendeDokumentasjonSteg(path, getStateData, arbeidsforhold, eksisterendeSak) ||
-                showFrilansOgEgenNæringOgAndreInntekter(path, getStateData)
+                showFrilansOgEgenNæringOgAndreInntekter(path, currentPath, getStateData)
                     ? [path]
                     : [],
             ),
-        [requiredSteps, getStateData, arbeidsforhold, eksisterendeSak],
+        [requiredSteps, currentPath, getStateData, arbeidsforhold, eksisterendeSak],
     );
 
     return useMemo(
