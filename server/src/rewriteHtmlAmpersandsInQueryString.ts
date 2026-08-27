95abdb2126c93c00682dfde929b52d950fcd5b56
johannbm <johannes.moskvil@gmail.com>
2026-08-27 21:32:47 +0200

    Legg til unicorn og bedre lint og tsc for server

--- server/src/rewriteHtmlAmpersandsInQueryString.ts
+++ server/src/rewriteHtmlAmpersandsInQueryString.ts
@@ -2,10 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 
 import { logger } from '@navikt/fp-server-utils';
 
-export const rewriteHtmlAmpersandsInQueryString = (req: Request, _res: Response, next: NextFunction) => {
-    if (req.url.includes('?') && req.url.includes('&amp;')) {
+export const rewriteHtmlAmpersandsInQueryString = (request: Request, _response: Response, next: NextFunction) => {
+    if (request.url.includes('?') && request.url.includes('&amp;')) {
         logger.info(`Bytter ut HTML-encoded ampersand med literal ampersand i query string`);
-        req.url = req.url.replaceAll('&amp;', '&');
+        request.url = request.url.replaceAll('&amp;', '&');
     }
     next();
 };
