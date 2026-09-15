0bee20ab8fb0f4fabdca0ba9c899271e83103705
AndreasDybdahl <andreas.dyb@gmail.com>
2026-09-15 12:48:55 +0200

    Foreslå aktivitetsfri kvote og ikke kvote med aktivitet

--- packages/uttaksplan/src/utils/forslag/ikkeDeltUttak.test.ts
+++ packages/uttaksplan/src/utils/forslag/ikkeDeltUttak.test.ts
@@ -36,12 +36,13 @@ describe('ikkeDeltUttak - Fødsel - Far/Medmor - WLB gjelder', () => {
             farOgFar: false,
         });
 
-        // Aktivitetsfri kvote skal ikke lenger foreslås automatisk, kun foreldrepenger med aktivitetskrav.
+        // Mor har ikke rett her, så det foreslås kun den mindre aktivitetsfrie kvoten (40 dager).
+        // Foreldrepenger med aktivitetskrav holdes i reserve slik at brukeren selv kan velge å bruke den.
         expect(forslag.length).toEqual(1);
         expect(forslag[0]!.fom).toEqual(startdato);
-        expect(forslag[0]!.tom).toEqual('2022-11-18');
+        expect(forslag[0]!.tom).toEqual('2022-09-30');
         expect(forslag[0]!.kontoType).toEqual('FORELDREPENGER');
-        expect(forslag[0]!.morsAktivitet).toEqual(undefined);
+        expect(forslag[0]!.morsAktivitet).toEqual('IKKE_OPPGITT');
     });
 
     it('skal legge til en periode på 8 uker etter fødsel hvis WLB gjelder og situasjon er fødsel og startdato blir satt til lørdag rett før fødsel', () => {
@@ -57,12 +58,13 @@ describe('ikkeDeltUttak - Fødsel - Far/Medmor - WLB gjelder', () => {
             erAleneOmOmsorg: false,
             farOgFar: false,
         });
-        // Aktivitetsfri kvote skal ikke lenger foreslås automatisk, kun foreldrepenger med aktivitetskrav.
+        // Mor har ikke rett her, så det foreslås kun den mindre aktivitetsfrie kvoten (40 dager).
+        // Foreldrepenger med aktivitetskrav holdes i reserve slik at brukeren selv kan velge å bruke den.
         expect(forslag.length).toEqual(1);
         expect(forslag[0]!.fom).toEqual(famDato);
-        expect(forslag[0]!.tom).toEqual('2022-11-18');
+        expect(forslag[0]!.tom).toEqual('2022-09-30');
         expect(forslag[0]!.kontoType).toEqual('FORELDREPENGER');
-        expect(forslag[0]!.morsAktivitet).toEqual(undefined);
+        expect(forslag[0]!.morsAktivitet).toEqual('IKKE_OPPGITT');
     });
 });
 
@@ -196,6 +198,27 @@ describe('ikkeDeltUttak - Fødsel - Far aleneOmOmsorg', () => {
         expect(forslag[0]!.fom).toEqual('2022-08-08');
         expect(forslag[0]!.tom).toEqual('2022-11-18');
     });
+
+    it('skal foreslå aktivitetsfri kvote (ikke aktivitetskrav) når bare far/medmor har rett, og holde foreldrepenger med aktivitetskrav i reserve', () => {
+        const aktivitetsfriKonto = { konto: 'AKTIVITETSFRI_KVOTE', dager: 40 } satisfies KontoDto;
+        const forslag = ikkeDeltUttak({
+            situasjon: 'fødsel',
+            famDato,
+            erFarEllerMedmor: true,
+            tilgjengeligeStønadskvoter: [foreldrepenger, aktivitetsfriKonto],
+            erMorUfør: false,
+            bareFarMedmorHarRett: true,
+            erAleneOmOmsorg: false,
+            farOgFar: false,
+        });
+
+        expect(forslag.length).toEqual(1);
+        expect(forslag[0]!.kontoType).toEqual('FORELDREPENGER');
+        expect(forslag[0]!.morsAktivitet).toEqual('IKKE_OPPGITT');
+        expect(forslag[0]!.forelder).toEqual('FAR_MEDMOR');
+        expect(forslag[0]!.fom).toEqual('2022-08-08');
+        expect(forslag[0]!.tom).toEqual('2022-09-30');
+    });
 });
 
 describe('ikkeDeltUttak - Adopsjon', () => {
@@ -230,7 +253,7 @@ describe('ikkeDeltUttak - Adopsjon', () => {
         expect(forslag[0]!.tom).toEqual('2022-11-18');
     });
 
-    it('skal gi far foreldrepenger (uten å automatisk bruke aktivitetsfri kvote) når bare far har rett ved adopsjon', () => {
+    it('skal gi far foreldrepenger uten aktivitetskrav (aktivitetsfri kvote) når bare far har rett ved adopsjon, og holde foreldrepenger med aktivitetskrav i reserve', () => {
         const forslag = ikkeDeltUttak({
             situasjon: 'adopsjon',
             famDato,
@@ -245,8 +268,8 @@ describe('ikkeDeltUttak - Adopsjon', () => {
         expect(forslag.length).toEqual(1);
         expect(forslag[0]!.kontoType).toEqual('FORELDREPENGER');
         expect(forslag[0]!.forelder).toEqual('FAR_MEDMOR');
-        expect(forslag[0]!.morsAktivitet).toBeUndefined();
+        expect(forslag[0]!.morsAktivitet).toEqual('IKKE_OPPGITT');
         expect(forslag[0]!.fom).toEqual('2022-08-08');
-        expect(forslag[0]!.tom).toEqual('2022-11-18');
+        expect(forslag[0]!.tom).toEqual('2022-09-30');
     });
 });
