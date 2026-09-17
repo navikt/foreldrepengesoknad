1d502014eaadda3a09a5f51416c851620397db74
tor røneid <tor.roneid@nav.no>
2026-09-17 09:38:41 +0200

    FIX

--- apps/veiviser-hvor-mye/src/AppContainer.test.tsx
+++ apps/veiviser-hvor-mye/src/AppContainer.test.tsx
@@ -39,8 +39,7 @@ describe('<AppContainer>', () => {
         await userEvent.type(måned3, '10000');
 
         await userEvent.click(screen.getByText('Se resultatet'));
-        await expect(screen.findByText('Oppsummering')).resolves.toBeInTheDocument();
-        // expect(screen.getByText('Oppsummering')).toBeInTheDocument();
+        expect(await screen.findByText('Oppsummering')).toBeInTheDocument();
         expect(screen.getByText('Gjennomsnittlig utbetaling med 100 % foreldrepenger i 49 uker')).toBeInTheDocument();
         await userEvent.click(screen.getByText('Tilbake til spørsmålene'));
 
